10acf424d156a7a7785480c186915f0c4115f237
Alan Knowles <alan@akbkhome.com>
2010-07-20 11:13:44 +0800

    I18n.php

--- I18n.php
+++ I18n.php
@@ -6,7 +6,7 @@
  * 
  * We basically load up all supported languages at the start of the application.
  * 
- * It returns
+ * By default tt returns
  * 
  * Pman.I18n.Data = {
       en : [ { code : 'end', title : 'English' }, .... ],
