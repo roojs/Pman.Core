9968e90a792ef1e623f9ef8a5f4057adcc807add
Alan Knowles <alan@akbkhome.com>
2010-09-10 11:33:19 +0800

    Pman.Gnumeric.js

--- Pman.Gnumeric.js
+++ Pman.Gnumeric.js
@@ -485,11 +485,11 @@ Roo.extend(Pman.Gnumeric, Roo.util.Observable, {
                 var x = new Roo.Template({ html: this.grid[r][c].value });
                 try {
                     var res = x.applyTemplate(data);
-                    Roo.log("set " + r  + "," + c + ":"+res)
+                    //Roo.log("set " + r  + "," + c + ":"+res)
                     this.set({ r: r, c: c}, x.applyTemplate(data));
                 } catch (e) {
-                    Roo.log(e.toString());
-                    Roo.log(e);
+                 //   Roo.log(e.toString());
+                  //  Roo.log(e);
                     // continue?
                 }
                 
