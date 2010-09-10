51cc7c58641fca93cac36023d71eafb1cea0fd21
Alan Knowles <alan@akbkhome.com>
2010-09-10 13:01:37 +0800

    Pman.js

--- Pman.js
+++ Pman.js
@@ -641,39 +641,10 @@ Pman = new Roo.Document(
     
     download : function(c) {
         
-        if (c.newWindow) {
-            // as ie seems buggy...
-            window.open( c.url + '?' + Roo.urlEncode(c.params || {}), '_blank');
-            return;
-            
-        }
-        
-        this.createCsvFrame();
-        function cb(){
-            var r = { responseText : "", responseXML : null };
-
-            var frame = this.csvFrame;
-
-            try { 
-                var doc = Roo.isIE ? 
-                    frame.contentWindow.document : 
-                    (frame.contentDocument || window.frames[this.csvFrame.id].document);
-                
-                if(doc && doc.body && doc.body.innerHTML.length){
-                  //  alert(doc.body.innerHTML);
-                    Roo.MessageBox.alert("Error download",doc.body.innerHTML);
-                }
-                 
-            }
-            catch(e) {
-            }
-
-            Roo.EventManager.removeListener(frame, 'load', cb, this);
- 
-        }
-        Roo.EventManager.on( this.csvFrame, 'load', cb, this);
-        this.csvFrame.src = c.url;
+        return new Pman.Download(c);
     },
+    
+    
     downloadRevision : function(doc, rev)
     {
         this.download({
