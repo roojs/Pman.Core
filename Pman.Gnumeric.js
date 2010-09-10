5ba99081cdf491433f10c9cba6edb867b34661d4
Alan Knowles <alan@akbkhome.com>
2010-09-10 10:06:14 +0800

    Pman.Gnumeric.js

--- Pman.Gnumeric.js
+++ Pman.Gnumeric.js
@@ -21,178 +21,229 @@
       url: rootURL + '/xxx/yyy/templates/reports/myreport.xml',
       data: { ..... }
     });
-   x.data({ ... }); // key value data looks for {value} in strings and replaces it..
+    
+   x.applyData({ ... }); // key value data looks for {value} in strings and replaces it..
+   
    x.set('A3', 'test');
+   
    mypanel.update(x.toHTML());
+   
    x.download()
  
- *  
+ 
+ * 
+ * FIXME: - sheet - we currently use first sheet only..
+ * 
+ * 
  */
 
 Pman.Gnumeric = function (cfg)
 {
+    cfg.data = cfg.data || {};
+    
+    
+    
+    
     Roo.apply(this,cfg);
+    
+    this.defaultCell = {
+        valueType : 0,
+        valueFormat : '',
+        value : '',
+        colspan: 1,
+        rowspan: 1
+          
+    };
+    
+    
     this.load();
     
+    this.applyData(cfg.data);
+    
+    
 }
+Roo.apply(Pman.Gnumeric.prototype, {
+    
+    /**
+     * @cfg {String} url the source of the Gnumeric document.
+     */
+    url : '',
+      /**
+     * @cfg {Object} data overlay data for spreadsheet - from constructor.
+     */
+    data : false,
+     
+    /**
+     * @prop {XmlDocument} doc the gnumeric xml document
+     */
+    doc : false,
+    
+    /**
+     * @prop {XmlDocument} doc the gnumeric xml document
+     */
+    sheet : false,
+    
+    /**
+     * toRC:
+     * convert 'A1' style position to row/column reference
+     * 
+     * @arg {String} k cell name
+     * @return {Object}  as { r: {Number} , c: {Number}  }
+     */
+    
+    toRC : function(k)
+    {
+          var c = k.charCodeAt(0)-64;
+        var n = k.substring(1);
+        if (k.charCodeAt(1) > 64) {
+            c *=26;
+            c+=k.charCodeAt(1)-64;
+            n = k.substring(2);
+        }
+        return { c:c -1 ,r: (n*1)-1 }
+    },
+      /**
+     * rangeToRC:
+     * convert 'A1:B1' style position to array of row/column references
+     * 
+     * @arg {String} k cell range
+     * @return {Array}  as [ { r: {Number} , c: {Number}  }. { r: {Number} , c: {Number}  } ]
+     */
+    rangeToRC : function(s) {
+        var ar = s.split(':');
+        return [ this.toRC(ar[0]) , this.toRC(ar[1])]
+    },
+    /**
+     * parseDoc:
+     * convert XML document into cells and other data..
+     * 
+     */
+    parseDoc : function() 
+    {
+      
+        
+        var sheet = _this.doc.getElementsByTagName('Sheet')[0];
+        var cells = sheet.getElementsByTagName('Cell');
+        var grid ={};
+        var rmax = 1;
+        var cmax = 1;
+        
+        Roo.each(cells, function(c) {
+           // Roo.log(c);
+            var row = c.getAttribute('Row') * 1;
+            var col = c.getAttribute('Col') * 1;
+            cmax = Math.max(col+1, cmax);
+            rmax = Math.max(row+1, rmax);
+            var vt = c.getAttribute('ValueType');
+            var vf = c.getAttribute('ValueFormat');
+            var val = c.textContent;
+            if (typeof(grid[row]) == 'undefined') {
+                grid[row] ={};
+            }
+            grid[row][col] = Roo.applyIf({
+                valueType : vt,
+                valueFormat : vf,
+                value : val,
+                dom: c
+            }, defaultCell);
+        });
+       
+        for (var r = 0; r < rmax;r++) {
+            if (typeof(grid[r]) == 'undefined') {
+              grid[r] ={};
+            }
+            for (var c = 0; c < cmax;c++) {
+                if (typeof(grid[r][c]) == 'undefined') {
+                    continue;
+                }
+                //this.print( "[" + r + "]["+c+"]=" + grid[r][c].value +'<br/>');
+            }
+        }
+        
+         var merge = sheet.getElementsByTagName('Merge');
+         var t= this;
+         Roo.each(merge, function(c) {
+            var rc = t.rangeToRC(c.textContent);
+            //Roo.log(JSON.stringify(rc))
+            if (typeof(grid[rc[0].r][rc[0].c]) == 'undefined') {
+                grid[rc[0].r][rc[0].c] =  Roo.apply({}, defaultCell);
+            }
+                
+            grid[rc[0].r][rc[0].c].colspan = (rc[1].c - rc[0].c) + 1;
+            grid[rc[0].r][rc[0].c].rowspan = (rc[1].r - rc[0].r) + 1;
+            for(var r = (rc[0].r); r < (rc[1].r+1); r++) {
+               for(var c = rc[0].c; c < (rc[1].c+1); c++) {
+    //            Roo.log('adding alias : ' + r+','+c);
+                   grid[r][c] = grid[rc[0].r][rc[0].c];
+               }
+           }
+            
+            
+        });
+        // read colinfo..
+         var ci = sheet.getElementsByTagName('ColInfo');
+         var colInfo = {};
+         Roo.each(ci, function(c) {
+            var count =c.getAttribute('Count') || 1;
+            var s =  c.getAttribute('No')*1;
+            for(var i =0; i < count; i++) {
+                colInfo[s+i] = Math.floor(c.getAttribute('Unit')*1);
+            }
+        });
+         ci = sheet.getElementsByTagName('RowInfo');
+    
+         var rowInfo = {};
+         Roo.each(ci, function(c) {
+            var count =c.getAttribute('Count') || 1;
+            var s =  c.getAttribute('No')*1;
+            for(var i =0; i < count; i++) {
+                rowInfo[s+i] = Math.floor(c.getAttribute('Unit')*1);
+            }
+        });
+    
+        
+        this.parseStyles();
+        
+        // apply styles.
+        Roo.each(this.styles, function(s) {
+       
+            for (var r = s.r; r < s.r1;r++) {
+                if (typeof(grid[r]) == 'undefined') {
+                   continue;
+                }
+                for (var c = s.c; c < s.c1;c++) {
+                   if (c > cmax) continue;
+    
+                    if (typeof(grid[r][c]) == 'undefined') grid[r][c] = Roo.apply({}, defaultCell);
+                    var g=grid[r][c];
+                    if (typeof(g.cls) =='undefined') g.cls = [];
+                    if (g.cls.indexOf(s.name)  > -1) continue;
+                    g.cls.push(s.name);
+                }
+            }
+        });
+        this.grid=  grid;
+        this.cmax = cmax;
+        this.rmax = rmax;
+        this.colInfo = colInfo;
+        this.rowInfo = rowInfo;
+        
+    },
 
-cset : function(cell, v) {
-                 function toRC(k) {
-                    var c = k.charCodeAt(0)-64;
-                    var n = k.substring(1);
-                    if (k.charCodeAt(1) > 64) {
-                        c *=26;
-                        c+=k.charCodeAt(1)-64;
-                        n = k.substring(2);
-                    }
-                    return { c:c -1 ,r: (n*1)-1 }
-                    
-                }
-                var cs=toRC(cell);
-                Roo.log(    this.grid[cs.r][cs.c]);
-                this.grid[cs.r][cs.c].value=  v;
-                // need to generate clell if it doe
-                if (typeof(this.grid[cs.r][cs.c].dom) == 'undefined') {
-                    Roo.log('no default content for cell:' + cell);
-                    return;
-                }
-                this.grid[cs.r][cs.c].dom.textContent=  v;
-            },
-            parseDoc : function() 
-            {
-                 function toRC(k) {
-                    var c = k.charCodeAt(0)-64;
-                    var n = k.substring(1);
-                    if (k.charCodeAt(1) > 64) {
-                        c *=26;
-                        c+=k.charCodeAt(1)-64;
-                        n = k.substring(2);
-                    }
-                    return { c:c -1 ,r: (n*1)-1 }
-                    
-                }
-                 
-                function rangeToRC(s) {
-                    var ar = s.split(':');
-                    return [ toRC(ar[0]) , toRC(ar[1])]
-                }
-                var defaultCell = {
-                            valueType : 0,
-                            valueFormat : '',
-                            value : '',
-                            colspan: 1,
-                            rowspan: 1
-                              
-                        };
-            
-            
-            
-                var sheet = _this.dialog.xml.getElementsByTagName('Sheet')[0];
-                var cells = sheet.getElementsByTagName('Cell');
-                var grid ={};
-                var rmax = 1
-                var cmax = 1
-                Roo.each(cells, function(c) {
-                   // Roo.log(c);
-                    var row = c.getAttribute('Row') * 1;
-                    var col = c.getAttribute('Col') * 1;
-                    cmax = Math.max(col+1, cmax);
-                    rmax = Math.max(row+1, rmax);
-                    var vt = c.getAttribute('ValueType');
-                    var vf = c.getAttribute('ValueFormat');
-                    var val = c.textContent;
-                    if (typeof(grid[row]) == 'undefined') {
-                        grid[row] ={};
-                    }
-                    grid[row][col] = Roo.applyIf({
-                        valueType : vt,
-                        valueFormat : vf,
-                        value : val,
-                        dom: c
-                    }, defaultCell);
-                });
-               
-                for (var r = 0; r < rmax;r++) {
-                    if (typeof(grid[r]) == 'undefined') {
-                      grid[r] ={};
-                    }
-                    for (var c = 0; c < cmax;c++) {
-                        if (typeof(grid[r][c]) == 'undefined') {
-                            continue;
-                        }
-                        //this.print( "[" + r + "]["+c+"]=" + grid[r][c].value +'<br/>');
-                    }
-                }
-                
-                 var merge = sheet.getElementsByTagName('Merge');
-                 var t= this;
-                 Roo.each(merge, function(c) {
-                    var rc = rangeToRC(c.textContent);
-                    //Roo.log(JSON.stringify(rc))
-                    if (typeof(grid[rc[0].r][rc[0].c]) == 'undefined') {
-                        grid[rc[0].r][rc[0].c] =  Roo.apply({}, defaultCell);
-                    }
-                        
-                    grid[rc[0].r][rc[0].c].colspan = (rc[1].c - rc[0].c) + 1;
-                    grid[rc[0].r][rc[0].c].rowspan = (rc[1].r - rc[0].r) + 1;
-                    for(var r = (rc[0].r); r < (rc[1].r+1); r++) {
-                       for(var c = rc[0].c; c < (rc[1].c+1); c++) {
-            //            Roo.log('adding alias : ' + r+','+c);
-                           grid[r][c] = grid[rc[0].r][rc[0].c];
-                       }
-                   }
-                    
-                    
-                });
-                // read colinfo..
-                 var ci = sheet.getElementsByTagName('ColInfo');
-                 var colInfo = {};
-                 Roo.each(ci, function(c) {
-                    var count =c.getAttribute('Count') || 1;
-                    var s =  c.getAttribute('No')*1;
-                    for(var i =0; i < count; i++) {
-                        colInfo[s+i] = Math.floor(c.getAttribute('Unit')*1);
-                    }
-                });
-                 ci = sheet.getElementsByTagName('RowInfo');
-            
-                 var rowInfo = {};
-                 Roo.each(ci, function(c) {
-                    var count =c.getAttribute('Count') || 1;
-                    var s =  c.getAttribute('No')*1;
-                    for(var i =0; i < count; i++) {
-                        rowInfo[s+i] = Math.floor(c.getAttribute('Unit')*1);
-                    }
-                });
-            
-                
-                this.parseStyles();
-                
-                // apply styles.
-                Roo.each(this.styles, function(s) {
-               
-                    for (var r = s.r; r < s.r1;r++) {
-                        if (typeof(grid[r]) == 'undefined') {
-                           continue;
-                        }
-                        for (var c = s.c; c < s.c1;c++) {
-                           if (c > cmax) continue;
-            
-                            if (typeof(grid[r][c]) == 'undefined') grid[r][c] = Roo.apply({}, defaultCell);
-                            var g=grid[r][c];
-                            if (typeof(g.cls) =='undefined') g.cls = [];
-                            if (g.cls.indexOf(s.name)  > -1) continue;
-                            g.cls.push(s.name);
-                        }
-                    }
-                });
-                this.grid=  grid;
-                this.cmax = cmax;
-                this.rmax = rmax;
-                this.colInfo = colInfo;
-                this.rowInfo = rowInfo;
-                
-            },
+
+    set : function(cell, v) {
+        
+        var cs= this.toRC(cell);
+        Roo.log(    this.grid[cs.r][cs.c]);
+        this.grid[cs.r][cs.c].value=  v;
+        // need to generate clell if it doe
+        if (typeof(this.grid[cs.r][cs.c].dom) == 'undefined') {
+            Roo.log('no default content for cell:' + cell);
+            return;
+        }
+        this.grid[cs.r][cs.c].dom.textContent=  v;
+    },
+           
             parseStyles : function() {
                 var sheet = this.xml.getElementsByTagName('Sheet')[0];
                 var srs = sheet.getElementsByTagName('StyleRegion');
