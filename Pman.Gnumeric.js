42265902c8d182865ce89c8b58b2e1d16519c844
Alan Knowles <alan@akbkhome.com>
2010-09-10 13:19:07 +0800

    Pman.Gnumeric.js

--- Pman.Gnumeric.js
+++ Pman.Gnumeric.js
@@ -553,7 +553,7 @@ Roo.extend(Pman.Gnumeric, Roo.util.Observable, {
         var x = new Pman.Download({
             method: 'POST',
             params : {
-               xml : 
+               xml : '',
                format : 'xml'
                debug : 0
                
