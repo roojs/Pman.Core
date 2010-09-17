612fedaf50c66170db2c1864859dffba5e4735d5
Alan Knowles <alan@akbkhome.com>
2010-09-17 15:48:55 +0800

    JsCompile.php

--- JsCompile.php
+++ JsCompile.php
@@ -36,7 +36,7 @@ class Pman_Core_JsCompile  extends Pman
             print_r($ar);
             exit;
         } else {
-            $p = $args[1]; 
+            $ar = array($args[1]); 
             //$ar = $args;
             //array_shift($ar);
         }
