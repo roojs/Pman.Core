1b343fe251944bf73de262ae434d7ec5f9728dfb
Alan Knowles <alan@akbkhome.com>
2010-09-17 15:50:22 +0800

    JsCompile.php

--- JsCompile.php
+++ JsCompile.php
@@ -69,7 +69,7 @@ class Pman_Core_JsCompile  extends Pman
         }
         
         
-        $src = realpath(dirname(__FILE__).'/../'. $proj);
+        $src =  dirname(__FILE__).'/../'. $proj;
         echo $src;exit;
         
         $tmp = ini_get('session.save_path')."/{$proj}_". posix_getuid(). '_'.md5($src);
