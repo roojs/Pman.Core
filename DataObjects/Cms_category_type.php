57ed497fa3b79924a205538d71301110186d1c10
Alan Knowles <alan@akbkhome.com>
2010-09-21 13:00:55 +0800

    DataObjects/Cms_category_type.php

---
+++ DataObjects/Cms_category_type.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Table Definition for cms_category_type
+ */
+require_once 'DB/DataObject.php';
+
+class Pman_Core_DataObjects_Cms_category_type extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'cms_category_type';               // table name
+    public $id;                              // int(11)  not_null primary_key auto_increment
+    public $name;                            // string(64)  not_null
+
+    
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
