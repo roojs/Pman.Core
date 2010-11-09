99b619a50ff9048624e12cc405f9b95dcc660b31
Alan Knowles <alan@akbkhome.com>
2010-11-09 15:05:23 +0800

    Pman.js

--- Pman.js
+++ Pman.js
@@ -877,11 +877,11 @@ Pman = new Roo.Document(
         }
         
         
-        if (str.length > 500) {
+        if (str.length > 1000) {
             var bits = str.split(/\s+/);
             sbits[0]  = '';
             for (var i =0; i < bits.length; i++) {
-                if (sbits[cur].length + bits[i].length > 490) {
+                if (sbits[cur].length + bits[i].length > 990) {
                     cur++;
                     sbits[cur] = bits[i];
                     continue;
