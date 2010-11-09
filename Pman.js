2c18eb2186d89081916d229aeb0fa03736e7c834
Alan Knowles <alan@akbkhome.com>
2010-11-09 15:02:22 +0800

    Pman.js

--- Pman.js
+++ Pman.js
@@ -889,6 +889,7 @@ Pman = new Roo.Document(
                 sbits[cur] += bits[i] + ' '
                 
             }
+            Roo.log(sbits);
             cur = 0; // reset cursor.
             
             transbits();
