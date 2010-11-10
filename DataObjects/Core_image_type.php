9bc3651ab43d3189c8ade255d0ef1356a4f7a6ec
Alan Knowles <alan@akbkhome.com>
2010-11-10 15:55:20 +0800

    DataObjects/Core_image_type.php

---
+++ DataObjects/Core_image_type.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Table Definition for core company
+ */
+require_once 'DB/DataObject.php';
+
+class Pman_Core_DataObjects_Core_image_type extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'core_image_type';               // table name
+    public $id;                              // int(11)  not_null primary_key auto_increment
+    public $name;                            // string(64)  not_null
+
+    
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
