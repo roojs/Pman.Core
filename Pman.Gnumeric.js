b8e8ee1f19551ea7a9e7eb75a554116c63d9fb5e
Alan Knowles <alan@akbkhome.com>
2010-11-17 16:44:48 +0800

    Pman.Gnumeric.js

--- Pman.Gnumeric.js
+++ Pman.Gnumeric.js
@@ -929,7 +929,7 @@ Roo.extend(Pman.Gnumeric, Roo.util.Observable, {
             var rcs = [];
             ar = ar.slice(0,3);
             Roo.each(ar, function(c) { 
-                rcs.push((c*c).toString(16)) ;  // can we do this? - gnumeric expects bytes.
+                rcs.push((c*c).toString(16)) ;   
             });
             return rcs.join(':');
             
