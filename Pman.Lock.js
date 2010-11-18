91d97bd7d8be65681f66cefe3a804f96f8eb6dc8
Alan Knowles <alan@akbkhome.com>
2010-11-18 17:34:58 +0800

    Pman.Lock.js

--- Pman.Lock.js
+++ Pman.Lock.js
@@ -35,28 +35,30 @@
  */
 Pman.Lock = function (cfg) {
     this.cfg = cfg;
-    this.attemptLock();
+    this.callLock();
 }
 
 Roo.apply(Pman.Lock.prototype, {
     
-    attemptLock : function()
+    callLock : function(force)
     {
+        force = force || 0;
         var _t = this
         Pman.Request({
             url : baseURL + 'Core/Lock/lock',
             params : {
                 on_table : cfg.table,
-                on_id : cfg.id
+                on_id : cfg.id,
+                force : force
             },
             failure : function() {
                 Roo.MessageBox.alert("Error", "Lock Request failed, please try again");
             },
             success : function(data)
             {
-                Roo.log(data);
                 
-                if (typeof(data) == 'object') {
+                
+                if (!force && typeof(data) == 'object') {
                     _t.confirmBreak(data);
                 }
                 
@@ -69,10 +71,20 @@ Roo.apply(Pman.Lock.prototype, {
     confirmBreak : function (ar)
     {
         
-        var msg = '';
-        Roo.each(ar, function(p) {
+        var msg = "This Record is Locked by the following people, <br/>" + 
+            "Do you want to continue, this will prevent these people from saving their changes<br/>";
             
-           }
+        Roo.each(ar, function(p) {
+            msg += '<br/>' + p.name + ' at ' + p.lock_created ;
+        })
+        var _t = this;
+        Roo.messageBox.confirm("Confirm breaking locks", msg, function(r) {
+            if (r != 'yes') {
+                return;
+            }
+            _t.callLock(1);
+        });
+            
         
         
         
