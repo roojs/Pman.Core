56f9992f2fcd4e274bb204ad145f7409c73b083a
Alan Knowles <alan@akbkhome.com>
2011-03-14 17:54:28 +0800

    Pman.I18n.js

--- Pman.I18n.js
+++ Pman.I18n.js
@@ -136,6 +136,7 @@ Pman.I18n = {
         });
         
         var xret = ret.sort(function(a,b) {
+            if (a[0] == '**') return -1; // other always at end..
             return a[1]  > b[1] ? 1 : -1;
         });
         
