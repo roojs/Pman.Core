2b742b32bbc6fe337646a932dd530d17e5b7f2d5
Alan Knowles <alan@akbkhome.com>
2011-03-14 18:43:50 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -287,14 +287,26 @@ CREATE TABLE  `translations` (
 ALTER TABLE translations ADD INDEX qlookup (module, tfile, tlang, tkey);
 
 
+# - used to trigger emails about changes to items being watched.
 
 CREATE TABLE `core_watch` (
   `ontable` varchar(128) NOT NULL,
   `onid` varchar(128) NOT NULL,
-  `user_id` varchar(128) NOT NULL,
+  `person_id` int(11) NOT NULL,
   `event` varchar(128) NOT NULL,
   `medium` varchar(128) NOT NULL,
   `active` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`ontable`,`onid`,`user_id`,`event`,`medium`)
 ) ;
 
+ALTER TABLE core_watch change change column user_id person_id int(11);
+ALTER TABLE core_watch change alter column onid int(11);
+
+# - used by email / tracker to handle alises
+
+CREATE TABLE `core_person_aliases` (
+ `person_id` varchar(128) DEFAULT NULL,
+  `alias` varchar(128) NOT NULL,
+  
+  PRIMARY KEY (`alias`)
+) ;
