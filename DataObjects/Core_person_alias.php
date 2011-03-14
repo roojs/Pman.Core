a22b15deb8cae1b5860c09db962ccee07f94d8b6
Alan Knowles <alan@akbkhome.com>
2011-03-14 18:47:11 +0800

    DataObjects/Core_person_alias.php

---
+++ DataObjects/Core_person_alias.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Table Definition for core_person_alias
+ */
+require_once 'DB/DataObject.php';
+
+class Pman_Core_DataObjects_Core_person_alias extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'core_person_alias';               // table name
+    public $id;                              // int(11)  not_null primary_key auto_increment
+    public $person_id;                       // string(128)  
+    public $alias;                           // string(254)  not_null
+
+    
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
