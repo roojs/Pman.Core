ba8989f7fffea9238d83c88b8b465c346f149796
Alan Knowles <alan@akkbhome.com>
2011-04-06 22:54:53 +0800

    Notify.php

--- Notify.php
+++ Notify.php
@@ -84,7 +84,7 @@ class Pman_Core_Notify extends Pman
         $pool = array();
         foreach($this->pool as $p) {
             $ar = proc_get_Status($p);
-            print_r($ar);
+           var_dump($ar);
             if (!$p['running']) {
                 $pool[] = $p;
             }
