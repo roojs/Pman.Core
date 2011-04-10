9bad910496237b3b9c3c1449a6f5b98699ca7e77
Alan Knowles <alan@akkbhome.com>
2011-04-10 15:52:02 +0800

    Notify.php

--- Notify.php
+++ Notify.php
@@ -67,9 +67,11 @@ class Pman_Core_Notify extends Pman
     
     function run($id)
     {
+        
+        $tn = tempnam(ini_get('session.save_path'),'stdout');
         $descriptorspec = array(
             0 => array("pipe", 'r'),  // stdin is a pipe that the child will read from
-            1 => array("pipe", 'w'),  // stdout is a pipe that the child will write to
+            1 => array("file", $tn, 'w'),  // stdout is a pipe that the child will write to
             2 => array("pipe", "w") // stderr is a file to write to
          );
         $php = $_SERVER["_"];
