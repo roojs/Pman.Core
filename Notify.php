5cb311cb290a40e0492827da33d455d05e6adef5
Alan Knowles <alan@akkbhome.com>
2011-04-10 15:40:52 +0800

    Notify.php

--- Notify.php
+++ Notify.php
@@ -68,9 +68,9 @@ class Pman_Core_Notify extends Pman
     function run($id)
     {
         $descriptorspec = array(
-            0 => array("file", "/dev/null", 'r'),  // stdin is a pipe that the child will read from
-            1 => array("file", "/dev/null", 'a'),  // stdout is a pipe that the child will write to
-            2 => array("file", "/dev/null", "a") // stderr is a file to write to
+            0 => array("pipe", 'r'),  // stdin is a pipe that the child will read from
+            1 => array("pipe", 'w'),  // stdout is a pipe that the child will write to
+            2 => array("pipe", "w") // stderr is a file to write to
          );
         $php = $_SERVER["_"];
         $cwd = getcwd(); // same as run on.. (so script should end up being same relatively..)
