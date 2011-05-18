e957fd0e2033b49dcf75889d1696f56ac1180d47
Alan Knowles <alan@akbkhome.com>
2011-05-18 12:20:52 +0800

    DataObjects/Core_enum.php

--- DataObjects/Core_enum.php
+++ DataObjects/Core_enum.php
@@ -9,12 +9,12 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
 
-    public $__table = 'core_enum';               // table name
+    public $__table = 'core_enum';                       // table name
     public $id;                              // int(11)  not_null primary_key auto_increment
-    public $name;                            // string(64)  not_null
-    public $etype;                            // string(64)  not_null
-    public $seqid;                            // string(64)  not_null
-    public $active;                            // string(64)  not_null
+    public $etype;                           // string(32)  not_null
+    public $name;                            // string(255)  not_null
+    public $active;                          // int(2)  not_null
+    public $seqid;                           // int(11)  not_null multiple_key
 
     
     /* the code above is auto generated do not remove the tag below */
