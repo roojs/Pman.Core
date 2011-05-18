8af85beae902d4271cd7e5d18de42863fb28926d
Alan Knowles <alan@akbkhome.com>
2011-05-18 12:23:46 +0800

    Generator.php

--- Generator.php
+++ Generator.php
@@ -116,7 +116,7 @@ class Pman_Core_Generator extends DB_DataObject_Generator
         $standard_database = $options['database'];
        
        
-        print_r($options);exit;
+         
        
        
        
