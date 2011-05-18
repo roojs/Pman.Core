62be03ac2db378995aecc77f4b232169dc123780
Alan Knowles <alan@akbkhome.com>
2011-05-18 12:20:52 +0800

    DataObjects/Companies.php

--- DataObjects/Companies.php
+++ DataObjects/Companies.php
@@ -11,7 +11,7 @@ class Pman_Core_DataObjects_Companies extends DB_DataObject
 
     public $__table = 'Companies';                       // table name
     public $code;                            // string(32)  not_null
-    public $name;                            // string(128)  multiple_key
+    public $name;                            // string(128)  
     public $remarks;                         // blob(65535)  blob
     public $owner_id;                        // int(11)  not_null
     public $address;                         // blob(65535)  blob
@@ -23,7 +23,6 @@ class Pman_Core_DataObjects_Companies extends DB_DataObject
     public $logo_id;                         // int(11)  not_null
     public $background_color;                // string(8)  not_null
     public $comptype;                        // string(8)  not_null
-    public $ava_craft;                       // string(254)  
     public $url;                             // string(254)  not_null
     public $main_office_id;                  // int(11)  not_null
     public $created_by;                      // int(11)  not_null
@@ -31,6 +30,9 @@ class Pman_Core_DataObjects_Companies extends DB_DataObject
     public $updated_by;                      // int(11)  not_null
     public $updated_dt;                      // datetime(19)  not_null binary
     public $passwd;                          // string(64)  not_null
+    public $dispatch_port;                   // string(255)  not_null
+    public $province;                        // string(255)  not_null
+    public $country;                         // string(4)  not_null
 
     
     /* the code above is auto generated do not remove the tag below */
