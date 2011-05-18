499a4d2a40c2ea64e5ab02fe2e892b8ecb698aec
Alan Knowles <alan@akbkhome.com>
2011-05-18 12:20:52 +0800

    DataObjects/Core_locking.php

--- DataObjects/Core_locking.php
+++ DataObjects/Core_locking.php
@@ -9,12 +9,12 @@ class Pman_Core_DataObjects_Core_locking extends DB_DataObject
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
 
-    public $__table = 'core_locking';               // table name
-    public $id;                              // int(11)  not_null primary_key auto_increment
-    public $person_id;                            // string(64)  not_null
-    public $on_id;                            // string(64)  not_null
-    public $on_table;                            // string(64)  not_null
-    public $created;                            // string(64)  not_null
+    public $__table = 'core_locking';                    // table name
+    public $int;                             // int(11)  not_null primary_key auto_increment
+    public $on_table;                        // string(64)  not_null multiple_key
+    public $on_id;                           // int(11)  not_null
+    public $person_id;                       // int(11)  not_null
+    public $created;                         // datetime(19)  not_null binary
 
     
     /* the code above is auto generated do not remove the tag below */
