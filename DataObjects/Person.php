4f2f8c8566de885cdd12702b5748d679add57e5f
Alan Knowles <alan@akbkhome.com>
2011-05-18 12:20:53 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -18,14 +18,16 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
     public $email;                           // string(128)  not_null
     public $company_id;                      // int(11)  
     public $role;                            // string(32)  not_null
-    public $active;                          // int(11)  
+    public $active;                          // int(11)  not_null
     public $remarks;                         // blob(65535)  not_null blob
     public $passwd;                          // string(64)  not_null
     public $owner_id;                        // int(11)  not_null
     public $lang;                            // string(8)  
     public $no_reset_sent;                   // int(11)  
-    public $project_id;                      // int(11)  
     public $action_type;                     // string(32)  
+    public $project_id;                      // int(11)  
+    public $deleted_by;                      // int(11)  not_null
+    public $deleted_dt;                      // datetime(19)  binary
 
     
     /* the code above is auto generated do not remove the tag below */
