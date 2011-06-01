f50b2beffdf76210618703df3b6d1a120ab3dad9
Alan Knowles <alan@akbkhome.com>
2011-06-01 14:33:15 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -120,9 +120,8 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
     /// check config 'auth_comptype' to see if we restrict access ..
     function verifyAuth()
     { 
-       
-        echo '<PRE>';print_r($this->company());exit;
-         if (!empty($ff->Pman['auth_comptype']) && $ff->Pman['auth_comptype'] != $this->company()->comptype) {
+        $ff= HTML_FlexyFramework::get();
+          if (!empty($ff->Pman['auth_comptype']) && $ff->Pman['auth_comptype'] != $this->company()->comptype) {
             $ff->page->jerr("Login not permited to outside companies");
         }
         return true;
