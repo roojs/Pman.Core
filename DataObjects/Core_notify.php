7efe5e2bd2204f5e421b2af8faa823dbe8c59420
Alan Knowles <alan@akbkhome.com>
2011-06-13 16:05:57 +0800

    DataObjects/Core_notify.php

--- DataObjects/Core_notify.php
+++ DataObjects/Core_notify.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Table Definition for core_notify
- */
+ *
+ * Table is designed to be used with a mailer to notify or issue
+ * emails (or maybe others later??)
+ *
+ *
+CREATE TABLE  core_notify  (
+  `id` int(11)  NOT NULL AUTO_INCREMENT,
+  `act_when` DATETIME NOT NULL,
+  `onid` int(11)  NOT NULL DEFAULT 0,
+  `ontable` varchar(128)  NOT NULL DEFAULT '',
+  `person_id` int(11)  NOT NULL DEFAULT 0,
+  `msgid` varchar(128)  NOT NULL  DEFAULT '',
+  `sent` DATETIME  NOT NULL,
+  `event_id` int(11)  NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  INDEX `lookup`(`act_when`, `msgid`)
+);
+*/
 require_once 'DB/DataObject.php';
 
 class Pman_Core_DataObjects_Core_notify extends DB_DataObject 
