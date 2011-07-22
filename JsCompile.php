c607cc824193ac1d7bfb65dee8fcd312bf1689cf
Alan Knowles <alan@akbkhome.com>
2011-07-22 16:59:18 +0800

    JsCompile.php

--- JsCompile.php
+++ JsCompile.php
@@ -242,8 +242,8 @@ class Pman_Core_JsCompile  extends Pman
         }
         $eoutput = escapeshellarg($output);
         $cmd = "$seed {$o['jspacker']}/pack.js  -o $eoutput " . implode($ofiles, ' ');
-        //echo "<PRE>$cmd\n";
-        //echo `$cmd`;
+        echo "<PRE>$cmd\n";
+        echo `$cmd`;
         `$cmd`;
         
         
