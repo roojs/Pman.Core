3357eccea279a35e67036b1c2e0fd2b1914d0457
Alan Knowles <alan@akbkhome.com>
2011-07-22 16:41:15 +0800

    JsCompile.php

--- JsCompile.php
+++ JsCompile.php
@@ -124,7 +124,7 @@ class Pman_Core_JsCompile  extends Pman
         $output = md5(serialize($arfiles)) .'.css';
         
         if (!file_exists($output_path.'/_cached_/'.$output)) {
-            $this->packCssCore($arfiles,$output_path.'/'.$output);
+            $this->packCssCore($arfiles,$output_path.'/_cached_/'.$output);
         }
         
         if (file_exists($output_path.'/'.$output)) {
