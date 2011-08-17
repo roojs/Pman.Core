48d96b3d9c04673069284d429b767c57d42cc909
Alan Knowles <alan@akbkhome.com>
2011-08-17 11:40:36 +0800

    JsCompile.php

--- JsCompile.php
+++ JsCompile.php
@@ -269,7 +269,7 @@ class Pman_Core_JsCompile  extends Pman
         
         $res = `$cmd`;
         //exit;
-        file_put_contents($output.'.log'. $cmd."\n\n". $res);
+        file_put_contents($output.'.log', $cmd."\n\n". $res);
         // since this only appears when we change.. it's ok to dump it out..
           echo "<!-- Compiled javascript
             " . htmlspecialchars($res) . "
