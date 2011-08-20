5fc93edaca81982463037b7a99ed618bcdd90cd0
Alan Knowles <alan@akbkhome.com>
2011-08-20 22:52:51 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -2,10 +2,39 @@
 
 
 
-class Pman_Core_UpdateDatabase
+require_once 'Pman.php';
+class Pman_Core_UpdateDatabase extends Pman
 {
     
+    static $cli_desc = "Update SQL - Beta";
+ 
+class Pman_Core_RunGenerator extends Pman
+{     
     
-    
+  
+    var $cli = false;
+    function getAuth() {
+        
+        
+        $ff = HTML_FlexyFramework::get();
+        if (!empty($ff->cli)) {
+            $this->cli = true;
+            return true;
+        }
+        
+        parent::getAuth(); // load company!
+        $au = $this->getAuthUser();
+        if (!$au || $au->company()->comptype != 'OWNER') {
+            $this->jerr("Not authenticated", array('authFailure' => true));
+        }
+        $this->authUser = $au;
+        return true;
+    }
+     
+    function get()
+    {
+        die("not working yet");
+        
+    }
     
 }
