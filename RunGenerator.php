45ab1fd2f635d6776507c35e552cfa7e258d1220
Alan Knowles <alan@akbkhome.com>
2011-09-05 13:44:02 +0800

    RunGenerator.php

--- RunGenerator.php
+++ RunGenerator.php
@@ -74,8 +74,9 @@ class Pman_Core_RunGenerator extends Pman
        
         
         $modules = is_string($opts['modules']) ? array($opts['modules']) : $opts['modules'];
-       
-        $x->start($this->cli, $modules, $lastarg);
+        $overwrite = is_string($opts['overwrite']) ? array($opts['overwrite']) : $opts['overwrite'];
+
+        $x->start($this->cli, $modules, overwrite);
         
         // technically it would be good to trash the cached ini files here.. 
         // however we can not really do that, as the ownships are off..
