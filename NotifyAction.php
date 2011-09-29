8bdfec56855b989ebf9325cae88e80fb5e7bd3f7
Alan Knowles <alan@roojs.com>
2011-09-29 22:10:08 +0800

    NotifyAction.php

---
+++ NotifyAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * in theory a generic 'action' handler..
+ *
+ * Part of the eventual workflow code...
+ * -> at present, just flags something as done.....
+ *
+ *
+ */
+
+class Pman_Core_NotifyAction extends Pman
+{
+    
+    
+}
