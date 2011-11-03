5757348e94abd474391696aa88676c599e8d8f54
Alan Knowles <alan@akbkhome.com>
2011-11-03 16:22:11 +0800

    Pman.GoogleTranslate.js

--- Pman.GoogleTranslate.js
+++ Pman.GoogleTranslate.js
@@ -31,7 +31,7 @@ Pman.GoogleTranslate = function(str, src, dest, cb, force) {
                 }
                 
                 
-            })
+            });
             
             
             
