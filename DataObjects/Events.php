7ea488e90557c14ac35a811588344145156da758
Alan Knowles <alan@akbkhome.com>
2011-11-25 13:22:50 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -98,7 +98,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
                        
                         $this->selectAdd( " distinct(join_on_id_{$jtn}.{$jc}  ) as on_id_{$jc} ");
                         $this->groupBy("on_id_{$jc} ");
-                        $this->whereAdd("{$jc} IS NOT NULL");
+                        $this->whereAdd("join_on_id_{$jtn}.{$jc} IS NOT NULL");
                     }
                     $this->selectAdd( "MAX(events.event_when) as event_when");
                     $this->orderBy('event_when DESC');
