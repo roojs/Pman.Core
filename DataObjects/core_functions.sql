e0ae82a310ebd9d56b8a0d4439a4c5dd6f6aec32
Alan Knowles <alan@roojs.com>
2012-01-28 16:09:57 +0800

    DataObjects/core_functions.sql

--- DataObjects/core_functions.sql
+++ DataObjects/core_functions.sql
@@ -13,3 +13,21 @@ CREATE FUNCTION i18n_translate(in_ltype  varchar(1) , in_lkey varchar(8), in_inl
         
     END $$
 DELIMITER ;
+
+
+
+DROP PROCEDURE IF EXISTS core_enum_seqmax_update;
+DELIMITER $$
+CREATE PROCEDURE core_enum_seqmax_update( etype varchar(128)) 
+        RETURNS INT(11) DETERMINISTIC
+    BEGIN
+        DECLARE ret  VARCHAR(64);
+        seqmax = (SELECT MAX(seqid) +1 FROM core_enum WHERE 
+
+        SET ret  = '';
+        SELECT lval INTO ret FROM i18n
+            WHERE ltype=in_ltype AND lkey=in_lkey and inlang=in_inlang LIMIT 1;
+        RETURN ret;
+        
+    END $$
+DELIMITER ;
