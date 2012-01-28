702068ecac4780a3a05b6a856cf5236b4225193a
Alan Knowles <alan@roojs.com>
2012-01-28 16:10:53 +0800

    DataObjects/core_functions.sql

--- DataObjects/core_functions.sql
+++ DataObjects/core_functions.sql
@@ -18,16 +18,12 @@ DELIMITER ;
 
 DROP PROCEDURE IF EXISTS core_enum_seqmax_update;
 DELIMITER $$
-CREATE PROCEDURE core_enum_seqmax_update( etype varchar(128)) 
-        RETURNS INT(11) DETERMINISTIC
-    BEGIN
-        DECLARE ret  VARCHAR(64);
-        seqmax = (SELECT MAX(seqid) +1 FROM core_enum WHERE 
-
-        SET ret  = '';
-        SELECT lval INTO ret FROM i18n
-            WHERE ltype=in_ltype AND lkey=in_lkey and inlang=in_inlang LIMIT 1;
-        RETURN ret;
+CREATE PROCEDURE core_enum_seqmax_update( etype varchar(128))
+BEGIN
+        DECLARE seqmax INT(11);
+        SELECT MAX(seqid) +1  INT seqmax FROM core_enum WHERE
+            etype = etype;
+        UPDATE core_enum SET seqmax = seqmax WHERE etype = etype;
         
     END $$
 DELIMITER ;
