66eb1380dba0b41b09ed8f5fe64b58abe10fa3a8
Alan Knowles <alan@roojs.com>
2012-02-15 12:55:25 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -133,9 +133,9 @@ class Pman_Core_NotifySend extends Pman
             $next_try_min = floor((time() - strtotime($last_event)) / 60) * 2;
         }
         $next_try = $next_try_min . ' MINUTES';
-        
+         
         // this may modify $p->email. (it will not update it though)
-        $email =  $this->makeEmail($o, $p, $last, $w);
+        $email =  $this->makeEmail($o, $p, $last, $w, $force);
         
         
         if ($email === true)  {
