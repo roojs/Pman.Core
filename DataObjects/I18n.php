b27e7dcf9badba674ac7a934aef50b7ec8c0f8f2
Alan Knowles <alan@roojs.com>
2012-05-02 12:16:50 +0800

    DataObjects/I18n.php

--- DataObjects/I18n.php
+++ DataObjects/I18n.php
@@ -108,7 +108,7 @@ class Pman_Core_DataObjects_I18n extends DB_DataObject
         if (!empty($q['query']['name'])) {
             DB_DAtaObject::debugLevel(1);
         
-            $this->whereAdd("title LIKE '". $this->escape($q['query']['name']). "%'");
+            $this->whereAdd("lval LIKE '". $this->escape($q['query']['name']). "%'");
         }
     }
     
