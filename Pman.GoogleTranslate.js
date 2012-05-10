f86b323409ee92a549df8315190dc693631a5a81
Alan Knowles <alan@akbkhome.com>
2012-05-10 10:01:58 +0800

    Pman.GoogleTranslate.js

--- Pman.GoogleTranslate.js
+++ Pman.GoogleTranslate.js
@@ -31,6 +31,7 @@ Pman.GoogleTranslate = function(str, src, dest, cb, force) {
                 },
                 failure : function() {
                     Roo.log("Google key fetch failed");
+                    // do not display error message...
                     return true;
                 }
                 
