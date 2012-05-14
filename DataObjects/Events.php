052019444eab34ac5f886e0e81f50530d654d1f0
Alan Knowles <alan@akbkhome.com>
2012-05-14 12:08:42 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -228,8 +228,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
                 LEFT JOIN {$tbl}  join_person_table_{$tbl}
                     ON {$tn}.person_id = join_person_table_{$tbl}.id
                         AND {$tn}.person_table = '{$tbl}'
-            
-            
+             
             ";
         }
         foreach($cols as $col=>$whens) {
