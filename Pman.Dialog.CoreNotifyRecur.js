65fa78331088c3c03719d36513936daf9df3c38c
Alan Knowles <alan@akbkhome.com>
2012-05-16 17:08:28 +0800

    Pman.Dialog.CoreNotifyRecur.bjs Pman.Dialog.CoreNotifyRecur.js

--- Pman.Dialog.CoreNotifyRecur.js
+++ Pman.Dialog.CoreNotifyRecur.js
@@ -420,7 +420,39 @@ Pman.Dialog.CoreNotifyRecur = {
                                 dataIndex : 'tz',
                                 header : 'Timezone',
                                 width : 100,
-                                renderer : function(v) { return String.format('{0}', v); }
+                                renderer : function(v) { return String.format('{0}', v); },
+                                editor : {
+                                    xtype: 'GridEditor',
+                                    xns: Roo.grid,
+                                    field : {
+                                        xtype: 'ComboBox',
+                                        xns: Roo.form,
+                                        allowBlank : false,
+                                        displayField : 'title',
+                                        editable : false,
+                                        fieldLabel : 'Country',
+                                        hiddenName : 'freq',
+                                        listWidth : 200,
+                                        mode : 'local',
+                                        name : 'freq_name',
+                                        tpl : '<div class="x-grid-cell-text x-btn button"><b>{title}</b> </div>',
+                                        triggerAction : 'all',
+                                        valueField : 'code',
+                                        width : 200,
+                                        store : {
+                                            xtype: 'SimpleStore',
+                                            xns: Roo.data,
+                                            data : [ 
+                                                [ 'HOURLY' , 'Hourly at' ] ,
+                                                   [ 'DAILY' , 'Daily at'] ,
+                                                    [ 'WEEKLY' , 'Weekly at'] ,
+                                                     [ 'Montly' , 'Montly at'] 
+                                            ],
+                                            fields : ['code', 'title'],
+                                            sortInfo : { field : 'title', direction: 'ASC' }
+                                        }
+                                    }
+                                }
                             },
                             {
                                 xtype: 'ColumnModel',
