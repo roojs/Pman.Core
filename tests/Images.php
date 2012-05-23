58ae991569e7ef053a70072695bf22d96ca622ee
Alan Knowles <alan@roojs.com>
2012-05-23 09:13:35 +0800

    tests/Images.php

---
+++ tests/Images.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'Tests.php';
+
+require_once 'Pman/Images/Images.php';
+
+// test...
+echo '<PRE>';
+Pman_Core_Images::replaceImg('
+        <img src="http://test.php/Core/Images/Thumb/200x40/34/test.png">
+        <img src="http://test.php/Images/34/test.png">
+        <img src="http://test.php/Images/Download/34/test.png">        
+        
+');
