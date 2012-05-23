d718a95ea9f588cef704c5ecd51d89adf54b95ca
Alan Knowles <alan@roojs.com>
2012-05-23 09:16:23 +0800

    tests/Tests.php

--- tests/Tests.php
+++ tests/Tests.php
@@ -2,7 +2,7 @@
 
 // include to make the tests run..
 // this needs fixing - probably needs to point to a test framework..
-$_test_dir  = dirname(__FILE__).'/home/gitlive/web.roojsolutions/';
+$_test_dir  = '/home/gitlive/web.roojsolutions/';
 
 ini_set('include_path', 
             $_test_dir  . ':' . 
