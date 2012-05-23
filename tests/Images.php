d04401c90cacc3bfea5c9a579b18a384d443559e
Alan Knowles <alan@roojs.com>
2012-05-23 09:56:58 +0800

    tests/Images.php

--- tests/Images.php
+++ tests/Images.php
@@ -1,14 +1,22 @@
 <?php
 
-require_once 'Tests.php';
+require_once 'Pman.php';
 
-require_once 'Pman/Core/Images.php';
-
-// test...
-echo '<PRE>';
-Pman_Core_Images::replaceImg('
-        <img src="http://test.php/Core/Images/Thumb/200x40/34/test.png">
-        <img src="http://test.php/Images/34/test.png" width="100">
-        <a href="http://test.php/Images/Download/34/test.png">test</a>
+class Pman_Core_test_Images extends Pman
+{
+     function get() {
+        // test...
+        echo '<PRE>';
+        Pman_Core_Images::replaceImg('
+                <img src="http://test.php/Core/Images/Thumb/200x40/34/test.png">
+                <img src="http://test.php/Images/34/test.png" width="100">
+                <a href="http://test.php/Images/Download/34/test.png">test</a>
+                
+        ');
         
-');
+     }
+     function output()
+     {
+        exit;
+     }
+}
