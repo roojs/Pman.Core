7d6fe88d150473a12535607728898df72ba8ebb9
Alan Knowles <alan@roojs.com>
2012-05-23 10:56:33 +0800

    tests/Images.php

--- tests/Images.php
+++ tests/Images.php
@@ -8,7 +8,7 @@ class Pman_Core_tests_Images extends Pman
         // test...
         echo '<PRE>';
         require_once 'Pman/Core/Images.php';
-        $ret = Pman_Core_Images::replaceImg('
+        $ret = Pman_Core_Images::replaceImageURLS('
                 <img src="http://test.php/Core/Images/Thumb/200x40/34/test.png">
                 <img src="http://test.php/Images/34/test.png" width="100">
                 <a href="http://test.php/Images/Download/34/test.png">test</a>
