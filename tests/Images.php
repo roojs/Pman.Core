54ed626ab37c43578618c05dcfef5b7d789e658e
Alan Knowles <alan@roojs.com>
2012-05-23 10:58:08 +0800

    tests/Images.php

--- tests/Images.php
+++ tests/Images.php
@@ -9,9 +9,9 @@ class Pman_Core_tests_Images extends Pman
         echo '<PRE>';
         require_once 'Pman/Core/Images.php';
         $ret = Pman_Core_Images::replaceImageURLS('
-                <img src="http://test.php/Core/Images/Thumb/200x40/34/test.png">
-                <img src="http://test.php/Images/34/test.png" width="100">
-                <a href="http://test.php/Images/Download/34/test.png">test</a>
+                <img src="http://www.roojs.com/index.php/Core/Images/Thumb/200x40/34/test.png">
+                <img src="http://www.roojs.com/index.php/Images/34/test.png" width="100">
+                <a href="http://www.roojs.com/index.php/Images/Download/34/test.png">test</a>
                 
         ', 'http://roojs.com/index.php/');
         echo htmlspecialchars($ret);
