473a9dbb046f78cd24fd6c9e35689721e4cbb7aa
Chris <chris@roojs.com>
2012-05-23 18:00:42 +0800

    DataObjects/Core_notify_recur.php

--- DataObjects/Core_notify_recur.php
+++ DataObjects/Core_notify_recur.php
@@ -143,7 +143,7 @@ class Pman_Core_DataObjects_Core_notify_recur extends DB_DataObject
     
     function generateNotifications(){
         //$this->notifytimes(2);
-        print_r($this->notifytimes(2));
+        error_log($this->notifytimes(2));
     }
     
 }
