b551585d54f226c0c68b9e26fb4d8e7a89f8e0ca
Alan Knowles <alan@roojs.com>
2012-05-23 09:43:07 +0800

    tests/Images.php

--- tests/Images.php
+++ tests/Images.php
@@ -9,6 +9,6 @@ echo '<PRE>';
 Pman_Core_Images::replaceImg('
         <img src="http://test.php/Core/Images/Thumb/200x40/34/test.png">
         <img src="http://test.php/Images/34/test.png" width="100">
-        <img src="http://test.php/Images/Download/34/test.png">        
+        <a href="http://test.php/Images/Download/34/test.png">test</a>
         
 ');
