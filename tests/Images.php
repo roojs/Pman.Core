cb0c002afea80d0c16dfe93dc67d3b90df49495e
Alan Knowles <alan@roojs.com>
2012-05-23 09:57:39 +0800

    tests/Images.php

--- tests/Images.php
+++ tests/Images.php
@@ -7,6 +7,7 @@ class Pman_Core_tests_Images extends Pman
      function get() {
         // test...
         echo '<PRE>';
+        require_once 'Pman/Core/Images.php';
         Pman_Core_Images::replaceImg('
                 <img src="http://test.php/Core/Images/Thumb/200x40/34/test.png">
                 <img src="http://test.php/Images/34/test.png" width="100">
