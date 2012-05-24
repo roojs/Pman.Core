0e80b890ab3c59453cbae40de1e05bcad17a38f0
Chris <chris@roojs.com>
2012-05-24 12:34:18 +0800

    DataObjects/Core_notify_recur.php

--- DataObjects/Core_notify_recur.php
+++ DataObjects/Core_notify_recur.php
@@ -48,10 +48,9 @@ class Pman_Core_DataObjects_Core_notify_recur extends DB_DataObject
     
     
     function notifytimesRange($advance) {
-        error_log($this->dtstart);
-        error_log($this->dtend);
+        error_log($advance);
         $this->start = date('Y-m-d H:i:s', max(strtotime("NOW - 24 HOURS"), strtotime($this->dtstart)));
-        $this->end  = date('Y-m-d H:i:s', min(strtotime("NOW"), strtotime($this->dtend)));
+        $this->end  = date('Y-m-d H:i:s', min(strtotime("NOW  + $advance DAYS"), strtotime($this->dtend)));
         error_log($this->start);
         error_log($this->end);
     
