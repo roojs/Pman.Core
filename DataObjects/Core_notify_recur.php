37608392c86d44f3951d83f469ac5feede630ca2
Chris <chris@roojs.com>
2012-05-24 13:38:55 +0800

    DataObjects/Core_notify_recur.php

--- DataObjects/Core_notify_recur.php
+++ DataObjects/Core_notify_recur.php
@@ -146,7 +146,7 @@ class Pman_Core_DataObjects_Core_notify_recur extends DB_DataObject
         //$this->notifytimes(2);
         DB_DataObject::debugLevel(1);
         $w = DB_DataObject::factory('core_notify_recur');
-        $w->findAll();
+        $w->find(true);
         $test = $w->fetchAll();
         
         //$test = $this->notifytimes(2);
