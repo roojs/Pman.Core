ab3cd7598972f66f0e7b1964b2f2cde23de9d6eb
Chris <chris@roojs.com>
2012-05-25 18:07:12 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -397,7 +397,7 @@ class Pman_Core_NotifySend extends Pman
     {
         $m = 'notify'. $notify->evtype;
         if (!empty($notify->evtype) && method_exists($object,'notify'. $notify->evtype)) {
-            $object->$m($rcpt, $last_sent_date, $notify, $force)
+            $object->$m($rcpt, $last_sent_date, $notify, $force);
         }
                 
         
