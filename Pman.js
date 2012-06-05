4dddb54b1d41d0791180458c85c252332915b387
Alan Knowles <alan@roojs.com>
2012-06-05 11:29:14 +0800

    Pman.js

--- Pman.js
+++ Pman.js
@@ -814,29 +814,7 @@ Pman = new Roo.Document(
         // new versions should have 'part' as [ module : part ]
         
         
-        if (typeof(obj.part) != 'undefined')  {
-            
-            var permname = obj.part.join('.');
-                // we now have permission...
-                // obj.moduleOwner '.' lname
-            Roo.log("CHECKING PERM: " + permname);
-            Roo.log("Exists: " + (Pman.hasPermExists(permname) ? 1 : 0));
-            Roo.log("has: " + (Pman.hasPerm(permname,'S') ? 1 : 0));
-            Roo.log(Pman.Login.authUser);
-            if (Pman.hasPermExists(permname) && !Pman.hasPerm(permname,'S')) {
-                // it's a turned off permission...
-                Roo.log(permname + " is Disabled for this user");
-                obj.disabled = true;
-                return;
-            }
-            if (appDisabled.indexOf(permname) > -1)  {
-                Roo.log(permname + " is Disabled for this site");
-                obj.disabled = true;
-                return;
-            }
-            
-            
-        }
+       
         
         if ( obj.isTop) {
             // false parent... use it..
@@ -881,7 +859,29 @@ Pman = new Roo.Document(
     
     beforebuild : function(obj)
     {
-        
+        if (typeof(obj.part) != 'undefined')  {
+           
+            var permname = obj.part.join('.');
+                // we now have permission...
+                // obj.moduleOwner '.' lname
+            Roo.log("CHECKING PERM: " + permname);
+            Roo.log("Exists: " + (Pman.hasPermExists(permname) ? 1 : 0));
+            Roo.log("has: " + (Pman.hasPerm(permname,'S') ? 1 : 0));
+            Roo.log(Pman.Login.authUser);
+            if (Pman.hasPermExists(permname) && !Pman.hasPerm(permname,'S')) {
+                // it's a turned off permission...
+                Roo.log(permname + " is Disabled for this user");
+                obj.disabled = true;
+                return;
+            }
+            if (appDisabled.indexOf(permname) > -1)  {
+                Roo.log(permname + " is Disabled for this site");
+                obj.disabled = true;
+                return;
+            }
+            
+            
+        }
         
         
     },
