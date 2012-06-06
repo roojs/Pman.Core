e520dd9f926ab3b242ce9acefc54e1b4c7af3ace
Alan Knowles <alan@roojs.com>
2012-06-06 20:58:19 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -105,7 +105,7 @@ class Pman_Core_SimpleExcel extends Pman
         foreach($data as $r=>$clo) {
             $cl = $clo;
             if (is_object($clo)) {
-                $cl = (array)$clo;
+                $cl = (array)$clo; // lossless converstion..
             }
             
             if (isset($cfg['row_height'])) {
