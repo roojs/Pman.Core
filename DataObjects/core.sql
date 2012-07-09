374feb9abd0929769f17b6e1049abaf15f0d1c9d
Alan Knowles <alan@roojs.com>
2012-07-09 13:20:28 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -66,10 +66,10 @@ ALTER TABLE Events ADD COLUMN   person_table varchar(64) default NULL;
 ALTER TABLE Events ADD COLUMN   remarks text ;
  
 
-#very old code..
+--#very old code..
 ALTER TABLE Events CHANGE COLUMN EventID id INT(11) AUTO_INCREMENT NOT NULL;
 
-# this are for pre-postgres support code..
+--# this are for pre-postgres support code..
 ALTER TABLE Events CHANGE COLUMN User person_name VARCHAR(128);
 ALTER TABLE Events RENAME COLUMN User TO person_name;
 ALTER TABLE Events CHANGE COLUMN Date event_when DATETIME ;
@@ -85,6 +85,7 @@ ALTER TABLE Events ADD INDEX lookupf (on_id, action, on_table, person_id, event_
 
 ALTER TABLE Events ADD INDEX lookuppt ( person_table);
 
+--# speeding up lookups. 
 ALTER TABLE Events ADD INDEX lookup_when( person_id, event_id );
 
 
