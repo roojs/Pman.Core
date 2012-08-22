6c7abd7277d865e62c7eae0aded067d770561634
Alan Knowles <alan@roojs.com>
2012-08-22 14:05:31 +0800

    tests/Tests.php

--- tests/Tests.php
+++ tests/Tests.php
@@ -2,17 +2,21 @@
 
 // include to make the tests run..
 // this needs fixing - probably needs to point to a test framework..
-$_test_dir  = '/var/www/web.roojsolutions/';
 
-ini_set('include_path', 
-            $_test_dir  . ':' . 
-            $_test_dir  .'/pearfix:' . 
-            $_test_dir  .'/pear:' . 
-            ini_get('include_path'));
+function init() {
+    global $_test_dir  ;
+    $_test_dir  = '/var/www/web.roojsolutions/';
+    
+    ini_set('include_path', 
+                $_test_dir  . ':' . 
+                $_test_dir  .'/pearfix:' . 
+                $_test_dir  .'/pear:' . 
+                ini_get('include_path'));
+    
+    define('DB_DATAOBJECT_NO_OVERLOAD', true);
+    
+    require_once 'HTML/FlexyFramework/Page.php';
+    
+    require_once 'DB/DataObject.php';
 
-define('DB_DATAOBJECT_NO_OVERLOAD', true);
-
-require_once 'HTML/FlexyFramework/Page.php';
-
-require_once 'DB/DataObject.php';
-
+}
