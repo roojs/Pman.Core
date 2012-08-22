17818500a54ccb491f33a9d95934407eeb0e7b04
Alan Knowles <alan@roojs.com>
2012-08-22 13:05:54 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -211,7 +211,7 @@ class Pman_Core_Mailer {
     {
         
         if (preg_match('#^file:///#', $url)) {
-            $file = preg_replace('#^file:///#', '', $url);
+            $file = preg_replace('#^file://#', '', $url);
             require_once 'File/MimeType.php';
             $m  = new File_MimeType();
             $mt = $m->fromFilename($file);
