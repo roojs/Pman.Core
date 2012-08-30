79c93af889ee582ad294ca76d959b8388cb802bc
Alan Knowles <alan@roojs.com>
2012-08-30 22:30:33 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -117,7 +117,7 @@ ALTER TABLE group_members ADD COLUMN  group_id int(11) default NULL;
 ALTER TABLE group_members ADD COLUMN   user_id int(11) NOT NULL default 0;
 
 -- BC name..
-RENAME TABLE Group_Members TO group_rights;
+RENAME TABLE Group_Rights TO group_rights;
 
 
 CREATE TABLE  group_rights  (
