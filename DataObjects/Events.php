e5153fe3c010d394f07b3fcc74421db20cedb4cb
Alan Knowles <alan@roojs.com>
2012-09-03 16:29:14 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -81,7 +81,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
             $this->selectAdd("count($tn.id) as qty");
             $this->selectAdd("count( distinct $tn.on_id) as uqty");
             $this->whereAdd('LENGTH(join_person_id_id.name) > 0 ');
-            $this->groupBy('person_id');
+            $this->groupBy('person_id,join_person_id_id.name');
         }
          if (isset($q['query']['table_sum'])) {
             //DB_DataObject::debugLevel(1);
