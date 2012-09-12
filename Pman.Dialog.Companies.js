02f60abdc21a203ddee5ceaf07d610abfafadb08
Chris <chris@roojs.com>
2012-09-12 15:11:13 +0800

    Pman.Dialog.Companies.js

--- Pman.Dialog.Companies.js
+++ Pman.Dialog.Companies.js
@@ -2,11 +2,7 @@
 
 
 Pman.Dialog.Companies =   new Roo.util.Observable({
-//    events : {
-//        'beforerender' : true, // trigger so we can add modules later..
-//        'show' : true, // trigger on showing form.. - to load additiona data..
-//        'beforesave' : true
-//    },
+    
     show : function (data, callback)
     {
         Pman.Dialog.CoreCompanies.show({id:data.id});
