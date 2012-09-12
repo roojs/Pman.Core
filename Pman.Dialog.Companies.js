58202dbdfda094ac47f05c633ea21db7cd50319d
Alan Knowles <alan@roojs.com>
2012-09-12 16:43:47 +0800

    Pman.Dialog.Companies.js

--- Pman.Dialog.Companies.js
+++ Pman.Dialog.Companies.js
@@ -21,7 +21,7 @@ Pman.Dialog.Companies =   new Roo.util.Observable({
     show : function (data, callback)
     {
         Pman.Dialog.CoreCompanies.show({id:data.id});
-    }
+    },
    
    dialog : false,
    form : false,
