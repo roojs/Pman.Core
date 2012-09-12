f822d6718be89fa4934aeaf1a32613a51db29943
Alan Knowles <alan@roojs.com>
2012-09-12 15:58:30 +0800

    Pman.Dialog.CoreCompanies.bjs Pman.Dialog.CoreCompanies.js

--- Pman.Dialog.CoreCompanies.js
+++ Pman.Dialog.CoreCompanies.js
@@ -123,7 +123,7 @@ Pman.Dialog.CoreCompanies = {
                                                 }
                                             },
                                             alwaysQuery : true,
-                                            displayField : 'name',
+                                            displayField : 'display_name',
                                             emptyText : "Select Type",
                                             fieldLabel : 'Type',
                                             forceSelection : true,
