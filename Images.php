1e2bafbb111e887028c7adaefb06679fd06b9288
Chris <chris@roojs.com>
2012-10-12 19:57:38 +0800

    Images.php

--- Images.php
+++ Images.php
@@ -79,7 +79,7 @@ class Pman_Core_Images extends Pman
             
         } else if (!empty($bits[0]) && $bits[0] == 'events') {
             $popts = PEAR::getStaticProperty('Pman','options');
-            $file = "{$popts['event_log_dir']}/{$bits[1]}.jpg";
+            $file = "{$popts['event_log_dir']}/{$bits[1]}";
             header ('Content-Type: image/jpeg');
             if(!empty($bits[2]) && $bits[2] == 'download'){
                 header("Content-Disposition: attachment; filename=\"".basename($file)."\";" );
@@ -87,7 +87,7 @@ class Pman_Core_Images extends Pman
                 flush();
                 readfile($file);
             }else{
-                $fh = fopen($file,'r');
+                $fh = fopen($file.'.jpg','r');
                 echo fread($fh,filesize($file));
             }
             exit;
