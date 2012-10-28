f87f238e7253310a136f537216401c638ffb239d
Alan Knowles <alan@akbkhome.com>
2012-10-28 21:54:42 +0800

    DataObjects/Core_watch.php

--- DataObjects/Core_watch.php
+++ DataObjects/Core_watch.php
@@ -107,7 +107,6 @@ class Pman_Core_DataObjects_Core_watch extends DB_DataObject
             }
             $nn->act_start( date("Y-m-d H:i:s", $when !== false ? strtotime($when) : time()) );
             $n->insert();
-                
             
         }
          
