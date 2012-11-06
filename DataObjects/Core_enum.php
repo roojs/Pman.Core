66ec8bb46b421b166966963ee459c7bfa1bf7e6a
Alan Knowles <alan@roojs.com>
2012-11-06 12:41:07 +0800

    DataObjects/Core_enum.php

--- DataObjects/Core_enum.php
+++ DataObjects/Core_enum.php
@@ -46,7 +46,7 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
          
     }
     
-    static function lookup($etype,$name) {
+    function lookup($etype,$name) {
         $ce = DB_DataObject::Factory('core_enum');
         $ce->etype = $etype;
         $ce->name = $name;
