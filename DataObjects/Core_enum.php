d8977a5b7b52bd9bcfc55cdb7abe7b9fa0ca805e
Alan Knowles <alan@roojs.com>
2012-11-07 11:33:35 +0800

    DataObjects/Core_enum.php

--- DataObjects/Core_enum.php
+++ DataObjects/Core_enum.php
@@ -57,5 +57,15 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         
     }
     
+    function lookupObject($etype,$name) {
+        $ce = DB_DataObject::Factory('core_enum');
+        $ce->etype = $etype;
+        $ce->name = $name;
+        if ($ce->find(true)) {
+            return $ce;
+        }
+        return false;
+        
+    }
     
 }
