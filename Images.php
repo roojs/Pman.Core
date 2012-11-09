f1362b5317fecd5f3d406db5d73b80a06aa12a92
Chris <chris@roojs.com>
2012-11-09 17:53:33 +0800

    DataObjects/Cms_category_type.php DataObjects/Core_enum.php DataObjects/Core_watch.php Mailer.php Notify.php NotifySend.php RunGenerator.php Images.php

--- Images.php
+++ Images.php
@@ -100,6 +100,7 @@ class Pman_Core_Images extends Pman
         
         if (strpos($id,':') > 0) {  // id format  tablename:id:-imgtype
             print_r($id);
+            exit;
             $onbits = explode(':', $id);
             if ((count($onbits) < 2)   || empty($onbits[1]) || !is_numeric($onbits[1]) || !strlen($onbits[0])) {
                 die("Bad url");
