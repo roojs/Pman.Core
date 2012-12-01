50169c9b231ce3b25986e4c6f04ee25327343d5d
Alan Knowles <alan@akbkhome.com>
2012-12-01 20:02:45 +0800

    Pman.Include.js

---
+++ Pman.Include.js
@@ -0,0 +1,14 @@
+/**
+ * include a script -- normaly used for testing..
+ *
+ *
+ */
+
+
+Pman.Include = function(sp) {
+    
+    var tag = '<script type="text/javascript"  src="'rootURL + sp;
+    Roo.get(documen.body).appendChild()
+    
+    
+)
