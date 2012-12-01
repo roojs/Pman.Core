72372db94a765bdca65245997a6d43facabbcdd9
Alan Knowles <alan@akbkhome.com>
2012-12-01 20:05:54 +0800

    Pman.Include.js

--- Pman.Include.js
+++ Pman.Include.js
@@ -6,9 +6,22 @@
 
 
 Pman.Include = function(sp) {
+    var head = document.getElementsByTagName("head")[0];
+ 
     
-    var tag = '<script type="text/javascript"  src="'rootURL + sp;
+    var script = document.createElement("script");
+    script.setAttribute("src",  rootURL + sp    +'?ts=' + Math.random() );
+    script.setAttribute("type", "text/javascript");
+    //script.setAttribute("id", trans.scriptId);
+    head.appendChild(script);
+    
     Roo.get(documen.body).appendChild()
-    
-    
+     
+)
+
+Pman.Include.cached = function(sp) {
+    var tag = '<script type="text/javascript"  src="' +
+        rootURL + sp     
+    Roo.get(documen.body).appendChild()
+     
 )
