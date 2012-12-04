f044adafe1c091c7b841c994362c4b2a3cb5ca53
Alan Knowles <alan@akbkhome.com>
2012-12-04 22:51:57 +0800

    DataObjects/Core_watch.php

--- DataObjects/Core_watch.php
+++ DataObjects/Core_watch.php
@@ -103,7 +103,7 @@ class Pman_Core_DataObjects_Core_watch extends DB_DataObject
             $nn->person_id = $w->person_id;
             
             $nf = clone($nn);
-            $nf->whereAdd("sent > '2000-01-01'");
+            $nf->whereAdd("sent < '2000-01-01'");
             if ($nf->count()) {
                 // we have a item in the queue for that waiting to be sent..
                 continue;
