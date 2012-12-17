b27785ea89decafab23efbabab0486850963335a
Alan Knowles <alan@roojs.com>
2012-12-17 11:10:58 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -810,6 +810,10 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
     function onInsert($req, $roo)  
     {
         $p = DB_DataObject::factory('Person');
+        
+        print_R($roo->authUser->id);
+        exit;
+        
         if ($roo->authUser->id < 0 && $p->count() == 1) {
             // this seems a bit risky...
             
