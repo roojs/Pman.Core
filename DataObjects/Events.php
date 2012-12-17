5cc6e48b55a792c033b1376312e7fd460e4ae9ad
Chris <chris@roojs.com>
2012-12-17 17:35:19 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -83,7 +83,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
             
             $this->_join = "LEFT JOIN {$jt->tableName()} AS join_person_id_id ON (join_person_id_id.id=Events.person_id)";
             $q['person_table'] = '';
-            
+            print_r($this->person_table);exit;
             // $this->_join
         
         }
