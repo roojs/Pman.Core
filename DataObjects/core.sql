6f451313121e6a6d8b58c6ceb1307d3e8661fd8c
Chris <chris@roojs.com>
2012-12-19 17:59:47 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -165,6 +165,8 @@ CREATE TABLE Office (
 ALTER TABLE Office ADD COLUMN  company_id int(11) NOT NULL default '0';
 ALTER TABLE Office ADD COLUMN    name varchar(64)  NOT NULL  DEFAULT '';
 ALTER TABLE Office ADD COLUMN    address text ;
+ALTER TABLE Office ADD COLUMN address2 TEXT;
+ALTER TABLE Office ADD COLUMN address3 TEXT;
 ALTER TABLE Office ADD COLUMN    phone varchar(32)  NOT NULL  DEFAULT '';
 ALTER TABLE Office ADD COLUMN    fax varchar(32)  NOT NULL  DEFAULT '';
 ALTER TABLE Office ADD COLUMN    email varchar(128)  NOT NULL  DEFAULT '';
