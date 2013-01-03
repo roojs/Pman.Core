c629b404e3873b5eb3c2db94f948b34cf32cfbff
Chris <chris@roojs.com>
2013-01-03 17:20:14 +0800

    DataObjects/I18n.php

--- DataObjects/I18n.php
+++ DataObjects/I18n.php
@@ -179,14 +179,14 @@ class Pman_Core_DataObjects_I18n extends DB_DataObject
                 require_once 'I18Nv2/Language.php';
                 $c = new I18Nv2_Language('en');
                 $ret =  array_keys($c->codes); // we need to make sure these are lowercase!!!
-                print_r($c);
+                
 //                foreach($ret as $k){
 //                    
 //                }
 //                        $ret[$i] = strol
                  
                 if (!empty($cfg['add_l'])) {
-                    $ret = array_merge($ret, array_keys($cfg['l']));
+                    $ret = array_merge($ret, array_keys($cfg['add_l']));
                 }
                 print_r('<PRE>');
                 print_r($ret);
