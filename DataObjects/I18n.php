a411146d2c9cbf19854dc8f6018ba96721cd1a87
Chris <chris@roojs.com>
2013-01-03 18:57:52 +0800

    DataObjects/I18n.php

--- DataObjects/I18n.php
+++ DataObjects/I18n.php
@@ -198,7 +198,7 @@ class Pman_Core_DataObjects_I18n extends DB_DataObject
         
         foreach ($ret as $k=>$v) {
             $ret[$k] = ($t=='l') ? $ret[$k] : strtoupper($v);
-            if($t=='l') print_r($ret);
+            if($t=='l') echo '<PRE>'.print_r($ret);
         }
 
         return $ret;
