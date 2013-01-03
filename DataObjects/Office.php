3a17497b8cfd4c7fabe2240a6bd890e91c475d0b
Chris <chris@roojs.com>
2013-01-03 15:19:28 +0800

    DataObjects/Office.php

--- DataObjects/Office.php
+++ DataObjects/Office.php
@@ -24,6 +24,11 @@ class Pman_Core_DataObjects_Office extends DB_DataObject
     
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
+    
+    function autoJoin()
+    {
+        
+    }
     function toEventString() {
         return $this->name;
     }
