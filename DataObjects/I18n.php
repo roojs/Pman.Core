dfa05b097149aeb55aa08b68e397f678ebaaead1
Chris <chris@roojs.com>
2013-01-03 17:02:44 +0800

    DataObjects/I18n.php

--- DataObjects/I18n.php
+++ DataObjects/I18n.php
@@ -178,7 +178,7 @@ class Pman_Core_DataObjects_I18n extends DB_DataObject
                 require_once 'I18Nv2/Language.php';
                 $c = new I18Nv2_Language('en');
                 $ret =  array_keys($c->codes); // we need to make sure these are lowercase!!!
-                print_r($ret);
+                //print_r($ret);
 //                foreach($ret as $k){
 //                    
 //                }
@@ -204,7 +204,7 @@ class Pman_Core_DataObjects_I18n extends DB_DataObject
             $ret[$k] = ($t=='l') ? $ret[$k] : strtoupper($v);
         }
         
-        
+        print_r($ret);
         return $ret;
     }
     
