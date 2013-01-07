da81da79dd53b9b21df4921a524ecdab4243bf92
Alan Knowles <alan@roojs.com>
2013-01-07 16:58:01 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -292,6 +292,7 @@ ALTER TABLE  Images ADD COLUMN linkurl VARCHAR(254) DEFAULT '' NOT NULL;
 ALTER TABLE  Images ADD COLUMN descript TEXT DEFAULT '' NOT NULL;
 ALTER TABLE  Images ADD COLUMN title VARCHAR(128) DEFAULT '' NOT NULL;
 
+ALTER TABLE Images    ALTER COLUMN   mimetype mimetype  varchar(128) NOT NULL default '';
 
 ALTER TABLE Images ADD INDEX lookup(ontable, onid);
 
