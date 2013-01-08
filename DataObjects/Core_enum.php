8fbe3d45d00820d612dd3ce06afa2301561b8802
Chris <chris@roojs.com>
2013-01-08 14:49:06 +0800

    DataObjects/Core_enum.php

--- DataObjects/Core_enum.php
+++ DataObjects/Core_enum.php
@@ -30,7 +30,7 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         
         if(!empty($q['!name'])){
             $names = explode($q['!name']);
-            $this->whereAddIn('name', $names, 'string', 'NOT');
+            $this->whereAddIn('name', $names, 'string');
             
             //print_r(11);
         }
