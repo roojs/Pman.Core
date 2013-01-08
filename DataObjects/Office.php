9206766f2ee160a268dab69cd21016226518ee76
Chris <chris@roojs.com>
2013-01-08 18:04:41 +0800

    DataObjects/Office.php

--- DataObjects/Office.php
+++ DataObjects/Office.php
@@ -57,7 +57,7 @@ class Pman_Core_DataObjects_Office extends DB_DataObject
                 (join_country.lkey = Office.country AND join_country.inlang = 'en')
             GROUP BY
                 Office.country
-                WHERE Office.id = 2 OR Office.id = 6
+                
         ";
         $item = DB_DataObject::Factory('I18n');
         $this->selectAs($item, 'country_id_%s', 'join_country');
