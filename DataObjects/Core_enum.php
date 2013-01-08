aaf5df1381d84ed5aec0e643f89218885e376c54
Chris <chris@roojs.com>
2013-01-08 14:54:25 +0800

    DataObjects/Core_enum.php

--- DataObjects/Core_enum.php
+++ DataObjects/Core_enum.php
@@ -31,7 +31,7 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         if(!empty($q['!name'])){
             $names = explode(',', $q['!name']);
             $this->whereAdd("
-                    core_enum.name NOT IN ({$q['!name']})
+                    core_enum.name NOT IN ('{$q['!name']}')
                 ");
             //$this->whereAddIn('name', $names, 'string', 'NOT');
             
