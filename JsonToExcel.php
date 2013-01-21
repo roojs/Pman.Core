9e5a8d694d593c9bf6c2a3f5f3b8a0c0a2f6a31e
Alan Knowles <alan@roojs.com>
2013-01-21 12:19:23 +0800

    JsonToExcel.php

--- JsonToExcel.php
+++ JsonToExcel.php
@@ -72,9 +72,9 @@ class Pman_Core_JsonToExcel extends Pman
          $workbook->close();
         
         require_once 'File/Convert.php';
-        $fc=  new File_Convert($this->outfile2, "application/vnd.ms-excel");
+        $fc=  new File_Convert($outfile2, "application/vnd.ms-excel");
         $fn = $fc->convert("application/vnd.ms-excel"); 
-        $fc->serve('attachment',$outfile2); // can fix IE Mess
+        $fc->serve('attachment','excel-'.date('Y-m-d-H-i-s').'.xls'); // can fix IE Mess
         unlink($outfile2); 
     }
     
