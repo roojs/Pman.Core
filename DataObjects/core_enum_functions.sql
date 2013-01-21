65a30d9d13958bd6f9eba296286713ee7f8ee583
Chris <chris@roojs.com>
2013-01-21 13:01:05 +0800

    DataObjects/core_enum_functions.sql

--- DataObjects/core_enum_functions.sql
+++ DataObjects/core_enum_functions.sql
@@ -12,19 +12,3 @@ CREATE FUNCTION core_enum_display_name(in_id INT(11))
         
     END $$
 DELIMITER ;
-
-
-
-DROP FUNCTION IF EXISTS i18n_translate;
-DELIMITER $$
-CREATE FUNCTION i18n_translate(in_ltype  varchar(1) , in_lkey varchar(8), in_inlang varchar(8)) 
-        RETURNS VARCHAR(64) DETERMINISTIC
-    BEGIN
-        DECLARE ret  VARCHAR(64);
-        SET ret  = '';
-        SELECT lval INTO ret FROM i18n
-            WHERE ltype=in_ltype AND lkey=in_lkey and inlang=in_inlang LIMIT 1;
-        RETURN ret;
-        
-    END $$
-DELIMITER ;
