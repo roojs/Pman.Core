4763bbc5849f151d5a2a828578255bc59f5fc4e3
Chris <chris@roojs.com>
2013-01-21 13:00:55 +0800

    DataObjects/core_enum_functions.sql

--- DataObjects/core_enum_functions.sql
+++ DataObjects/core_enum_functions.sql
@@ -1,8 +1,24 @@
 
 DROP FUNCTION IF EXISTS i18n_translate;
 DELIMITER $$
-CREATE FUNCTION core_enum_display_name(id INT(11))
+CREATE FUNCTION core_enum_display_name(in_id INT(11))
         RETURNS VARCHAR(256) DETERMINISTIC
+    BEGIN
+        DECLARE ret  VARCHAR(256);
+        SET ret  = '';
+        SELECT display_name INTO ret FROM core_enum
+            WHERE id=in_id LIMIT 1;
+        RETURN ret;
+        
+    END $$
+DELIMITER ;
+
+
+
+DROP FUNCTION IF EXISTS i18n_translate;
+DELIMITER $$
+CREATE FUNCTION i18n_translate(in_ltype  varchar(1) , in_lkey varchar(8), in_inlang varchar(8)) 
+        RETURNS VARCHAR(64) DETERMINISTIC
     BEGIN
         DECLARE ret  VARCHAR(64);
         SET ret  = '';
