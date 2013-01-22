8440f10dcb0ad0c3daebe74bb70bc900048f680a
Edward <edward@roojs.com>
2013-01-22 15:27:28 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -150,7 +150,7 @@ class Pman_Core_SimpleExcel extends Pman
             print_r($start_row);
             print_r('<br/>');
             print_r($c);
-            
+            print_r('<br/>');
             $worksheet->write($start_row, $c, $col_cfg['header']);
             $worksheet->setColumn ( $c, $c, $col_cfg['width'] / 5);
              
