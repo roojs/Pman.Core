978e8935caef2b7a84fdc6532ca24d973d9a4a6a
Edward <edward@roojs.com>
2013-01-22 15:26:59 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -155,6 +155,7 @@ class Pman_Core_SimpleExcel extends Pman
             $worksheet->setColumn ( $c, $c, $col_cfg['width'] / 5);
              
         }
+        exit;
         $start_row++;
         $hasRender  = false;
            //     DB_DataObject::debugLevel(1);
