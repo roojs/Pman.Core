6f97e0623002b9271689f86cd3d81b496e165915
Edward <edward@roojs.com>
2013-01-23 15:17:27 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -146,9 +146,14 @@ class Pman_Core_SimpleExcel extends Pman
          
          
         foreach($cfg['cols'] as $c=>$col_cfg) {
-            
-            
-            
+            print_r($col_cfg);
+            print_r('<br/>');
+            print_r($start_row);
+            print_r('<br/>');
+            print_r($c);
+            print_r('<br/>');
+            print_r($col_cfg['header']);
+            print_r('<br/>');
             $worksheet->write($start_row, $c, $col_cfg['header']);
             $worksheet->setColumn ( $c, $c, $col_cfg['width'] / 5);
              
