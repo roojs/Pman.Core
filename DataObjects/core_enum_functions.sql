6c731d6853c3d6b7266f8e186baab8edb7ec473a
Chris <chris@roojs.com>
2013-01-23 17:11:51 +0800

    DataObjects/core_enum_functions.sql

--- DataObjects/core_enum_functions.sql
+++ DataObjects/core_enum_functions.sql
@@ -24,3 +24,16 @@ CREATE FUNCTION core_enum_name(in_id INT(11))
         RETURN ret;
     END $$
 DELIMITER ;
+
+DROP FUNCTION IF EXISTS core_enum_id_by_name;
+DELIMITER $$
+CREATE FUNCTION core_enum_id_by_name(in_name VARCHAR(256))
+        RETURNS INT(11) DETERMINISTIC
+    BEGIN
+        DECLARE ret  INT(11);
+        SET ret  = '';
+        SELECT id INTO ret FROM core_enum
+            WHERE name=in_name LIMIT 1;
+        RETURN ret;
+    END $$
+DELIMITER ;
