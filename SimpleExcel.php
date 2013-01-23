09f4e36f0c4b1e80240546b62f54c6461251cc73
Edward <edward@roojs.com>
2013-01-23 16:16:28 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -163,6 +163,7 @@ class Pman_Core_SimpleExcel extends Pman
             foreach($cfg['cols']  as $c=>$col_cfg) {
                 $v = isset($cl[$col_cfg['dataIndex']]) ? $cl[$col_cfg['dataIndex']] : '';
                 if (empty($cl[$col_cfg['dataIndex']])) {
+                    $worksheet->write($start_row+$r, $c, '', $formats['gray']);
                     continue;
                 }
                 if (isset($col_cfg['txtrenderer'])) {
