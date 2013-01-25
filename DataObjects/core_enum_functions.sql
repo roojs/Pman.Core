f89f1a5b01cda87725e919d6b939deb023df064d
Chris <chris@roojs.com>
2013-01-25 13:45:42 +0800

    DataObjects/core_enum_functions.sql

--- DataObjects/core_enum_functions.sql
+++ DataObjects/core_enum_functions.sql
@@ -25,6 +25,19 @@ CREATE FUNCTION core_enum_name(in_id INT(11))
     END $$
 DELIMITER ;
 
+DROP FUNCTION IF EXISTS core_enum_name_to_display_name;
+DELIMITER $$
+CREATE FUNCTION core_enum_name_to_display_name(in_etype VARCHAR(256), in_name VARCHAR(256))
+        RETURNS VARCHAR(256) DETERMINISTIC
+    BEGIN
+        DECLARE ret VARCHAR(256);
+        SET ret  = '';
+        SELECT display_name INTO ret FROM core_enum
+            WHERE name=in_name AND etype=in_etype LIMIT 1;
+        RETURN ret;
+    END $$
+DELIMITER ;
+
 DROP FUNCTION IF EXISTS core_enum_id_by_name;
 DELIMITER $$
 CREATE FUNCTION core_enum_id_by_name(in_name VARCHAR(256))
