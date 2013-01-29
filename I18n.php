e02f1e661854c8b773ad5b26a1b4b4d1797d4a5d
Chris <chris@roojs.com>
2013-01-29 13:30:44 +0800

    I18n.php

--- I18n.php
+++ I18n.php
@@ -290,6 +290,8 @@ class Pman_Core_I18n extends Pman
             // this may fail...
             $f = @file_get_contents('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
             if (!strlen($f)) {
+                $target = dirname(__FILE__).'eurofxref-daily.xml';
+                
                 return false;
             }
             file_put_contents($target,$f);
