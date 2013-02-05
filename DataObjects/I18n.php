d4fdb35d840725626d4007c74f01aa05cba5f837
Chris <chris@roojs.com>
2013-02-05 14:32:47 +0800

    DataObjects/I18n.php

--- DataObjects/I18n.php
+++ DataObjects/I18n.php
@@ -327,7 +327,7 @@ class Pman_Core_DataObjects_I18n extends DB_DataObject
         if ($orig_lang == 'zh_HK') {
             // then translation is by default in simplified.
             print_r($ret);
-            $ret = iconv("UTF-8", "GB", $ret);
+            $ret = iconv("UTF-8", "GB//TRANSLIT", $ret);
             print_r($ret);
             $ret = iconv("GB", "BIG5", $ret);
             print_r($ret);
