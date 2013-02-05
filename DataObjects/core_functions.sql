03923556be4a72958308b72a6d9ffc5f065d6f25
Chris <chris@roojs.com>
2013-02-05 13:55:29 +0800

    DataObjects/core_functions.sql

--- DataObjects/core_functions.sql
+++ DataObjects/core_functions.sql
@@ -12,7 +12,7 @@ CREATE FUNCTION i18n_translate(in_ltype  varchar(1) , in_lkey varchar(8), in_inl
         SELECT id, lval  INTO v_id, ret FROM i18n
             WHERE ltype=in_ltype AND lkey=in_lkey and inlang=in_inlang LIMIT 1;
 
-        IF id > 0 THEN 
+        IF v_id  > 0 THEN 
             RETURN ret;
         END IF;
 
@@ -21,7 +21,7 @@ CREATE FUNCTION i18n_translate(in_ltype  varchar(1) , in_lkey varchar(8), in_inl
         SELECT id, lval  INTO v_id, ret FROM i18n
             WHERE ltype=in_ltype AND lkey=in_lkey and inlang='en' LIMIT 1;
 
-        IF id > 0 THEN 
+        IF v_id > 0 THEN 
             RETURN ret;
         END IF;
         
