70a34eb467a6bafab685a509e71379d99adfb9c9
Edward <edward@roojs.com>
2013-03-14 19:59:05 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -66,6 +66,7 @@ class Pman_Core_SimpleExcel extends Pman
         $formats = array();
        
         $cfg['formats'] = isset($cfg['formats']) ? $cfg['formats'] : array();
+        print_r($cfg);exit;
         foreach($cfg['formats'] as $f=>$fcfg) {
             $formats[$f] = & $workbook->addFormat();
             foreach($fcfg as $k=>$v) {
