69c8e7601bccae0adc350e651e408450a273213f
Edward <edward@roojs.com>
2013-03-18 12:38:22 +0800

    ExcelToJson.php

--- ExcelToJson.php
+++ ExcelToJson.php
@@ -58,7 +58,7 @@ class Pman_Core_ExcelToJson extends Pman_Roo
             }
             
             if(!$header){
-               $ret[$n[0]] = $n[1];
+               $ret[str_replace(':', '', $n[0])] = $n[1];
                continue;
             }
             
