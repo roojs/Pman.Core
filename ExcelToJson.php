2435eae5c4a11d01026ed44f47bb755531fc45ca
Edward <edward@roojs.com>
2013-03-18 11:47:07 +0800

    ExcelToJson.php

--- ExcelToJson.php
+++ ExcelToJson.php
@@ -97,7 +97,7 @@ class Pman_Core_ExcelToJson extends Pman_Roo
         foreach ($rows['TRANSFER ITEMS'] as $r){
             $itemsite = DB_DataObject::factory('itemsite');
             $itemsite->autoJoin();
-            $itemsite->whereAdd("join_itemsite_item_id_item_id.item_number = {$this->escape($r['ITEM CODE'])}");
+            $itemsite->whereAdd("join_itemsite_item_id_item_id.item_number = {$itemsite->escape($r['ITEM CODE'])}");
             $itemsite->find(true);
             print_r($itemsite);exit;
 //            if(!$itemsite->get('join_itemsite_item_id_item_id.item_number', )){
