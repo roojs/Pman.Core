ebb16dec6cff6e83093aebb722cf4e34624de857
Edward <edward@roojs.com>
2013-03-18 12:22:24 +0800

    ExcelToJson.php

--- ExcelToJson.php
+++ ExcelToJson.php
@@ -58,7 +58,7 @@ class Pman_Core_ExcelToJson extends Pman_Roo
             }
             
             if(!$header){
-               $rows[strtoupper($n[0])] = $n[1];
+               $ret[$n[0]] = $n[1];
                continue;
             }
             
@@ -102,7 +102,7 @@ class Pman_Core_ExcelToJson extends Pman_Roo
                 $this->jerr("error occur on getting item with reference " . $r['ITEM CODE']);
             }
             
-            $ret[] = array(
+            $ret['data'] = array(
                 'itemsite_item_id' => $itemsite->itemsite_item_id,
                 'itemsite_id' => $itemsite->pid(),
                 'itemsite_item_id_item_number' => $r['ITEM CODE'],
