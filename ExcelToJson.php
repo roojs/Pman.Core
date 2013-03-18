04285adecaef91e8d508e826993048c19041c32a
Edward <edward@roojs.com>
2013-03-18 11:44:31 +0800

    ExcelToJson.php

--- ExcelToJson.php
+++ ExcelToJson.php
@@ -96,8 +96,8 @@ class Pman_Core_ExcelToJson extends Pman_Roo
         
         foreach ($rows['TRANSFER ITEMS'] as $r){
             $itemsite = DB_DataObject::factory('itemsite');
-//            $itemsite->autoJoin();
-            if(!$itemsite->get('itemsite_id', 5583)){
+            $itemsite->autoJoin();
+            if(!$itemsite->get('join_itemsite_item_id_item_id.item_number', $r['ITEM CODE'])){
                 $this->jerr("error occur on getting item with reference " . $r['ITEM CODE']);
             }
             print_r($itemsite);exit;
