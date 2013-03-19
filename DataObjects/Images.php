ddb9cc6837a942ee3b229295a75ebe10ab844bbc
Edward <edward@roojs.com>
2013-03-19 15:45:53 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -56,6 +56,7 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
     
     function beforeInsert($q, $roo) 
     {
+        print_r( 'run');exit;
         if (isset($q['_remote_upload'])) {
             require_once 'System.php';
             $tmpdir  = System::mktemp("-d remote_upload");
