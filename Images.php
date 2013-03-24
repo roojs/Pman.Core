7665f0af1ae3055044a9a6ab6d0deee824ba157e
Alan Knowles <alan@akbkhome.com>
2013-03-25 07:33:03 +0800

    Images.php

--- Images.php
+++ Images.php
@@ -234,7 +234,7 @@ class Pman_Core_Images extends Pman
  
     function serve($img)
     {
-        $this->sessionState(0);
+        $this->sessionState(0); // turn off session... - locking...
         
         require_once 'File/Convert.php';
         if (!file_exists($img->getStoreName())) {
