ebd3c6efa0f328ac26a9c620bb5815419a30d4fd
Alan Knowles <alan@roojs.com>
2013-04-01 17:38:51 +0800

    ExcelToJson.php

--- ExcelToJson.php
+++ ExcelToJson.php
@@ -89,6 +89,9 @@ class Pman_Core_ExcelToJson extends Pman_Roo
         }
         fclose($fh);
         
+        $ret = array('extra' => $extra, 'data' => $rows);
+        echo 'about to return?';print_R($ret);exit;
+        
         return array('extra' => $extra, 'data' => $rows);;
     }
 }
