9530b443bf3458c43e12689a95f473a6afb86ea6
Alan Knowles <alan@roojs.com>
2013-04-03 19:00:42 +0800

    Tests/I18n.php

---
+++ Tests/I18n.php
@@ -0,0 +1,23 @@
+<?php
+
+class Pman_Core_Tests_I18n extends Pman
+{
+    function getAuth()
+    {
+        if (!$this->cli) {
+            die("not cli?");
+        }
+        
+    }
+    function get()
+    {
+        require_once 'Pman/Core/I18n.php';
+         $i = new Pman_Core_I18n();
+         $ret = $i->convertCurrency(100,"HKD","USD");
+        var_dump($ret); 
+        
+        
+    }
+    
+    
+}
