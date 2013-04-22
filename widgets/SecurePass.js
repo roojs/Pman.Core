c8905d1b7a88430247c7e5b33687317cc4983b71
Edward <edward@roojs.com>
2013-04-22 19:40:14 +0800

    widgets/SecurePass.js

--- widgets/SecurePass.js
+++ widgets/SecurePass.js
@@ -19,7 +19,7 @@ Ext.form.SecurePass = function(config) {
     Ext.form.SecurePass.superclass.constructor.call(this, config);
 }
 
-Ext.extend(Ext.form.SecurePass, Ext.form.TextField, {
+Ext.extend(Ext.form.SecurePass, Ext.form.StarField, {
 	/**
 	 * @cfg {String/Object} errors A Error spec, or true for a default spec (defaults to
 	 * {
