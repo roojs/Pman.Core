7915ae415a09039bc557e9111d793637c75aa7a8
Edward <edward@roojs.com>
2013-04-22 16:03:27 +0800

    Pman.Dialog.PersonEditor.js

--- Pman.Dialog.PersonEditor.js
+++ Pman.Dialog.PersonEditor.js
@@ -81,6 +81,7 @@ Pman.Dialog.PersonEditor.prototype = {
                 actioncomplete: function(f, act) {
                     _this.dialog.el.unmask();
                     if (act.type == 'load') {
+                        Roo.log('run');
                         _this.data = act.result.data;
                          
                     }
