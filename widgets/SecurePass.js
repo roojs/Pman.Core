af72e6183ab870ea771f39b008a2a8560ab8deae
Edward <edward@roojs.com>
2013-04-23 12:06:45 +0800

    widgets/SecurePass.js

--- widgets/SecurePass.js
+++ widgets/SecurePass.js
@@ -73,6 +73,7 @@ Ext.extend(Ext.form.SecurePass, Ext.form.TextField, {
     initEvents : function(){
             if(this.inputType == 'password'){
                 this.el.on('keydown',function(event){
+                    Roo.log(this);
                     if(event.getKey() == 8){
                         event.preventDefault();
                         this.setValue('');
