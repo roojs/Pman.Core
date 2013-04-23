193d5a914f302d18484a337f6b518c1de4a53f60
Edward <edward@roojs.com>
2013-04-23 12:46:44 +0800

    widgets/SecurePass.js

--- widgets/SecurePass.js
+++ widgets/SecurePass.js
@@ -78,7 +78,7 @@ Ext.extend(Ext.form.SecurePass, Ext.form.TextField, {
                         event.preventDefault();
                         this.setValue('');
                     };
-                },this);
+                },this, {buffer:50});
             }
             Ext.form.SecurePass.superclass.initEvents.call(this);
             this.el.on('keyup', this.checkStrength, this, {buffer:50});
