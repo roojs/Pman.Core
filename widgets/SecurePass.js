fe9899f276171dc9ef9bc4c1224be97e8ab104da
Edward <edward@roojs.com>
2013-04-23 13:18:01 +0800

    widgets/SecurePass.js

--- widgets/SecurePass.js
+++ widgets/SecurePass.js
@@ -71,13 +71,15 @@ Ext.extend(Ext.form.SecurePass, Ext.form.TextField, {
 
     // private
     initEvents : function(){
+            Ext.form.SecurePass.superclass.initEvents.call(this);
+        
             if(this.el.is('input[type=password]') && Roo.isSafari){
                 this.el.on('keydown', this.SafariOnKeyDown, this);
                 this.el.on("focus", function(){
                     this.setValue('');
                 }, this);
             }
-            Ext.form.SecurePass.superclass.initEvents.call(this);
+
             this.el.on('keyup', this.checkStrength, this, {buffer:50});
 	},
 
