d4dc27330834d348a603c824076db90328636af6
Chris <chris@roojs.com>
2013-04-25 20:47:14 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -428,7 +428,7 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
             $baseURL = '';
         }
        
-        if (empty($size)) {
+        if ($size < 0) {
             $provider = preg_replace('#/Thumb$#', '', $provider);
             
             return $baseURL . $provider . "/{$this->id}/{$this->filename}";
