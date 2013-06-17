86c64b15917d05c9591ae2de3d69304bd5b15545
Chris <chris@roojs.com>
2013-06-17 17:05:09 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -440,11 +440,11 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
         //$size = max(100, (int) $size);
         //$size = min(1024, (int) $size);
         // the size should 200x150 to convert
-        $size = preg_split('/(x|c)/', $size);
-        if(empty($size[1])){
-            $size[1] = 0;
+        $sizear = preg_split('/(x|c)/', $size);
+        if(empty($sizear[1])){
+            $sizear[1] = 0;
         }
-        $size = implode('x', $size);
+        $size = implode(strpos($size,'c') > -1 ? 'c' : 'x', $sizear);
         $fc = $this->toFileConvert();
         $fc->convert($this->mimetype, $size);
         
