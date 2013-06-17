8308a8a189849328d19e9514a7d1013525e2d9b9
Alan Knowles <alan@akbkhome.com>
2013-06-18 07:19:09 +0800

    Notify.php

--- Notify.php
+++ Notify.php
@@ -252,7 +252,7 @@ class Pman_Core_Notify extends Pman
         
         foreach($requeue as $p) {
             $pp = clone($p);
-            $p->event_when = strtotime($p->event_when .  ' + 1 MINUTE');
+            $p->act_when = strtotime($p->act_when .  ' + 1 MINUTE');
             $p->update($pp);
             
         }
