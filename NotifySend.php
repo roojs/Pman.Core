bebf086939e8dd54c444768c51f5bb680a7ceb3e
Edward <edward@roojs.com>
2013-06-26 12:30:29 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -99,7 +99,7 @@ class Pman_Core_NotifySend extends Pman
         }
         
         $sent = preg_match('/^0000/', $w->sent) ? false : true;
-        print_r($sent);exit;
+        
         if (!$force && (!empty($w->msgid) || $sent)) {
             $ww = clone($w);
             if (!$sent) { 
