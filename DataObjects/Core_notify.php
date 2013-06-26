7d3b56d07ff0023a4e05df08fcacdc55031bb33b
Edward <edward@roojs.com>
2013-06-26 13:12:15 +0800

    DataObjects/Core_notify.php

--- DataObjects/Core_notify.php
+++ DataObjects/Core_notify.php
@@ -68,9 +68,7 @@ class Pman_Core_DataObjects_Core_notify extends DB_DataObject
         if ($this->onid == 0) {
             return $c; // empty dataobject.
         }
-        $test = DB_DataObject::factory('crm_mailing_list_queue');
-        $test->get(1);
-        print_r($test);exit;
+        
         $c->autoJoin();
         
         if ($c->get($this->onid)) {
