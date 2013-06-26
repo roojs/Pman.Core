6fbd456c6aa2f74d3e9415d5dd0ec520ee539efe
Edward <edward@roojs.com>
2013-06-26 19:13:57 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -64,9 +64,10 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
             }
             
             $path = $tmpdir . '/' . basename($q['_remote_upload']);
-            
+            $fetch  = false;
             if(!file_exists($path)){
-                // use HTTP_Request...
+                $fetch = true;
+                // use HTTP_Request
                file_put_contents($path, file_get_contents($q['_remote_upload'])); 
             }
             
