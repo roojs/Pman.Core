355712b7c4082fb8c4b913552fd1ebe9a72e8f36
Edward <edward@roojs.com>
2013-06-26 19:18:40 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -69,7 +69,7 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
                 // use HTTP_Request
                file_put_contents($path, file_get_contents($q['_remote_upload'])); 
             }
-            
+            exit;
             $imageInfo = getimagesize($path);
             require_once 'File/MimeType.php';
             $y = new File_MimeType();
