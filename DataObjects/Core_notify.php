58eba46bf3e5a5e7bf9725a10fe3f2117798213a
Alan Knowles <alan@roojs.com>
2013-07-10 19:54:19 +0800

    DataObjects/Core_notify.php

--- DataObjects/Core_notify.php
+++ DataObjects/Core_notify.php
@@ -149,7 +149,8 @@ class Pman_Core_DataObjects_Core_notify extends DB_DataObject
                     $this->whereAdd("msg_id  != ''");
                     break;
                 case 'FAILED';
-                    $this->whereAdd('event_id > 0 AND act_when <= NOW() ');
+                    
+                    $this->whereAdd('msg_id  = '' AND event_id > 0  ');
                     
                     break;
                 case 'PENDING';
