f16112bf0a9379949c601804fb4b7ebf489ff89b
Chris <chris@roojs.com>
2013-08-20 14:26:46 +0800

    Images.php

--- Images.php
+++ Images.php
@@ -243,7 +243,7 @@ class Pman_Core_Images extends Pman
                 urlencode("Original file was missing : " . $img->getStoreName()));
     
         }
-        
+        print_r($img);exit;
         $x = $img->toFileConvert();
         if (empty($this->as_mimetype)) {
             $this->as_mimetype  = $img->mimetype;
