12db14646a59981d57ef5f3c5eb17be8ca5344a0
Edward <edward@roojs.com>
2013-09-16 12:10:57 +0800

    Roo.form.HtmlEditor.ToolbarContext.stores.js

---
+++ Roo.form.HtmlEditor.ToolbarContext.stores.js
@@ -0,0 +1,41 @@
+Roo.form.HtmlEditor.ToolbarContext.stores = {
+   
+   'font-family' :  {
+      
+            xtype : 'Store',
+             // load using HTTP
+            proxy: {
+               xtype : 'HttpProxy',
+               url: baseURL + '/Roo/Core_enum',
+               method: 'GET'
+            },
+            reader : {
+                xtype: 'JsonReader',
+                xns: Roo.data,
+                id : 'id',
+                root : 'data',
+                totalProperty : 'total',
+                
+                fields : [
+                    { name:'id','type':'int'},
+                    { name : 'val' , mapping: 'name'} ,
+                    { name:'display', mapping: 'display_name'}
+                ]
+            },
+           listeners : {
+               beforeload : function(st,o)
+               {
+                   // compnay myst be set..
+                    o.params.etype = 'HtmlEditor.font-family';
+                    o.params.active = 1;
+                    //o.params._requestMeta=0; // do not fetch meta..
+                    this.reader.metaFromRemote = 1;
+               },
+               loadexception : Pman.loadException
+           
+           },
+           sortInfo: {
+               field: 'display', direction: 'ASC'
+           }
+    } 
+};
