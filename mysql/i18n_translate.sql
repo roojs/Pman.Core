5a7c4015d9a3728279245aa5c5cf1771d05c45df
Alan Knowles <alan@roojs.com>
2013-10-11 17:29:20 +0800

    mysql/i18n_translate.sql

---
+++ mysql/i18n_translate.sql
@@ -0,0 +1,14 @@
+
+DROP FUNCTION IF EXISTS i18n_translate;
+DELIMITER $$
+CREATE FUNCTION i18n_translate(in_ltype  varchar(1) , in_lkey varchar(8), in_inlang varchar(8)) 
+        RETURNS VARCHAR(64) DETERMINISTIC
+    BEGIN
+        DECLARE ret  VARCHAR(64);
+        SET ret  = '';
+        SELECT lval INTO ret FROM i18n
+            WHERE ltype=in_ltype AND lkey=in_lkey and inlang=in_inlang LIMIT 1;
+        RETURN ret;
+        
+    END $$
+DELIMITER ;
