4ae9089cef58565803ece8a6aa265740a392b4e5
Alan Knowles <alan@roojs.com>
2013-10-11 17:25:44 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -282,35 +282,6 @@ ALTER TABLE  ProjectDirectory ADD COLUMN   role varchar(16) NOT NULL DEFAULT '';
 ALTER TABLE ProjectDirectory ADD INDEX plookup (project_id,person_id, ispm, role);
 
 
-CREATE TABLE   Images (
-  id int(11) NOT NULL auto_increment,
-  PRIMARY KEY  (id)
-);
-
-ALTER TABLE Images    ADD COLUMN   filename varchar(255) NOT NULL default '';
-ALTER TABLE Images    ADD COLUMN   ontable varchar(32) NOT NULL default '';
-ALTER TABLE Images    ADD COLUMN   onid int(11) NOT NULL default '0';
-ALTER TABLE Images    ADD COLUMN   mimetype varchar(64) NOT NULL default '';
-ALTER TABLE Images    ADD COLUMN   width int(11) NOT NULL default '0';
-ALTER TABLE Images    ADD COLUMN   height int(11) NOT NULL default '0';
-ALTER TABLE Images    ADD COLUMN   filesize int(11) NOT NULL default '0';
-ALTER TABLE Images    ADD COLUMN   displayorder int(11) NOT NULL default '0';
-ALTER TABLE Images    ADD COLUMN   language varchar(6) NOT NULL default 'en';
-ALTER TABLE Images    ADD COLUMN   parent_image_id int(11) NOT NULL default '0';
-
-ALTER TABLE  Images ADD COLUMN created datetime ;
-ALTER TABLE  Images ADD COLUMN imgtype VARCHAR(32) DEFAULT '' NOT NULL;
-ALTER TABLE  Images ADD COLUMN linkurl VARCHAR(254) DEFAULT '' NOT NULL;
-ALTER TABLE  Images ADD COLUMN descript TEXT DEFAULT '' NOT NULL;
-ALTER TABLE  Images ADD COLUMN title VARCHAR(128) DEFAULT '' NOT NULL;
-
-ALTER TABLE Images    CHANGE COLUMN   mimetype mimetype  varchar(128) NOT NULL default '';
--- postgres
-ALTER TABLE Images    ALTER COLUMN   mimetype  TYPE  varchar(128) ;
-
-ALTER TABLE Images ADD INDEX lookup(ontable, onid);
-
-ALTER TABLE Images ADD INDEX lookupc(created, ontable, onid);
  
 --// old core image type - merged into enum.
 DROP TABLE core_image_type;
