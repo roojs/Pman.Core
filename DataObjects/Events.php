621e9bfcf0fea05af48a81349dc13cfc2484c832
Edward <edward@roojs.com>
2013-10-16 17:39:05 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -278,7 +278,6 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
         } 
           
         $this->person_name = $au && !empty($au->name) ? $au->name : '';
-        print_r($au);exit;
         $this->person_id = $au ? $au->id : -1;
         $this->person_table = $au ? $au->tableName() : '';
         $this->ipaddr = isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : 'cli';
