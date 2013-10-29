7bf54c82f1893f3500a28bf25108afadbf1d9235
Edward <edward@roojs.com>
2013-10-29 17:52:46 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -351,7 +351,7 @@ class Pman_Core_UpdateDatabase extends Pman
             
             // INDEX lookup
             if ($tbl && preg_match('#INDEX lookup+([\w|\W]+)#i',  $l, $m)) {
-               $extra[] = "CREATE INDEX lookup_idx ON {$tbl} USING btree {$m[1]};";
+               $extra[] = "CREATE INDEX lookup_idx ON '{$tbl}' USING btree {$m[1]};";
                $last = array_pop($ret);
                $ret[] = trim($last, ",");
                continue;
