1f05d115261d6468be767c0660af3c3cb678064f
Edward <edward@roojs.com>
2013-10-29 15:54:45 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -330,7 +330,7 @@ class Pman_Core_UpdateDatabase extends Pman
                 
             }else{
                 if (preg_match('#enum\([\w|\W]+#i',  $l, $m)) {
-                    $name = substr($l, 0, (strlen($l) - strlen($m[0])));
+                    $name = trim(substr($l, 0, (strlen($l) - strlen($m[0]))));
                     print_r($name);
                 }
             }
