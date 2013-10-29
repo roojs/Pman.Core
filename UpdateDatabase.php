e277eeea7d410083e69516b802901ee968bd3900
Edward <edward@roojs.com>
2013-10-29 14:55:56 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -321,6 +321,8 @@ class Pman_Core_UpdateDatabase extends Pman
             
             if (preg_match('#enum\(#i',  $l, $m)) {
                 print_r($m);exit;
+                $extra = "CREATE TYPE {$tbl}_enum AS ENUM ('sad', 'ok', 'happy')";
+                
                // $extra[]  =   "drop table {$tbl};";
              }
             //if () // find enum , then 
