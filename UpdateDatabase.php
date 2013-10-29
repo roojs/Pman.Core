4c0a876d6780ae0b83ec9849107bd63301e9734f
Edward <edward@roojs.com>
2013-10-29 15:27:48 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -320,7 +320,10 @@ class Pman_Core_UpdateDatabase extends Pman
             }
             
             if (preg_match('#alter\s+table\s+#i',  $l, $m)) {
-                print_r($l."\n");
+                if (preg_match('#enum\(#i',  $l, $m)) {
+                    print_r($l."\n");
+                }
+                
                 
             }
 //            if (preg_match('#enum\(#i',  $l, $m)) {
