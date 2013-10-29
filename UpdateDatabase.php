aca8dfed6074bd8e43f3cf39b7d2f3f1b6484aff
Edward <edward@roojs.com>
2013-10-29 18:32:39 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -390,7 +390,6 @@ class Pman_Core_UpdateDatabase extends Pman
             
         }
         $ret = array_merge($extra,$ret);
-        $ret = array_merge($ret, $unique);
 //        echo implode("\n", $ret); exit;
         
         file_put_contents($fn, implode("\n", $ret));
