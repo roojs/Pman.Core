bb8e511a5d1408085920b4fb4e4efd79e1bfd605
Edward <edward@roojs.com>
2013-10-30 13:55:17 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -312,7 +312,6 @@ class Pman_Core_UpdateDatabase extends Pman
                 $tbl = $m[1];
              }
             if (preg_match('#create\s+table\s+\`([a-z0-9_]+)\`#i',  $l, $m)) {
-                print_r($m);exit;
                 $tbl = 'shop_' . strtolower($m[1]);
                 $l = preg_replace('#create\s+table\s+\`{0,1}([a-z0-9_]+)\`{0,1}#i', "CREATE TABLE {$tbl}", $l);
              }
