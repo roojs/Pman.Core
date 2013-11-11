bb2c063a3cc28ceef8db52af3b65924529dec21b
Alan Knowles <alan@roojs.com>
2013-11-11 17:07:10 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -237,7 +237,7 @@ class Pman_Core_UpdateDatabase extends Pman
             // look in pgsql.ini
             if (!empty($this->cli_options['init'])) {
                 $this->importpgsqldir($dburl, $this->rootDir. "/Pman/$m/pgsql.init");
-                $this->importpgsqldir($dburl, $this->rootDir. "/Pman/$m/pgsql.initdata", );
+                $this->importpgsqldir($dburl, $this->rootDir. "/Pman/$m/pgsql.initdata");
             
                 
             }
