ad50b96fb3980c6b05a49b51ce9d02ec4aaed32e
Chris <chris@roojs.com>
2013-11-12 16:28:47 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -202,6 +202,7 @@ class Pman_Core_Mailer {
             $parts[1] = $mime->headers($parts[1]);
         
         }
+        print_r('parts');
         print_r($parts);exit;
         if($this->attachments){
             //if got a attachments
