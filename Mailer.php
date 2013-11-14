2993eb4abb1dbd39e2ba866aa421e73b477ff899
Chris <chris@roojs.com>
2013-11-14 19:08:02 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -243,7 +243,7 @@ class Pman_Core_Mailer {
         }
         echo '<PRE>';
         print_r('parts');
-//        print_r($parts);
+        print_r($parts[2]);
         exit;
        // list($recipents,$headers,$body) = $parts;
         return array(
