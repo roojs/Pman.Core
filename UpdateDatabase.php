974cdda08aebc4e49d7a490eb64f9c97e391d149
Alan Knowles <alan@roojs.com>
2013-11-15 17:16:32 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -256,6 +256,7 @@ class Pman_Core_UpdateDatabase extends Pman
         
         $ar = $this->modulesList();
        
+        print_R($ar);
         foreach($ar as $m) {
              echo "Importing SQL from module $m\n";
             if (!empty($this->opts['only-module-sql']) && $m != $this->opts['only-module-sql']) {
