28a8a470572b0ab5f90dcde51dc84ea23ab7bb6c
Edward <edward@roojs.com>
2013-11-19 21:00:35 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -178,6 +178,7 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
     function getStoreName() 
     {
         $opts = HTML_FlexyFramework::get()->Pman;
+        print_r($this->filename);exit;
         $fn = preg_replace('/[^a-z0-9\.]+/i', '_', $this->filename);
         return implode( '/', array(
             $opts['storedir'], '_images_', date('Y/m', strtotime($this->created)), $this->id . '-'. $fn
