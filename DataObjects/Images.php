af919b7a0d62ed32aa78ed7630c60568b449fdae
Edward <edward@roojs.com>
2013-11-19 21:17:27 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -601,7 +601,7 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
         $roo->loadMap($r);
         $r->limit(1);
         $r->find(true);
-        exit;
+        
         $roo->jok($r->toArray());
          
     }
