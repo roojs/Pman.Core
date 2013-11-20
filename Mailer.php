8b6c76fe0cb7591317090023f91d4c697feedbee
Chris <chris@roojs.com>
2013-11-20 18:47:13 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -233,7 +233,7 @@ class Pman_Core_Mailer {
                 $mime->addAttachment(
                         $attch['file'],
                         $attch['mimetype'],
-                        (empty($attch['name'])) ? $attch['name'] : '',
+                        (!empty($attch['name'])) ? $attch['name'] : '',
                         true
                 );
             }
