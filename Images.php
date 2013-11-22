c8b80fcb616569293a62cbdbaf55401c9c8216b2
Edward <edward@roojs.com>
2013-11-22 12:22:11 +0800

    Images.php

--- Images.php
+++ Images.php
@@ -55,10 +55,10 @@ class Pman_Core_Images extends Pman
     
     function get($s) // determin what to serve!!!!
     {
-        print_r($s);exit;
         $this->as_mimetype = empty($_REQUEST['as']) ? '' : $_REQUEST['as'];
         
         $bits= explode('/', $s);
+        print_r($bits);exit;
         $id = 0;
 //        var_dump($bits);die('in');
         // without id as first part...
