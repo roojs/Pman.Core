7a0655e7ae8f96dda55d37fc63e6d13adb7d98bf
Alan Knowles <alan@roojs.com>
2013-12-09 15:13:16 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -320,8 +320,8 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
             $_SESSION[__CLASS__][$sesPrefix .'-auth'] = serialize($u);
             return true; 
         }
-        var_dump(session_id());
-        var_dump($_SESSION[__CLASS__]);
+        //var_dump(session_id());
+        //var_dump($_SESSION[__CLASS__]);
         
         if (!empty(   $_SESSION[__CLASS__][$sesPrefix .'-empty'] )) {
             return false;
