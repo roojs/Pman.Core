dcd23fa17727bb0d9e509c84f3d15cbd94fa5036
Edward <edward@roojs.com>
2014-01-02 15:23:53 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -478,9 +478,10 @@ class Pman_Core_NotifySend extends Pman
         //var_dump($m);
         
         if (!empty($notify->evtype) && method_exists($object,$m)) {
+            print_r('aa');
             return $object->$m($rcpt, $last_sent_date, $notify, $force);
         }
-                
+            print_r('bb');    
         if (!method_exists($object, 'toEmail')) {
             //var_Dump($object);
             //exit;
