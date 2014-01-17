038791ddc89877d697b1b60086b09a4631c33fe8
Edward <edward@roojs.com>
2014-01-17 17:34:15 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -245,7 +245,7 @@ class Pman_Core_SimpleExcel extends Pman
         }
         
         // merge cell
-        print_r($this->_merged_ranges);exit;
+        print_r($worksheet->_merged_ranges);exit;
         if(!empty($cfg['merged_ranges'])){
             $this->_merged_ranges = $cfg['merged_ranges'];
         }
