26978050812898198d646bf5897e31a167a37480
Edward <edward@roojs.com>
2014-02-19 19:02:07 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -467,7 +467,7 @@ CREATE  TABLE core_mailing_list_message (
   plaintext TEXT NULL ,
   name VARCHAR(255) NOT NULL DEFAULT '',
   updated_dt DATETIME NOT NULL ,
-  from_email VARCHAR(254) NULL DEFAULT ''
+  from_email VARCHAR(254) NULL DEFAULT '',
   from_name VARCHAR(254) NULL DEFAULT '',
   owner_id INT(11) NOT NULL DEFAULT 0,
   is_system INT(2) NOT NULL DEFAULT 1,
