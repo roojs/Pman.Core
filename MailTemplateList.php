d5930fe14fd290bbcfb0dd07607ae8d5d43f7469
Edward <edward@roojs.com>
2014-02-19 14:42:31 +0800

    MailTemplateList.php

--- MailTemplateList.php
+++ MailTemplateList.php
@@ -16,8 +16,8 @@ class Pman_Core_MailTemplateList extends Pman
 
     function get()
     {
-        $fopts = HTML_FlexyFramework::get()->page;
-        print_r($fopts);
+        $fopts = HTML_FlexyFramework::get()->HTML_Template_Flexy;
+        print_r(array_unique($fopts->templateDir));
         exit;
     }
      
