def3cb56ad1d697488102f4b07050f67317c117a
Edward <edward@roojs.com>
2014-02-19 19:11:46 +0800

    ConvertStyle.php

--- ConvertStyle.php
+++ ConvertStyle.php
@@ -18,9 +18,7 @@ class Pman_Core_ConvertStyle extends Pman
     }
     
     function relPath($base, $url)
-    {   
-        print_r($base);
-        print_r($url);exit;
+    {  
         if (preg_match('/^(http|https|mailto):/',$url)) {
             return $url;
         }
