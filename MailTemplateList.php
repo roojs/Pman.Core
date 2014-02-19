f85821a4fd0b33333a4760b55d3d40aef9d7aa58
Edward <edward@roojs.com>
2014-02-19 14:29:58 +0800

    MailTemplateList.php

---
+++ MailTemplateList.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MailTemplateList
+ *
+ * @author edward
+ */
+class MailTemplateList {
+    //put your code here
+}
+
+?>
