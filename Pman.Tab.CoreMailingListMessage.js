56e11b0130a82532e169944080d821ea86138d0e
Edward <edward@roojs.com>
2014-02-19 12:32:10 +0800

    Pman.Tab.CoreMailingListMessage.bjs Pman.Tab.CoreMailingListMessage.js

--- Pman.Tab.CoreMailingListMessage.js
+++ Pman.Tab.CoreMailingListMessage.js
@@ -361,7 +361,7 @@ Pman.Tab.CoreMailingListMessage = new Roo.XComponent({
                                     xtype: 'HttpProxy',
                                     xns: Roo.data,
                                     method : 'GET',
-                                    url : baseURL+'/Roo/crm_mailing_list_message.php'
+                                    url : baseURL+'/Roo/Core_mailing_list_message.php'
                                 },
                                 reader : {
                                     xtype: 'JsonReader',
