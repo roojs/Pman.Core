b9239ee3b51d2cd5f1d842d5d419455cec862956
Edward <edward@roojs.com>
2014-02-19 16:45:32 +0800

    ConvertStyle.php

--- ConvertStyle.php
+++ ConvertStyle.php
@@ -64,23 +64,23 @@ class Pman_Core_ConvertStyle extends Pman
     
     function convertStyle($url, $file)
     {
-        if(!empty($url))
-        {
-            $host = parse_url($url);
-            require_once 'System.php';
-            $wget = System::which('wget');
-            if (!$wget) {
-                $this->jerr("no wget");
-            }
-            $cmd =  $wget . ' -q -O -  ' . escapeshellarg($url);
-            
-            //echo $cmd; exit;
-            $data = `$cmd`;
-            
-            if (!trim(strlen($data))) {
-                $this->jerr("url returned an empty string");
-            }
-        }
+//        if(!empty($url))
+//        {
+//            $host = parse_url($url);
+//            require_once 'System.php';
+//            $wget = System::which('wget');
+//            if (!$wget) {
+//                $this->jerr("no wget");
+//            }
+//            $cmd =  $wget . ' -q -O -  ' . escapeshellarg($url);
+//            
+//            //echo $cmd; exit;
+//            $data = `$cmd`;
+//            
+//            if (!trim(strlen($data))) {
+//                $this->jerr("url returned an empty string");
+//            }
+//        }
         
         if(file_exists($file))
         {
