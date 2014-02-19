32383ca1765a2856a4799e98d2f67f9477e9b337
Edward <edward@roojs.com>
2014-02-19 17:31:44 +0800

    ConvertStyle.php

--- ConvertStyle.php
+++ ConvertStyle.php
@@ -135,7 +135,7 @@ class Pman_Core_ConvertStyle extends Pman
         
         foreach ($xpath->query('//style') as $s){
             $style = $doc->saveHTML($s);
-            
+            print_r($style);
 //            $this->styleSheets[] = $this->$this->replaceImageUrl($s)
             echo '<br/>';
         }
