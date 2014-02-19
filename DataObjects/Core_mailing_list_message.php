29b9d2f1c2a19de1edd7de17cf34b27ff097373b
Edward <edward@roojs.com>
2014-02-19 12:33:21 +0800

    DataObjects/Core_mailing_list_message.php

---
+++ DataObjects/Core_mailing_list_message.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Core_mailing_list_message
+ *
+ * @author edward
+ */
+class Core_mailing_list_message {
+    //put your code here
+}
+
+?>
