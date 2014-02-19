076c73372767f9614abe43b14dba18fa219ecb39
Edward <edward@roojs.com>
2014-02-19 12:47:47 +0800

    Pman.Dialog.CoreMailingListMessage.bjs Pman.Dialog.CoreMailingListMessage.js

--- Pman.Dialog.CoreMailingListMessage.js
+++ Pman.Dialog.CoreMailingListMessage.js
@@ -1,1 +1,35 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Pman.Dialog.CoreMailingListMessage = {
+
+    dialog : false,
+    callback:  false,
+
+    show : function(data, cb)
+    {
+        if (!this.dialog) {
+            this.create();
+        }
+
+        this.callback = cb;
+        this.data = data;
+        this.dialog.show(this.data._el);
+        if (this.form) {
+           this.form.reset();
+           this.form.setValues(data);
+           this.form.fireEvent('actioncomplete', this.form,  { type: 'setdata', data: data });
+        }
+
+    },
+
+    create : function()
+    {
+        var _this = this;
+        this.dialog = Roo.factory({
+            xtype: 'LayoutDialog',
+            xns: Roo
+        });
+    }
+};
