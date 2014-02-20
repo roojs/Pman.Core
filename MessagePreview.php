cd394059c9e7d77590114a7918a1cc0d8c20e447
Edward <edward@roojs.com>
2014-02-20 14:06:30 +0800

    MessagePreview.php

--- MessagePreview.php
+++ MessagePreview.php
@@ -39,6 +39,36 @@ class Pman_Core_MessagePreview extends Pman
         if(empty($_REQUEST['_id'])){
             $this->jerr('id got error');
         }
+        if(empty($_REQUEST['_action'])){
+            $this->jerr('preview type not set');
+        }
+        $mid = $_REQUEST['_id'];
+        
+        $cfg = HTML_FlexyFramework::get()->Pman_Crm;
+        
+       
+       
+        $q = DB_DataObject::factory('crm_mailing_list_queue');
+        $q->id = 'test-message-'. $mid;
+        $q->message_id = $mid;
+        $q->message_id_subject = $q->message()->subject;
+        $q->message_id_from_email = $cfg['from_email'];
+        $q->message_id_from_name = "Email Test"; 
+        
+        $q->cachedMailWithOutImages(true);
+        $r = $q->getMailerObject($this->authUser, false, false, true);
+        //print_r($r->toData());
+        $ret = $r->toData();
+        $images = file_get_contents(session_save_path() . '/email-cache-' . getenv('APACHE_RUN_USER') . '/mail/' . $q->id . '-images.txt');
+       // var_dump($images);exit;
+        
+        $ret['body'] = str_replace('%Images%', $images, $ret['body']);
+        
+        $sent = $r->send($ret);
+        if(!is_object($sent)){
+            $this->jok('SUCCESS');
+        }
+        $this->jerr('error!!:' . $sent->toString());
         
     }
     
