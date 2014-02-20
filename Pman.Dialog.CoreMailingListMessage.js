3c27cc9a2c590546024357b1c24e5773e4ca2908
Edward <edward@roojs.com>
2014-02-20 13:07:29 +0800

    Pman.Dialog.CoreMailingListMessage.bjs Pman.Dialog.CoreMailingListMessage.js

--- Pman.Dialog.CoreMailingListMessage.js
+++ Pman.Dialog.CoreMailingListMessage.js
@@ -950,7 +950,9 @@ Pman.Dialog.CoreMailingListMessage = {
                                                 return;
                                             }
                                             
-                                            Pman.Dialog.Image.show( { id : 0, onid: _this.data.id, ontable: 'core_mailing_list_message' }, function() {
+                                            var ontable = (_this.data.module) ? _this.data.module + '_mailing_list_message' : 'crm_mailing_list_message';
+                                            
+                                            Pman.Dialog.Image.show( { id : 0, onid: _this.data.id, ontable: ontable }, function() {
                                                 _this.igrid.getDataSource().load({});
                                             }); 
                                         }
