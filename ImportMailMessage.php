468e882a1a00b967b78bf39b1429348f8976bc5b
Edward <edward@roojs.com>
2014-02-20 12:48:11 +0800

    ImportMailMessage.php

---
+++ ImportMailMessage.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ImportMailMessage
+ *
+ * @author edward
+ */
+class ImportMailMessage {
+    //put your code here
+}
+
+?>
