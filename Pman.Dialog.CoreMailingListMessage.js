69fd69cf3f3cf8df99b4ab61e5b9e75b3ee8a3a8
Edward <edward@roojs.com>
2014-02-20 13:44:42 +0800

    Pman.Dialog.CoreMailingListMessage.bjs Pman.Dialog.CoreMailingListMessage.js

--- Pman.Dialog.CoreMailingListMessage.js
+++ Pman.Dialog.CoreMailingListMessage.js
@@ -313,9 +313,9 @@ Pman.Dialog.CoreMailingListMessage = {
                                                         _this.dialog.el.mask("Loading");
                                                         this.load({ method: 'GET', params: { '_id' : _this.data.id }});
                                                         if(_this.data.module == 'crm'){
-                                                            _this.preview_btn.show();
                                                             _this.html_preview.show();
                                                         }
+                                                        _this.preview_btn.show();
                                                         
                                                     } else {
                                                         _this.form.setValues({
