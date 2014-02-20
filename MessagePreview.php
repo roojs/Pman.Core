e139dd3a839e3f6f0f0fa5791ebdb46c0a93da52
Edward <edward@roojs.com>
2014-02-20 13:32:25 +0800

    MessagePreview.php

---
+++ MessagePreview.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MessagePreview
+ *
+ * @author edward
+ */
+class MessagePreview {
+    //put your code here
+}
+
+?>
