91a5b82303b43f6f261ce8f12ab9ef1ef3b73012
Edward <edward@roojs.com>
2014-02-20 13:16:27 +0800

    Pman.Tab.CoreMailingListMessage.bjs Pman.Tab.CoreMailingListMessage.js

--- Pman.Tab.CoreMailingListMessage.js
+++ Pman.Tab.CoreMailingListMessage.js
@@ -56,7 +56,7 @@ Pman.Tab.CoreMailingListMessage = new Roo.XComponent({
                                 {
                                     if (!_this.dialog) return;
                                     var data = this.getDataSource().getAt(rowIndex).data;
-                                    _this.dialog.show( data , function() {
+                                    _this.dialog.show( {id : data.id, module : 'core'} , function() {
                                         _this.grid.footer.onClick('first');
                                     }); 
                                 },
