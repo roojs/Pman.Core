d66fa3cf6b91d59ca01e431433f5a75070c2596c
Edward <edward@roojs.com>
2014-02-21 12:19:51 +0800

    DataObjects/Core_email.php

---
+++ DataObjects/Core_email.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Core_email
+ *
+ * @author edward
+ */
+class Core_email {
+    //put your code here
+}
+
+?>
