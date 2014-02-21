712834420f987b6481f34d11ceac00285cf8cba4
Edward <edward@roojs.com>
2014-02-21 16:08:01 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -272,22 +272,7 @@ Content-Type: text/plain; charset=utf-8; format=flowed
 Content-Transfer-Encoding: 7bit
 
 {$this->plaintext}
-    
---alt-{$random_hash}
-Content-Type: multipart/related; boundary=rel-{$random_hash}
 
---rel-{$random_hash}
-Content-Type: text/html; charset=utf-8
-Content-Transfer-Encoding: 7bit
-
-{$this->bodytext}
-
-");  
-
-        fwrite($fh,"%Images%
---rel-{$random_hash}--
-
---alt-{$random_hash}--
 ");
         fclose($fh);
         
