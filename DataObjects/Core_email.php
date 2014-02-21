824c5cf0fb4b4ed04ffcdc54111d5cf131a625a6
Edward <edward@roojs.com>
2014-02-21 19:53:53 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -281,7 +281,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
         }
          
         require_once 'Pman/Core/Mailer.php';
-        
+        print_r($this->tableName() . '-' . $this->id);exit;
         $templateDir = session_save_path() . '/email-cache-' . $ui['name'] ;
         $r = new Pman_Core_Mailer(array(
             'template'=> $this->tableName() . '-' . $this->id,
