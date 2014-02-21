1134d5db30cf3e64d83c4664db348f75e85cf980
Edward <edward@roojs.com>
2014-02-21 15:59:40 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -199,8 +199,6 @@ class Pman_Core_Mailer {
             $mime->setTXTBody($parts[2]);
             $mime->setHTMLBody($htmlbody);
 //            var_dump($mime);exit;
-            echo '<PRE>';
-            print_r($this->images);exit;
             foreach($this->images as $cid=>$cdata) { 
             
                 $mime->addHTMLImage(
