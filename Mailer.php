4bc02b148a6ea954c8b4c267440f40526c272371
Edward <edward@roojs.com>
2014-02-21 18:50:07 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -378,7 +378,7 @@ class Pman_Core_Mailer {
         $cache = ini_get('session.save_path')."/Pman_Core_Mailer-{$user}/" . md5($url);
         if (file_exists($cache) and filemtime($cache) > strtotime('NOW - 1 WEEK')) {
             $ret =  json_decode($cache);
-            print_r($ret);exit;
+            print_r($cache);exit;
             $ret['file'] = $cache . '.data';
             return $ret;
         }
