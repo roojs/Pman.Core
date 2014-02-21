ef409da3c94221cbb45ea5ef52b4eae9172ad85f
Edward <edward@roojs.com>
2014-02-21 12:16:57 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -458,19 +458,3 @@ ALTER TABLE  core_notify_recur  ADD INDEX lookup(person_id, dtstart, dtend, tz, 
 -- old design..
 ALTER TABLE  core_notify_recur  CHANGE COLUMN tz  tz varchar(64)  NOT NULL;
 
-
-
-CREATE  TABLE core_mailing_list_message (
-  id INT(11) NOT NULL AUTO_INCREMENT ,
-  subject TEXT NULL ,
-  bodytext TEXT NULL ,
-  plaintext TEXT NULL ,
-  name VARCHAR(255) NOT NULL DEFAULT '',
-  updated_dt DATETIME NOT NULL ,
-  from_email VARCHAR(254) NULL DEFAULT '',
-  from_name VARCHAR(254) NULL DEFAULT '',
-  owner_id INT(11) NOT NULL DEFAULT 0,
-  is_system INT(2) NOT NULL DEFAULT 1,
-  PRIMARY KEY (id)
-);
-
