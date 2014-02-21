dbeadc157a1d35556483abc984d0e0935718cd8c
Edward <edward@roojs.com>
2014-02-21 19:09:30 +0800

    MessagePreview.php

--- MessagePreview.php
+++ MessagePreview.php
@@ -20,17 +20,43 @@ class Pman_Core_MessagePreview extends Pman
     
     function get()
     {
-        if(empty($_REQUEST['_id'])){
+        if(empty($_REQUEST['_id']) || empty($_REQUEST['_table'])){
             $this->jerr('Missing Options');
         }
         
-        $mlq = DB_DataObject::factory('core_email');
+        $mlq = DB_DataObject::factory($_REQUEST['_table']);
         
         $mlq->get($_REQUEST['_id']);
         
         $this->msg = $mlq;
 
         $this->showHtml = isset($_REQUEST['_as_html']) ? true : false;
+        
+    }
+    
+    function post()
+    {
+        if(empty($_REQUEST['_id']) || empty($_REQUEST['_table'])){
+            $this->jerr('Missing Options');
+        }
+        
+        $mid = $_REQUEST['_id'];
+        
+        $mlq = DB_DataObject::factory($_REQUEST['_table']);
+        
+        $mlq->get($_REQUEST['_id']);
+        
+        $content = array(
+            'template' => $mlq->name,
+            'person' => $this->authUser
+        );
+        
+        $sent = DB_DataObject::factory('crm_mailing_list_message')->send($content);
+        
+        if(!is_object($sent)){
+            $this->jok('SUCCESS');
+        }
+        $this->jerr('error!!:' . $sent->toString());
         
     }
 }
