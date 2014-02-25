46caa2406e1587d25e68916df1468123c8940161
Edward <edward@roojs.com>
2014-02-25 16:58:59 +0800

    DataObjects/I18n.php

--- DataObjects/I18n.php
+++ DataObjects/I18n.php
@@ -116,6 +116,7 @@ class Pman_Core_DataObjects_I18n extends DB_DataObject
         
         if (!empty($q['_filtered']) && !empty($this->ltype)) {
             $cfg = $this->cfg();
+            print_r($cfg);exit;
             $filter = $cfg[$this->ltype];
             $this->whereAddIn('lkey', $filter, 'string'); 
             
