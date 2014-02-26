fa1bb3e31ac29cf31fa10f126b7fde44bcd2864b
Edward <edward@roojs.com>
2014-02-26 12:09:39 +0800

    Pman.Download.js

--- Pman.Download.js
+++ Pman.Download.js
@@ -51,7 +51,7 @@ Pman.Download = function(cfg)
     
     if (this.newWindow && this.method == 'GET') {
             // as ie seems buggy...
-        window.open( cfg.url + '?' + Roo.urlEncode(cfg.params || {}), '_blank');
+        window.open( this.url + '?' + Roo.urlEncode(this.params || {}), '_blank');
         return ; 
         
     }
