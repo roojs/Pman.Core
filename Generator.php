e63d99e0fce056a6da8c3d8a544bf92946ca8091
Alan Knowles <alan@roojs.com>
2014-02-27 11:38:58 +0800

    Generator.php

--- Generator.php
+++ Generator.php
@@ -45,7 +45,8 @@ $this->def['forms'][$table][$t->name] = $form;
 class Pman_Core_Generator extends DB_DataObject_Generator
 {
     
-
+    static $cli_desc = "Generate the dataobjects";
+    
    
     // inherrited..
     // $tablekeys
