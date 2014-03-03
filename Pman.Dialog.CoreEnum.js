c1a5142646bc6184d93fb1f4d78f2c2a39177108
Edward <edward@roojs.com>
2014-03-03 17:09:17 +0800

    Pman.Dialog.CoreEnum.bjs Pman.Dialog.CoreEnum.js

--- Pman.Dialog.CoreEnum.js
+++ Pman.Dialog.CoreEnum.js
@@ -60,7 +60,7 @@ Pman.Dialog.CoreEnum = {
                                 {
                                   if (action.type == 'setdata') {
                                         Roo.log(_this.data);
-                                        if(typeof(_this.data.etype == 'undefined') || !_this.data.etype.length){
+                                        if((typeof(_this.data.etype) == 'undefined') || !_this.data.etype.length){
                                             Roo.MessageBox.alert('Error', 'Missing etype');
                                             _this.dialog.hide();
                                             return;
