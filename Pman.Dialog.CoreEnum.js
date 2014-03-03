0fe590b7589d051fb2b8da4a414e177d073f1130
Edward <edward@roojs.com>
2014-03-03 17:09:41 +0800

    Pman.Dialog.CoreEnum.bjs Pman.Dialog.CoreEnum.js

--- Pman.Dialog.CoreEnum.js
+++ Pman.Dialog.CoreEnum.js
@@ -39,7 +39,7 @@ Pman.Dialog.CoreEnum = {
             background : true,
             closable : false,
             collapsible : false,
-            height : 100,
+            height : 300,
             modal : true,
             resizable : false,
             title : "Add / Edit Core Enum",
