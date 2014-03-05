e622e986f1373ee3cb39370e123ad3c42055aa1f
Alan Knowles <alan@roojs.com>
2014-03-05 16:56:48 +0800

    DataObjects/Core_watch.php

--- DataObjects/Core_watch.php
+++ DataObjects/Core_watch.php
@@ -58,8 +58,8 @@ class Pman_Core_DataObjects_Core_watch extends DB_DataObject
         if (!empty($q['_split_event_name'])) {
             $this->selectAdd("
                 
-                substr( event, LOCATE(event, '.')) as event_left,
-                substr( event, 0, LOCATE(event, '.')) as event_right        
+                substr( event, 1, LOCATE(event, '.')) as event_left,
+                substr( event,   LOCATE(event, '.')) as event_right        
                              
             ");
             
