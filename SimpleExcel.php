48ae7d415c63fd8609c074aad2c2c72bd2301d4f
Edward <edward@roojs.com>
2014-03-12 18:18:34 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -315,7 +315,7 @@ class Pman_Core_SimpleExcel extends Pman
                 continue;
             }
             
-            $v = '=$speaker.$A$2:$A$15';
+            $v = '=speaker!$A$2:$A$15';
             
             $v = @iconv('UTF-8', 'UTF-8//IGNORE', $v);
             
