b9c830d71b324c95f723c33f353342401badc622
Edward <edward@roojs.com>
2014-03-13 19:10:48 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -333,7 +333,7 @@ class Pman_Core_SimpleExcel extends Pman
                     $validator->_fixedList = 1;
                     $validator->_type = 3;
                     $validator->_incell=true;
-                    
+                    $validator->_value = 'Q2:Q10';
                     
 //                    $validator->setList('Q2:Q10');
                     
