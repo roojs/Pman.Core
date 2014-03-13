ed6647b8fed8bf58baca5a9915711632f16dc830
Edward <edward@roojs.com>
2014-03-13 18:49:09 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -334,6 +334,7 @@ class Pman_Core_SimpleExcel extends Pman
                     $validator->_type = 3;
                     $validator->_incell=true;
                     $validator->setFormula1('$D$2:$D$6');
+                    print_r($validator);exit;
 //                    $validator->setList('Q2:Q10');
                     
                     $this->worksheet->setValidation(2,1,2,1,$validator);
