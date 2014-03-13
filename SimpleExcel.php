b74dfb6fb57035ecd8eef377be7f30eeb56fd7a0
Edward <edward@roojs.com>
2014-03-13 13:37:10 +0800

    SimpleExcel.php

--- SimpleExcel.php
+++ SimpleExcel.php
@@ -327,7 +327,7 @@ class Pman_Core_SimpleExcel extends Pman
             if ( (is_numeric($v) &&  strlen($v) > 1 && substr($v,0,1) == '0' && substr($v,1,1) != '.') 
                     || 
                     $dataFormat == 'string' ) {
-                if($worksheet_name == 'event' && $start_row == 2){
+                if($worksheet_name == 'event' && $start_row == 2 && $c == 2){
                     $worksheet->writeString($start_row+$r, $c, $v, $format);
                 }
                 
