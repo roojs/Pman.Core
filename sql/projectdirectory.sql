c298cfd0bdc9b258acdbe838c9b458044290c8ac
Alan Knowles <alan@roojs.com>
2014-03-17 16:03:46 +0800

    sql/projectdirectory.sql

---
+++ sql/projectdirectory.sql
@@ -0,0 +1,18 @@
+
+-- we duplicate office_id and company_id here...
+-- not sure if we should keep doing that in the new design...
+-- we should improve our links code to handle this..
+
+
+CREATE TABLE ProjectDirectory (
+  id int(11) NOT NULL auto_increment,
+  PRIMARY KEY  (id)
+) ;
+
+ALTER TABLE  ProjectDirectory ADD COLUMN   project_id int(11) NOT NULL DEFAULT 0;
+ALTER TABLE  ProjectDirectory ADD COLUMN   person_id int(11) NOT NULL DEFAULT 0;
+ALTER TABLE  ProjectDirectory ADD COLUMN   ispm int(11) NOT NULL DEFAULT 0;
+ALTER TABLE  ProjectDirectory ADD COLUMN   role varchar(32) NOT NULL DEFAULT '';
+
+ALTER TABLE ProjectDirectory ADD INDEX plookup (project_id,person_id, ispm, role);
+
