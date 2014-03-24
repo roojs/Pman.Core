a235566e9989cb2ef7fa0d3f20882505f0c5ddc5
Edward <edward@roojs.com>
2014-03-24 18:23:25 +0800

    MailTemplateList.php

--- MailTemplateList.php
+++ MailTemplateList.php
@@ -10,7 +10,7 @@ class Pman_Core_MailTemplateList extends Pman_Core_ConvertStyle
         $fopts = HTML_FlexyFramework::get()->HTML_Template_Flexy;
         
         $templateDir = explode(PATH_SEPARATOR, $fopts['templateDir']);
-        print_r($templateDir);exit;
+        
         $base = 'http://' . $_SERVER['SERVER_NAME'] . $this->rootURL;
         
 //        $path = '/home/edward/gitlive/web.madeinnz/Pman/Shop/templates/mail/register.body.html';
