bee7067da122b9e612ad69e5e80bd3c555ff33fd
Chris <chris@roojs.com>
2014-04-02 18:35:41 +0800

    GoogleTranslate.php

--- GoogleTranslate.php
+++ GoogleTranslate.php
@@ -46,7 +46,8 @@ class Pman_Core_GoogleTranslate extends Pman
         curl_setopt($handle, CURLOPT_POST, count($param));
         curl_setopt($handle, CURLOPT_POSTFIELDS, http_build_query($param));
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        
+        curl_setopt($handle, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: GET'));
+ 
         $response = curl_exec($handle);
         
         print_r(http_build_query($param));
