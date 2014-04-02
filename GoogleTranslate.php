0402400d4448402c50ce324a81babcc656696f49
Chris <chris@roojs.com>
2014-04-02 18:27:58 +0800

    GoogleTranslate.php

--- GoogleTranslate.php
+++ GoogleTranslate.php
@@ -39,13 +39,13 @@ class Pman_Core_GoogleTranslate extends Pman
             'target' => $_REQUEST['dest']
         );
         
-        $url = 'https://www.googleapis.com/language/translate/v2?'.http_build_query($param);
+        $url = 'https://www.googleapis.com/language/translate/v2'.http_build_query($param);
 
         $handle = curl_init();
         curl_setopt($handle,CURLOPT_URL, $url);
 //        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($handle, CURLOPT_POST, count($param));
-        curl_setopt($handle, CURLOPT_POSTFIELDS, $param);
+        curl_setopt($handle, CURLOPT_POSTFIELDS, http_build_query($param));
         $response = curl_exec($handle);                 
         $responseDecoded = json_decode($response, true);
         curl_close($handle);
