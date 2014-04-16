ae69a2cdb06a2580ad809497711f64f84b67fbc1
Edward <edward@roojs.com>
2014-04-16 18:18:06 +0800

    Pman.Dialog.CoreCompanies.bjs Pman.Dialog.CoreCompanies.js

--- Pman.Dialog.CoreCompanies.js
+++ Pman.Dialog.CoreCompanies.js
@@ -131,7 +131,7 @@ Pman.Dialog.CoreCompanies = {
                                             listWidth : 250,
                                             loadingText : "Searching...",
                                             minChars : 2,
-                                            name : 'comptype_display',
+                                            name : 'comptype_display_name',
                                             pageSize : 20,
                                             qtip : "Select type",
                                             queryParam : 'query[name]',
