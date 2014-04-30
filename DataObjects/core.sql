0f7b5c792be2115f69f65c691255174ef2defc8d
Alan Knowles <alan@roojs.com>
2014-04-30 17:50:27 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -160,32 +160,7 @@ ALTER TABLE  core_locking ADD COLUMN  created datetime ;
 alter table  core_locking ADD  INDEX lookup(on_table, on_id, person_id, created);
 -- oops... - wrong name of pid.
 alter table  core_locking change column `int` id int(11) auto_increment not null;
-
--- a generic enumeraction
-
-CREATE TABLE   core_enum (
-  id int(11)  NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id)
  
-);
- 
-alter table  core_enum ADD COLUMN  etype varchar(32)  NOT NULL DEFAULT '';
-alter table  core_enum ADD COLUMN  name varchar(255)  NOT NULL DEFAULT '';
-alter table  core_enum ADD COLUMN  active int(2)  NOT NULL DEFAULT 1;
-alter table  core_enum ADD COLUMN  seqid int(11)  NOT NULL DEFAULT 0;
-alter table  core_enum ADD COLUMN  seqmax int(11)  NOT NULL DEFAULT 0;
-alter table  core_enum ADD COLUMN  display_name varchar(255)  NOT NULL DEFAULT '';
-
-
-ALTER TABLE core_enum ADD COLUMN is_system_enum INT(2) NOT NULL DEFAULT 0;
-ALTER TABLE core_enum CHANGE COLUMN display_name display_name TEXT NOT NULL DEFAULT '';
-
-alter table  core_enum ADD  INDEX lookup(seqid, active, name, etype);
-
-
-UPDATE core_enum SET display_name = name WHERE display_name = '';
-
-
 -- ----------------------------
 
 CREATE TABLE  translations (
