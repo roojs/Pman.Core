50421d4a87841ea63108a3aabc2774399a4944cf
Edward <edward@roojs.com>
2014-05-07 16:15:11 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -96,6 +96,28 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
             $roo->jok($r->URL(-1,'/Images') . '#attachment-'.  $r->id);
         }
         
+        if(isset($q['_auto_save'])){
+            require_once 'System.php';
+            
+            $tmpdir  = System::mktemp("-d auto_save");
+            
+            $path = $tmpdir . '/' . time();
+            
+            if(!file_exists($path)){
+               file_put_contents($path, $q['_source']); 
+            }
+            
+            $this->setFrom($q);
+            
+            if (!$this->createFrom($path)) {
+                $roo->jerr("error on auto save making image");
+            }
+            
+            $roo->addEvent("AUTOSAVE", $this, $this->toEventString());
+            
+            $this->jok("OK");
+        }
+        
     }
     
      
