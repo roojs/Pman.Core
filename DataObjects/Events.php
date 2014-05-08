331892fc2f61d31198b5e43f1ce3214c6759b20a
Edward <edward@roojs.com>
2014-05-08 13:15:13 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -460,6 +460,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
             
             $source = json_decode(file_get_contents($images->getStoreName()))->source;
             
+            $data[$k]['source'] = $source;
         }
     }
 }
