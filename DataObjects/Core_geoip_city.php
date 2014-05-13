40538797c9a6dd0e2ea2b48f85dbdd947884942e
Edward <edward@roojs.com>
2014-05-13 17:16:21 +0800

    DataObjects/Core_geoip_city.php

---
+++ DataObjects/Core_geoip_city.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Core_geoip_city
+ *
+ * @author edward
+ */
+class Core_geoip_city {
+    //put your code here
+}
+
+?>
