62ff7ec8d543768ad15dbc734d9f8b24c9bee729
Edward <edward@roojs.com>
2014-05-13 15:25:59 +0800

    sql/core_geoip_city.sql sql/core_geoip_location.sql

--- sql/core_geoip_city.sql
+++ sql/core_geoip_city.sql
@@ -2,12 +2,13 @@ CREATE  TABLE core_geoip_city (
     id INT(11) NOT NULL AUTO_INCREMENT ,
     city_name TEXT NOT NULL DEFAULT ''
     country_id INT(11) NOT NULL DEFAULT 0,
-    location_id INT(11) NOT NULL DEFAULT 0,
     metro_code INT(11) NOT NULL DEFAULT 0,
     time_zone TEXT NOT NULL DEFAULT ''
     PRIMARY KEY (id)
 );
 
-CREATE INDEX country_id_idx ON core_geoip_country (country_id) USING BTREE;
-CREATE INDEX network_ip_id_idx ON core_geoip_country (network_ip_id) USING BTREE;
 CREATE INDEX city_name_idx ON core_geoip_country (city_name) USING BTREE;
+CREATE INDEX country_id_idx ON core_geoip_country (country_id) USING BTREE;
+CREATE INDEX metro_code_idx ON core_geoip_country (metro_code) USING BTREE;
+CREATE INDEX time_zone_idx ON core_geoip_country (time_zone) USING BTREE;
+
---
+++ sql/core_geoip_location.sql
@@ -0,0 +1,12 @@
+CREATE  TABLE core_geoip_location (
+    id INT(11) NOT NULL AUTO_INCREMENT ,
+    start_ip TEXT NOT NULL DEFAULT ''
+    mask_length INT(11) NOT NULL DEFAULT 0,
+    postal_code INT(11) NOT NULL DEFAULT 0,
+    
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX country_id_idx ON core_geoip_country (country_id) USING BTREE;
+CREATE INDEX network_ip_id_idx ON core_geoip_country (network_ip_id) USING BTREE;
+CREATE INDEX city_name_idx ON core_geoip_country (city_name) USING BTREE;
