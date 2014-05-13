264a8c01ebfd60863beec1c287c1f6812697fa43
Edward <edward@roojs.com>
2014-05-13 15:08:54 +0800

    sql/core_geoip_city.sql

--- sql/core_geoip_city.sql
+++ sql/core_geoip_city.sql
@@ -1,6 +1,7 @@
 CREATE  TABLE core_geoip_city (
     id INT(11) NOT NULL AUTO_INCREMENT ,
     city_name TEXT NOT NULL DEFAULT ''
+    country_id INT(11) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 
