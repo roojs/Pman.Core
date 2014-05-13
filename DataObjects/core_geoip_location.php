28e3f1d05ca0a30e06b326430a2a3f0cc075b4d4
Edward <edward@roojs.com>
2014-05-13 17:13:49 +0800

    DataObjects/core_geoip_location.php

---
+++ DataObjects/core_geoip_location.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of core_geoip_location
+ *
+ * @author edward
+ */
+class core_geoip_location {
+    //put your code here
+}
+
+?>
