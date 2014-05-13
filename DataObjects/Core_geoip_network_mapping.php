900238a8d070fa8dc5261aaf14af5538582ac5e3
Edward <edward@roojs.com>
2014-05-13 17:18:20 +0800

    DataObjects/Core_geoip_network_mapping.php

---
+++ DataObjects/Core_geoip_network_mapping.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Core_geoip_network_mapping
+ *
+ * @author edward
+ */
+class Core_geoip_network_mapping {
+    //put your code here
+}
+
+?>
