0fadef5c0354056d3e6ffd5b122975e8f4e8e0ba
Edward <edward@roojs.com>
2014-05-13 17:19:41 +0800

    DataObjects/Core_geoip_network_mapping.php

--- DataObjects/Core_geoip_network_mapping.php
+++ DataObjects/Core_geoip_network_mapping.php
@@ -1,17 +1,23 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Core_geoip_network_mapping
- *
- * @author edward
+ * Table Definition for core_geoip_network_mapping
  */
-class Core_geoip_network_mapping {
-    //put your code here
-}
+require_once 'DB/DataObject.php';
 
-?>
+class Pman_Core_DataObjects_Core_geoip_network_mapping extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'core_geoip_network_mapping';                       // table name
+    public $id;
+    public $start_ip;
+    public $mask_length;
+    public $city_id;
+    
+        
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+    
+    
+}
