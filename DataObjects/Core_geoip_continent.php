94c4bd34cc3f96ab3b14fcef267f7035339457f5
Edward <edward@roojs.com>
2014-05-13 17:09:28 +0800

    DataObjects/Core_geoip_continent.php

---
+++ DataObjects/Core_geoip_continent.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Core_geoip_continent
+ *
+ * @author edward
+ */
+class Core_geoip_continent {
+    //put your code here
+}
+
+?>
