5b99ebfcbb044b20396460b2be6cf2629163fd56
Edward <edward@roojs.com>
2014-05-13 15:39:32 +0800

    sql/core_geoip_location.sql

--- sql/core_geoip_location.sql
+++ sql/core_geoip_location.sql
@@ -1,6 +1,6 @@
 CREATE  TABLE core_geoip_location (
     id INT(11) NOT NULL AUTO_INCREMENT ,
-    city_id INT(11) NOT NULL DEFAULT 0
+    city_id INT(11) NOT NULL DEFAULT 0,
     latitude INT(11) NOT NULL DEFAULT 0,
     longitude INT(11) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
