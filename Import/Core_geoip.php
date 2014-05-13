4b7010481bf98253163ccb6aa161b1f9347205a2
Edward <edward@roojs.com>
2014-05-13 16:54:45 +0800

    Import/Core_geoip.php

--- Import/Core_geoip.php
+++ Import/Core_geoip.php
@@ -8,8 +8,6 @@ class Pman_Core_Import_Core_geoip extends Pman_Roo
     
     static $cli_opts = array();
     
-    static $id_mapping = array();
-    
     function getAuth()
     {
         $ff = HTML_FlexyFramework::get();
@@ -36,6 +34,8 @@ class Pman_Core_Import_Core_geoip extends Pman_Roo
         if(!file_exists($location) || !file_exists($block)){
             $this->jerr('GeoLite2-City-Locations.csv OR GeoLite2-City-Blocks.csv does not exists?!');
         }
+        
+        static $id_mapping = array();
         
         print_r($this->id_mapping);exit;
         
