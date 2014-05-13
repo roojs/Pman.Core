b21473637b2c18ad4106b55a8ae4877d16217e91
Edward <edward@roojs.com>
2014-05-13 18:55:54 +0800

    sql/core_geoip_network_mapping.sql

--- sql/core_geoip_network_mapping.sql
+++ sql/core_geoip_network_mapping.sql
@@ -1,7 +1,7 @@
 CREATE  TABLE core_geoip_network_mapping (
     id INT(11) NOT NULL AUTO_INCREMENT ,
     start_ip TEXT NOT NULL DEFAULT '',
-    mask_length INT(11) NOT NUL DEFAULT 0,
+    mask_length INT(11) NOT NULL DEFAULT 0,
     city_id INT(11) NOT NULL DEFAULT 0,
     is_anonymous_proxy INT(2) NOT NULL DEFAULT 0,
     is_satellite_provider INT(2) NOT NULL DEFAULT 0,
