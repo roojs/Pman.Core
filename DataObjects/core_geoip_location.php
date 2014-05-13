bdf9423212a8f4e6a03f2a6ec819790af5a509b7
Edward <edward@roojs.com>
2014-05-13 17:14:09 +0800

    DataObjects/core_geoip_location.php

--- DataObjects/core_geoip_location.php
+++ DataObjects/core_geoip_location.php
@@ -1,17 +1,22 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of core_geoip_location
- *
- * @author edward
+ * Table Definition for core_geoip_location
  */
-class core_geoip_location {
-    //put your code here
-}
+require_once 'DB/DataObject.php';
 
-?>
+class Pman_Core_DataObjects_Core_geoip_location extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'core_geoip_location';                       // table name
+    public $id;
+    public $continent_id;
+    public $code;
+    public $name;
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+    
+    
+}
