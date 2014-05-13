b83b7fcea1d09f6b929045bf6e3b9e435bb75553
Edward <edward@roojs.com>
2014-05-13 17:11:23 +0800

    DataObjects/Core_geoip_country.php

---
+++ DataObjects/Core_geoip_country.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Core_geoip_country
+ *
+ * @author edward
+ */
+class Core_geoip_country {
+    //put your code here
+}
+
+?>
