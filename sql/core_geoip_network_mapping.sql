b3aca29922a2a401d54c1d08181aa9ade9536869
Edward <edward@roojs.com>
2014-05-13 18:02:19 +0800

    sql/core_geoip_network_mapping.sql

--- sql/core_geoip_network_mapping.sql
+++ sql/core_geoip_network_mapping.sql
@@ -11,3 +11,5 @@ CREATE  TABLE core_geoip_network_mapping (
 CREATE INDEX start_ip_idx ON core_geoip_country (start_ip) USING BTREE;
 CREATE INDEX mask_length_idx ON core_geoip_country (mask_length) USING BTREE;
 CREATE INDEX city_id_idx ON core_geoip_country (city_id) USING BTREE;
+CREATE INDEX is_anonymous_proxy_idx ON core_geoip_country (is_anonymous_proxy) USING BTREE;
+CREATE INDEX is_satellite_provider_idx ON core_geoip_country (is_satellite_provider) USING BTREE;
