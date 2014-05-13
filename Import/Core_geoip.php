24e464e1e9e07acc6ac6252cc4d7f4623354eae8
Edward <edward@roojs.com>
2014-05-13 16:52:52 +0800

    Import/Core_geoip.php

--- Import/Core_geoip.php
+++ Import/Core_geoip.php
@@ -8,6 +8,7 @@ class Pman_Core_Import_Core_geoip extends Pman_Roo
     
     static $cli_opts = array();
     
+    
     function getAuth()
     {
         $ff = HTML_FlexyFramework::get();
@@ -16,6 +17,11 @@ class Pman_Core_Import_Core_geoip extends Pman_Roo
         }
         HTML_FlexyFramework::ensureSingle(__FILE__, $this);
         return true;
+    }
+    
+    function post()
+    {
+        $this->get();
     }
     
     function get()
@@ -30,41 +36,11 @@ class Pman_Core_Import_Core_geoip extends Pman_Roo
             $this->jerr('GeoLite2-City-Locations.csv OR GeoLite2-City-Blocks.csv does not exists?!');
         }
         
-        $this->jerr('exists');
-//        $fc = new File_Convert($cp, 'application/vnd.ms-excel');
-//        //var_Dump($img->getStoreName());
-//        $csv = $fc->convert('text/csv');
-//        unlink($cp);
-//        //var_dump($csv);
-//        $this->importCsv($csv);
-    }
-    
-    function post()
-    {
-//        $this->transObj = DB_DataObject::Factory('custinfo');
-//        
-//        $this->transObj->query('BEGIN');
-//        
-//        PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, array($this, 'onPearError'));
-//        
-//        $img = DB_DataObject::Factory('images');
-//        $img->setFrom(array(
-//            'onid' => 0,
-//            'ontable' => 'ipshead'
-//        ));
-//        $img->onUpload(false);
-//        
-//        require_once 'File/Convert.php';
-//        $fc = new File_Convert($img->getStoreName(), $img->mimetype );
-//        $csv = $fc->convert('text/csv');
-//        $this->importCsv($csv);
-    }
-    
-    function importCsv($csv)
-    {
+        $this->insertLocation($location);
         
-        exit;
+        $this->insertBlock($block);
     }
+    
     
     function log($str)
     {
