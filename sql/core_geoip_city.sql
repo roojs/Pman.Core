05578fcb336c4b146a802eb8c04317b1be116dde
Edward <edward@roojs.com>
2014-05-13 15:37:03 +0800

    sql/core_geoip_city.sql

--- sql/core_geoip_city.sql
+++ sql/core_geoip_city.sql
@@ -2,6 +2,7 @@ CREATE  TABLE core_geoip_city (
     id INT(11) NOT NULL AUTO_INCREMENT ,
     city_name TEXT NOT NULL DEFAULT ''
     country_id INT(11) NOT NULL DEFAULT 0,
+    postal_code INT(11) NOT NULL DEFAULT 0,
     metro_code INT(11) NOT NULL DEFAULT 0,
     time_zone TEXT NOT NULL DEFAULT ''
     PRIMARY KEY (id)
@@ -9,6 +10,7 @@ CREATE  TABLE core_geoip_city (
 
 CREATE INDEX city_name_idx ON core_geoip_country (city_name) USING BTREE;
 CREATE INDEX country_id_idx ON core_geoip_country (country_id) USING BTREE;
+CREATE INDEX postal_code_idx ON core_geoip_country (postal_code) USING BTREE;
 CREATE INDEX metro_code_idx ON core_geoip_country (metro_code) USING BTREE;
 CREATE INDEX time_zone_idx ON core_geoip_country (time_zone) USING BTREE;
 
