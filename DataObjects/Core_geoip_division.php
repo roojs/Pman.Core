5d6c98227b2b4d9b4fef04b2ae7a14e19ff4cef4
Edward <edward@roojs.com>
2014-05-13 17:19:57 +0800

    DataObjects/Core_geoip_division.php

---
+++ DataObjects/Core_geoip_division.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Core_geoip_division
+ *
+ * @author edward
+ */
+class Core_geoip_division {
+    //put your code here
+}
+
+?>
