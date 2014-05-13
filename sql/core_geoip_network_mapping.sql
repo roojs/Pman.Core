889f5db862d1b96ec44e72f9565477a861fe6f69
Edward <edward@roojs.com>
2014-05-13 17:19:20 +0800

    sql/core_geoip_network_mapping.sql

--- sql/core_geoip_network_mapping.sql
+++ sql/core_geoip_network_mapping.sql
@@ -1,9 +1,11 @@
 CREATE  TABLE core_geoip_network_mapping (
     id INT(11) NOT NULL AUTO_INCREMENT ,
-    network_start_ip TEXT NOT NULL DEFAULT '',
-    network_mask_length INT(11) NOT NUL DEFAULT 0,
+    start_ip TEXT NOT NULL DEFAULT '',
+    mask_length INT(11) NOT NUL DEFAULT 0,
     city_id INT(11) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 
+CREATE INDEX start_ip_idx ON core_geoip_country (start_ip) USING BTREE;
+CREATE INDEX mask_length_idx ON core_geoip_country (mask_length) USING BTREE;
 CREATE INDEX city_id_idx ON core_geoip_country (city_id) USING BTREE;
