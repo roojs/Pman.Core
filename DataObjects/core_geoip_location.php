c35a36f304fde79f04b2b00f4c57fc70c4bae865
Edward <edward@roojs.com>
2014-05-13 17:14:58 +0800

    DataObjects/core_geoip_location.php

--- DataObjects/core_geoip_location.php
+++ DataObjects/core_geoip_location.php
@@ -11,10 +11,10 @@ class Pman_Core_DataObjects_Core_geoip_location extends DB_DataObject
 
     public $__table = 'core_geoip_location';                       // table name
     public $id;
-    public $continent_id;
-    public $code;
-    public $name;
-
+    public $latitude;
+    public $longitude;
+    public $city_id;
+    
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
     
