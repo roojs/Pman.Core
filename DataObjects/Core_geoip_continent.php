2f8cf38249e4bdd6f81f24d7037e5b5aaa21b55b
Edward <edward@roojs.com>
2014-05-13 17:10:31 +0800

    DataObjects/Core_geoip_continent.php

--- DataObjects/Core_geoip_continent.php
+++ DataObjects/Core_geoip_continent.php
@@ -1,17 +1,22 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Core_geoip_continent
- *
- * @author edward
+ * Table Definition for Companies
  */
-class Core_geoip_continent {
-    //put your code here
-}
+require_once 'DB/DataObject.php';
 
-?>
+class Pman_Core_DataObjects_Companies extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'core_geoip_continent';                       // table name
+    public $id;
+    public $continent_code;
+    public $continent_name;
+
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+    
+    
+}
