17fab9e84b5c67c03b242f60e687a9156e8530d3
Edward <edward@roojs.com>
2014-05-13 15:10:24 +0800

    sql/core_geoip_city.sql

--- sql/core_geoip_city.sql
+++ sql/core_geoip_city.sql
@@ -2,7 +2,9 @@ CREATE  TABLE core_geoip_city (
     id INT(11) NOT NULL AUTO_INCREMENT ,
     city_name TEXT NOT NULL DEFAULT ''
     country_id INT(11) NOT NULL DEFAULT 0,
+    network_ip_id INT(11) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 
+CREATE INDEX country_id_idx ON core_geoip_country (country_id) USING BTREE;
 CREATE INDEX city_name_idx ON core_geoip_country (city_name) USING BTREE;
