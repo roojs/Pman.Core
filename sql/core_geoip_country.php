ae715c213c444f74c622786171ed73b24f5810a4
Edward <edward@roojs.com>
2014-05-13 14:15:33 +0800

    sql/core_geoip_country.php

---
+++ sql/core_geoip_country.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of core_geoip_country
+ *
+ * @author edward
+ */
+class core_geoip_country {
+    //put your code here
+}
+
+?>
