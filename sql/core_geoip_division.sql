2c67535277efa4a8415cba0c3cd6e9f89c37e9e9
Edward <edward@roojs.com>
2014-05-13 17:20:31 +0800

    sql/core_geoip_division.sql

--- sql/core_geoip_division.sql
+++ sql/core_geoip_division.sql
@@ -1,9 +1,9 @@
 CREATE  TABLE core_geoip_division (
     id INT(11) NOT NULL AUTO_INCREMENT ,
-    division_code TEXT NOT NULL DEFAULT '',
-    division_name TEXT NOT NULL DEFAULT '',
+    code TEXT NOT NULL DEFAULT '',
+    name TEXT NOT NULL DEFAULT '',
     PRIMARY KEY (id)
 );
 
-CREATE INDEX division_code_idx ON core_geoip_country (division_code) USING BTREE;
-CREATE INDEX division_name_idx ON core_geoip_country (division_name) USING BTREE;
+CREATE INDEX code_idx ON core_geoip_country (code) USING BTREE;
+CREATE INDEX name_idx ON core_geoip_country (name) USING BTREE;
