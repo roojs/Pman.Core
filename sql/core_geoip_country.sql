819a93049596d14a6ece1dd709beabae1091f7de
Edward <edward@roojs.com>
2014-05-13 17:12:50 +0800

    sql/core_geoip_country.sql

--- sql/core_geoip_country.sql
+++ sql/core_geoip_country.sql
@@ -1,11 +1,11 @@
 CREATE  TABLE core_geoip_country (
     id INT(11) NOT NULL AUTO_INCREMENT ,
+    code TEXT NOT NULL DEFAULT '',
+    name TEXT NOT NULL DEFAULT ''
     continent_id INT(11) NOT NULL DEFAULT 0,
-    country_code TEXT NOT NULL DEFAULT '',
-    country_name TEXT NOT NULL DEFAULT ''
     PRIMARY KEY (id)
 );
 
 CREATE INDEX continent_id_idx ON core_geoip_country (continent_id) USING BTREE;
-CREATE INDEX country_code_idx ON core_geoip_country (country_code) USING BTREE;
-CREATE INDEX country_name_idx ON core_geoip_country (country_name) USING BTREE;
+CREATE INDEX code_idx ON core_geoip_country (code) USING BTREE;
+CREATE INDEX name_idx ON core_geoip_country (name) USING BTREE;
