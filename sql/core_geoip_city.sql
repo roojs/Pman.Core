7da47e5f4795e4cd6ed266a9534a4d7bbbb0b6df
Edward <edward@roojs.com>
2014-05-14 15:27:53 +0800

    sql/core_geoip_city.sql

--- sql/core_geoip_city.sql
+++ sql/core_geoip_city.sql
@@ -9,10 +9,10 @@ CREATE  TABLE core_geoip_city (
     PRIMARY KEY (id)
 );
 
-CREATE INDEX name_idx ON core_geoip_country (name) USING BTREE;
-CREATE INDEX country_id_idx ON core_geoip_country (country_id) USING BTREE;
-CREATE INDEX division_id_idx ON core_geoip_country (division_id) USING BTREE;
-CREATE INDEX postal_code_idx ON core_geoip_country (postal_code) USING BTREE;
-CREATE INDEX metro_code_idx ON core_geoip_country (metro_code) USING BTREE;
-CREATE INDEX time_zone_idx ON core_geoip_country (time_zone) USING BTREE;
+CREATE INDEX name_idx ON core_geoip_city (name) USING BTREE;
+CREATE INDEX country_id_idx ON core_geoip_city (country_id) USING BTREE;
+CREATE INDEX division_id_idx ON core_geoip_city (division_id) USING BTREE;
+CREATE INDEX postal_code_idx ON core_geoip_city (postal_code) USING BTREE;
+CREATE INDEX metro_code_idx ON core_geoip_city (metro_code) USING BTREE;
+CREATE INDEX time_zone_idx ON core_geoip_city (time_zone) USING BTREE;
 
