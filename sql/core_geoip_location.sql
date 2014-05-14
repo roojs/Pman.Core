5f2a370109f64962cdfdc5a97b8aabdbb8cc744e
Edward <edward@roojs.com>
2014-05-14 16:49:42 +0800

    sql/core_geoip_location.sql

--- sql/core_geoip_location.sql
+++ sql/core_geoip_location.sql
@@ -1,11 +1,11 @@
 CREATE  TABLE core_geoip_location (
     id INT(11) NOT NULL AUTO_INCREMENT ,
-    latitude INT(11) NOT NULL DEFAULT 0,
-    longitude INT(11) NOT NULL DEFAULT 0,
+    latitude DECIMAL(11,8) NOT NULL DEFAULT 0,
+    longitude DECIMAL(11,8) NOT NULL DEFAULT 0,
     city_id INT(11) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 
-CREATE INDEX latitude_idx ON core_geoip_country (latitude) USING BTREE;
-CREATE INDEX longitude_idx ON core_geoip_country (longitude) USING BTREE;
-CREATE INDEX city_id_idx ON core_geoip_country (city_id) USING BTREE;
+CREATE INDEX latitude_idx ON core_geoip_location (latitude) USING BTREE;
+CREATE INDEX longitude_idx ON core_geoip_location (longitude) USING BTREE;
+CREATE INDEX city_id_idx ON core_geoip_location (city_id) USING BTREE;
