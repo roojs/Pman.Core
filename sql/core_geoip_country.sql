46491484564ec616343b60f97cdf3fd32554d2a0
Edward <edward@roojs.com>
2014-05-14 14:47:17 +0800

    sql/core_geoip_country.sql

--- sql/core_geoip_country.sql
+++ sql/core_geoip_country.sql
@@ -1,7 +1,7 @@
 CREATE  TABLE core_geoip_country (
     id INT(11) NOT NULL AUTO_INCREMENT ,
-    code TEXT NOT NULL DEFAULT '',
-    name TEXT NOT NULL DEFAULT '',
+    code VARCHAR(32) NOT NULL DEFAULT '',
+    name VARCHAR(255) NOT NULL DEFAULT '',
     continent_id INT(11) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
