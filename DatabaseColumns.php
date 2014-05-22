3a0c977f5eaa9f5ef33d95a04fc0b7c6d03b04a4
Alan Knowles <alan@roojs.com>
2014-05-22 16:09:22 +0800

    DatabaseColumns.php

--- DatabaseColumns.php
+++ DatabaseColumns.php
@@ -42,36 +42,5 @@ class Pman_Core_DatabaseColumns extends Pman {
         }
         
         
-        require_once 'Pman/Core/SimpleExcel.php';
-        $x = new Pman_Core_SimpleExcel(
-            array($bt,$et, $ut), array(
-            'formats' => array(
-                'vtop' => array('vAlign' => 'top'),
-                'vcenter' => array('vAlign' => 'vcenter'),
-                
-                'percent' => array('vAlign' => 'vcenter', 'numFormat' => '0%'),
-                'money' => array('vAlign' => 'vcenter', 'numFormat' => '$#,###'),
-                'date' => array('vAlign' => 'vcenter', 'numFormat' => 'd/M/Y'),
-            ),
-            'workbooks' => array(
-                array(
-                    'workbook' => 'Business',
-                    'cols' => explode('|', 'keyIdentity_taxId|description|type|countryIso3|countryRegion|addr1|addr2|addr3|city|zip')
-                ),
-                array(
-                    'workbook' => 'Employment',
-                    'cols' => explode('|', 'emailAddress|identity_taxId')
-                ),
-                array(
-                    'workbook' => 'Users',
-                    'cols' => explode('|', 'generateLogin|firstName|middleName|lastName|emailAddress|locale|countryIso3|countryRegion|addr1|addr2|addr3|city|zip|userStatus')
-                ),
-            )
-            
-        ));
-        $x->send('test_'. date('d_M_Y').'.xls');
-        $this->jdata($ret);
-        
-        
     }
 }
