0bc07c836c4f92f1dadae8d3c9131161c8d735e5
Edward <edward@roojs.com>
2014-06-03 14:25:07 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -173,6 +173,7 @@ class Pman_Core_NotifySend extends Pman
          
         // this may modify $p->email. (it will not update it though)
         $email =  $this->makeEmail($o, $p, $last, $w, $force);
+        exit;
         
         if ($email === true)  {
             
