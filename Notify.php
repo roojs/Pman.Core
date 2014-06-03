1e167762b9fbb86730d4265b0f7cd8fc3af01fab
Edward <edward@roojs.com>
2014-06-03 14:02:38 +0800

    Notify.php

--- Notify.php
+++ Notify.php
@@ -196,6 +196,8 @@ class Pman_Core_Notify extends Pman
         
         $ar = $w->fetchAll();
         
+        print_r($ar);exit;
+        
         if (!empty($opts['list'])) {
             if (empty($ar)) {
                 die("Nothing in Queue\n");
