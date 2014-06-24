e59739b142439eeed81b141c0fdce64ff9eeae4e
Edward <edward@roojs.com>
2014-06-24 19:28:42 +0800

    mysql/core_cities_merge.sql

--- mysql/core_cities_merge.sql
+++ mysql/core_cities_merge.sql
@@ -3,58 +3,59 @@ DROP FUNCTION IF EXISTS core_cities_merge;
 DELIMITER $$
 CREATE FUNCTION core_cities_merge()  RETURNS TEXT DETERMINISTIC
     BEGIN
-        DECLARE done INT DEFAULT FALSE;
+        DECLARE co_done INT DEFAULT FALSE;
+        DECLARE re_done INT DEFAULT FALSE;
+        DECLARE ci_done INT DEFAULT FALSE;
+
         DECLARE v_iso TEXT DEFAULT '';
         DECLARE v_local_name TEXT DEFAULT '';
         DECLARE v_type TEXT DEFAULT '';
         DECLARE v_in_location INT DEFAULT 0;
         DECLARE v_id INT DEFAULT 0;
 
-        DECLARE csr CURSOR FOR 
+        DECLARE co_csr CURSOR FOR 
         SELECT 
             iso,local_name,type,in_location
         FROM 
             meta_location
+        WHERE
+            type = 'CO';
 
-        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+        DECLARE re_csr CURSOR FOR 
+        SELECT 
+            iso,local_name,type,in_location
+        FROM 
+            meta_location
+        WHERE
+            type = 'RE';
 
-        OPEN csr;
-        read_loop: LOOP
-            FETCH csr INTO v_iso,v_local_name,v_type,v_in_location;
+        DECLARE ci_csr CURSOR FOR 
+        SELECT 
+            iso,local_name,type,in_location
+        FROM 
+            meta_location
+        WHERE
+            type = 'CI';
+
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET co_done = TRUE;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET re_done = TRUE;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET ci_done = TRUE;
+
+
+        OPEN co_csr;
+        co_loop: LOOP
+            FETCH co_csr INTO v_iso,v_local_name,v_type,v_in_location;
             
             SET v_id = 0;
 
-            IF v_type = 'CO' THEN
-                SELECT id INTO v_id FROM core_geoip_country WHERE code = v_iso;
-                IF(id = 0) THEN
-                    INSERT INTO core_geoip_country (code, name, continent_id) VALUES (v_iso, v_local_name, 0);
-                END IF;
+            SELECT id INTO v_id FROM core_geoip_country WHERE code = v_iso;
 
-                ITERATE read_loop;
-                
+            IF(v_id > 0) THEN
+                INSERT INTO core_geoip_country (code, name, continent_id) VALUES (v_iso, v_local_name, 0);
             END IF;
 
-            IF v_type = 'RE' THEN
-                SELECT id INTO v_id FROM core_geoip_country WHERE code = v_iso;
-                IF(id = 0) THEN
-                    INSERT INTO core_geoip_country (code, name, continent_id) VALUES (v_iso, v_local_name, 0);
-                END IF;
-
-                ITERATE read_loop;
+--             ITERATE read_loop;
                 
-            END IF;
-
-            IF v_type = 'CI' THEN
-                SELECT id INTO v_id FROM core_geoip_country WHERE code = v_iso;
-                IF(id = 0) THEN
-                    INSERT INTO core_geoip_country (code, name, continent_id) VALUES (v_iso, v_local_name, 0);
-                END IF;
-
-                ITERATE read_loop;
-                
-            END IF;
-                    
-
             IF done THEN
               LEAVE read_loop;
             END IF;
