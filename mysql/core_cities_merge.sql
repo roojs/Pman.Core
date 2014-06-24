3d8af1aec1ec033adf398c491ba175d4e320f22d
Edward <edward@roojs.com>
2014-06-24 17:57:58 +0800

    mysql/core_cities_merge.sql

--- mysql/core_cities_merge.sql
+++ mysql/core_cities_merge.sql
@@ -3,8 +3,22 @@ DROP FUNCTION IF EXISTS core_cities_merge;
 DELIMITER $$
 CREATE FUNCTION core_cities_merge()  RETURNS TEXT DETERMINISTIC
     BEGIN
-        
+        DECLARE done INT DEFAULT FALSE;
+        DECLARE v_iso TEXT DEFAULT '';
+        DECLARE csr CURSOR FOR 
+        SELECT iso INTO  FROM meta_location;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
+        OPEN csr;
+        read_loop: LOOP
+        FETCH csr INTO v_counter;
+
+            IF done THEN
+              LEAVE read_loop;
+            END IF;
+
+        END LOOP;
+        CLOSE csr;
 
     END $$
 DELIMITER ; 
