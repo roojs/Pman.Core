7824ce9f17c84912c5575c331976203f3d1e3aa0
Edward <edward@roojs.com>
2014-06-24 18:06:28 +0800

    mysql/core_cities_merge.sql

--- mysql/core_cities_merge.sql
+++ mysql/core_cities_merge.sql
@@ -5,14 +5,25 @@ CREATE FUNCTION core_cities_merge()  RETURNS TEXT DETERMINISTIC
     BEGIN
         DECLARE done INT DEFAULT FALSE;
         DECLARE v_iso TEXT DEFAULT '';
+        DECLARE v_local_name TEXT DEFAULT '';
+        DECLARE v_type TEXT DEFAULT '';
+        DECLARE v_in_location INT DEFAULT 0;
+
         DECLARE csr CURSOR FOR 
-        SELECT iso INTO  FROM meta_location;
+        SELECT 
+            iso INTO v_iso ,
+            local_name INTO v_local_name,
+            type INTO v_type,
+            in_location INTO v_in_location
+        FROM 
+            meta_location
+        LIMIT 1;
         DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
         OPEN csr;
         read_loop: LOOP
-        FETCH csr INTO v_counter;
-
+            FETCH csr;
+            
             IF done THEN
               LEAVE read_loop;
             END IF;
