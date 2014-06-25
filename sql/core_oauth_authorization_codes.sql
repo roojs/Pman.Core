191f86933ba0eb8e4a832e4a42d10a807b2411a9
Edward <edward@roojs.com>
2014-06-25 12:45:39 +0800

    sql/core_oauth_authorization_codes.sql

--- sql/core_oauth_authorization_codes.sql
+++ sql/core_oauth_authorization_codes.sql
@@ -8,3 +8,8 @@ CREATE TABLE core_oauth_authorization_codes (
     scope VARCHAR(2000),
     PRIMARY KEY (id)
 );
+
+CREATE INDEX authorization_code_idx ON core_oauth_authorization_codes (authorization_code) USING BTREE;
+CREATE INDEX client_id_idx ON core_oauth_authorization_codes (client_id) USING BTREE;
+CREATE INDEX user_id_idx ON core_oauth_authorization_codes (user_id) USING BTREE;
+CREATE INDEX redirect_uri_idx ON core_oauth_authorization_codes (redirect_uri) USING BTREE;
