b492cf29f5a606fa6efaabd4224f167bd37e7bac
Edward <edward@roojs.com>
2014-06-25 12:45:57 +0800

    sql/core_oauth_jwt.sql

--- sql/core_oauth_jwt.sql
+++ sql/core_oauth_jwt.sql
@@ -1,7 +1,7 @@
 CREATE TABLE core_oauth_jwt (
+    id INT(11) NOT NULL AUTO_INCREMENT,
     client_id VARCHAR(80) NOT NULL, 
     subject VARCHAR(80), 
-    public_key VARCHAR(2000), 
-    CONSTRAINT client_id_pk 
-    PRIMARY KEY (client_id)
+    public_key VARCHAR(2000),
+    PRIMARY KEY (id)
 );
