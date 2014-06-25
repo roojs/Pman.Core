cc2a9b31688ca3ac25f989f30fb5ff905e34c3c5
Edward <edward@roojs.com>
2014-06-25 12:41:08 +0800

    sql/core_oauth_access_tokens.sql

--- sql/core_oauth_access_tokens.sql
+++ sql/core_oauth_access_tokens.sql
@@ -1,9 +1,13 @@
 CREATE TABLE core_oauth_access_tokens (
+    id INT(11) NOT NULL AUTO_INCREMENT,
     access_token VARCHAR(40) NOT NULL, 
     client_id VARCHAR(80) NOT NULL, 
     user_id VARCHAR(255), 
     expires TIMESTAMP NOT NULL, 
     scope VARCHAR(2000), 
-    CONSTRAINT access_token_pk 
-    PRIMARY KEY (access_token)
+    PRIMARY KEY (id)
 );
+
+CREATE INDEX access_token_idx ON core_oauth_access_tokens (access_token) USING BTREE;
+CREATE INDEX client_id_idx ON core_oauth_access_tokens (client_id) USING BTREE;
+CREATE INDEX user_id_idx ON core_oauth_access_tokens (user_id) USING BTREE;
