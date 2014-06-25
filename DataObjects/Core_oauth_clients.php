01d131bcaa9af9a08b68db9ba41abe38a2cfdbf9
Edward <edward@roojs.com>
2014-06-25 12:15:03 +0800

    DataObjects/Core_oauth_clients.php

--- DataObjects/Core_oauth_clients.php
+++ DataObjects/Core_oauth_clients.php
@@ -1,17 +1,31 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Core_oauth_clients
+ * Table Definition for core_oauth_clients
  *
- * @author edward
  */
-class Core_oauth_clients {
-    //put your code here
-}
+require_once 'DB/DataObject.php';
 
-?>
+class Pman_Core_DataObjects_Core_oauth_clients extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'core_oauth_clients';         // table name
+    public $client_id;                              // varchar
+    public $client_secret;                          // varchar
+    public $redirect_uri;                           // varchar
+    public $grant_types;                            // varchar
+    public $scope;                                  // varchar
+    public $user_id;                                // varchar
+    
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+    /** make sure there is a watch for this user.. */
+    
+    
+    function applyFilters($q,$au, $roo)
+    {
+        
+    }
+    
+}
