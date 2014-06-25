2c8855264f92366c04634bc75ffe21353266a12b
Edward <edward@roojs.com>
2014-06-25 11:48:49 +0800

    Pman.Tab.CoreOAuthClient.bjs Pman.Tab.CoreOAuthClient.js

--- Pman.Tab.CoreOAuthClient.js
+++ Pman.Tab.CoreOAuthClient.js
@@ -1,1 +1,34 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Pman.Tab.CoreOAuthClient = new Roo.XComponent({
+    part     :  ["Core","OAuthClient"],
+    order    : '001-Pman.Tab.CoreOAuthClient',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            xtype: 'NestedLayoutPanel',
+            xns: Roo,
+            region : 'center',
+            title : "Email Template",
+            layout : {
+                xtype: 'BorderLayout',
+                xns: Roo,
+                center : {
+                    xtype: 'LayoutRegion',
+                    xns: Roo,
+                    autoScroll : false,
+                    split : true
+                }
+            }
+        };
+    }
+});
