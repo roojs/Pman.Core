ba4226c431a1ace014e41fa45f4b470e8bb25a99
Edward <edward@roojs.com>
2014-06-25 12:21:51 +0800

    Pman.Tab.CoreOAuthClient.bjs Pman.Tab.CoreOAuthClient.js

--- Pman.Tab.CoreOAuthClient.js
+++ Pman.Tab.CoreOAuthClient.js
@@ -54,12 +54,11 @@ Pman.Tab.CoreOAuthClient = new Roo.XComponent({
                                 },
                                 afteredit : function (e)
                                 {
-                                    Roo.log(e);
                                     if(e.originalValue == e.value || !e.value.length){
                                         return false;
                                     }
                                     
-                                    Roo.log('commit');
+                                    e.record.commit();
                                 }
                             },
                             autoExpandColumn : 'redirect_uri',
