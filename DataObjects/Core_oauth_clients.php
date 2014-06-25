52269a49deafdc5eed6dd41a8fea00689da15290
Edward <edward@roojs.com>
2014-06-25 12:12:34 +0800

    DataObjects/Core_oauth_clients.php

---
+++ DataObjects/Core_oauth_clients.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Core_oauth_clients
+ *
+ * @author edward
+ */
+class Core_oauth_clients {
+    //put your code here
+}
+
+?>
