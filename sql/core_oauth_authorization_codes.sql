56129dc78a5ad9833f6b2a37f4aaa050ede02621
Edward <edward@roojs.com>
2014-06-25 12:45:06 +0800

    sql/core_oauth_authorization_codes.sql

--- sql/core_oauth_authorization_codes.sql
+++ sql/core_oauth_authorization_codes.sql
@@ -1,10 +1,10 @@
 CREATE TABLE core_oauth_authorization_codes (
+    id INT(11) NOT NULL AUTO_INCREMENT,
     authorization_code VARCHAR(40) NOT NULL, 
     client_id VARCHAR(80) NOT NULL, 
     user_id VARCHAR(255), 
     redirect_uri VARCHAR(2000), 
     expires TIMESTAMP NOT NULL, 
-    scope VARCHAR(2000), 
-    CONSTRAINT auth_code_pk 
-    PRIMARY KEY (authorization_code)
+    scope VARCHAR(2000),
+    PRIMARY KEY (id)
 );
