3af637c945b628ee3b15435d57b8707bc0ddf77d
Edward <edward@roojs.com>
2014-06-25 12:33:06 +0800

    sql/core_oauth_clients.sql

--- sql/core_oauth_clients.sql
+++ sql/core_oauth_clients.sql
@@ -1,10 +1,10 @@
 CREATE TABLE core_oauth_clients (
+    id INT(11) NOT NULL AUTO_INCREMENT ,
     client_id VARCHAR(80) NOT NULL,
     client_secret VARCHAR(80) NOT NULL, 
     redirect_uri VARCHAR(2000) NOT NULL, 
     grant_types VARCHAR(80), 
     scope VARCHAR(100), 
-    user_id VARCHAR(80), 
-    CONSTRAINT client_id_pk 
-    PRIMARY KEY (client_id)
+    user_id VARCHAR(80),
+    PRIMARY KEY (id)
 );
