81dd31ec0fa8b809084198785b6f9097941fabb5
Edward <edward@roojs.com>
2014-06-25 12:46:41 +0800

    sql/core_oauth_refresh_tokens.sql

--- sql/core_oauth_refresh_tokens.sql
+++ sql/core_oauth_refresh_tokens.sql
@@ -1,9 +1,9 @@
 CREATE TABLE core_oauth_refresh_tokens (
+    id INT(11) NOT NULL AUTO_INCREMENT,
     refresh_token VARCHAR(40) NOT NULL, 
     client_id VARCHAR(80) NOT NULL, 
     user_id VARCHAR(255), 
     expires TIMESTAMP NOT NULL, 
-    scope VARCHAR(2000), 
-    CONSTRAINT refresh_token_pk 
-    PRIMARY KEY (refresh_token)
+    scope VARCHAR(2000),
+    PRIMARY KEY (id)
 );
