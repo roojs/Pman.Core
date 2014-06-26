42cf0851c13a7e8443c2705d0ae5ffdfe0ae29bb
Edward <edward@roojs.com>
2014-06-26 18:25:03 +0800

    mysql/core_cities_merge.sql

--- mysql/core_cities_merge.sql
+++ mysql/core_cities_merge.sql
@@ -286,13 +286,7 @@ CREATE FUNCTION core_country_locations()  RETURNS INT DETERMINISTIC
                 END IF;
                 
             END IF;
-
-            
-
-            IF(v_id_tmp = 0) THEN
-                INSERT INTO core_geoip_country (code, name, continent_id) VALUES (v_iso, v_local_name, 0);
-            END IF;
-                
+    
             IF v_count = v_total THEN
               LEAVE co_loop;
             END IF;
