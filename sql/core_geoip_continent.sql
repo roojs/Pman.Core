d2d8d43e352d6d356270fc3b73de300e4b52afcd
Edward <edward@roojs.com>
2014-06-27 16:47:03 +0800

    sql/core_geoip_continent.sql

--- sql/core_geoip_continent.sql
+++ sql/core_geoip_continent.sql
@@ -1,9 +1,11 @@
-CREATE  TABLE core_geoip_continent (
-    id INT(11) NOT NULL AUTO_INCREMENT ,
-    code VARCHAR(32) NOT NULL DEFAULT '',
-    name VARCHAR(255) NOT NULL DEFAULT '',
-    PRIMARY KEY (id)
-);
+DROP TABLE IF EXISTS core_geoip_continent;
 
-CREATE INDEX code_idx ON core_geoip_continent (code) USING BTREE;
-CREATE INDEX name_idx ON core_geoip_continent (name) USING BTREE;
+-- CREATE  TABLE core_geoip_continent (
+--     id INT(11) NOT NULL AUTO_INCREMENT ,
+--     code VARCHAR(32) NOT NULL DEFAULT '',
+--     name VARCHAR(255) NOT NULL DEFAULT '',
+--     PRIMARY KEY (id)
+-- );
+-- 
+-- CREATE INDEX code_idx ON core_geoip_continent (code) USING BTREE;
+-- CREATE INDEX name_idx ON core_geoip_continent (name) USING BTREE;
