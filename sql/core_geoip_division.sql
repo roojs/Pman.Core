7b7634740841116ad1c176a6dc78535b2efa143b
Edward <edward@roojs.com>
2014-06-27 16:47:46 +0800

    sql/core_geoip_division.sql

--- sql/core_geoip_division.sql
+++ sql/core_geoip_division.sql
@@ -1,13 +1,15 @@
-CREATE  TABLE core_geoip_division (
-    id INT(11) NOT NULL AUTO_INCREMENT ,
-    code VARCHAR(32) NOT NULL DEFAULT '',
-    name VARCHAR(255) NOT NULL DEFAULT '',
-    country_id INT(11) NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
-);
+DROP TABLE IF EXISTS core_geoip_division;
 
-CREATE INDEX code_idx ON core_geoip_division (code) USING BTREE;
-CREATE INDEX name_idx ON core_geoip_division (name) USING BTREE;
-CREATE INDEX country_id_idx ON core_geoip_division (country_id) USING BTREE;
-
-CREATE INDEX cdoe_country_id_idx ON core_geoip_division (code,country_id) USING BTREE;
+-- CREATE  TABLE core_geoip_division (
+--     id INT(11) NOT NULL AUTO_INCREMENT ,
+--     code VARCHAR(32) NOT NULL DEFAULT '',
+--     name VARCHAR(255) NOT NULL DEFAULT '',
+--     country_id INT(11) NOT NULL DEFAULT 0,
+--     PRIMARY KEY (id)
+-- );
+-- 
+-- CREATE INDEX code_idx ON core_geoip_division (code) USING BTREE;
+-- CREATE INDEX name_idx ON core_geoip_division (name) USING BTREE;
+-- CREATE INDEX country_id_idx ON core_geoip_division (country_id) USING BTREE;
+-- 
+-- CREATE INDEX cdoe_country_id_idx ON core_geoip_division (code,country_id) USING BTREE;
