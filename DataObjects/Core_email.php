37e5effead62ec78ff68e3ebe0fe84a829f84b70
Edward <edward@roojs.com>
2014-07-09 13:41:32 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -278,6 +278,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
         $cachePath = session_save_path() . '/email-cache-' . $ui['name'] . '/mail/' . $this->tableName() . '-' . $this->id . '.txt';
         
         if($force || !$this->isGenerated($cachePath)){
+            print_r($contents['replace_links']);exit;
             $this->cachedMailWithOutImages($force, empty($contents['replace_links']) ? false : $contents['replace_links']);
         }
          
