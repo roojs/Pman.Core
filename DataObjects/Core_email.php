3668cab5f5c9d2dfe49a46665ac36ba4e8454f3a
Edward <edward@roojs.com>
2014-08-05 12:32:01 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -331,7 +331,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
 
         fwrite($fh, implode("\n", array(
             "From: {if:t.messageFrom}{t.messageFrom:h}{else:}{t.messageFrom():h}{end:}",
-            "To: {if:t.person}{t.person.getEmailFrom():h}{else:}{foreach:rcpts,v}{v:h}{end:}{end:}",
+            "To: {if:t.person}{t.person.getEmailFrom():h}{else:}{foreach:rcpts,v}{v:h},{end:}{end:}",
             "Subject: {t.subject} ",
             "X-Message-ID: {t.id} ",
             "{if:t.replyTo}Reply-To: {t.replyTo:h}{end:}"
