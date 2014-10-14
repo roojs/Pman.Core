69c056f76e64aacd70b029957207d03fa638a7cd
Edward <edward@roojs.com>
2014-10-14 13:23:14 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -247,7 +247,7 @@ class Pman_Core_Mailer {
             
             $isMime = true;
         }
-        print_R($mime->get());exit;
+        print_R($mime->getHTMLBody());exit;
         if($isMime){
             $parts[2] = $mime->get();
             $parts[1] = $mime->headers($parts[1]);
