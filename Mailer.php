4696c9a432b0f2138b6100eb9d6dc17492d21bb9
Edward <edward@roojs.com>
2014-10-14 13:20:38 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -253,7 +253,7 @@ class Pman_Core_Mailer {
             $parts[1] = $mime->headers($parts[1]);
         }
 //        echo '<PRE>';
-//        print_r('parts');
+        print_r('parts');
 //        print_r($parts[2]);
 //        exit;
        // list($recipents,$headers,$body) = $parts;
