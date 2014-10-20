578b4a47423513504f714467ac732ba3818efc03
Edward <edward@roojs.com>
2014-10-20 16:00:17 +0800

    UpdateCurrencyRate.php

--- UpdateCurrencyRate.php
+++ UpdateCurrencyRate.php
@@ -84,7 +84,9 @@ class Pman_Core_UpdateCurrencyRate extends Pman
         
         $elements = $xpath->query("table/tr/td/pre", $ts->item(0));
         
-        $rate = str_getcsv($elements->item(0)->nodeValue, ",", "\"", "\\");
+        ini_set("auto_detect_line_endings", true);
+        
+        $rate = str_getcsv($elements->item(0)->nodeValue, ",", "\"");
         
         print_r($rate);exit;
         
