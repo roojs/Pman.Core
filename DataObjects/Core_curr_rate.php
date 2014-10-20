344f98c7f3119bb43a7ac5cb94c6269faf5c8638
Edward <edward@roojs.com>
2014-10-20 13:30:29 +0800

    DataObjects/Core_curr_rate.php

--- DataObjects/Core_curr_rate.php
+++ DataObjects/Core_curr_rate.php
@@ -1,7 +1,27 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Table Definition for core_curr_rate
  */
-?>
+require_once 'DB/DataObject.php';
+
+class Pman_Core_DataObjects_Core_curr_rate extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+  
+    public $__table = 'core_curr_rate';    // table name
+    public $id;
+    public $curr;
+    public $rate;
+    public $from;
+    public $to;
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+    
+    function applyFilters($q, $au, $roo)
+    {
+        
+    }
+    
+}
