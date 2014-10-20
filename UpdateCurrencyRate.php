c9c60b8f6b8a6fbd05eaff2d6cd8b5e9bef6c272
Edward <edward@roojs.com>
2014-10-20 16:14:37 +0800

    UpdateCurrencyRate.php

--- UpdateCurrencyRate.php
+++ UpdateCurrencyRate.php
@@ -82,7 +82,7 @@ class Pman_Core_UpdateCurrencyRate extends Pman
         
         $elements = $xpath->query("//td[@id='content_section']/table/tr[last()]/td/table");
         
-        print_r($elements);exit;
+        print_r($elements->item(0));exit;
         
     }
     
