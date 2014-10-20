c9f7d1212d3eb991bda63cc79b399a59d669dad6
Edward <edward@roojs.com>
2014-10-20 15:30:05 +0800

    UpdateCurrencyRate.php

--- UpdateCurrencyRate.php
+++ UpdateCurrencyRate.php
@@ -80,6 +80,8 @@ class Pman_Core_UpdateCurrencyRate extends Pman
         
         $xpath = new DOMXpath($doc);
         
+        $ts = $xpath->query("//table[@id='converter_table']");
+        echo $ts->item(0);exit;
         $elements = $xpath->query("//table[@id='converter_table']");
         
         foreach($elements as $el) {
