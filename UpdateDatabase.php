27916bdf4a16eb82a0d36369d94d1882a426a20a
Edward <edward@roojs.com>
2014-10-31 16:13:50 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -290,7 +290,7 @@ class Pman_Core_UpdateDatabase extends Pman
         //usort($files, $lsort);
         
         if($dir = '/home/edward/gitlive/web.registrar/Pman/Registrar/pgsql'){
-            print_R('run');exit;
+            print_R($files);exit;
         }
         
         foreach($files as $bfn) {
