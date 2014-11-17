471371639d24708ab92026d78f85880efa5aa298
Alan Knowles <alan@roojs.com>
2014-11-17 14:49:47 +0800

    mysql/core_enum_display_name.sql

---
+++ mysql/core_enum_display_name.sql
@@ -0,0 +1,13 @@
+
+DROP FUNCTION IF EXISTS core_enum_display_name;
+DELIMITER $$
+CREATE FUNCTION core_enum_display_name(in_id INT(11))
+        RETURNS VARCHAR(256) DETERMINISTIC
+    BEGIN
+        DECLARE ret  VARCHAR(256);
+        SET ret  = '';
+        SELECT display_name INTO ret FROM core_enum
+            WHERE id=in_id LIMIT 1;
+        RETURN ret;
+    END $$
+DELIMITER ;
