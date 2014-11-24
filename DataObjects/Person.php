d1a3613949ea47ae1f0f350f8d96aa19ef1ba6cb
Edward <edward@roojs.com>
2014-11-24 15:59:28 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -615,7 +615,6 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
         $isOwner = $this->company()->comptype == 'OWNER';
         $g = DB_DataObject::Factory('group_members');
         $grps = $g->listGroupMembership($this);
-        print_R($grps);exit;
        //var_dump($grps);
         $isAdmin = $g->inAdmin;
         //echo '<PRE>'; print_r($grps);var_dump($isAdmin);
