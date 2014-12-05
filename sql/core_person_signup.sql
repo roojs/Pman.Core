76b76a4bc2e1f82829553633a51a92a4dffe8d38
Alan Knowles <alan@roojs.com>
2014-12-05 15:36:32 +0800

    sql/core_person_signup.sql

--- sql/core_person_signup.sql
+++ sql/core_person_signup.sql
@@ -3,19 +3,19 @@
 -- used to store signups - before they have been verified.
 -- once the verified link has been pressed, use a add event, copy to a person, and remove record..
 
-CREATE TABLE Person (
+CREATE TABLE core_person_signup (
   id int(11) NOT NULL auto_increment,
   PRIMARY KEY  (id)
 ) ;
 
 
-ALTER TABLE Person ADD COLUMN   name varchar(128)  NOT NULL  DEFAULT '';
-ALTER TABLE Person ADD COLUMN   honor varchar(32) NOT NULL DEFAULT '';
-ALTER TABLE Person ADD COLUMN   firstname varchar(128) NOT NULL DEFAULT '';
-ALTER TABLE Person ADD COLUMN   lastname varchar(128) NOT NULL DEFAULT '';
-ALTER TABLE Person ADD COLUMN   firstname_alt varchar(128) NOT NULL DEFAULT '';
-ALTER TABLE Person ADD COLUMN   lastname_alt varchar(128) NOT NULL DEFAULT '';
+ALTER TABLE core_person_signup ADD COLUMN   name varchar(128)  NOT NULL  DEFAULT '';
+ALTER TABLE core_person_signup ADD COLUMN   honor varchar(32) NOT NULL DEFAULT '';
+ALTER TABLE core_person_signup ADD COLUMN   firstname varchar(128) NOT NULL DEFAULT '';
+ALTER TABLE core_person_signup ADD COLUMN   lastname varchar(128) NOT NULL DEFAULT '';
+ALTER TABLE core_person_signup ADD COLUMN   firstname_alt varchar(128) NOT NULL DEFAULT '';
+ALTER TABLE core_person_signup ADD COLUMN   lastname_alt varchar(128) NOT NULL DEFAULT '';
 
-ALTER TABLE Person ADD COLUMN   email varchar(256)  NOT NULL DEFAULT '';
-ALTER TABLE Person ADD COLUMN   verify_key varchar(256)  NOT NULL DEFAULT '';
-ALTER TABLE Person ADD COLUMN   created_dt DATETIME  NOT NULL;
+ALTER TABLE core_person_signup ADD COLUMN   email varchar(256)  NOT NULL DEFAULT '';
+ALTER TABLE core_person_signup ADD COLUMN   verify_key varchar(256)  NOT NULL DEFAULT '';
+ALTER TABLE core_person_signup ADD COLUMN   created_dt DATETIME  NOT NULL;
