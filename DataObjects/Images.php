94f3670404aaeda243bbd3d01a575e86af1e8f40
Edward <edward@roojs.com>
2014-12-15 15:13:04 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -357,7 +357,7 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
         }
         
         $c = clone($this);
-        $c->whereAddIn($this->tableName() . '.ontable1', array( $obj->tableName(), $obj->__table) , 'string');
+        $c->whereAddIn($this->tableName() . '.ontable', array( $obj->tableName(), $obj->__table) , 'string');
         $c->onid = $obj->id;
         $c->autoJoin();
         if (!empty($mime_like)) {
