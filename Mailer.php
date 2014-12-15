68f32e3dc7888c4da5be70011192989c09b34c28
Edward <edward@roojs.com>
2014-12-15 16:48:40 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -457,15 +457,15 @@ class Pman_Core_Mailer {
         $uinfo = posix_getpwuid( posix_getuid () ); 
         $user = $uinfo['name']; 
         
-//        $cache = ini_get('session.save_path')."/Pman_Core_Mailer-{$user}/" . md5($url);
+        $cache = ini_get('session.save_path')."/Pman_Core_Mailer-{$user}/" . md5($url);
 //        if (file_exists($cache) and filemtime($cache) > strtotime('NOW - 1 WEEK')) {
 //            $ret =  json_decode(file_get_contents($cache), true);
 //            $ret['file'] = $cache . '.data';
 //            return $ret;
 //        }
-//        if (!file_exists(dirname($cache))) {
-//            mkdir(dirname($cache),0700, true);
-//        }
+        if (!file_exists(dirname($cache))) {
+            mkdir(dirname($cache),0700, true);
+        }
         
         require_once 'HTTP/Request.php';
         $a = new HTTP_Request($url);
