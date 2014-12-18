f16ce37fa3ce72b205713400a8db7f177c0330ba
Edward <edward@roojs.com>
2014-12-18 10:58:14 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -37,6 +37,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
     {
         $tn = $this->tableName();
         // if not empty on_table
+        $q['person_table'] = 'cntct';
         if(!empty($q['person_table'])){
             $jt = DB_DataObject::factory($q['person_table']);
         
