6a326b5af531dbdb5c3f2de932326f39115e38af
Edward <edward@roojs.com>
2014-12-18 11:15:29 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -36,7 +36,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
     function applyFilters($q, $au ,$roo)
     {
         $tn = $this->tableName();
-        print_r($_DB_DATAOBJECT);exit;
+        print_r($this->databaseStructure());exit;
         // if not empty on_table
         if(!empty($q['person_table'])){
             $jt = DB_DataObject::factory($q['person_table']);
