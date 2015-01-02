67ba153229dc9bedcc06fff73cbd4f5ffb0f8ff4
Micheal <micheal@roojs.com>
2015-01-02 16:04:29 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -167,7 +167,7 @@ class Pman_Core_Mailer {
         
         /* use variables from this object to ouput data. */
         $mailtext = $template->bufferedOutputObject($content);
-        //print_r($mailtext);exit;
+        print_r($mailtext);exit;
        
         
         
