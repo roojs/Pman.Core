6a24c6a0d8c7e8310eaea37e03afa9fd99483c43
Micheal <micheal@roojs.com>
2015-01-02 15:19:32 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -282,7 +282,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
     function send($obj, $force = true, $send = true)
     {   
         $contents = (array)$obj;
-        
+        var_dump($this);
         if(empty($this->id)){
             $this->get('name', $contents['template']);
         }
