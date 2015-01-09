b9cdad98c7376bd412c01af727ac4c12032e7023
Alan Knowles <alan@roojs.com>
2015-01-09 14:30:06 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -289,7 +289,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
         
         if(empty($this->id)){
             $p = new PEAR();
-            return $p->raiseError("template [{$contents['template']}]has not been set");
+            return $p->raiseError("template [{$contents['template']}] has not been set");
         }
         if(empty($contents['subject'])){
            $contents['subject'] = $this->subject; 
