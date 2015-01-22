2aac97541be6f07c59afc738d6aa3b61ba23e05a
Edward <edward@roojs.com>
2015-01-22 16:23:42 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -249,7 +249,7 @@ class Pman_Core_NotifySend extends Pman
             $w->msgid = '';
             $w->event_id = $ev->id;
             $w->update($ww);
-            die(date('Y-m-d h:i:s ') . "INVALID ADDRESS1: " . $p->email. "\n");
+            die(date('Y-m-d h:i:s ') . "INVALID ADDRESS: " . $p->email. "\n");
             
         }
         
