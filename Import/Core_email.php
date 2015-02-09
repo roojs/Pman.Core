790d376e63eb2da24ba249ec111e19fbcafc14e2
Alan Knowles <alan@roojs.com>
2015-02-09 17:57:26 +0800

    Import/Core_email.php

--- Import/Core_email.php
+++ Import/Core_email.php
@@ -6,6 +6,70 @@ require_once 'Pman/Roo.php';
 
 class Pman_Hydra_Import_Core_email extends Pman_Roo
 {
+       static $cli_desc = "Import an email into core_email template"; 
+    
+    static $cli_opts = array(
+        'file' => array(
+            'desc' => 'Turn on debugging (see DataObjects debugLevel )',
+            'default' => 0,
+            'short' => 'v',
+            'min' => 1,
+            'max' => 1,
+            
+        ),
+        'list' => array(
+            'desc' => 'List message to send, do not send them..',
+            'default' => 0,
+            'short' => 'l',
+            'min' => 0,
+            'max' => 0,
+            
+        ),
+        'old' => array(
+            'desc' => 'Show old messages.. (and new messages...)',
+            'default' => 0,
+            'short' => 'o',
+            'min' => 0,
+            'max' => 0,
+            
+        ),
+        'force' => array(
+            'desc' => 'Force redelivery, even if it has been sent before or not queued...',
+            'default' => 0,
+            'short' => 'f',
+            'min' => 0,
+            'max' => 0,
+        ),
+        'generate' => array(
+            'desc' => 'Generate notifications for a table, eg. cash_invoice',
+            'default' => '',
+            'short' => 'g',
+            'min' => 0,
+            'max' => 1,
+        ),
+         'limit' => array(
+            'desc' => 'Limit search for no. to send to ',
+            'default' => 1000,
+            'short' => 'L',
+            'min' => 0,
+            'max' => 999,
+        ),
+        'dryrun' => array(
+            'desc' => 'Dry run - do not send.',
+            'default' => 0,
+            'short' => 'D',
+            'min' => 0,
+            'max' => 0,
+        ),
+        'poolsize' => array(
+            'desc' => 'Pool size',
+            'default' => 10,
+            'short' => 'P',
+            'min' => 0,
+            'max' => 100,
+        ),
+    );
+    
     function getAuth()
     {
         $ff = HTML_FlexyFramework::get();
