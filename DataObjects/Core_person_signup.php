334d02c2f17634d38771be3e3d313ee9948d5b83
Edward <edward@roojs.com>
2015-02-24 16:27:27 +0800

    DataObjects/Core_person_signup.php

--- DataObjects/Core_person_signup.php
+++ DataObjects/Core_person_signup.php
@@ -1,9 +1,9 @@
 <?php
+
 /**
  * Table Definition for Person
  */
 require_once 'DB/DataObject.php';
-
 
 class Pman_Core_DataObjects_Core_person_signup extends DB_DataObject 
 {
@@ -13,50 +13,41 @@ class Pman_Core_DataObjects_Core_person_signup extends DB_DataObject
     public $__table = 'core_person_signup';                          // table name
     public $id;                              // int(11)  not_null primary_key auto_increment
     public $email;                           // string(128)  not_null
-    
     public $name;                            // string(128)  not_null
     public $firstname;                            // string(128)  not_null
     public $lastname;                            // string(128)  not_null
     public $firstname_alt;                            // string(128)  not_null
     public $lastname_alt;                            // string(128)  not_null
-    
-    
     public $honor;                            // string(128)  not_null
     public $verify_key;                      // int(11)
     public $verified;
-    
-
     public $created_dt;                      // datetime(19)  binary
 
-    
-    
-    
-    
-    function verify($key)
+    function verify($key) 
     {
         // if key matches verify_key
         // copy into person or other entity...
         // and delete....
-        if($this->get("verify_key",$key)){
+        if ($this->get("verify_key", $key)) {
             $p = DB_DataObject::factory('person');
             $p->setFrom(array(
-                'honor'=>$this->honor,
-                'name'=>$this->name,
-                'email'=>$this->email,
-                'firstname'=>$this->firstname,
-                'lastname'=>$this->lastname,
-                'firstname_alt'=>$this->firstname_alt,
-                'lastname_alt'=>$this->lastname_alt));
+                'honor' => $this->honor,
+                'name' => $this->name,
+                'email' => $this->email,
+                'firstname' => $this->firstname,
+                'lastname' => $this->lastname,
+                'firstname_alt' => $this->firstname_alt,
+                'lastname_alt' => $this->lastname_alt));
 
-            if($p->insert()){
-                
+            if ($p->insert()) {
+
                 $temp_pwd = $p->generatePassword();
 
                 $this->delete();
 
                 //login
                 @session_start();
-        
+
                 $_SESSION['Hydra']['authUser'] = $p ? serialize($p) : false;
 
                 //mail pwd
@@ -65,43 +56,46 @@ class Pman_Core_DataObjects_Core_person_signup extends DB_DataObject
                     $this->jerr("can not find template");
                 }
                 $ret = $c->send(array(
-                   'rcpts' => $this->email,
-                   'honor' => $this->honor.". ".$this->lastname,
-                   'password' => $temp_pwd
-                ), true);
-         
+                    'rcpts' => $this->email,
+                    'honor' => $this->honor . ". " . $this->lastname,
+                    'password' => $temp_pwd
+                        ), true);
+
                 if (is_object($ret)) {
                     return false;
                 }
                 return true;
-            }else{
+            } else {
                 return false;
-            }   
+            }
         }
         return false;
-        
     }
 
-
-    function convertTo($table){
+    function convertTo($table) 
+    {
         $t = DB_DataObject::factory($table);
-        $ret = $t->get('email',$this->email);
-        if($ret != 0){
+        $ret = $t->get('email', $this->email);
+        if ($ret != 0) {
             return false;
-        }else{
+        } else {
             $t->setFrom(array(
-                'honor'=>$this->honor,
-                'name'=>$this->name,
-                'email'=>$this->email,
-                'firstname'=>$this->firstname,
-                'lastname'=>$this->lastname,
-                'firstname_alt'=>$this->firstname_alt,
-                'lastname_alt'=>$this->lastname_alt));
+                'honor' => $this->honor,
+                'name' => $this->name,
+                'email' => $this->email,
+                'firstname' => $this->firstname,
+                'lastname' => $this->lastname,
+                'firstname_alt' => $this->firstname_alt,
+                'lastname_alt' => $this->lastname_alt));
 
             $t->insert();
             return true;
         }
     }
+
+    function sendMail()
+    {
+        
+    }
 }
 
-    
