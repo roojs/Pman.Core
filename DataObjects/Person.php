baf8dff29c09a487f8eec88eaea000b1552df100
Alan Knowles <alan@roojs.com>
2015-03-02 15:54:17 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -452,7 +452,7 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
         $ff= HTML_FlexyFramework::get();
         $sesPrefix = $ff->appNameShort .'-' .get_class($this) .'-'.$db->dsn['database'] ;
 
-
+        @session_start();
         $_SESSION[get_class($this)][$sesPrefix .'-auth'] = serialize($this);
         
     }
@@ -462,7 +462,7 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
         $db = $this->getDatabaseConnection();
         $ff= HTML_FlexyFramework::get();
         $sesPrefix = $ff->appNameShort .'-' .get_class($this) .'-'.$db->dsn['database'] ;
-
+        @session_start();
         $_SESSION[get_class($this)][$sesPrefix .'-auth'] = "";
        
         
