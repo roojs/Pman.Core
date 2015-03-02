1d1653e55aad5b2a083b6a651c5dea70e127a07d
Alan Knowles <alan@roojs.com>
2015-03-02 16:25:06 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -265,14 +265,13 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
         if (!empty($_SESSION[get_class($this)][$sesPrefix .'-auth'])) {
             // in session...
             $a = unserialize($_SESSION[get_class($this)][$sesPrefix .'-auth']);
-            
+            tableName
             $u = DB_DataObject::factory($this->tableName());
             if ($a->id && $u->get($a->id)) { //&& strlen($u->passwd)) {
               
                 return $u->verifyAuth();
                 
-   
-                return true;
+    ;
             }
             
             unset($_SESSION[get_class($this)][$sesPrefix .'-auth']);
