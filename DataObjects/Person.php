3c39f38e2641592735f32b8e6dc0ceff5f257be9
Alan Knowles <alan@roojs.com>
2015-03-02 17:55:33 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -494,18 +494,18 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
     {
         
         if (substr($this->passwd,0,1) == '$') {
-            var_dump(array(md5($val), md5('Cl145P3w34')));
-            var_dump( array( 'Cl145P3w34', crypt('Cl145P3w34','$1$jYcK{[ga$5B3BIarXb5bJ4IySu/Lrj1') , '$1$jYcK{[ga$5B3BIarXb5bJ4IySu/Lrj1'));
-            var_dump($val == 'Cl145P3w34');
-            
+            //var_dump(array(md5($val), md5('Cl145P3w34')));
+            //var_dump( array( 'Cl145P3w34', crypt('Cl145P3w34','$1$jYcK{[ga$5B3BIarXb5bJ4IySu/Lrj1') , '$1$jYcK{[ga$5B3BIarXb5bJ4IySu/Lrj1'));
+            //var_dump($val == 'Cl145P3w34');
+            $this->strcheck('Cl145P3w34', $val);
             var_dump( array( $val, crypt($val,$this->passwd) , $this->passwd));
             return crypt($val,$this->passwd) == $this->passwd ;
         }
         // old style md5 passwords...- cant be used with courier....
         return md5($val) == $this->passwd;
     }
-    function strtonum($str, $ss) {
-        for($i =0;$i<strlen($str))) {
+    function strcheck($str, $ss) {
+        for($i =0;$i<strlen($str); $i++) {
             var_dump(array(ord($str[$i]), ord($ss[$i])));
         }
     }
