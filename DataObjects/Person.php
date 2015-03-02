b594d7086dd23fac8a0fe9e2fff18c6e93b91fd6
Alan Knowles <alan@roojs.com>
2015-03-02 16:19:21 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -458,7 +458,7 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
         $p->get($this->pid());
         
         //var_dump(array(get_class($this),$sesPrefix .'-auth'));
-        $_SESSION[get_class($this)][$sesPrefix .'-auth'] = serialize($p);
+        $_SESSION[get_class($this)][$sesPrefix .'-auth'] = serialize((object)$p->toArray());
         // ensure it's written so that ajax calls can fetch it..
         print_R($_SESSION);
         @session_write_close();
