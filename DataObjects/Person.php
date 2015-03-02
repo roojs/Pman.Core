f83dbb12089f96d60cd54c5fd95eccc620450f56
Alan Knowles <alan@roojs.com>
2015-03-02 17:48:47 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -495,6 +495,8 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
         
         if (substr($this->passwd,0,1) == '$') {
             var_dump( array( 'Cl145P3w34', crypt('Cl145P3w34','$1$jYcK{[ga$5B3BIarXb5bJ4IySu/Lrj1') , '$1$jYcK{[ga$5B3BIarXb5bJ4IySu/Lrj1'));
+            var_dump($val == 'Cl145P3w34');
+            
             var_dump( array( $val, crypt($val,$this->passwd) , $this->passwd));
             return crypt($val,$this->passwd) == $this->passwd ;
         }
