f6c7fbe5e5386745f82d98f73338681d07354553
Alan Knowles <alan@roojs.com>
2015-04-16 15:24:35 +0800

    sql/events.sql

--- sql/events.sql
+++ sql/events.sql
@@ -37,3 +37,5 @@ ALTER TABLE Events CHANGE COLUMN action action varchar(64)  default NULL;
 
 ALTER TABLE Events ADD INDEX lookupf (on_id, action, on_table, person_id, event_when, person_table);
   
+  
+ALTER TABLE Events ENGINE=InnoDB;
