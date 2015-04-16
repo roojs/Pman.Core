112ab70860e21401f4bf7fc20d25e7f9f1270b90
Alan Knowles <alan@roojs.com>
2015-04-16 15:42:17 +0800

    mysql/core_notify_recur_trigger_core_before_insert_method_id.sql

--- mysql/core_notify_recur_trigger_core_before_insert_method_id.sql
+++ mysql/core_notify_recur_trigger_core_before_insert_method_id.sql
@@ -16,18 +16,8 @@ CREATE PROCEDURE core_notify_recur_trigger_core_before_insert_method_id ( i_id I
     
  END;
  
-
--- see if this can be done as a trigger
-
-CREATE DEFINER=`root`@`localhost` TRIGGER `core_notify_recur_before_insert_core`
-                BEFORE INSERT ON `core_notify_recur`
-    FOR EACH ROW
-    
-       IF (NEW.method_id > 0) THEN
-           CALL core_enum_trigger_check('core_notify_recur', NEW.method_id);
-       END IF;
-            
-    END;
 $$
+-- see if this can be done as a trigger
+ 
 
 DELIMITER ;
