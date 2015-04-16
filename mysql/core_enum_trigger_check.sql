b03523e2869e93533c1025ccf4a205f346069df1
Alan Knowles <alan@roojs.com>
2015-04-16 13:25:21 +0800

    mysql/core_enum_trigger_check.sql

--- mysql/core_enum_trigger_check.sql
+++ mysql/core_enum_trigger_check.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE core_enum_trigger_check (i_etype VARCHAR(128) , i_id INT)
  BEGIN
     DECLARE v_cnt INT;
     SET v_cnt = 0;
-    SELECT count(*) INTO v_cnt WHERE etype = i_etype, id = i_id;
+    SELECT count(*) INTO v_cnt FROM core_enum WHERE etype = i_etype, id = i_id;
     IF (v_cnt < 1) THEN
          UPDATE `Core Enum Does not exist` SET x = 1;
  END;
