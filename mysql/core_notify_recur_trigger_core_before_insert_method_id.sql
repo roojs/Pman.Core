da0b2b4d1535cb335c04911287ef2c05b1b2fb4c
Alan Knowles <alan@roojs.com>
2015-04-16 13:30:10 +0800

    MOVED mysql/core_notify_recur_trigger_core_before_insert.sql to mysql/core_notify_recur_trigger_core_before_insert_method_id.sql

---
+++ mysql/core_notify_recur_trigger_core_before_insert_method_id.sql
@@ -0,0 +1,26 @@
+
+
+
+core_notify_recur_trigger_core_before_insert
+
+
+
+DROP PROCEDURE IF EXISTS core_notify_recur_trigger_core_before_insert;
+
+DELIMITER $$
+ 
+CREATE PROCEDURE core_enum_trigger_check (i_etype VARCHAR(128) , i_id INT)
+ BEGIN
+    DECLARE v_cnt INT;
+    SET v_cnt = 0;
+    -- should this check active???
+    
+    SELECT count(*) INTO v_cnt FROM core_enum WHERE etype = i_etype AND  id = i_id;
+    
+    IF (v_cnt < 1) THEN
+        UPDATE `Core Enum Does not exist` SET x = 1;
+    END IF;
+ END;
+$$
+
+DELIMITER ;
