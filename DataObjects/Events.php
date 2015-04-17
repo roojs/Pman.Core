0798b69ff302d239acfa20aa014f3b8d4db0861b
Edward <edward@roojs.com>
2015-04-17 16:30:35 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -423,7 +423,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
     
     function writeEventLog($extra_data  = '')
     {
-        print_R($deleted);exit;
+        print_R($this->deleted);exit;
         $ff  = HTML_FlexyFramework::get();
         if (empty($ff->Pman['event_log_dir'])) {
             return false;
