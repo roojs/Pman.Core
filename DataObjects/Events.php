6e0c6098788c6fa84b887a43b75e708eba101a93
Edward <edward@roojs.com>
2015-04-17 17:41:36 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -492,6 +492,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
             'GET' => empty($_GET) ? array() : $_GET,
             'POST' =>$p,
             'FILES' => $files,
+            'RELATED_DATAOBJECT' => $deleted
         );
         if (!empty($extra_data)) {
             $out['EXTRA'] = $extra_data;
