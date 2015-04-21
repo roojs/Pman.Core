3e365456e8de42d18e79a9e63aa9631393aefe0f
Alan Knowles <alan@roojs.com>
2015-04-21 18:18:22 +0800

    sql/projectdirectory.sql

--- sql/projectdirectory.sql
+++ sql/projectdirectory.sql
@@ -15,6 +15,7 @@ ALTER TABLE  ProjectDirectory ADD COLUMN   ispm int(11) NOT NULL DEFAULT 0;
 ALTER TABLE  ProjectDirectory ADD COLUMN   role varchar(32) NOT NULL DEFAULT '';
 
 ALTER TABLE  ProjectDirectory ADD COLUMN   company_id int(11) NOT NULL DEFAULT 0;
+ALTER TABLE  ProjectDirectory ADD COLUMN   office_id int(11) NOT NULL DEFAULT 0;
 
 
 ALTER TABLE ProjectDirectory ADD INDEX plookup (project_id,person_id, ispm, role);
