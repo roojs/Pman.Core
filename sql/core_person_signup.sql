c938faf75b4a8658fe54546d447e24ad41199891
Edward <edward@roojs.com>
2015-04-24 15:13:21 +0800

    sql/core_person_signup.sql

--- sql/core_person_signup.sql
+++ sql/core_person_signup.sql
@@ -28,4 +28,6 @@ ALTER TABLE core_person_signup ADD COLUMN   person_table TEXT NOT NULL  default 
 
 ALTER TABLE core_person_signup ADD COLUMN   inviter_id int(11) NOT NULL  default 0;
 
+ALTER TABLE core_person_signup ADD COLUMN   crm_mailing_list_id int(11) NOT NULL  default 0;
+
 ALTER TABLE core_person_signup ENGINE=InnoDB;
