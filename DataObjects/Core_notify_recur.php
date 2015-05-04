032a182471b2a42a40d070b2756653032b55c698
Edward <edward@roojs.com>
2015-05-04 16:09:02 +0800

    DataObjects/Core_notify_recur.php

--- DataObjects/Core_notify_recur.php
+++ DataObjects/Core_notify_recur.php
@@ -222,7 +222,7 @@ class Pman_Core_DataObjects_Core_notify_recur extends DB_DataObject
             $clipping_keywords = DB_DataObject::factory('clipping_keywords');
             $clipping_keywords->whereAddIn('id', $keywords, 'int');
             
-            $ret['keywords'] = $clipping_keywords->fetchAll('keyword');
+            $ret['keywords'] = implode(',', $clipping_keywords->fetchAll('keyword'));
         }
         
         return $ret;
