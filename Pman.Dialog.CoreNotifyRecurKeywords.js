cfc0b34b2445c3b9b090a42cd0840cc875460a20
Edward <edward@roojs.com>
2015-05-04 15:47:35 +0800

    Pman.Dialog.CoreNotifyRecurKeywords.bjs Pman.Dialog.CoreNotifyRecurKeywords.js

--- Pman.Dialog.CoreNotifyRecurKeywords.js
+++ Pman.Dialog.CoreNotifyRecurKeywords.js
@@ -263,7 +263,6 @@ Pman.Dialog.CoreNotifyRecurKeywords = {
            idField : 'id',
            listWidth : 200,
            minChars : 2,
-           nameField : 'display_name',
            queryParam : 'search[display_name]',
            tpl : '<div class=\"x-grid-cell-text x-btn button\"><b>{keyword}</b> </div>',
            triggerAction : 'all',
