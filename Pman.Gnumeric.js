c4c1fcfa00930edda3b982bc6464469d19af9482
Edward <edward@roojs.com>
2015-06-18 18:27:07 +0800

    Pman.Gnumeric.js

--- Pman.Gnumeric.js
+++ Pman.Gnumeric.js
@@ -1390,7 +1390,7 @@ Roo.extend(Pman.Gnumeric, Roo.util.Observable, {
                         value.length && 
                         !isNaN(value * 1)
                     ){
-                        value = new Date(value * 24 * 60 * 60 * 1000 + new Date('1900-01-01').getTime());
+                        value = new Date(value * 24 * 60 * 60 * 1000 + new Date('1900-01-01').getTime()).format('d-M-Y');
                     }
                     
                 } catch(e) {
