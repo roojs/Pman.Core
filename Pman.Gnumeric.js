d69b66248208bf8862eb06b82c2bded2d75628a3
Edward <edward@roojs.com>
2015-06-18 17:56:03 +0800

    Pman.Gnumeric.js

--- Pman.Gnumeric.js
+++ Pman.Gnumeric.js
@@ -1389,10 +1389,8 @@ Roo.extend(Pman.Gnumeric, Roo.util.Observable, {
 //                ){
 //                    
 //                }
-                if(typeof(g.styles[0].firstElementChild.attributes) != 'undefined'){
-                    for (var i = 0; i <= g.styles[0].firstElementChild.attributes.length; i++){
-                       // Roo.log(g.styles[0].firstElementChild.attributes[i]);
-                    }
+                if(typeof(g.styles[0].firstElementChild.attributes[12].value) != 'undefined'){
+                    Roo.log(g.styles[0].firstElementChild.attributes[12].value);
                 }
                 
                 
