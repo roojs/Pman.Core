6fc26e869614cd5fe41f21a21b29db8a326533bb
Edward <edward@roojs.com>
2015-07-14 16:34:39 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -638,7 +638,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
             $table->insert();
              
         }
-        
+        print_R($restored);exit;
         $roo->jok('RESTORE');
         
     }
