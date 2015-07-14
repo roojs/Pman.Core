5cc009bee67bec4098ed370542183dd739db2b5a
Edward <edward@roojs.com>
2015-07-14 15:56:40 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -657,7 +657,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
                     
                 }
             }
-            
+            print_r($affects);exit;
             unset($d['_table']);
             
             $table->setFrom($d);
