6e8bb7e147b6e0d3dfc89d21821083a279c78511
Alan Knowles <alan@roojs.com>
2015-08-20 10:37:27 +0800

    Notify.php

--- Notify.php
+++ Notify.php
@@ -192,7 +192,7 @@ class Pman_Core_Notify extends Pman
     
             $w->orderBy('act_when ASC'); // oldest first.
             
-            $this->logecho("QUEUE is {$w->count()}");
+            $this->logecho("QUEUE is {$w->count()} only runing " ((int) $opts['limit']));
             
             $w->limit($opts['limit']); // we can run 1000 ...
         }
