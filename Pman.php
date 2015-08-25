3644c5b8de234cfe34f93d49ad1324557fc0c2e5
Edward <edward@roojs.com>
2015-08-25 13:44:08 +0800

    Pman.php

--- Pman.php
+++ Pman.php
@@ -1,8 +1,19 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * this is loaded by the pman admin..
+ *
+ * it checks that required variables are set...
+ *
  */
-
+class Pman_Core_Pman {
+    
+    
+    function init ($pg)
+    {
+        
+    }
+    
+    
+    
+}
