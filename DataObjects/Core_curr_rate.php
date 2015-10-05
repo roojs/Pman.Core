ccf2a0f1fcf7804d555ab8de02700229d4649094
Alan Knowles <alan@roojs.com>
2015-10-05 17:18:20 +0800

    DataObjects/Core_curr_rate.php

--- DataObjects/Core_curr_rate.php
+++ DataObjects/Core_curr_rate.php
@@ -172,7 +172,7 @@ class Pman_Core_DataObjects_Core_curr_rate extends DB_DataObject
     function currentRates()
     {
         $this->loadRates();
-        DB_DataObject::debugLevel(1);
+       // DB_DataObject::debugLevel(1);
         $c = DB_DAtaObject::factory('core_curr_rate');
         $c->whereAdd('from_dt < NOW() AND to_dt > NOW()');
         $c->find();
