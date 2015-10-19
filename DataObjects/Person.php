eec698192a81756193163a16345c2b7feb72d8b3
Alan Knowles <alan@roojs.com>
2015-10-19 10:35:26 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -1149,6 +1149,7 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
         }
     }
     
+    // this is for the To "{getEmailName()}" <email@address>
     function getEmailName()
     {
         $name = array();
