e52c0ff51bee6caf98b314f4c2267553c27b6efe
edward <edward@roojs.com>
2015-11-10 11:38:19 +0800

    DataObjects/Core_notify_recur.php

--- DataObjects/Core_notify_recur.php
+++ DataObjects/Core_notify_recur.php
@@ -142,7 +142,7 @@ class Pman_Core_DataObjects_Core_notify_recur extends DB_DataObject
         
 
         $notifytimes = $this->notifyTimes(2);
-        ////print_R($notifytimes);
+        print_R($notifytimes);exit;
         
         $newSearch = DB_DataObject::factory('core_notify');
         $newSearch->whereAdd( 'act_start > NOW()');
