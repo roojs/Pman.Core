ba449c6620e4e359e57b4a184f622c1445689915
edward <edward@roojs.com>
2015-11-10 14:43:35 +0800

    Pman.Dialog.CoreEmail.bjs Pman.Dialog.CoreEmail.js

--- Pman.Dialog.CoreEmail.js
+++ Pman.Dialog.CoreEmail.js
@@ -1222,7 +1222,7 @@ Pman.Dialog.CoreEmail = {
              
              if(width > 50){
                  width = 50;
-                 height = Math.round(height *width / 50);
+                 height = Math.round(height * width / 50);
              }
              
             return '<img src="' + baseURL + '/Images/' + r.data.id + '/' + r.data.filename + '" width="' + width + '" height="' + height + '" />';
