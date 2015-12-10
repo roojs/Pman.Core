f3e9477ac4582d9b9d3b85af15828e732d9a89e5
edward <edward@roojs.com>
2015-12-10 14:20:19 +0800

    UpdateDatabase/MysqlLinks.php

--- UpdateDatabase/MysqlLinks.php
+++ UpdateDatabase/MysqlLinks.php
@@ -446,7 +446,7 @@ class Pman_Core_UpdateDatabase_MysqlLinks {
             echo "CALL mysql_change_charset('{$tbl}') \n";
             $ce = DB_DataObject::factory('core_enum');
             $ce->query("CALL mysql_change_charset('{$tbl}')");
-            $ce->fetch();
+            $ce->free();
         }
     }
     
