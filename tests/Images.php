7e2b484eed0d7e57055ee84330c13e326a2a1f68
Alan Knowles <alan@roojs.com>
2016-01-14 12:14:21 +0800

    tests/Images.php

--- tests/Images.php
+++ tests/Images.php
@@ -8,7 +8,10 @@ class Pman_Core_tests_Images extends Pman
         // test...
         echo '<PRE>';
         require_once 'Pman/Core/Images.php';
-        init();
+        //init();
+        
+        //$d = DB_DataObject::factory('')
+        
         $ret = Pman_Core_Images::replaceImageURLS('
                 <img src="http://www.roojs.com/index.php/Core/Images/Thumb/200x40/34/test.png">
                 <img src="http://www.roojs.com/index.php/Images/34/test.png" width="100">
