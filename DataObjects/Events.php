4004030f09d16e78c240731a766fb07874995053
edward <edward@roojs.com>
2016-02-16 15:45:19 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -492,9 +492,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
         $files = array();
          
         $i = 0;
-        print_R($_FILES);exit;
         foreach ($_FILES as $k=>$f){
-            
             // does not handle any other file[] arrary very well..
             if (empty($f['tmp_name']) || !file_exists($f['tmp_name'])) {
                 continue;
