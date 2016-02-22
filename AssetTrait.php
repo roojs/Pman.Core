ec538d02ced7350bea5cdbc5c801e77e786d11b1
Alan Knowles <alan@roojs.com>
2016-02-22 17:21:53 +0800

    AssetTrait.php

--- AssetTrait.php
+++ AssetTrait.php
@@ -240,7 +240,7 @@ trait Pman_Core_AssetTrait {
         
         
         $smod = str_replace('/','.',$path);
-        
+        var_dump($this->baseDir);exit;
         $output = date('Y-m-d-H-i-s-', $maxtime). $smod .'-'.md5(serialize(array($this->baseDir, $arfiles))) .'.css';
          
         $asset = $ff->project == 'Pman' ? '/Core/Asset/css/' : '/Asset/css/';
