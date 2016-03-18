71b0a0d2f46b7d540369815c490fd41f56937cf4
Alan Knowles <alan@roojs.com>
2016-03-18 10:32:31 +0800

    JsCompile.php

--- JsCompile.php
+++ JsCompile.php
@@ -415,7 +415,7 @@ class Pman_Core_JsCompile  extends Pman
         
         echo '<script type="text/javascript"> alert('. json_encode("Error: Javascript Compile failed\n" . $res) .');</script>';
         
-        echo "<!-- JS COMPILE ERROR: packed file did not exist  -->";
+        echo "\n<!-- JS COMPILE ERROR: packed file did not exist  -->\n";
         return false;
         
     }
