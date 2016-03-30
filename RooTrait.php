a4fa2d08104627b063946ea6ebfeb9aa5cc7e1c7
edward <edward@roojs.com>
2016-03-30 18:56:11 +0800

    RooTrait.php

--- RooTrait.php
+++ RooTrait.php
@@ -133,8 +133,6 @@ trait Pman_Core_RooTrait {
             return;
         }
         
-        $au = $this->getAuthUser();
-       
         $e = DB_DataObject::factory('Events');
         $e->init($act,$obj,$remarks); 
          
