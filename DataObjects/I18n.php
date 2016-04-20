9052dd7f5d765f24f4192f44cfbb186781211951
edward <edward@roojs.com>
2016-04-20 11:39:19 +0800

    DataObjects/I18n.php

--- DataObjects/I18n.php
+++ DataObjects/I18n.php
@@ -255,6 +255,7 @@ class Pman_Core_DataObjects_I18n extends DB_DataObject
                 require_once 'I18Nv2/PhonePrefix.php';
                 $c = new I18Nv2_PhonePrefix('en');
                 $ret =  array_keys($c->codes);
+                print_R($ret);exit;
                 if (!empty($cfg['add_p'])) {
                     $ret = array_merge($ret, array_keys($cfg['add_p']));
                 }
