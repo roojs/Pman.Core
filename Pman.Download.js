a7958d1d49bdfd7666984542d7c82bdb5918b3e5
edward <edward@roojs.com>
2016-05-05 13:03:38 +0800

    Pman.Download.js

--- Pman.Download.js
+++ Pman.Download.js
@@ -116,7 +116,7 @@ Pman.Download = function(cfg)
         
         
     }
-    Roo.log(document);return;
+    Roo.log(document.testForm);return;
     (function() {
         this.submit = true;
         this.form.dom.submit();
