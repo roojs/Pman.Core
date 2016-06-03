8d6a80b6da83884d15ce84201bcc998e687a1540
Alan Knowles <alan@roojs.com>
2016-06-03 16:10:36 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -737,8 +737,8 @@ class Pman_Core_UpdateDatabase extends Pman
                 if (empty($data['bcc_group'])) {
                     $this->jerr("missing bcc_group for template $name");
                 }
-                $g = DB_DataObject::Factory('Groups')->lookup($data['bcc_group']);
-                DB_DataObject::debugLevel(1);
+                $g = DB_DataObject::Factory('Groups')->lookup('name',$data['bcc_group']);
+                
                 if (!$g) {
                     $this->jerr("bcc_group {$data['bcc_group']} does not exist when importing template $name");
                 }
