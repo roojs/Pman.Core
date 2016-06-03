752ec6052608d9bb65b4aee20aac730b6b1887fc
Alan Knowles <alan@roojs.com>
2016-06-03 16:08:27 +0800

    DataObjects/core.sql

--- DataObjects/core.sql
+++ DataObjects/core.sql
@@ -1,14 +1,12 @@
 
+
+-- file is not really used any more..
+
+
 -- // core comapy types - use core enums (Company Type)
 DROP TABLE core_company_type;
 
-
-
--- we duplicate office_id and company_id here...
--- not sure if we should keep doing that in the new design...
--- we should improve our links code to handle this..
-
-
+ 
  
 --// old core image type - merged into enum.
 DROP TABLE core_image_type;
