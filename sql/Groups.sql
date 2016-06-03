b650d4b637f7558de1cc20108b4f014308295d05
Alan Knowles <alan@roojs.com>
2016-06-03 16:06:37 +0800

    sql/Groups.sql

--- sql/Groups.sql
+++ sql/Groups.sql
@@ -1,21 +1,16 @@
 
-CREATE TABLE Office (
-  id int(11) NOT NULL auto_increment,
- 
-  PRIMARY KEY  (id)
+CREATE TABLE Groups (
+  id int(11) NOT NULL auto_increment,  
+  PRIMARY KEY   (id)
 );
 
+ALTER TABLE Groups ADD COLUMN name varchar(64)  NOT NULL DEFAULT '';
+ALTER TABLE Groups ADD COLUMN   type int(11)  NOT NULL DEFAULT 0;
+ALTER TABLE Groups ADD COLUMN leader int(11)  NOT NULL default 0;
+#old mysql..
+update Groups set type=0 where type is null;
 
-ALTER TABLE Office ADD COLUMN  company_id int(11) NOT NULL default '0';
-ALTER TABLE Office ADD COLUMN    name varchar(64)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN    address text ;
-ALTER TABLE Office ADD COLUMN address2 TEXT;
-ALTER TABLE Office ADD COLUMN address3 TEXT;
-ALTER TABLE Office ADD COLUMN    phone varchar(32)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN    fax varchar(32)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN    email varchar(128)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN    role varchar(32)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN country VARCHAR(4) NULL;
+ALTER TABLE Groups CHANGE COLUMN type type int(11)  NOT NULL  default 0;
 
-ALTER TABLE Office ADD COLUMN display_name VARCHAR(4) NULL;
+
 
