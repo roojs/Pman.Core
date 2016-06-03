e00ada466023345419e550e98c06b0cbd2157cd8
Alan Knowles <alan@roojs.com>
2016-06-03 16:07:28 +0800

    sql/group_rights.sql

--- sql/group_rights.sql
+++ sql/group_rights.sql
@@ -9,6 +9,6 @@ ALTER TABLE group_rights ADD COLUMN    rightname varchar(64)  NOT NULL DEFAULT '
 ALTER TABLE group_rights ADD COLUMN     group_id int(11) NOT NULL DEFAULT 0;
 ALTER TABLE group_rights ADD COLUMN   accessmask varchar(10)  NOT NULL DEFAULT '';
 
-#old mysql.
+-- old mysql.
 ALTER TABLE group_rights CHANGE COLUMN AccessMask accessmask varchar(10)  NOT NULL DEFAULT '';
 
