d4da8c18d438c23f1ec80b3cb3998a4907b7c7f3
edward <edward@roojs.com>
2016-07-14 11:38:33 +0800

    NotifySmtpCheck.php

--- NotifySmtpCheck.php
+++ NotifySmtpCheck.php
@@ -20,12 +20,21 @@ class Pman_Core_NotifySmtpCheck extends Pman
         require_once "Mail.php";
         
         foreach ($ff->Core_Notify['routes'] as $server => $settings){
-            if(empty($settings['username']) || empty($settings['password'])){
-                $error[] = "{$server} missing username or password";
+            if(empty($settings['domains']) || empty($settings['username']) || empty($settings['password'])){
+                $error[] = "{$server} missing domains / username / password";
                 continue;
             }
             
-            $smtp = Mail::factory();
+            foreach ($settings['domains'] as $dom){
+                $mailer = Mail::factory('smtp', array(
+                    'host'    => $dom ,
+                    'localhost' => $server,
+                    'timeout' => 15,
+                    
+                ));
+            }
+            
+            
         }
     }
     
