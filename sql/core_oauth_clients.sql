c0bc3fb3989d5fc3d30085fff5363ebd838d1517
Alan Knowles <alan@roojs.com>
2016-07-14 13:30:55 +0800

    sql/core_oauth_clients.sql

--- sql/core_oauth_clients.sql
+++ sql/core_oauth_clients.sql
@@ -13,5 +13,4 @@ CREATE INDEX client_id_idx ON core_oauth_clients (client_id) USING BTREE;
 CREATE INDEX client_secret_idx ON core_oauth_clients (client_secret) USING BTREE;
 CREATE INDEX redirect_uri_idx ON core_oauth_clients (redirect_uri) USING BTREE;
 CREATE INDEX user_id_idx ON core_oauth_clients (user_id) USING BTREE;
-
-call mysql_change_engine('core_oauth_clients');
+ 
