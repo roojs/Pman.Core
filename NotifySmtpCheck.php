ed9c53546631ff49c46c98a0da9b507cd3c0f891
edward <edward@roojs.com>
2016-07-14 15:44:00 +0800

    NotifySmtpCheck.php

--- NotifySmtpCheck.php
+++ NotifySmtpCheck.php
@@ -43,7 +43,7 @@ class Pman_Core_NotifySmtpCheck extends Pman
 
             $smtp = new Net_SMTP($server, $settings['port'], $helo, false, 0, $socket_options);
 
-            $smtp->setDebug(true);
+//            $smtp->setDebug(true);
             
             echo "Connecting : {$server}:{$settings['port']} \n";
             
