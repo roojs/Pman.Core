f155a4221594371a1c379fc6eb43dce94928b97d
Alan Knowles <alan@roojs.com>
2016-07-14 13:42:37 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -416,7 +416,8 @@ class Pman_Core_UpdateDatabase extends Pman
                         case 1060: //    Duplicate column name
                         case 1061: // Duplicate key name - triggered by add index.. but could hide error. - unlikely though.
                         case 1091: // drop index -- name does not exist.. might hide errors..
-                            
+                        
+                        case 1146: // drop a index on an unknown table.. - happens rarely...
                         case 1054: // Unknown column -- triggered by CHANGE COLUMN - but may hide other errrors..
                             $continue = 1;
                             break;
