b694b8a3c13a0b255f55f763fab9c3cbf6412f0e
edward <edward@roojs.com>
2016-07-14 11:14:10 +0800

    NotifySmtpCheck.php

--- NotifySmtpCheck.php
+++ NotifySmtpCheck.php
@@ -4,21 +4,9 @@ require_once 'Pman.php';
 
 class Pman_Core_NotifySmtpCheck extends Pman
 {
-    function getAuth() 
+    function check()
     {
         $ff = HTML_FlexyFramework::get();
-        if (!empty($ff->cli)) {
-            $this->cli = true;
-            return true;
-        }
-        
-        return false;
-        
-    }
-     
-    function get($args, $opts)
-    {
-        
     }
     
 }
