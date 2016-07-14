40a1a5f78c30b26d121d2899285c7b89793b1698
edward <edward@roojs.com>
2016-07-14 12:28:47 +0800

    NotifySmtpCheck.php

--- NotifySmtpCheck.php
+++ NotifySmtpCheck.php
@@ -42,9 +42,12 @@ class Pman_Core_NotifySmtpCheck extends Pman
                 );
                 
                 $smtpmx = new Mail_smtpmx($params);
+                $smtpmx->debug = true;
                 
                 $smtpmx->_smtp = new Net_SMTP($server, $smtpmx->port, $smtpmx->mailname);
-                    
+                
+                $smtpmx->_smtp->setDebug(true);
+                
 //                print_R($smtpmx->_smtp);exit;
                 
                 $res = $smtpmx->_smtp->connect($smtpmx->timeout);
