693698656dc80262998838e8de7351c15c1b0a1b
edward <edward@roojs.com>
2016-07-18 15:24:00 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -790,8 +790,8 @@ class Pman_Core_UpdateDatabase extends Pman
                     $this->jerr("bcc_group {$data['bcc_group']} does not exist when importing template $name");
                 }
                 
-                if (!$g->members('email')) {
-                      $this->jerr("bcc_group {$data['bcc_group']} does not have any members");
+                if (empty($g->members('email'))) {
+                    $this->jerr("bcc_group {$data['bcc_group']} does not have any members");
                 }
                 
                 $cm->bcc_group = $g->id;
