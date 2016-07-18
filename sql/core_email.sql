879bdaf562efe1d33be280bcfdefef1e098acb91
edward <edward@roojs.com>
2016-07-18 13:32:33 +0800

    sql/core_email.sql

--- sql/core_email.sql
+++ sql/core_email.sql
@@ -13,11 +13,11 @@ CREATE  TABLE core_email (
 );
 
 ALTER TABLE core_email ADD COLUMN active INT(2) NOT NULL DEFAULT 1;
-
 ALTER TABLE core_email MODIFY COLUMN active INT(2) NOT NULL DEFAULT 1;
 
 -- need to store BCC data here.
 ALTER TABLE core_email ADD COLUMN bcc_group INT(11) NOT NULL DEFAULT 0;
+ALTER TABLE core_email MODIFY COLUMN bcc_group INT(11) NOT NULL DEFAULT 0;
 
 -- each email template should have  a test class with a static method ::test_{name}
 ALTER TABLE core_email ADD COLUMN test_class VARCHAR(254) default 0;
