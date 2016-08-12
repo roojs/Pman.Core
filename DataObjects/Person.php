77a872226018733bb1c397716373a379d943001d
edward <edward@roojs.com>
2016-08-12 18:28:53 +0800

    DataObjects/Person.php

--- DataObjects/Person.php
+++ DataObjects/Person.php
@@ -369,7 +369,7 @@ class Pman_Core_DataObjects_Person extends DB_DataObject
     
     function getAuthUser()
     {
-        if (!$this->isAuth()) {)
+        if (!$this->isAuth()) {
             return false;
         }
         
