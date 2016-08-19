7cb64338c9220b39c800cb64bd1a20e62acb7a50
edward <edward@roojs.com>
2016-08-19 16:07:17 +0800

    Pman.Tab.GroupsList.js

--- Pman.Tab.GroupsList.js
+++ Pman.Tab.GroupsList.js
@@ -155,6 +155,8 @@ Pman.Tab.GroupsList.prototype = {
         this.panel  = parentLayout.add(region,  new Ext.GridPanel(this.grid ,
             { fitToframe: true,fitContainer: true, title: _this.title, id : _this.id, background: true})
         );
+
+        Roo.log(this.panel);
         this.grid.render();
         
         
