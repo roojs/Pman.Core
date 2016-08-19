25cba6c2ffd887364954f2de7e583a1736f7a815
edward <edward@roojs.com>
2016-08-19 15:53:55 +0800

    Pman.Tab.GroupsList.js

--- Pman.Tab.GroupsList.js
+++ Pman.Tab.GroupsList.js
@@ -18,8 +18,6 @@ Pman.Tab.GroupsList.prototype = {
     disabled : false,
     add : function(parentLayout, region) {
         
-        Roo.log('GroupsList add called!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-        
         var _this = this;
         
         var refreshPager = function() {
