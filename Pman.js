f840d9f7ffb4c4ff77b99d6ed4757cc1a5260690
edward <edward@roojs.com>
2016-08-19 16:21:01 +0800

    Pman.js

--- Pman.js
+++ Pman.js
@@ -933,6 +933,7 @@ Pman = new Roo.Document(
            // honour DEPRICATED permname setings..
            // new code should use PART name, and matching permissions.
             if (this.permname && this.permname.length) {
+                Roo.log(this.permname);
                 if (!Pman.hasPerm(this.permname, 'S')) {
                     return;
                 }
