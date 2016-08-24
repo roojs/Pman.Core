85173d8d7b7769f4dbeaa4249d75297eb8f52981
edward <edward@roojs.com>
2016-08-24 16:12:01 +0800

    sql/core_event_audit.sql

--- sql/core_event_audit.sql
+++ sql/core_event_audit.sql
@@ -7,7 +7,7 @@ CREATE TABLE  core_event_audit  (
 ALTER TABLE core_event_audit ADD COLUMN   event_id int(11)  NOT NULL DEFAULT 0;
 ALTER TABLE core_event_audit ADD COLUMN       name varchar(128)  NOT NULL DEFAULT '';
 ALTER TABLE core_event_audit ADD COLUMN       old_audit_id int(11)  NOT NULL DEFAULT 0;
-ALTER TABLE core_event_audit ADD COLUMN       newvalue BLOB  NOT NULL DEFAULT '';
+ALTER TABLE core_event_audit ADD COLUMN       newvalue BLOB  NOT NULL;
 ALTER TABLE core_event_audit ADD   INDEX lookup(event_id, name, old_audit_id);
 
 
