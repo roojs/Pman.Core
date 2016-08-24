643cb41ae702a4caf40b5558d5b1bb896e22ba87
edward <edward@roojs.com>
2016-08-24 16:14:22 +0800

    sql/images.sql

--- sql/images.sql
+++ sql/images.sql
@@ -18,7 +18,7 @@ ALTER TABLE Images    ADD COLUMN   parent_image_id int(11) NOT NULL default '0';
 ALTER TABLE  Images ADD COLUMN created datetime ;
 ALTER TABLE  Images ADD COLUMN imgtype VARCHAR(32) DEFAULT '' NOT NULL;
 ALTER TABLE  Images ADD COLUMN linkurl VARCHAR(254) DEFAULT '' NOT NULL;
-ALTER TABLE  Images ADD COLUMN descript TEXT DEFAULT '' ;
+ALTER TABLE  Images ADD COLUMN descript TEXT NOT NULL;
 ALTER TABLE  Images ADD COLUMN title VARCHAR(128) DEFAULT '' NOT NULL;
 
 ALTER TABLE Images    CHANGE COLUMN   mimetype mimetype  varchar(128) NOT NULL default '';
