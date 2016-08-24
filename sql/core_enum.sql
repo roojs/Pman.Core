e5f9065a31aff3f5566384b1d28fc460e265fb49
edward <edward@roojs.com>
2016-08-24 16:23:40 +0800

    sql/core_enum.sql

--- sql/core_enum.sql
+++ sql/core_enum.sql
@@ -13,7 +13,7 @@ alter table  core_enum ADD COLUMN  display_name varchar(255)  NOT NULL DEFAULT '
 
 
 ALTER TABLE core_enum ADD COLUMN is_system_enum INT(2) NOT NULL DEFAULT 0;
-ALTER TABLE core_enum CHANGE COLUMN display_name display_name TEXT NOT NULL DEFAULT '';
+ALTER TABLE core_enum CHANGE COLUMN display_name display_name TEXT NOT NULL;
 
 alter table  core_enum ADD  INDEX lookup(seqid, active, name, etype);
 
