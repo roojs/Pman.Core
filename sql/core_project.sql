ac28e43de9be351b3e9e4d58fced674d8ec81731
Alan Knowles <alan@roojs.com>
2016-10-07 13:43:29 +0800

    sql/core_project.sql

--- sql/core_project.sql
+++ sql/core_project.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE Projects (
+CREATE TABLE core_project (
   id int(11) NOT NULL auto_increment,
   PRIMARY KEY  (id)
   
@@ -7,23 +7,23 @@ CREATE TABLE Projects (
 
 
 
-ALTER TABLE  Projects  ADD COLUMN name varchar(254)  NOT NULL  DEFAULT '';
-ALTER TABLE  Projects  ADD COLUMN   remarks text ;
-ALTER TABLE  Projects  ADD COLUMN   owner_id int(11) default NULL;
-ALTER TABLE  Projects  ADD COLUMN   code varchar(32)  NOT NULL  DEFAULT '';
-ALTER TABLE  Projects  ADD COLUMN   active int(11) default '1';
-ALTER TABLE  Projects  ADD COLUMN   type varchar(1)  NOT NULL default 'P';
-ALTER TABLE  Projects  ADD COLUMN   client_id int(11) NOT NULL default '0';
-ALTER TABLE  Projects  ADD COLUMN   team_id int(11) NOT NULL default '0';
-ALTER TABLE  Projects  ADD COLUMN file_location varchar(254)    NOT NULL default '';
-ALTER TABLE  Projects  ADD COLUMN open_date date default NULL;
-ALTER TABLE  Projects  ADD COLUMN open_by int(11) NOT NULL default '0';
-ALTER TABLE  Projects  ADD COLUMN updated_dt DATETIME NOT NULL;
+ALTER TABLE  core_project  ADD COLUMN name varchar(254)  NOT NULL  DEFAULT '';
+ALTER TABLE  core_project  ADD COLUMN   remarks text ;
+ALTER TABLE  core_project  ADD COLUMN   owner_id int(11) default NULL;
+ALTER TABLE  core_project  ADD COLUMN   code varchar(32)  NOT NULL  DEFAULT '';
+ALTER TABLE  core_project  ADD COLUMN   active int(11) default '1';
+ALTER TABLE  core_project  ADD COLUMN   type varchar(1)  NOT NULL default 'P';
+ALTER TABLE  core_project  ADD COLUMN   client_id int(11) NOT NULL default '0';
+ALTER TABLE  core_project  ADD COLUMN   team_id int(11) NOT NULL default '0';
+ALTER TABLE  core_project  ADD COLUMN file_location varchar(254)    NOT NULL default '';
+ALTER TABLE  core_project  ADD COLUMN open_date date default NULL;
+ALTER TABLE  core_project  ADD COLUMN open_by int(11) NOT NULL default '0';
+ALTER TABLE  core_project  ADD COLUMN updated_dt DATETIME NOT NULL;
 
 -- these should be removed, as they are code specific..
-ALTER TABLE Projects ADD COLUMN countries varchar(128)  NOT NULL DEFAULT '';
-ALTER TABLE Projects  ADD COLUMN languages varchar(128)  NOT NULL DEFAULT '';
-ALTER TABLE  Projects ADD COLUMN agency_id int(11)  NOT NULL DEFAULT 0 ;
+ALTER TABLE core_project ADD COLUMN countries varchar(128)  NOT NULL DEFAULT '';
+ALTER TABLE core_project  ADD COLUMN languages varchar(128)  NOT NULL DEFAULT '';
+ALTER TABLE  core_project ADD COLUMN agency_id int(11)  NOT NULL DEFAULT 0 ;
 
-ALTER TABLE Projects ADD INDEX plookup (code);
+ALTER TABLE core_project ADD INDEX plookup (code);
  
