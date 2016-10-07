87e9a55dcd1af65bdf61be713cdc4def0ec44254
Alan Knowles <alan@roojs.com>
2016-10-07 14:30:43 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -5,7 +5,7 @@
 require_once 'DB/DataObject.php';
 
 
-class Pman_Core_DataObjects_Person extends DB_DataObject 
+class Pman_Core_DataObjects_Core_person extends DB_DataObject 
 {
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
