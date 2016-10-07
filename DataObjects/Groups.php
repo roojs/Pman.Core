eabba6407d26615b59ff89a77728c31309db679e
Alan Knowles <alan@roojs.com>
2016-10-07 13:01:32 +0800

    DataObjects/Groups.php

--- DataObjects/Groups.php
+++ DataObjects/Groups.php
@@ -5,7 +5,7 @@
  */
 require_once 'Pman/Core/DataObjects/Core_groups.php';
 
-class Pman_Core_DataObjects_Core_groups extends Pman_Core_DataObjects_Groups
+class  Pman_Core_DataObjects_Groups extends Pman_Core_DataObjects_Core_groups
 {
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
