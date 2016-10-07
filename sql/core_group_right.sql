ad6fbdd380b3629aabb68068d39db4cd946b06cb
Alan Knowles <alan@roojs.com>
2016-10-07 13:37:29 +0800

    MOVED sql/group_rights.sql to sql/core_group_right.sql

---
+++ sql/core_group_right.sql
@@ -0,0 +1,14 @@
+RENAME TABLE Group_Rights TO group_rights;
+
+
+CREATE TABLE  group_rights  (
+    id int(11)  NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id)
+);
+ALTER TABLE group_rights ADD COLUMN    rightname varchar(64)  NOT NULL DEFAULT '';
+ALTER TABLE group_rights ADD COLUMN     group_id int(11) NOT NULL DEFAULT 0;
+ALTER TABLE group_rights ADD COLUMN   accessmask varchar(10)  NOT NULL DEFAULT '';
+
+-- old mysql.
+ALTER TABLE group_rights CHANGE COLUMN AccessMask accessmask varchar(10)  NOT NULL DEFAULT '';
+
