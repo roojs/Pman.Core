f044fa83fa2e370d12a64b53fa6b4367b6a40012
Alan Knowles <alan@roojs.com>
2016-10-07 13:43:46 +0800

    sql/core_office.sql

--- sql/core_office.sql
+++ sql/core_office.sql
@@ -1,21 +1,21 @@
 
-CREATE TABLE Office (
+CREATE TABLE core_office (
   id int(11) NOT NULL auto_increment,
  
   PRIMARY KEY  (id)
 );
 
 
-ALTER TABLE Office ADD COLUMN  company_id int(11) NOT NULL default '0';
-ALTER TABLE Office ADD COLUMN    name varchar(64)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN    address text ;
-ALTER TABLE Office ADD COLUMN address2 TEXT;
-ALTER TABLE Office ADD COLUMN address3 TEXT;
-ALTER TABLE Office ADD COLUMN    phone varchar(32)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN    fax varchar(32)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN    email varchar(128)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN    role varchar(32)  NOT NULL  DEFAULT '';
-ALTER TABLE Office ADD COLUMN country VARCHAR(4) NULL;
+ALTER TABLE core_office ADD COLUMN  company_id int(11) NOT NULL default '0';
+ALTER TABLE core_office ADD COLUMN    name varchar(64)  NOT NULL  DEFAULT '';
+ALTER TABLE core_office ADD COLUMN    address text ;
+ALTER TABLE core_office ADD COLUMN address2 TEXT;
+ALTER TABLE core_office ADD COLUMN address3 TEXT;
+ALTER TABLE core_office ADD COLUMN    phone varchar(32)  NOT NULL  DEFAULT '';
+ALTER TABLE core_office ADD COLUMN    fax varchar(32)  NOT NULL  DEFAULT '';
+ALTER TABLE core_office ADD COLUMN    email varchar(128)  NOT NULL  DEFAULT '';
+ALTER TABLE core_office ADD COLUMN    role varchar(32)  NOT NULL  DEFAULT '';
+ALTER TABLE core_office ADD COLUMN country VARCHAR(4) NULL;
 
-ALTER TABLE Office ADD COLUMN display_name VARCHAR(4) NULL;
+ALTER TABLE core_office ADD COLUMN display_name VARCHAR(4) NULL;
 
