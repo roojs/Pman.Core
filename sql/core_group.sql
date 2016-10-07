492f164070c4e3b125c8bdb917a1cbe809f84b0d
Alan Knowles <alan@roojs.com>
2016-10-07 13:18:32 +0800

    sql/core_group.sql

--- sql/core_group.sql
+++ sql/core_group.sql
@@ -1,15 +1,15 @@
 
 
 
--- used to be Groups .... we will need to flip all old code to use this...
+-- IF you are working on an old system... - copy the changes to Groups.sql...
  
-CREATE TABLE core_groups (
+CREATE TABLE core_group (
   id int(11) NOT NULL auto_increment,  
   PRIMARY KEY   (id)
 );
 
-ALTER TABLE Groups ADD COLUMN name varchar(128)  NOT NULL DEFAULT '';
-ALTER TABLE Groups ADD COLUMN type  int(11)  NOT NULL DEFAULT 0;
-ALTER TABLE Groups ADD COLUMN leader int(11)  NOT NULL default 0;
-ALTER TABLE Groups ADD COLUMN is_system int(2) NOT NULL default 0;
+ALTER TABLE core_group ADD COLUMN name varchar(128)  NOT NULL DEFAULT '';
+ALTER TABLE core_group ADD COLUMN type  int(11)  NOT NULL DEFAULT 0;
+ALTER TABLE core_group ADD COLUMN leader int(11)  NOT NULL default 0;
+ALTER TABLE core_group ADD COLUMN is_system int(2) NOT NULL default 0;
 
