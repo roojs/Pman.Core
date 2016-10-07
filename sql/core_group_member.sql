8834dd09132066e26b332ee3ae12cc8006685060
Alan Knowles <alan@roojs.com>
2016-10-07 13:42:48 +0800

    sql/core_group_member.sql

--- sql/core_group_member.sql
+++ sql/core_group_member.sql
@@ -1,11 +1,10 @@
 -- BC name..
-RENAME TABLE Group_Members TO group_members;
-
-CREATE TABLE  group_members  (
+ 
+CREATE TABLE  core_group_member  (
     id int(11)  NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id)
 );
-ALTER TABLE group_members ADD COLUMN  group_id int(11) default NULL;
-ALTER TABLE group_members ADD COLUMN   user_id int(11) NOT NULL default 0;
+ALTER TABLE core_group_member ADD COLUMN  group_id int(11) default NULL;
+ALTER TABLE core_group_member ADD COLUMN   user_id int(11) NOT NULL default 0;
 
  
