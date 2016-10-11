ebd741e0cbbfe4ea41cf248a50035ef10b5793fe
Alan Knowles <alan@roojs.com>
2016-10-11 11:08:33 +0800

    mysql/core_person_get_project_id.sql

--- mysql/core_person_get_project_id.sql
+++ mysql/core_person_get_project_id.sql
@@ -1,16 +1,16 @@
 
 
-DROP FUNCTION IF EXISTS person_get_project_id;
+DROP FUNCTION IF EXISTS core_person_get_project_id;
 
 DELIMITER $$
-CREATE FUNCTION person_get_project_id(  
+CREATE FUNCTION core_person_get_project_id(  
        in_id INT(11)
     )  RETURNS INT(11) DETERMINISTIC
     BEGIN
         
         DECLARE v_id INT(11);
         SET v_id = 0;
-        SELECT project_id INTO v_id FROM Person WHERE id  = in_id LIMIT 1;
+        SELECT project_id INTO v_id FROM core_person WHERE id  = in_id LIMIT 1;
         RETURN v_id;
     END $$
 
