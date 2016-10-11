2b368f9e65cba5d009c1e144c067a8a68a31283a
Alan Knowles <alan@roojs.com>
2016-10-11 11:06:43 +0800

    mysql/core_person_get_action_type.sql

--- mysql/core_person_get_action_type.sql
+++ mysql/core_person_get_action_type.sql
@@ -1,13 +1,13 @@
 
-DROP FUNCTION IF EXISTS person_get_action_type;
+DROP FUNCTION IF EXISTS core_person_get_action_type;
 
 DELIMITER $$
-CREATE FUNCTION person_get_action_type(in_id INT(11))  RETURNS VARCHAR(32) DETERMINISTIC
+CREATE FUNCTION core_person_get_action_type(in_id INT(11))  RETURNS VARCHAR(32) DETERMINISTIC
     BEGIN
         
         DECLARE v_ret VARCHAR(32);
         SET v_ret= '';
-        SELECT action_type INTO v_ret  FROM Person WHERE id = in_id  LIMIT 1;
+        SELECT action_type INTO v_ret  FROM core_person WHERE id = in_id  LIMIT 1;
         RETURN v_ret;
     END $$
 DELIMITER ;
