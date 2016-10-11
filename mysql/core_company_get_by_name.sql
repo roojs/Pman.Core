b04347f4f0f6f1f909fde9974bf2d60a4e2586b7
Alan Knowles <alan@roojs.com>
2016-10-11 11:04:28 +0800

    mysql/core_company_get_by_name.sql

--- mysql/core_company_get_by_name.sql
+++ mysql/core_company_get_by_name.sql
@@ -1,12 +1,12 @@
 
  
-DROP FUNCTION IF EXISTS company_get_by_name;
+DROP FUNCTION IF EXISTS core_company_get_by_name;
 DELIMITER $$
-CREATE FUNCTION company_get_by_name(in_name VARCHAR(128))  RETURNS INT(11) DETERMINISTIC
+CREATE FUNCTION core_company_get_by_name(in_name VARCHAR(128))  RETURNS INT(11) DETERMINISTIC
     BEGIN
         DECLARE v_ret INT(11);
         SET v_ret= 0;
-        SELECT id    INTO v_ret  FROM Companies WHERE name = in_name LIMIT 1;
+        SELECT id    INTO v_ret  FROM core_company WHERE name = in_name LIMIT 1;
         RETURN v_ret;
     END $$
 DELIMITER ; 
