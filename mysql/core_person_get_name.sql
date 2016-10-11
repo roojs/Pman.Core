af435bbb27d3536194869fef714eb8959a590069
Alan Knowles <alan@roojs.com>
2016-10-11 11:08:11 +0800

    mysql/core_person_get_name.sql

--- mysql/core_person_get_name.sql
+++ mysql/core_person_get_name.sql
@@ -1,14 +1,14 @@
 
 
-DROP FUNCTION IF EXISTS person_get_name;
+DROP FUNCTION IF EXISTS core_person_get_name;
 
 DELIMITER $$
-CREATE FUNCTION person_get_name(in_id INT(11))  RETURNS VARCHAR(254) DETERMINISTIC
+CREATE FUNCTION core_person_get_name(in_id INT(11))  RETURNS VARCHAR(254) DETERMINISTIC
     BEGIN
         
         DECLARE v_ret VARCHAR(254);
         SET v_ret= '';
-        SELECT name INTO v_ret  FROM Person WHERE id = in_id  LIMIT 1;
+        SELECT name INTO v_ret  FROM core_person WHERE id = in_id  LIMIT 1;
         RETURN v_ret;
     END $$
 DELIMITER ;
