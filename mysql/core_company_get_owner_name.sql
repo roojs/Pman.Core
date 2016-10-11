cceb074d111ab5160937f079ca5ea9a07e92d3d0
Alan Knowles <alan@roojs.com>
2016-10-11 11:05:30 +0800

    mysql/core_company_get_owner_name.sql

--- mysql/core_company_get_owner_name.sql
+++ mysql/core_company_get_owner_name.sql
@@ -1,11 +1,11 @@
 
-DROP FUNCTION IF EXISTS company_get_owner_name;
+DROP FUNCTION IF EXISTS core_company_get_owner_name;
 DELIMITER $$
-CREATE FUNCTION company_get_owner_name()  RETURNS VARCHAR(254) DETERMINISTIC
+CREATE FUNCTION core_company_get_owner_name()  RETURNS VARCHAR(254) DETERMINISTIC
     BEGIN
         DECLARE v_ret VARCHAR(254);
         SET v_ret= '';
-        SELECT name INTO v_ret  FROM Companies WHERE isOwner = 1 LIMIT 1;
+        SELECT name INTO v_ret  FROM core_company WHERE isOwner = 1 LIMIT 1;
         RETURN v_ret;
     END $$
 DELIMITER ;
