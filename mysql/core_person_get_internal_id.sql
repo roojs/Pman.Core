ab680cd1d467a007e8b65baab2f4d24031727ee9
Alan Knowles <alan@roojs.com>
2016-10-11 11:07:36 +0800

    MOVED mysql/person_get_internal_id.sql to mysql/core_person_get_internal_id.sql

---
+++ mysql/core_person_get_internal_id.sql
@@ -0,0 +1,32 @@
+
+DROP FUNCTION IF EXISTS person_get_internal_id;
+
+DELIMITER $$
+CREATE FUNCTION person_get_internal_id(  
+        in_addr VARCHAR(254)
+        
+
+    )  RETURNS INT(11) DETERMINISTIC
+    BEGIN
+        
+        DECLARE v_id INT(11);
+        DECLARE v_company_id INT(11);
+        SET v_id = 0;
+        SET v_company_id = 0;
+        
+        SELECT company_get_owner() INTO v_company_id;
+        IF v_company_id < 1 THEN
+            RETURN 0;
+        END IF;
+        
+        SELECT id INTO v_id FROM Person WHERE
+            company_id = v_company_id
+            AND email = in_addr LIMIT 1;
+        
+        RETURN v_id;
+        
+    
+        
+        
+    END $$
+DELIMITER ;
