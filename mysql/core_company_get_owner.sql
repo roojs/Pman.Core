1369442fa5e820ec0b64f3fa7cc8c90cd8d386d4
Alan Knowles <alan@roojs.com>
2016-10-11 11:05:10 +0800

    mysql/core_company_get_owner.sql

--- mysql/core_company_get_owner.sql
+++ mysql/core_company_get_owner.sql
@@ -1,11 +1,11 @@
-DROP FUNCTION IF EXISTS company_get_owner;
+DROP FUNCTION IF EXISTS core_company_get_owner;
 
 DELIMITER $$
-CREATE FUNCTION company_get_owner()  RETURNS INT(11) DETERMINISTIC
+CREATE FUNCTION core_company_get_owner()  RETURNS INT(11) DETERMINISTIC
     BEGIN
         DECLARE v_id INT(11);
         SET v_id = 0;
-        SELECT id INTO v_id  FROM Companies WHERE isOwner = 1 LIMIT 1;
+        SELECT id INTO v_id  FROM core_company WHERE isOwner = 1 LIMIT 1;
         RETURN v_id;
     END $$
 DELIMITER ;
