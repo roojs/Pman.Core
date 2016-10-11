274163f8e3c9210ccd00e98fa2d0decc96ce5326
Alan Knowles <alan@roojs.com>
2016-10-11 11:04:08 +0800

    mysql/core_company_owner_create.sql

--- mysql/core_company_owner_create.sql
+++ mysql/core_company_owner_create.sql
@@ -1,20 +1,20 @@
 
-DROP FUNCTION  IF EXISTS companies_owner_create;
+DROP FUNCTION  IF EXISTS core_company_owner_create;
     
 DELIMITER $$
-CREATE FUNCTION companies_owner_create(in_code  VARCHAR(32) , in_name VARCHAR(64)) RETURNS VARCHAR(64) DETERMINISTIC
+CREATE FUNCTION core_company_owner_create(in_code  VARCHAR(32) , in_name VARCHAR(64)) RETURNS VARCHAR(64) DETERMINISTIC
 BEGIN
     DECLARE v_id INT(11);
     SET v_id = 0;
     #// check if person exists..
-    SELECT id INTO v_id FROM Companies
+    SELECT id INTO v_id FROM core_company
         WHERE comptype='OWNER' LIMIT 1;
 
     IF v_id  != 0 THEN
         RETURN CONCAT('DUPE - Company', in_name);
     END IF;
     
-    INSERT INTO Companies (code, name, comptype, comptype_id) VALUES (
+    INSERT INTO core_company (code, name, comptype, comptype_id) VALUES (
         in_code,in_name,'OWNER', core_enum_id_by_name('COMPTYPE', 'OWNER' )
     );
         
