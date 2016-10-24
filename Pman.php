34a6faf992e7c9503be6b98087a4f98f174966f1
Alan Knowles <alan@roojs.com>
2016-10-24 10:36:20 +0800

    Pman.php

--- Pman.php
+++ Pman.php
@@ -15,6 +15,7 @@ class Pman_Core_Pman {
         
         $group = DB_DataObject::factory('core_group');
         
+        print_R($group);exit;
         // group should be auto created - by update-database...
         
         
