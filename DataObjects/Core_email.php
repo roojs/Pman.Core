b88e44360d609a2b79f4871a13666e2543232d0c
edward <edward@roojs.com>
2016-10-25 11:52:19 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -57,6 +57,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
     
     function beforeUpdate($old, $request,$roo)
     {   
+        print_R($request);exit;
         if (!empty($request['_make_copy'])) {
             $this->makeCopy($roo);
             
