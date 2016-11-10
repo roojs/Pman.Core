bd641c46f5dc2a024b9150517ee1728562754ab3
edward <edward@roojs.com>
2016-11-10 15:44:40 +0800

    MessagePreview.php

--- MessagePreview.php
+++ MessagePreview.php
@@ -36,8 +36,6 @@ class Pman_Core_MessagePreview extends Pman
     
     function post()
     {
-        print_r($_REQUEST);
-        exit;
         if(empty($_REQUEST['_id']) || empty($_REQUEST['_table'])){
             $this->jerr('Missing Options');
         }
