1d09ff399f0c6651e110a588ed85bf0e32deb05c
Alan Knowles <alan@roojs.com>
2017-03-16 14:26:42 +0800

    Process/FixDataObjectCtor.php

--- Process/FixDataObjectCtor.php
+++ Process/FixDataObjectCtor.php
@@ -50,7 +50,8 @@ class Pman_Core_Process_FixDataObjectCtor extends Pman
         }
         $c = preg_replace("/new DataObjects_([a-z_]+)/i", "DB_DataObject::factory('\1')", $c);
         
-        
+        $c = preg_replace("/DataObjects_([a-z_]+)::/i", "DB_DataObject::factory('\1')", $c);
+
       
         if ($old_c == $c) {
             echo "$file: SKIP NO CHANGES\n";
