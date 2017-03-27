e4590f6c8bab5b7c6952d9c52a104a9d2f23618e
edward <edward@roojs.com>
2017-03-27 17:59:04 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -371,10 +371,13 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
             $cfg['rcpts'] = $contents['rcpts'];
         }
         
+        if (isset($contents['attachments'])) {
+            $cfg['attachments'] = $contents['attachments'];
+        }
+        
         if (isset($contents['mailer_opts']) && is_array($contents['mailer_opts'])) {
             $cfg = array_merge($contents['mailer_opts'], $cfg);
         }
-        
         
         $r = new Pman_Core_Mailer($cfg);
         
