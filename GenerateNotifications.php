64c5f5b1617e0b8cc433affa9155653b6048baf7
Alan Knowles <alan@roojs.com>
2017-04-25 12:12:51 +0800

    GenerateNotifications.php

--- GenerateNotifications.php
+++ GenerateNotifications.php
@@ -7,6 +7,12 @@ class Pman_Core_GenerateNotifications {
     function generate($roo)
     {
         
+     
+        
+        $w = DB_DataObject::factory('core_notify_recur');
+        if (is_a($w, 'DB_DataObject')) {
+            $w->generateNotifications();
+        }
         
     }
     
