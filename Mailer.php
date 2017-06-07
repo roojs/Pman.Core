61ad2bb8dcc0239da57e442d7af18e2154c37c9b
edward <edward@roojs.com>
2017-06-07 16:41:27 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -160,10 +160,6 @@ class Pman_Core_Mailer {
         $html_tmp_opts = $tmp_opts;
         $htmltemplate = new HTML_Template_Flexy( $html_tmp_opts );
         
-//        $htmltemplate->resolvePath('mail/'.$templateFile.'.body.html');
-        
-        print_r('test');exit;
-                
         if (is_string($htmltemplate->resolvePath('mail/'.$templateFile.'.body.html')) ) {
             // then we have a multi-part email...
             
