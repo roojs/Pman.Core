7ea696cc685b70c684f20a44ba8c8e3810808c5c
Alan Knowles <alan@roojs.com>
2017-07-04 10:26:22 +0800

    mysql/core_notify_trigger_after_update.sql

--- mysql/core_notify_trigger_after_update.sql
+++ mysql/core_notify_trigger_after_update.sql
@@ -10,11 +10,11 @@ CREATE TRIGGER core_notify_trigger_after_update
         FOR EACH ROW
         BEGIN
             -- make sure that act_start does not get modified if sent is set.
-            IF OLD.sent IS NOT NULL  AND  ( NEW.act_start != OLD.act_start  OR NEW.act_when != OLD.act_when )  THEN
+            IF (OLD.sent IS NOT NULL AND  OLD.sent != '0000-00-00 00:00:00')
+                AND  ( NEW.act_start != OLD.act_start  OR NEW.act_when != OLD.act_when )  THEN
                   UPDATE `Error: Can not update core_notify action dates  after its sent` SET x = 1;
             END IF;
         END;
-
 $$
  
 
