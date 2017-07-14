e848912eeab1751521f61ff6ebc95e836f1c6cee
Alan Knowles <alan@roojs.com>
2017-07-14 15:15:24 +0800

    JsCompile.php

--- JsCompile.php
+++ JsCompile.php
@@ -264,10 +264,7 @@ class Pman_Core_JsCompile  extends Pman
         if (isset($o['packseed'])) {
             return $this->packSeed($files,$output,$translation_base);
         }
-        if (!empty($o['disable_packer'])) {
-            echo "<!-- packer disabled    disable_packer  != false -->";
-            return;
-        }
+        
         
         require_once 'System.php';
         $packer = System::which('roojspacker');
