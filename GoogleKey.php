83518168264f2d6772a8623db0368a36fd1916e2
Alan Knowles <alan@roojs.com>
2017-08-04 10:23:12 +0800

    GoogleKey.php

--- GoogleKey.php
+++ GoogleKey.php
@@ -22,12 +22,12 @@ class Pman_Core_GoogleKey extends Pman
         
         $this->authUser = $au;
     }
-    function get() {
+    function get($v, $opts=array()) {
         // for testing..
         return $this->post();
     }
     
-    function post()
+    function post($v)
     {
         $pc = HTML_FlexyFramework::get()->Pman_Core;
         if (empty($pc['googlekey'])) {
