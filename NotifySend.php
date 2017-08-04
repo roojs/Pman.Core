65fd5773b07a3389aa95905d91bc002e8658a98b
Alan Knowles <alan@roojs.com>
2017-08-04 14:51:41 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -350,7 +350,7 @@ class Pman_Core_NotifySend extends Pman
         
         $w->to_email = $p->email; 
         //$this->addEvent('NOTIFY', $w, 'GREYLISTED ' . $p->email . ' ' . $res->toString());
-        $w->act_when = date('Y-m-d H:i:s', strtotime('NOW + ' . $retry . ' MINUTES'));
+        $w->act_when =  $w->sent == '0000-00-00 00:00:00' ? date('Y-m-d H:i:s', strtotime('NOW + ' . $retry . ' MINUTES')) : $w->act_when;
         $w->update($ww);
         
         $ww = clone($w);   
