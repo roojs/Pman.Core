850f656ee966f7cf848396daac219ca1588ea2cd
Alan Knowles <alan@roojs.com>
2017-08-25 12:28:19 +0800

    Pman.js

--- Pman.js
+++ Pman.js
@@ -393,7 +393,7 @@ Pman = new Roo.Document(
         
         if (Pman.Login.authUser.id < 0) {
             // admin company has been created - create the user..
-            if (Pman.Login.authUser.company_id_id* 1 > 0) {
+            if (Pman.Login.authUser.company_id* 1 > 0) {
                 forceAdmin();
                 return;
             }
