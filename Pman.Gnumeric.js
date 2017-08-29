8acf95af96b377b5aec67c8a5f5a2018cdf678c4
edward <edward@roojs.com>
2017-08-29 16:39:24 +0800

    Pman.Gnumeric.js

--- Pman.Gnumeric.js
+++ Pman.Gnumeric.js
@@ -185,9 +185,6 @@ Roo.extend(Pman.Gnumeric, Roo.util.Observable, {
             url: this.url,
             method:  'GET',
             success : function(resp, opts) {
-                
-                Roo.log('resp:');
-                Roo.log(resp);
                 _t.response = resp;
                 _t.doc = resp.responseXML;
                 
