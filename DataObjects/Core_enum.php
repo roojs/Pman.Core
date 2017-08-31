863d72ffe9ebcd59c515639246b9af68f50785e5
john <john@roojs.com>
2017-08-31 09:50:27 +0800

    DataObjects/Core_enum.php

--- DataObjects/Core_enum.php
+++ DataObjects/Core_enum.php
@@ -4,7 +4,7 @@
  */
 class_exists('DB_DataObject') ? '' : require_once 'DB/DataObject.php';
 
-class Pman_Core_DataObjects_Core_enum extends DB_DataObject 
+class Pman_Core_DataObjects_Core_enum extends DB_DataObject
 {
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
@@ -17,18 +17,18 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
     public $seqid;                           // int(11)  not_null multiple_key
     public $seqmax;                           // int(11)  not_null multiple_key
     public $display_name;
-    
+
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
-    
+
     function applyFilters($q, $au, $roo)
     {
-        
+
 //        DB_DataObject::debugLevel(1);
         if (!empty($q['query']['empty_etype'])) {
             $this->whereAdd("core_enum.etype = ''");
         }
-        
+
         // this should be handled by roo... using '!name[0]' ....
         if(!empty($q['!name'])){
             $names = is_array($q['!name']) ? $q['!name'] : explode(',', $q['!name']);
@@ -45,9 +45,9 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
             $this->whereAdd("
                 core_enum.display_name LIKE '{$name}%'
             ");
-            
+
         }
-        
+
         if(!empty($q['query']['search'])) {
             $name = $this->escape($q['query']['search']);
             // ilike on postgres?!?
@@ -66,13 +66,13 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
                     core_enum.display_name LIKE '{$name}%'
             ");
         }
-        
+
         if (isset($q['_etypes'])) {
             $this->whereAddIn('core_enum.etype', explode(',', $q['_etypes']), 'string');
         }
-        
+
     }
-    
+
     function checkPerm($lvl, $au, $req=null)
     {
         if (!$au) {
@@ -80,15 +80,15 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         }
         return true;
     }
-    
-    
+
+
     function autoJoinCmsTranslate($lang)
     {
         $l = $this->escape($lang);
-        
+
         $this->_join .= "
-            LEFT JOIN 
-                cms_templatestr 
+            LEFT JOIN
+                cms_templatestr
             ON
                 cms_templatestr.lang = '$l'
             AND
@@ -98,23 +98,23 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
             AND
                 cms_templatestr.on_col = 'display_name'
         ";
-        
+
         $this->selectAdd("
-            CASE WHEN 
-                '$l' = 'en' THEN display_name 
+            CASE WHEN
+                '$l' = 'en' THEN display_name
             ELSE
                 CASE WHEN cms_templatestr.txt IS NOT NULL AND cms_templatestr.txt != '' THEN
                     cms_templatestr.txt
                 ELSE
                     display_name
                 END
-            END as  display_name_tr 
+            END as  display_name_tr
         ");
-        
+
     }
-    
+
     function postListFilter($data, $authUser, $q) {
-        
+
        /* if(!empty($q['cmsTab'])){
             $ret = array();
             foreach($data as $k=>$v){
@@ -131,16 +131,16 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         }
         */
         return $data;
-        
+
     }
-    
-    
+
+
     function beforeUpdate($old, $request,$roo)
-    {   
+    {
         if(!empty($request['_merge_id'])){
             $this->merge($request['_merge_id'], $roo);
         }
-        
+
         $tn = $this->tableName();
         $x = $this->factory($tn);
         // check if record exists?
@@ -157,18 +157,18 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
     {
         $tn = $this->tableName();
         $x = $this->factory($tn);
-        
+
         if(empty($req['etype']) || !strlen(trim($req['etype'])) ){
-            
+
             if (empty($req['name']) || !strlen(trim($req['name']))) {
                 $roo->jerr('name or etype missing');
             }
-            
+
             if($x->get('name', $req['name'])){
                 $roo->jerr("name already exists - '{$req['name']}'"  );
             }
         } else if (!empty($req['_bulk_names'])) {
-            
+
             $lines = explode("\n", $req['_bulk_names']);
             foreach($lines as $l) {
                 $l = trim($l);
@@ -180,22 +180,22 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
                     'etype' => $req['etype'],
                     'name' => array_shift($bits)
                 );
-                
+
                 $rr['display_name'] = empty($bits) ? $rr['name'] : $bits[0];
-                
+
                 $x = $this->factory($tn);
                 $x->beforeInsert($rr, $roo);
                 $x->setFrom($rr);
                 $x->insert();
-                
+
             }
             $roo->jok("inserted");
-            
+
         } else {
             if (empty($req['name']) || !strlen(trim($req['name']))) {
                 $roo->jerr('name missing');
             }
-            
+
             $x->whereAdd("etype = '{$this->escape($req['etype'])}' AND name = '{$this->escape($req['name'])}'");
             $x->find(true);
             if($x->count() > 0){
@@ -203,14 +203,14 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
             }
         }
     }
-    
+
     function onInsert($req, $roo)
     {
         $x = $this->factory($this->tableName());
         $x->query("SELECT core_enum_seqmax_update('". $this->escape($this->etype) ."')"); // no idea why need to do this!!??
-         
+
     }
-    
+
     function onUpdate($old, $req, $roo)
     {
         $x = $this->factory($this->tableName());
@@ -218,13 +218,13 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         if ($old->etype != $this->etype) {
             $x->query("SELECT core_enum_seqmax_update('". $this->escape($old->etype) ."')");
         }
-        
+
         if($this->name != $old->name && !empty($old->name) && empty($old->etype) && empty($this->etype)){
-            $x->query("UPDATE core_enum SET etype = '". $this->escape($this->name) 
+            $x->query("UPDATE core_enum SET etype = '". $this->escape($this->name)
                 ."' WHERE etype = '". $this->escape($old->name)."'");
         }
     }
-    
+
     /**
      * lookup by etype/name and return id
      */
@@ -237,9 +237,9 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         }
         return 0;
     }
-    
+
     function lookupCreate($etype,$name, $display_name=false) {
-        
+
         // check
         $ce = DB_DataObject::Factory('core_enum');
         $ce->setFrom(array(
@@ -250,7 +250,7 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
             $ce->display_name = $etype;
             $ce->insert();
         }
-        
+
         $ce = DB_DataObject::Factory('core_enum');
         $ce->etype = $etype;
         $ce->name = $name;
@@ -260,35 +260,35 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         $ce->active = 1;
         $ce->display_name = $display_name === false ? $ce->name : $display_name;
         return  $ce->insert();
-        
+
     }
-    
+
     function lookupById($id) {
         $ce = DB_DataObject::Factory('core_enum');
         $ce->get($id);
         return $ce;
     }
-    
+
     /**
-     * 
-     * 
-     * 
+     *
+     *
+     *
      * @param string $etype
      * @param array $name array of name
-     * @return array ID of core_enum 
+     * @return array ID of core_enum
      */
-    
+
     function lookupAllByName($etype,$names) {
         $ce = DB_DataObject::Factory('core_enum');
         $ce->etype = $etype;
         $ce->whereAddIn('name', $names, 'string');
-        
+
         if ($ce->count() > 0) {
             return $ce->fetchAll('id');
         }
         return array();
     }
-    
+
     function fetchAllByType($etype, $fetchArg1=false, $fetchArg2=false, $fetchArg3=false)
     {
         $x = DB_DataObject::factory('core_enum');
@@ -297,10 +297,10 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         $x->orderBy('seqid ASC');
         return $x->fetchAll($fetchArg1, $fetchArg2, $fetchArg3);
     }
-    
+
     function lookupObject($etype,$name, $create= false)
     {
-        
+
         static $cache = array();
         $key = "$etype:$name";
         if (isset($cache[$key]) ) {
@@ -318,20 +318,18 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
             $ce->insert();
             $cache[$key] = $ce;
             return $ce->id;
-            
+
         }
-        
-        
         return false;
-        
+
     }
      // fixme - all calls should be to initDatabase, we need to remove initEnums
     function initDatabase($roo, $data)
     {
         $this->initEnums($data);
     }
-    
-    
+
+
     function initEnums($data, $base = array())
     {
         // base only contains etype...
@@ -349,78 +347,78 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
         }
         foreach($data as $row) {
             $t = DB_DAtaObject::Factory('core_enum');
-            
+
             $t->etype = isset($row['etype']) ? $row['etype'] : '';
             $t->etype = isset($base['etype']) ? $base['etype'] : $t->etype ;
-            
+
             $t->name = isset($row['name']) ? $row['name'] : '';
-            
+
             if (empty($t->name)) {
                 print_R($data);
                 die("ERROR: invalid name used for core_enum\n\n");
             }
-            
+
             if (!$t->count()) {
                 // base already gave it the etype..
                 $t->setFrom($row);
-                
-                
+
+
                 //$t->is_system_enum = 1; // this should be on the caller..
-                
+
                 if (!empty($row['seqid']) && !is_numeric($row['seqid'])) {
                     $t->seqid = $seq_id;
                     $seq_id++;
                 }
-                
+
                 $t->insert();
             }else{
                 $t->find(true); // fetch it..
                 $o = clone($t);
-                
+
                 if ( isset($row['is_system_enum'])) {
                      $t->is_system_enum = isset($row['is_system_enum']) ? $row['is_system_enum'] : $t->is_system_enum;
                 }
-                
+
                 $t->display_name = isset($row['display_name']) ? $row['display_name'] : $t->display_name;
-                
+
                 $t->seqid = isset($row['seqid']) ? $row['seqid'] : $t->seqid;
-                
+
                 $t->update($o);
-                
+
             }
             if (!empty($row['cn'])) {
                 $this->initEnums($row['cn'], array('etype' => $t->name));
             }
         }
-        
+
     }
-    
+
     function merge($merge_to, $roo)
     {
         $affects  = array();
-        
+
         $all_links = $GLOBALS['_DB_DATAOBJECT']['LINKS'][$this->_database];
-        
+
         foreach($all_links as $tbl => $links) {
             foreach($links as $col => $totbl_col) {
                 $to = explode(':', $totbl_col);
                 if ($to[0] != $this->tableName()) {
                     continue;
                 }
-                
+
                 $affects[$tbl .'.' . $col] = true;
             }
         }
-        
+
         foreach($affects as $k => $true) {
             $ka = explode('.', $k);
 
             $chk = DB_DataObject::factory($ka[0]);
-            
+
             if (!is_a($chk,'DB_DataObject')) {
                 $roo->jerr('Unable to load referenced table, check the links config: ' .$ka[0]);
             }
-            
+
             $chk->{$ka[1]} = $this->id;
 
             foreach ($chk->fetchAll() as $c){
@@ -429,12 +427,12 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
                 $c->update($cc);
             }
         }
-        
+
         $this->delete();
-        
+
         $roo->jok('Merged');
-        
+
     }
-    
-    
+
+
 }
