a5d5148fa84a7dfb05c0151f8931dd218011402c
edward <edward@roojs.com>
2017-08-31 15:54:26 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -149,7 +149,6 @@ class Pman_Core_UpdateDatabase extends Pman
         
         $this->local_base_url = $ff->Pman['local_base_url'];
         
-        print_R($this->local_base_url);exit;
         if(!empty($ff->Core_Notify)){
 //            require_once 'Pman/Core/NotifySmtpCheck.php';
 //            $x = new Pman_Core_NotifySmtpCheck();
