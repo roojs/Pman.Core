1aff900118ef6b532428c96aecb791b0463a28cc
KH Lau <khlau@roojs.com>
2017-09-04 10:26:01 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -25,7 +25,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
     public $person_id;                       // int(11)  
     public $remarks;                         // blob(65535)  blob
     public $person_table;                    // string(64)
-    public $investor_id;                           // int(11)  
+    public $coba_investor_id;                     // int(11)  
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
@@ -117,7 +117,10 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
             $this->whereAdd("$tn.on_table = ''");
         }
         
-        
+        if (!empty($q['query']['to'])) {
+            $dt = date('Y-m-d' , strtotime($q['query']['to']));
+            $this->whereAdd(" {$tn}.event_when <=  '$dt' ");
+        }
         
         if (isset($q['query']['person_sum'])) {
             //DB_DataObject::debugLevel(1);
