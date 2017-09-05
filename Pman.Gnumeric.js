7c54b2de84131a34a2a4ef29925fcde494f113ce
edward <edward@roojs.com>
2017-09-05 18:33:36 +0800

    Pman.Gnumeric.js

--- Pman.Gnumeric.js
+++ Pman.Gnumeric.js
@@ -700,9 +700,6 @@ Roo.extend(Pman.Gnumeric, Roo.util.Observable, {
         
         data = data || this.data;
         
-        Roo.log('run??');
-        Roo.log(data);
-        
         for (var r = 0; r < this.rmax;r++) {
             if (typeof(this.grid[r]) == 'undefined') {
                 continue;
