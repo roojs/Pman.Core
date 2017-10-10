cd7399a5866dea693882403713b3c310c08070b6
edward <edward@roojs.com>
2017-10-10 20:07:33 +0800

    DataObjects/Core_group.php

--- DataObjects/Core_group.php
+++ DataObjects/Core_group.php
@@ -52,6 +52,7 @@ class Pman_Core_DataObjects_Core_group extends DB_DataObject
     
     function beforeInsert($q,$roo)
     {
+        print_R($q);exit;
         if (isset($q['_action'])) {
             // add // sub...
             $g = clone($this);
