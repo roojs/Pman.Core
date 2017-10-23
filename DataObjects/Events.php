78e40df0ceaf1cde749ae226c5ad5c35a4574b95
KH Lau <khlau@roojs.com>
2017-10-23 15:42:43 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -39,6 +39,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
         // if not empty on_table
         
         if(!empty($q['person_table'])){
+        		
             $jt = DB_DataObject::factory($q['person_table']);
 
             $et = DB_DataObject::factory($tn);
