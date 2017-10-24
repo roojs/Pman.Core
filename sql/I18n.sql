8900e404e79b68e0a21767edb92bcf83848c3509
john <john@roojs.com>
2017-10-24 10:56:53 +0800

    sql/I18n.sql

--- sql/I18n.sql
+++ sql/I18n.sql
@@ -3,7 +3,7 @@
 CREATE TABLE  i18n (
   id int(11)  NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id)
-  
+
 ) ENGINE=InnoDB;
 
 ALTER TABLE  i18n ADD COLUMN   ltype varchar(1)  NOT NULL DEFAULT '';
@@ -11,6 +11,7 @@ ALTER TABLE  i18n ADD COLUMN   lkey varchar(8)  NOT NULL DEFAULT '';
 ALTER TABLE  i18n ADD COLUMN   inlang varchar(8)  NOT NULL DEFAULT '';
 ALTER TABLE  i18n ADD COLUMN   lval varchar(64)  NOT NULL DEFAULT '';
 ALTER TABLE i18n ADD COLUMN is_active int(1) NOT NULL DEFAULT 1;
-  
+ALTER TABLE i18n ADD COLUMN is_prefer int(2) NOT NULL DEFAULT 0;
+
 ALTER TABLE i18n ADD INDEX lookup (ltype, lkey, inlang);
 ALTER TABLE i18n ADD INDEX lookup_a (ltype, lkey, inlang, is_active);
