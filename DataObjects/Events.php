da63930ebe5f5dd518d2d09aa336261085f6d696
KH Lau <khlau@roojs.com>
2017-10-27 16:40:36 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -321,6 +321,8 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
         } 
           
         $this->person_name = $au && !empty($au->name) ? $au->name : '';
+        print_r($au);
+        exit;
         //$this->who = $au && !empty($au->name) ? $au->name : '';
         if (empty($au) || (isset($au->id) && empty($au->id))) {
             // not authenticated - and a standard id based object
