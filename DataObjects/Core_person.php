a7c7a6e34de1075ff7e3d5745bdeb44194519bbb
edward <edward@roojs.com>
2017-11-02 19:12:22 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -1337,9 +1337,9 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
             return false;
         }
         
-        $uri = "otpauth://totp/ACME%20Co:{$this->email}?secret={$this->oath_key}&issuer=ACME%20Co&algorithm=SHA1&digits=6&period=30"
+        $uri = "otpauth://totp/ACME%20Co:{$this->email}?secret={$this->oath_key}&issuer=ACME%20Co&algorithm=SHA1&digits=6&period=30";
         
-        $base64 = base64_encode(file_get_contents("https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl={$this->oath_key}"));
+        $base64 = base64_encode(file_get_contents("https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl={$uri}"));
         
         return "data:image/png;base64,{$base64}";
     }
