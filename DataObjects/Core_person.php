cc30ef574f59f5e25d1d77ad37dfa908eb8d69ee
edward <edward@roojs.com>
2017-11-02 19:23:51 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -1325,13 +1325,7 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
     
     function generateOathKey()
     {
-        require_once 'Text/Password.php';
-        
-//        $key = strtr(ucfirst(Text_Password::create(20)).ucfirst(Text_Password::create(20)), array(
-//        "a"=>"4", "e"=>"3",  "i"=>"1",  "o"=>"0", "s"=>"5",  "t"=>"7"));
-        
         $hex = bin2hex(openssl_random_pseudo_bytes(10));
-        print_R($hex);exit;
         $this->oath_key = $hex;
         return $this->oath_key;
         
