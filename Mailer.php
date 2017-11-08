930b55128fbf644045e93cb552c3df45bbf56112
john <john@roojs.com>
2017-11-08 17:47:28 +0800

    Mailer.php

--- Mailer.php
+++ Mailer.php
@@ -314,8 +314,10 @@ class Pman_Core_Mailer {
         
         $pg = $ff->page;
         
-        print_r('die');exit;
         $email = is_array($email)  ? $email : $this->toData();
+        print_r('die');exit;
+        
+        
         if (is_a($email, 'PEAR_Error')) {
             $pg->addEvent("COREMAILER-FAIL",  false, "email toData failed"); 
       
