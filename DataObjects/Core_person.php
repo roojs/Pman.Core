cbf88d6338d5f7dccc7cbdf9a7ff770145d132eb
edward <edward@roojs.com>
2017-11-13 12:35:25 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -332,8 +332,6 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
                 if(!$default_admin->get($member->user_id)){
                     $default_admin = false;
                 }
-                
-                print_R($default_admin);exit;
             }
         }
         
