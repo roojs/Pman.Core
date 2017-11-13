cea1376a696f120bb3bac1d1c49c35f8a456c388
edward <edward@roojs.com>
2017-11-13 12:41:39 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -352,9 +352,6 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
             ) &&
             ($default_admin ||  $u->get('email', $ff->Pman['local_autoauth']))
         ) {
-            echo "admin info start...\n";
-            print_R(serialize($default_admin));
-            echo "admin info end...\n";
             $_SESSION[get_class($this)][$sesPrefix .'-auth'] = serialize($default_admin ? $default_admin : $u);
             return true;
         }
