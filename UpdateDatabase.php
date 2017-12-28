db63320725abbc8006eeb9809ab37787d75342e3
john <john@roojs.com>
2017-12-28 15:18:15 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -858,7 +858,8 @@ class Pman_Core_UpdateDatabase extends Pman
                 $got_it = false;
                 
             }
-            //
+            print_r('got it');
+            print_r($got_it);
             if (!$got_it) {
                 $this->jerr("template {$name} does not have a test method {$clsname}::test_{$name}");
             }
