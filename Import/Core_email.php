3f54837f944055995e82c30fce8bd50bad25c40b
john <john@roojs.com>
2017-12-28 15:38:44 +0800

    Import/Core_email.php

--- Import/Core_email.php
+++ Import/Core_email.php
@@ -54,7 +54,7 @@ class Pman_Core_Import_Core_email extends Pman
         $template_name = preg_replace('/\.[a-z]+$/i', '', basename($opts['file']));
         
         if (!file_exists($opts['file'])) {
-            print_r('die here');exit;
+            print_r($opts['file']);exit;
             $this->jerr("file does not exist : " . $opts['file']);
         }
         
