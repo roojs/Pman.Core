7fb1e364c547b55bef287b0d7366506eb6d4692a
edward <edward@roojs.com>
2018-01-09 15:39:19 +0800

    sql/core_ip_access.sql

--- sql/core_ip_access.sql
+++ sql/core_ip_access.sql
@@ -7,6 +7,7 @@ CREATE TABLE core_ip_access (
 ALTER TABLE core_ip_access ADD COLUMN ip VARCHAR(32) NOT NULL DEFAULT '';
 ALTER TABLE core_ip_access ADD COLUMN created_dt DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00';
 ALTER TABLE core_ip_access ADD COLUMN status INT(2) NOT NULL DEFAULT 0; -- (-1 blocked, 0 new, 1 approved)
+ALTER TABLE core_ip_access ADD COLUMN authorized_by INT(11) NOT NULL DEFAULT 0;
 
 
 
