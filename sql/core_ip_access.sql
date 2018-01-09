6c070bbf62ae8113834ae7082d880506a602904c
edward <edward@roojs.com>
2018-01-09 16:12:07 +0800

    sql/core_ip_access.sql

--- sql/core_ip_access.sql
+++ sql/core_ip_access.sql
@@ -9,9 +9,5 @@ ALTER TABLE core_ip_access ADD COLUMN created_dt DATETIME NOT NULL DEFAULT '0000
 ALTER TABLE core_ip_access ADD COLUMN status INT(2) NOT NULL DEFAULT 0; -- (-1 blocked, 0 new, 1 approved)
 ALTER TABLE core_ip_access ADD COLUMN authorized_by INT(11) NOT NULL DEFAULT 0;
 ALTER TABLE core_ip_access ADD COLUMN authorized_key VARCHAR(256) NOT NULL DEFAULT '';
-ALTER TABLE core_ip_access ADD COLUMN authorized_by INT(11) NOT NULL DEFAULT 0;
-ALTER TABLE core_ip_access ADD COLUMN authorized_key VARCHAR(256) NOT NULL DEFAULT '';
 
-ALTER TABLE core_locking ADD  INDEX lookup(on_table, on_id, person_id, created);
--- oops... - wrong name of pid.
-alter table  core_locking change column `int` id int(11) auto_increment not null;
+ALTER TABLE core_ip_access ADD INDEX ip_status_lookup(ip, status);
