4fe139fef02c12a2ea1b980e3b94379726ce0d88
edward <edward@roojs.com>
2018-01-09 16:08:37 +0800

    sql/core_ip_access.sql

--- sql/core_ip_access.sql
+++ sql/core_ip_access.sql
@@ -9,13 +9,9 @@ ALTER TABLE core_ip_access ADD COLUMN created_dt DATETIME NOT NULL DEFAULT '0000
 ALTER TABLE core_ip_access ADD COLUMN status INT(2) NOT NULL DEFAULT 0; -- (-1 blocked, 0 new, 1 approved)
 ALTER TABLE core_ip_access ADD COLUMN authorized_by INT(11) NOT NULL DEFAULT 0;
 ALTER TABLE core_ip_access ADD COLUMN authorized_key VARCHAR(256) NOT NULL DEFAULT '';
+ALTER TABLE core_ip_access ADD COLUMN authorized_by INT(11) NOT NULL DEFAULT 0;
+ALTER TABLE core_ip_access ADD COLUMN authorized_key VARCHAR(256) NOT NULL DEFAULT '';
 
-
-
-ALTER TABLE  core_locking ADD COLUMN    on_id int(11)  NOT NULL DEFAULT 0;
-ALTER TABLE  core_locking ADD COLUMN  person_id int(11)  NOT NULL DEFAULT 0;
-ALTER TABLE  core_locking ADD COLUMN  created datetime ;
-
-alter table  core_locking ADD  INDEX lookup(on_table, on_id, person_id, created);
+ALTER TABLE core_locking ADD  INDEX lookup(on_table, on_id, person_id, created);
 -- oops... - wrong name of pid.
 alter table  core_locking change column `int` id int(11) auto_increment not null;
