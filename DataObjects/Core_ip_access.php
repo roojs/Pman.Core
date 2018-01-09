cb6f8b8c74805d5b7c31c96f38636f16de345083
edward <edward@roojs.com>
2018-01-09 16:15:04 +0800

    DataObjects/Core_ip_access.php

--- DataObjects/Core_ip_access.php
+++ DataObjects/Core_ip_access.php
@@ -1,8 +1,25 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class_exists('DB_DataObject') ? '' : require_once 'DB/DataObject.php';
 
+class Pman_Core_DataObjects_Core_ip_access extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'core_ip_access';
+    public $id;
+    public $ip;
+    public $created_dt;
+    public $status;
+    public $authorized_by;
+    public $authorized_key;
+    
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+    
+    function applyFilters($q, $au)
+    {
+        
+    }
+}
