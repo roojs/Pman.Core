b92e52ec52ee0aaddaa2195af705b5f6ace25d06
KH Lau <khlau@roojs.com>
2018-01-10 18:39:10 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -312,7 +312,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
             $this->get('name', $contents['template']);
         }
         
-			var_dump($this->active);exit;        
+			var_dump($contents['template']);exit;        
         
         if(empty($this->active)){
             return $p->raiseError("template [{$contents['template']}] is Disabled");
