d3f5fb1a8030ca421609e458e7a0943364584599
john <john@roojs.com>
2018-01-11 16:40:56 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -349,7 +349,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
         if (!empty($contents['subject_replace'])) {
             foreach ($contents['mapping'] as $pattern => $replace) {
                 $contents['subject'] = preg_replace($pattern,$replace,$contents['subject']);
-                var_dump('replace');
+                var_dump($contents['subject']);
             }
         }
         
