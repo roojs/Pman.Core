c3efa61e0b8ab30df1cb8ab53490786d26abeac2
edward <edward@roojs.com>
2018-01-15 10:38:12 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -257,9 +257,15 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
         
     }
     
-    function permission_filter()
+    function applyPermissionFilters()
     {
-        
+        if (!$au->hasPerm("Admin.Admin_Tab", 'S') &&  !$au->hasPerm("BAdmin.BAdmin", 'S')) {
+            //DB_DataObject::DebugLevel(1);
+            // they can only view their changes..
+            $this->whereAdd("($tn.person_id = {$au->id} OR $tn.person_id = 0)");
+//            $this->person_id = $au->id;
+            
+        }
     }
     
     /**
