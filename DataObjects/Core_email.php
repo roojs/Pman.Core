374412a0b0b2320208b76f632446229be85705cd
KH Lau <khlau@roojs.com>
2018-02-02 15:12:23 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -82,9 +82,9 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
                 'groud_id' => $this->to_group
             ));
             
-            //if (!$c->find(true)) {
-            //    
-            //}
+            if (!$c->find(true)) {
+              echo "not found";  
+            }
             
             print_r($this->to_group);
             //print_r($c);
