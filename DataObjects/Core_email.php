3a4fefa61a569b5336f568ad5bbbe3761b95d40f
Alan Knowles <alan@roojs.com>
2018-02-05 11:55:44 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -79,7 +79,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
             $c = DB_DataObject::factory('core_group_member');            
             $c->whereAdd("group_id = {$this->to_group}");
                         
-            if (!$c->find(true)) {
+            if (!$c->count()) {
                 if (empty($request['_ignore_group_count'])) {
                     $roo->jerr('no_member',array('errcode'=> 100));
                 }
