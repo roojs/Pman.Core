7a7700488011546d7f153374a0512af6a5881fa9
KH Lau <khlau@roojs.com>
2018-02-07 15:56:18 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -78,6 +78,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
         	   
             $c = DB_DataObject::factory('core_group_member');            
             $c->whereAdd("group_id = {$this->to_group}");
+            print_r($request['_ignore_group_count']);
             print_r($c->count());exit;            
             if ($c->count() && empty($request['_ignore_group_count'])) {
                 $roo->jerr('no_member',array('errcode'=> 100));
