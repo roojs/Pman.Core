f559f67276617e3a2d4028c4b492c1ce981125e5
edward <edward@roojs.com>
2018-02-14 16:21:05 +0800

    DataObjects/Core_ip_access.php

--- DataObjects/Core_ip_access.php
+++ DataObjects/Core_ip_access.php
@@ -44,7 +44,7 @@ class Pman_Core_DataObjects_Core_ip_access extends DB_DataObject
             
         $conn = new Net_XMPP('talk.google.com', 5222, $ff->Pman['XMPP']['username'], $ff->Pman['XMPP']['password'], 'xmpphp', 'gmail.com', $printlog=false, $loglevel=Net_XMPP_Log::LEVEL_VERBOSE);
 
-        print_R("{$ff->baseURL}/Core/VerifyAccess");exit;
+        $url = "{$ff->baseURL}/Core/VerifyAccess";
         
         try {
             $conn->connect();
