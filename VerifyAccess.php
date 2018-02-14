8846bebf767b001205e75bb012a1f70864a3469a
edward <edward@roojs.com>
2018-02-14 17:46:56 +0800

    VerifyAccess.php

--- VerifyAccess.php
+++ VerifyAccess.php
@@ -1,8 +1,20 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'Pman.php';
 
+class Pman_Core_VerifyAccess extends Pman
+{
+    /*
+     * This is a public page
+     */
+    function getAuth() 
+    {
+        return true;
+    }
+    
+    function get($id)
+    {
+        
+    }
+    
+}
