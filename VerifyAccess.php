f17cc11b5e3f272c8853816a89f2869ab9b06b9b
edward <edward@roojs.com>
2018-02-14 17:56:43 +0800

    VerifyAccess.php

--- VerifyAccess.php
+++ VerifyAccess.php
@@ -14,8 +14,28 @@ class Pman_Core_VerifyAccess extends Pman
     
     function get($id)
     {
-        
         @list($id, $key) = explode('/', $id);
+        
+        $ff = HTML_FlexyFramework::get();
+        
+        $core_person = DB_DataObject::factory('core_person');
+        $core_ip_access = DB_DataObject::factory('core_ip_access');
+        
+        if(
+                empty($ff->Pman['ip_management']) || 
+                empty($ff->Pman['XMPP']) ||
+                empty($ff->Pman['XMPP']['to']) ||
+                !$core_person->get('email', $ff->Pman['XMPP']['to']) ||
+                empty($id) ||
+                empty($key) ||
+                !$core_ip_access->get($id) ||
+                $core_ip_access->authorized_key != $key
+        ) {
+            $this->jerr('Invalid URL');
+        }
+        
+        
+        
         
         
         
