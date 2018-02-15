371e8660cd5c835bf097f214382141172a0bcb39
KH Lau <khlau@roojs.com>
2018-02-15 10:25:31 +0800

    DataObjects/Core_events_archive.php

--- DataObjects/Core_events_archive.php
+++ DataObjects/Core_events_archive.php
@@ -2,9 +2,9 @@
 /**
  * Table Definition for core_events_archive
  */
-require_once 'Pman/Core/DataObjects/Events.php';
+require_once 'Events.php';
 
-class Pman_PressRelease_DataObjects_Core_events_archive extends Pman_Core_DataObjects_Events
+class Pman_Core_DataObjects_Core_events_archive extends Pman_Core_DataObjects_Events
 {
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
