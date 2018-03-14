62a758eef49de16e45ff373dd6a57f2d42e89085
Alan Knowles <alan@roojs.com>
2018-03-14 17:50:22 +0800

    sql/core_email.sql

--- sql/core_email.sql
+++ sql/core_email.sql
@@ -20,8 +20,8 @@ ALTER TABLE core_email CHANGE COLUMN bcc_group_ bcc_group_id INT(11) NOT NULL DE
 ALTER TABLE core_email ADD COLUMN bcc_group_id INT(11) NOT NULL DEFAULT 0;
 
 -- each email template should have  a test class with a static method ::test_{name}
+ALTER TABLE core_email CHANGE COLUMN test_class VARCHAR(254) NOT NULL DEFAULT '';
 ALTER TABLE core_email ADD COLUMN test_class VARCHAR(254) NOT NULL DEFAULT '';
-ALTER TABLE core_email MODIFY COLUMN test_class VARCHAR(254) NOT NULL DEFAULT '';
 
 ALTER TABLE core_email CHANGE COLUMN  in_group to_group_id INT(11) NOT NULL DEFAULT -1;
 ALTER TABLE core_email ADD COLUMN    to_group_id INT(11) NOT NULL DEFAULT -1;
