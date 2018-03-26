1be157d7159abd552bfb15bc236127485c454caa
edward <edward@roojs.com>
2018-03-26 16:58:35 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -536,7 +536,7 @@ Content-Transfer-Encoding: 7bit
         if (!file_exists(dirname($cachePath))) {
             mkdir(dirname($cachePath), 0700, true);
         }
-        
+        print_R($this->bodytext);exit;
         file_put_contents($cachePath, $this->bodytext);
         
     }
