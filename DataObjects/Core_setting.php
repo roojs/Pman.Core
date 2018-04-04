8005ba1b21b6051e322d8e0b74845e0c8421da1d
john <john@roojs.com>
2018-04-04 16:19:19 +0800

    sync

---
+++ DataObjects/Core_setting.php
@@ -0,0 +1,71 @@
+<?php
+
+class_exists('DB_DataObject') ? '' : require_once 'DB/DataObject.php';
+
+class Pman_Core_DataObjects_Core_setting extends DB_DataObject
+{
+    public $__table = 'core_setting';
+    
+    function initKeys($dir)
+    {
+        //reject when keys exist
+        if(
+            !file_exists($dir) ||
+            file_exists("{$dir}/pub.key") ||
+            file_exists("{$dir}/pri.key")
+        ){
+            return;
+        }
+        
+        $ssl = openssl_pkey_new(array(
+            "digest_alg" => "sha512",  
+            "private_key_bits" => 1024, //returns cipher in 128 characters
+            "private_key_type" => OPENSSL_KEYTYPE_RSA
+        ));
+        
+        openssl_pkey_export($ssl, $pri_key);
+        $pub_key = openssl_pkey_get_details($ssl);
+        $pub_key = $pub_key["key"];
+        
+        file_put_contents("{$dir}/pub.key",$pub_key);
+        file_put_contents("{$dir}/pri.key",$pri_key);
+    }
+    
+    function getSetting($m,$n)
+    {
+        $s = DB_DataObject::factory('core_setting');
+        $s->setFrom(array(
+            'module' => $q['module'],
+            'name' => $q['name']
+        ));
+        if($s->find(true)) {
+            return $s;
+        }
+        return false;
+    }
+    
+    function beforeInsert($q, $roo)
+    {
+        return;
+    }
+    
+    function initSetting($a)
+    {
+        //foreach ($a) > insert
+        if(empty($a)) {
+            return;
+        }
+        
+        foreach ($a as $s) {
+            
+        }
+        
+        return;
+    }
+    
+    function encryptVal($v) {
+        
+    }
+    
+    
+}
