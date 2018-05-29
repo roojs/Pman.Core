726d4f07fab1bef8ed59f8919c7b90974af404e2
Alan Knowles <alan@roojs.com>
2018-05-29 13:02:16 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -363,8 +363,9 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
         if (!empty($this->bcc_group_id) && empty($contents['rcpts_group'])) {
             $admin = DB_DAtaObject::Factory('core_group')->lookupMembersByGroupId($this->bcc_group_id,'email');
             
-            if (empty($admin)) {
-                return $p->raiseError("template [{$contents['template']}] - bcc group is empty");
+            if (!empty($admin)) {
+                $contents['bcc'] = $admin ;
+                //return $p->raiseError("template [{$contents['template']}] - bcc group is empty");
             }
             
             $contents['bcc'] = $admin ;
