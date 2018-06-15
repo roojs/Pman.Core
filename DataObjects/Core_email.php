4eabf4324eb141ff167c96ef211adf883f80cc1a
Alan Knowles <alan@roojs.com>
2018-06-15 15:43:51 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -364,7 +364,7 @@ class Pman_Core_DataObjects_Core_email extends DB_DataObject
             $admin_grp = DB_DAtaObject::Factory('core_group')->load($this->bcc_group_id);
 	    $admin = $admin_grp->members('email');
             
-            if (empty($admin) && $admin_grp->name != 'Empty Group') {
+            if (empty($admin) && $admin_grp->name != 'Empty Group') { // allow 'empty group mname'
                 return $p->raiseError("template [{$contents['template']}] - bcc group is empty");
             }
             
