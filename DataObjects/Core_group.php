fbb3d85f6ba0c99e69dd0f0b6239cdc33a0b90c2
Alan Knowles <alan@roojs.com>
2018-06-29 15:08:27 +0800

    DataObjects/Core_group.php

--- DataObjects/Core_group.php
+++ DataObjects/Core_group.php
@@ -152,7 +152,7 @@ class Pman_Core_DataObjects_Core_group extends DB_DataObject
         $gm = DB_Dataobject::factory('core_group_member');
         $gm->group_id = $this->id;
         $gm->autoJoin();
-        PDO_DAtaObject::DebugLevel(1)
+        PDO_DAtaObject::DebugLevel(1); 
         return $gm->count();
     }
 
