2162f518335738ec4c812f667b7bfcdd857089b0
benny <benny@roojs.com>
2018-07-13 16:14:13 +0800

    Pman.Tab.PersonList.js

--- Pman.Tab.PersonList.js
+++ Pman.Tab.PersonList.js
@@ -458,7 +458,7 @@ Pman.Tab.PersonList.prototype = {
                     //Pman.genericDelete(_this, 'Person'); 
                     
                     var rec = _this.grid.ds.getAt(_this.grid.selModel.last);
-                    console.log(_this.grid);  
+                    console.log(rec);  
                     var ids = [];
                     ids.push(rec.id);
                     if (!ids.length) {
