0b7522409eb5b230697f211c78aea8d820318471
benny <benny@roojs.com>
2018-07-20 17:45:21 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -38,7 +38,9 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
                 $tn.filename LIKE '%{$this->escape($q['search']['filename'])}%'
             ");
         }
- 
+        echo $this->whereAdd("
+                $tn.filename LIKE '%{$this->escape($q['search']['filename'])}%'
+            "); die;
 
     }
     
