9681d5cc558f0615330827c8bbd38fe950d5c151
benny <benny@roojs.com>
2018-07-26 14:42:53 +0800

    DataObjects/Core_group.php

--- DataObjects/Core_group.php
+++ DataObjects/Core_group.php
@@ -53,8 +53,10 @@ class Pman_Core_DataObjects_Core_group extends DB_DataObject
                 $roo->jok($core_group->memberCount());
             }
         }
+        $cgmDBObj = DB_DataObject::Factory('core_group_member');
+        $cpObj = DB_DataObject::Factory('core_person');
         $cgm = DB_DataObject::Factory('core_group_member')->tableName();
-        
+        $cpObj->active = 1; // set the where condition with active = 1
         /*$this->selectAdd("
            (
             SELECT 
