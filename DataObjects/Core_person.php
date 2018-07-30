1476801dd72b299278bdae6a7bad03a653ede9f6
benny <benny@roojs.com>
2018-07-30 12:51:11 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -1113,6 +1113,7 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
        
         if($member->find(true)){
             $default_admin = DB_DataObject::factory($this->tableName());
+            echo $default_admin->get($member->user_id); die;
             if(!$default_admin->get($member->user_id)){
                 $e->delete(true);
                 
