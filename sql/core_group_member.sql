193b463dbc1f4fc16c4228b46f749d3418515348
Alan Knowles <alan@roojs.com>
2018-08-01 11:06:17 +0800

    sql/core_group_member.sql

--- sql/core_group_member.sql
+++ sql/core_group_member.sql
@@ -4,7 +4,9 @@ CREATE TABLE  core_group_member  (
     id int(11)  NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id)
 );
-ALTER TABLE core_group_member ADD COLUMN  group_id int(11) default NULL;
+
+ALTER TABLE core_group_member ALTER COLUMN  group_id  group_id int(11) NOT NULL default 0;
+ALTER TABLE core_group_member ADD COLUMN  group_id int(11) NOT NULL default 0;
 ALTER TABLE core_group_member ADD COLUMN   user_id int(11) NOT NULL default 0;
 
-ALTER TABLE ADD UNIQUE KEY `unique_group_user` (`group_id`,`user_id`);
+ALTER TABLE core_group_member ADD UNIQUE KEY  `unique_group_user` (`group_id`,`user_id`);
