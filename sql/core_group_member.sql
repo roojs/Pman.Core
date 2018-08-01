487c548917a1f916ea3377358b5ecdb0893586a3
Steven <steven@roojs.com>
2018-08-01 12:12:54 +0800

    sql/core_group_member.sql

--- sql/core_group_member.sql
+++ sql/core_group_member.sql
@@ -1,12 +1,13 @@
 -- BC name..
- 
+
 CREATE TABLE  core_group_member  (
     id int(11)  NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id)
 );
 
-ALTER TABLE core_group_member ALTER COLUMN  group_id  group_id int(11) NOT NULL default 0;
+ALTER TABLE core_group_member CHANGE COLUMN  group_id  group_id int(11) NOT NULL default 0;
 ALTER TABLE core_group_member ADD COLUMN  group_id int(11) NOT NULL default 0;
+
 ALTER TABLE core_group_member ADD COLUMN   user_id int(11) NOT NULL default 0;
 
 ALTER TABLE core_group_member ADD UNIQUE KEY  unique_group_user (group_id,user_id);
