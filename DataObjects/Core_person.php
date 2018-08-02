700c78d4214a88be40776e2b3f4dcc169efb1b8d
benny <benny@roojs.com>
2018-08-02 16:29:51 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -1406,7 +1406,7 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
             // insert data into core_person_member...
             //PDO_DataObject::Factory('coremytable_group_member')->set([ 'group_id' => 'test', 'user_id' => "{$this->tableName()}.id"])->insert();
             foreach($group_id_arr as $gid){
-                PDO_DataObject::Factory('coremytable_group_member')->set([ 'group_id' => $gid, 'user_id' => $this->tableName().id])->insert();
+                PDO_DataObject::Factory('coremytable_group_member')->set([ 'group_id' => $gid, 'user_id' => "{$this->tableName()}.id"])->insert();
             }
         }
     }
