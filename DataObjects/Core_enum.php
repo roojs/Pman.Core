512a29be689e52c52c8bb33e3f0094e20a25cc31
benny <benny@roojs.com>
2018-08-22 16:07:22 +0800

    DataObjects/Core_enum.php

--- DataObjects/Core_enum.php
+++ DataObjects/Core_enum.php
@@ -156,7 +156,7 @@ class Pman_Core_DataObjects_Core_enum extends DB_DataObject
     {
         $tn = $this->tableName();
         $x = $this->factory($tn);
-        echo $x; exit;
+        var_dump($x); exit;
         if(empty($req['etype']) || !strlen(trim($req['etype'])) ){
 
             if (empty($req['name']) || !strlen(trim($req['name']))) {
