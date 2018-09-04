8d43bffcc80eef19de6e793be5e21a24a07c6e36
benny <benny@roojs.com>
2018-09-04 11:36:25 +0800

    DataObjects/Core_company.php

--- DataObjects/Core_company.php
+++ DataObjects/Core_company.php
@@ -268,6 +268,7 @@ class Pman_Core_DataObjects_Core_Company extends DB_DataObject
             $this->delete_dt = date('Y-m-d H:i:s');
             $this->delete_by = $roo->getAuthUser();
             $this->id = $q['_ids'];
+            var_dump($this); die;
             $this->update();
             //$roo->query("UPDATE {$old->tableName()} SET deleted_by = {$roo->getAuthUser()} , deleted_dt = {$delete_dt} WHERE id = {$req['_update_id']}");
             //$this->addEvent("UPDATE", false, "update core_company record");
