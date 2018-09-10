45fa994fd7541e13c2d9eb26711f09e9e133092c
Alan Knowles <alan@roojs.com>
2018-09-10 10:42:55 +0800

    sql/core_group_member.sql

--- sql/core_group_member.sql
+++ sql/core_group_member.sql
@@ -10,4 +10,21 @@ ALTER TABLE core_group_member ADD COLUMN  group_id int(11) NOT NULL default 0;
 
 ALTER TABLE core_group_member ADD COLUMN   user_id int(11) NOT NULL default 0;
 
+
+-- remove duplicates...
+delete from  core_group_member where id in (
+        select mid FROM (
+            select
+                concat(group_id,'-',user_id) as uid,
+                max(id) as mid,
+                count(*) as n
+            from
+                core_group_member
+            group by
+                group_id,user_id
+            having n > 1
+        ) s
+)
+
+
 ALTER TABLE core_group_member ADD UNIQUE KEY  unique_group_user (group_id,user_id);
