7be43acea4f026c0defe83bc2de1f3c40fb0fb47
benny <benny@roojs.com>
2018-09-12 15:46:32 +0800

    Pman.Dialog.CoreEnumMerge.bjs Pman.Dialog.CoreEnumMerge.js

--- Pman.Dialog.CoreEnumMerge.js
+++ Pman.Dialog.CoreEnumMerge.js
@@ -130,8 +130,8 @@ Pman.Dialog.CoreEnumMerge = {
                    var title = _this.data.title  || _this.data.etype;
                   _this.dialog.setTitle("Delete selected " + title + " and merge data with");
                   
-                  console.log("_ids:>>>>>"+_this.data._ids);
-                  if(!_this.data._ids){
+          
+                  if(_this.data._ids != ""){
                       this.load({ method: 'GET', params: { '_id' : _this.data.id }});  
                       _this.form.findField('id').setValue(_this.data.id);
                       return;
