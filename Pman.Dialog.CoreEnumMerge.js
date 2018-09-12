c8f10474bbf659224d846154da5669b1d57ea5ef
benny <benny@roojs.com>
2018-09-12 12:38:29 +0800

    Pman.Dialog.CoreEnumMerge.bjs Pman.Dialog.CoreEnumMerge.js

--- Pman.Dialog.CoreEnumMerge.js
+++ Pman.Dialog.CoreEnumMerge.js
@@ -132,8 +132,9 @@ Pman.Dialog.CoreEnumMerge = {
                   _this.form.findField('_ids').setValue(_this.data._ids);
                   
                   if(!_this.data._ids){
-                   this.load({ method: 'GET', params: { '_id' : _this.data.id }});  
-                   _this.form.findField('_names').setValue(_this.data.name);
+                      this.load({ method: 'GET', params: { '_id' : _this.data.id }});  
+                      _this.form.findField('_names').setValue(_this.data.name);
+                      return;
                   }
                   
                   _this.data.findField('_names').setValue(_this.data.materialList);
