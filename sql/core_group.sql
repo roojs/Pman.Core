8d417f93916340671805ab67b2ef6b8ea400b108
Edward <edward@roojs.com>
2018-09-14 10:17:45 +0800

    sql/core_group.sql

--- sql/core_group.sql
+++ sql/core_group.sql
@@ -9,6 +9,7 @@ CREATE TABLE core_group (
 );
 
 ALTER TABLE core_group ADD COLUMN name varchar(128)  NOT NULL DEFAULT '';
+ALTER TABLE core_group ADD COLUMN display_name varchar(128)  NOT NULL DEFAULT '';
 ALTER TABLE core_group ADD COLUMN type  int(11)  NOT NULL DEFAULT 0;
 ALTER TABLE core_group ADD COLUMN leader int(11)  NOT NULL default 0;
 ALTER TABLE core_group ADD COLUMN is_system int(2) NOT NULL default 0;
