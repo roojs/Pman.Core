f291b63a2fd77265faf2ee80197dafe7058c4162
benny <benny@roojs.com>
2018-09-18 10:57:14 +0800

    DataObjects/Core_company.php

--- DataObjects/Core_company.php
+++ DataObjects/Core_company.php
@@ -260,12 +260,12 @@ class Pman_Core_DataObjects_Core_Company extends DB_DataObject
     function beforeUpdate($old, $q,$roo)
     {
          
-        if(!empty($q['_flag_delete']) && $q['_flag_delete'] * 1 == 1){
+        if(!empty($q['_flag_delete'])){
             $this->deleted_dt = $this->sqlValue("NOW()");
             $this->deleted_by = $roo->getAuthUser()->id;
         }
         
-        if(!empty($q['_flag_undelete']) && $q['_flag_undelete'] * 1 == 1){
+        if(!empty($q['_flag_undelete'])){
             $this->deleted_dt = "";
             $this->deleted_by = 0;
         }
