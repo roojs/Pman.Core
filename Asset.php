623de40a0c2ac603b389b3ab010415161209c316
Edward <edward@roojs.com>
2018-10-04 20:26:19 +0800

    Asset.php

--- Asset.php
+++ Asset.php
@@ -36,8 +36,6 @@ class Pman_Core_Asset extends Pman {
     
     function get($s='', $opts = Array())
     {
-
-        
         $this->sessionState(0);
         
         $bits = explode('/', $s);
