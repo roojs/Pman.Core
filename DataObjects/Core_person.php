56c54b25d387ae14b8a38db5d20c5fe74f0af808
john <john@roojs.com>
2018-10-09 14:35:42 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -772,7 +772,6 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
         // missing id for core_person mgmt
         if(!empty($q['_to_qr_code'])){
             
-            // quite dangerous..
             $au = $this->getAuthUser();
             
             $o = clone($au);
