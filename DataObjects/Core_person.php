d8205a87b26ed8bbb4331583a47cae1909dc5020
john <john@roojs.com>
2018-10-09 13:48:30 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -670,8 +670,6 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
         $aur['dailykey'] = '';
         $aur['oath_key'] = '';
         
-        $aur['oath_key_enable'] = !empty($this->oath_key);
-        
         return $aur;
     }
     
