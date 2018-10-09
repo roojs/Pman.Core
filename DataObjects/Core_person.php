ab4eeb3b5bc3fdc566ff6097b7c95f2497df3bbd
john <john@roojs.com>
2018-10-09 16:51:54 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -1446,7 +1446,7 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
         $issuer = (empty($this->name)) ? 
             rawurlencode('ROOJS') : rawurlencode($this->name);
         
-        $uri = "otpauth://totp/{$issuer}:{$this->email}?secret={$this->oath_key}&issuer={$issuer}&algorithm=SHA1&digits=6&period=30";
+        $uri = "otpauth://totp/{$issuer}:{$this->email}?secret={$oath_key}&issuer={$issuer}&algorithm=SHA1&digits=6&period=30";
         
         require_once 'Image/QRCode.php';
         
