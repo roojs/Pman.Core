99de97548dff1e66964f827e1e5cf43ca78985db
Alan Knowles <alan@roojs.com>
2018-10-11 16:26:31 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -514,7 +514,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
             @mkdir(dirname($file),0700,true); // this might fail if it does not have correct permissions..
             if (!file_exists(dirname($file))) {
                 //print_r($this);
-                die("could not create $file - permissons are not correct\n"); // fatal, otherwise we loop!?
+                die("Events:: writeEventLog: could not create $file - permissons are not correct\n"); // fatal, otherwise we loop!?
             }
             
         }
