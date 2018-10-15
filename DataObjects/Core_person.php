06960c8cdfa73aed8fd540930f0b76844a2c0a10
Alan Knowles <alan@roojs.com>
2018-10-15 13:23:53 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -346,6 +346,7 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
             
             $user = $default_admin ? $default_admin->toArray() : $u->toArray();
             
+            // if we request other URLS.. then we get auto logged in..
             $_SESSION[get_class($this)][$sesPrefix .'-auth'] = serialize((object) $user);
             return true;
         }
