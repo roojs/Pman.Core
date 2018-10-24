a98c0adbf3b7407d62850c87db4f80d6dc4ec884
Alan Knowles <alan@roojs.com>
2018-10-24 14:36:01 +0800

    mysql/core_notify_trigger_after_delete.sql

--- mysql/core_notify_trigger_after_delete.sql
+++ mysql/core_notify_trigger_after_delete.sql
@@ -9,6 +9,7 @@ DELIMITER $$
 CREATE TRIGGER core_notify_trigger_after_delete AFTER DELETE on core_notify
     FOR EACH ROW
         BEGIN
+            -- I think we changed it so that sent is not null, deafult '0000....'
             if old.sent is not null AND old.sent > '0001-01-01 00:00:00' THEN 
                 UPDATE `Error: Can not delete core_notify after it is sent` SET x = 1;
             END IF;
