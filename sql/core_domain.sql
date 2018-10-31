e4e27425b4828af3643837eabb0d7c2a99efedf7
Alan Knowles <alan@roojs.com>
2018-10-31 14:51:05 +0800

    sql/core_domain.sql

--- sql/core_domain.sql
+++ sql/core_domain.sql
@@ -2,7 +2,7 @@
 CREATE TABLE core_domain (
   id int(11) NOT NULL auto_increment,
   PRIMARY KEY  (id)
-) ENGINE=innodb;
+) ENGINE=innodb CHARSET=utf8mb4  CHARSET=utf8 ;
 
 
 ALTER TABLE core_domain ADD COLUMN domain VARCHAR(255) NOT NULL DEFAULT '';
