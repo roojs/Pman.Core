3221e19d66907a05c9d215232a51346421b8249c
Edward <edward@roojs.com>
2018-11-08 14:05:13 +0800

    sql/core_person_alias.sql

--- sql/core_person_alias.sql
+++ sql/core_person_alias.sql
@@ -8,3 +8,5 @@ ALTER TABLE core_person_alias ADD COLUMN   person_id varchar(128) DEFAULT NULL;
 ALTER TABLE core_person_alias ADD COLUMN  alias varchar(254) NOT NULL DEFAULT '';
   
 ALTER TABLE core_person_alias ADD INDEX alias (alias);
+
+ALTER TABLE core_person_alias ADD INDEX lookup_person_id (person_id);
