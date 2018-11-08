95d8e3c01a86762a6a2e4a4b3731eaf6302657cf
Edward <edward@roojs.com>
2018-11-08 13:19:39 +0800

    sql/core_person.sql

--- sql/core_person.sql
+++ sql/core_person.sql
@@ -104,3 +104,5 @@ ALTER TABLE core_person add index lookup_owner(owner_id);
 ALTER TABLE core_person ADD COLUMN post_code VARCHAR(256) NOT NULL DEFAULT '';
 
 ALTER TABLE core_person ADD COLUMN oath_key VARCHAR(254) NOT NULL DEFAULT ''; 
+
+alter table core_person add index lookup_company_id( company_id);
