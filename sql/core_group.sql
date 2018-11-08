97b2cd167b49300549225721ca211d80475140ba
Edward <edward@roojs.com>
2018-11-08 14:03:22 +0800

    sql/core_group.sql

--- sql/core_group.sql
+++ sql/core_group.sql
@@ -14,3 +14,4 @@ ALTER TABLE core_group ADD COLUMN type  int(11)  NOT NULL DEFAULT 0;
 ALTER TABLE core_group ADD COLUMN leader int(11)  NOT NULL default 0;
 ALTER TABLE core_group ADD COLUMN is_system int(2) NOT NULL default 0;
 
+ALTER TABLE core_group ADD INDEX lookup_leader (leader);
