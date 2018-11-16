a2c032a69988e1cec4124a62d15d0d48f099958d
Alan Knowles <alan@roojs.com>
2018-11-16 12:44:11 +0800

    Uncommited changes synced

--- sql/core_setting.sql
+++ sql/core_setting.sql
@@ -4,18 +4,12 @@ CREATE TABLE  core_setting  (
 );
 
 ALTER TABLE core_setting ADD COLUMN module VARCHAR(64) NOT NULL DEFAULT '';
-
 ALTER TABLE core_setting ADD COLUMN name VARCHAR(64) NOT NULL DEFAULT '';
-
 ALTER TABLE core_setting ADD COLUMN description VARCHAR(128) NOT NULL DEFAULT '';
-
-ALTER TABLE core_setting CHANGE COLUMN  val val  TEXT DEFAULT '';
-ALTER TABLE core_setting ADD COLUMN         val        TEXT NOT NULL DEFAULT '';
-
+ALTER TABLE core_setting CHANGE COLUMN  val val  BLOB DEFAULT '';
+ALTER TABLE core_setting ADD COLUMN val  BLOB NOT NULL DEFAULT '';
 ALTER TABLE core_setting ADD COLUMN updated_dt DATE NOT NULL DEFAULT '0000-00-00';
-
 ALTER TABLE core_setting ADD COLUMN is_encrypt INT(2) NOT NULL DEFAULT 1;
-
 ALTER TABLE core_setting ADD COLUMN is_valid INT(2) NOT NULL DEFAULT 0;
 
 ALTER TABLE core_setting ADD INDEX lookup_module_name(module, name);
