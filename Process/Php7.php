b341a92085a9d4a5cfb1d2043e599420472aa6c8
Edward <edward@roojs.com>
2018-12-03 10:55:46 +0800

    Process/Php7.php

--- Process/Php7.php
+++ Process/Php7.php
@@ -34,7 +34,7 @@ class Pman_Core_Process_Php7 extends Pman
                 $errline
             ));
         });
-        print_R($this->bootLoader);exit;
+        print_R($this->bootLoader->project);exit;
         $this->scan(array("Pman"));
         $this->scan(array("Hebe"));
         
