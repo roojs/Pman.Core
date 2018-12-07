5689425d46d1fcfb165128facdb5aa22da387975
Alan Knowles <alan@roojs.com>
2018-12-07 09:54:06 +0800

    UpdateDatabase.php

--- UpdateDatabase.php
+++ UpdateDatabase.php
@@ -90,7 +90,14 @@ class Pman_Core_UpdateDatabase extends Pman
             
         ),
     );
-    
+    var $emailTemplates = array(
+        'ADMIN_PASSWORD_RESET' => array(
+            'bcc_group' => 'Administrators',
+            'test_class' => 'Pman/Core/DataObjects/Core_person',
+            'to_group' => 'Review Submission Group',
+            'active' => 0,
+            'description' => '3. Application submitted - email to admin with deposit slip'
+        ),
     static function cli_opts()
     {
         
