6be23511eb5d1e15160703f7cdd6882b2cf68cf3
Alan Knowles <alan@roojs.com>
2019-01-17 13:15:04 +0800

    _translations_/Bjs.php

--- _translations_/Bjs.php
+++ _translations_/Bjs.php
@@ -9,6 +9,11 @@
 class Pman_Core_Bjs {
     
     
+    function formFields($file)
+    {
+        $json = json_decode(file_get_contents($file));
     
+        return $this->iterateFields($json->items,$res);
+    }
     
 }
