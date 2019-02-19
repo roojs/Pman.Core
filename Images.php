43abf3a844684f5550da7c7a5b4a45b3c45e8374
Edward <edward@roojs.com>
2019-02-19 17:15:31 +0800

    Images.php

--- Images.php
+++ Images.php
@@ -317,7 +317,7 @@ class Pman_Core_Images extends Pman
         } else {
             $x->convert( $this->as_mimetype, $this->size);
         }
-        print_R($x);exit;
+        
         $x->serve();
         exit;
         
