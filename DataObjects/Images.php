ad47ceb1a1f7fa9adcecd85b6e7b32f1f21603c5
Edward <edward@roojs.com>
2019-03-29 13:06:36 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -491,10 +491,9 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
             
         }
         
-        $filename = preg_replace("/[-]+/", '-', preg_replace("/[^A-Za-z0-9.]/", '-', $this->filename));
+        $filename = explode('.', preg_replace("/[-]+/", '-', preg_replace("/[^A-Za-z0-9.]/", '-', $this->filename)));
         
-        @list($name, $ext) = explode('.', $filename);
-                
+        print_R($filename);exit;
         if(strlen($filename) > 32) {
             echo "{$this->filename}";
             echo "<br/>";
