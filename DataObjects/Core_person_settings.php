4f40a9efcaa6a1d88fb16fa932eacafaf99637ed
Edward <edward@roojs.com>
2019-04-01 17:50:56 +0800

    DataObjects/Core_person_settings.php

--- DataObjects/Core_person_settings.php
+++ DataObjects/Core_person_settings.php
@@ -1,8 +1,21 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Table Definition for Core_person_settings
  */
+class_exists('DB_DataObject') ? '' : require_once 'DB/DataObject.php';
 
+class Pman_Core_DataObjects_Core_person_settings extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'core_person_settings';
+    public $id;
+    public $person_id;
+    public $scope;
+    public $data;
+    
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+    
+ }
