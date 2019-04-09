be3cbe9086fe24cf2eb1b7ee57129fc0c0637ef8
Edward <edward@roojs.com>
2019-04-09 13:31:16 +0800

    Pman.Dialog.Image.bjs Pman.Dialog.Image.js

--- Pman.Dialog.Image.js
+++ Pman.Dialog.Image.js
@@ -54,7 +54,7 @@ Pman.Dialog.Image = {
     closable : false,
     collapsible : false,
     haveProgress : false,
-    height : 140,
+    height : 150,
     modal : true,
     resizable : false,
     shadow : true,
