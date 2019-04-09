fed6064caa23f98dbae645ec3ed04ed0ac6624d3
Edward <edward@roojs.com>
2019-04-09 13:27:37 +0800

    Pman.Dialog.Image.bjs Pman.Dialog.Image.js

--- Pman.Dialog.Image.js
+++ Pman.Dialog.Image.js
@@ -54,7 +54,7 @@ Pman.Dialog.Image = {
     closable : false,
     collapsible : false,
     haveProgress : false,
-    height : 140,
+    height : 150,
     modal : true,
     resizable : false,
     shadow : true,
@@ -224,8 +224,11 @@ Pman.Dialog.Image = {
               
                   _this.form.findField('imgtype').hide();
                   
+                  _this.dialog.resizeTo(500, 150);
+                  
                   if(_this.data._show_image_type){
                       _this.form.findField('imgtype').show();
+                      _this.dialog.resizeTo(500, 180);
                   }
                   
                   this.url = _this.data._url ? _this.data._url : baseURL + '/Roo/Images.php';
