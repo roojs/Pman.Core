19447ce4e1bd1cbb94a1994fecd5011ae4b9e907
Alan Knowles <alan@roojs.com>
2019-07-04 15:23:43 +0800

    Fix #5911 - issue with password

--- widgets/SecurePass.js
+++ widgets/SecurePass.js
@@ -179,7 +179,7 @@ Ext.extend(Ext.form.SecurePass, Ext.form.TextField, {
             return true;
         }
         
-        if ('[\x21-\x7e]*'.match(value)) {
+        if (value.match(/[\x21-\x7e]*/)) {
             this.markInvalid(this.errors.PwdBadChar);
             return false;
         }
