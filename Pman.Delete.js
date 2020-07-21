3c32040ca8b807469e6bd499ab5a85a2681f455c
Alan Knowles <alan@roojs.com>
2020-07-21 14:56:09 +0800

    Pman.Delete.js

--- Pman.Delete.js
+++ Pman.Delete.js
@@ -43,20 +43,7 @@ Pman.Delete = {
                 }
                 tab.grid.getView().mainWrap.mask("Deleting");
                 Pman.Delete.simpleCall(tab, tbl, r, function(response) {
-                    tab.grid.getView().mainWrap.unmask();
-                    if ( tab.paging ) {
-                        tab.paging.onClick('refresh');   
-                    } else if (tab.grid.footer && tab.grid.footer.onClick) {
-                        // new xtype built grids
-                        tab.grid.footer.onClick('refresh');   
-                    } else if (tab.refresh) {
-                        tab.refresh(); // this might cause problems as panels have a refresh method?
-                    } else {
-                        tab.grid.getDataSource().load();
-                    }
-                    
-                    
-                    
+                    Pman.Delete.completed(tab);
                 });
                 
             }
@@ -93,66 +80,57 @@ Pman.Delete = {
     
     progress : function(tab, tbl) {
         
-        var r = [];
- 
-            
-        var s = tab.grid.getSelectionModel().getSelections();
-        if (!s.length)  {
-            Roo.MessageBox.alert("Error", "Select at least one Row to delete" );
-            return '';
+        var r = this.selections(tab);
+        if (!r.length) {
+            return;
         }
-        var reader = tab.grid.reader || tab.grid.ds.reader;
-        for(var i = 0; i < s.length; i++) {
-            r.push(reader.getId(s[i].json));
-        }
-    
+        
+        
         Roo.MessageBox.confirm("Confirm", "Are you sure you want to delete that?",
             function(btn) {
                 if (btn != 'yes') {
                     return;
                 }
-                // what about the toolbar??
-                tab.grid.getView().mainWrap.mask("Deleting");
-                new Pman.Request({
-                    url: baseURL + '/Roo/'+tbl+'.php',
-                    method: 'POST',
-                    params: {
-                        _delete : r.join(',')
-                    },
-                    success: function(response) {
-                        tab.grid.getView().mainWrap.unmask();
-                        if ( tab.paging ) {
-                            tab.paging.onClick('refresh');   
-                        } else if (tab.grid.footer && tab.grid.footer.onClick) {
-                            // new xtype built grids
-                            tab.grid.footer.onClick('refresh');   
-                        } else if (tab.refresh) {
-                            tab.refresh(); // this might cause problems as panels have a refresh method?
-                        } else {
-                            tab.grid.getDataSource().load();
+                Roo.MessageBox.progress("Delete Records", "Deleting 1/" + r.length );
+                
+                
+                var p = 0;
+                
+                var delItem = function ()
+                {
+                    Roo.MessageBox.updateProgress(  (p+1)/r.length, "Deleting " + (1+p) + "/" + r.length );
+                    Pman.Delete.simpleCall(tab, tbl, [ r[p] ], function(response) {
+                        
+                        p++;
+                        if (p == r.length) {
+                            Roo.MessageBox.hide();
+                            Pman.Delete.completed(tab);
+                            return;
                         }
                         
-                        
-                        
-                    },
-                    failure: function(act) {
-                        
-                        Roo.log(act);
-                        var msg = '';
-                        try {
-                            msg = act.errorMsg;
-                        } catch(e) {
-                            msg = "Error deleting";
-                        }
-                        tab.grid.getView().mainWrap.unmask();
-                        Roo.MessageBox.alert("Error",  msg);
-                    }
-                    
-                });
+                        delItem();
+                    });
+                }
+                delItem();
+               
+                
             }
             
         );
-        return '';
     },
+    completed : function()
+    {
+        tab.grid.getView().mainWrap.unmask();
+        if ( tab.paging ) {
+            tab.paging.onClick('refresh');   
+        } else if (tab.grid.footer && tab.grid.footer.onClick) {
+            // new xtype built grids
+            tab.grid.footer.onClick('refresh');   
+        } else if (tab.refresh) {
+            tab.refresh(); // this might cause problems as panels have a refresh method?
+        } else {
+            tab.grid.getDataSource().load();
+        }
     
+    }
 }
