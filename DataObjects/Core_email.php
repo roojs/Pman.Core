4d88c3447e01db90b90fe09836c6982658a5f04b
Alan Knowles <alan@roojs.com>
2020-09-24 11:49:56 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -675,5 +675,60 @@ Content-Transfer-Encoding: 7bit
     }
     
     
-    
+    function testData()
+    {
+	// should return the formated email???
+	print_r(function_get_args());
+            
+	    /*
+	    $core_email = DB_DataObject::factory('core_email');
+        
+        if(!$core_email->get($_REQUEST['_id'])){
+            $this->jerr('Invalid Message ID');
+        }
+        
+        if(empty($core_email->test_class)){
+            $this->jerr("[{$core_email->name}] does not has test class");
+        }
+        
+        require_once "{$core_email->test_class}.php";
+        
+        $cls = str_replace('/', '_', $core_email->test_class);
+        
+        $x = new $cls;
+        
+        $method = "test_{$core_email->name}";
+        
+        if(!method_exists($x, $method)){
+            $this->jerr("{$method} does not exists in {$cls}");
+        }
+        
+        $content = $x->{$method}($this, $this->authUser);
+        
+        $content['bcc'] = array();
+       
+        
+        
+        $cn = DB_DataObject::factory('core_notify');
+        $cn->setFrom(array(
+            'evtype'        => 'Core_email:testData',
+            'onid'          => $_REQUEST['_id'],
+            'ontable'       => $_REQUEST['_table'],
+            'person_id'     => $this->authUser->id,
+            'person_table'  => 'Person',
+            'act_when'      => $cn->sqlValue("NOW()"),
+            'act_start'     => $cn->sqlValue("NOW()")
+        ));
+        
+        $cn->insert();
+        
+        $sent = $cn->sendManual();
+        
+        if(get_class($sent) != 'Pman_Core_NotifySend_Exception_Success'){
+            $this->jerr($sent->getMessage());
+        }
+        
+        $this->jok("SUCCESS");
+     */
+    }
 }
