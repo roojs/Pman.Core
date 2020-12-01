65320e009ce3566e2b709f294be455173a2d0ca9
Alan Knowles <alan@roojs.com>
2020-12-01 15:49:44 +0800

    sql/core_template.sql

---
+++ sql/core_template.sql
@@ -0,0 +1,11 @@
+-- template .
+CREATE TABLE  core_template (
+  id int(11)  NOT NULL AUTO_INCREMENT,
+  template varchar(254)  NOT NULL,
+  updated datetime  NOT NULL,
+  lang varchar(6)  NOT NULL,
+  PRIMARY KEY (id),
+  INDEX lookup(template, lang)
+);
+
+ALTER TABLE core_template ADD COLUMN view_name varchar(32) NOT NULL default '';
