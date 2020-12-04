d8bad62e84a69489534ddcc16a0f9f294adc1cce
Alan Knowles <alan@roojs.com>
2020-12-04 10:59:49 +0800

    sql/core_template.sql

--- sql/core_template.sql
+++ sql/core_template.sql
@@ -10,3 +10,4 @@ CREATE TABLE  core_template (
 
 ALTER TABLE core_template ADD COLUMN view_name varchar(32) NOT NULL default '';
 ALTER TABLE core_template ADD COLUMN filetype varchar(32) NOT NULL default '';
+alter table core_template add index lookupa(view_name, template., lang, filetype);
