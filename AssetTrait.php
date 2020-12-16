fef291850e1478d1f55b2ab473fa45e5e71d7237
Alan Knowles <alan@roojs.com>
2020-12-16 14:46:29 +0800

    AssetTrait.php

--- AssetTrait.php
+++ AssetTrait.php
@@ -338,10 +338,10 @@ trait Pman_Core_AssetTrait {
             $fd = dirname($fp);
                 
                 
-            $cmd = "{$sassc}  --sourcemap=auto -I {$fd} -I {$this->rootDir}/roojs1/scss/bootstrap $smod.scss {$compiledir}/{$output}";
+            $cmd = "{$sassc} --style=compressed  --sourcemap=auto -I {$fd} -I {$this->rootDir}/roojs1/scss/bootstrap $smod.scss {$compiledir}/{$output}";
             echo "$cmd\n";
             echo `$cmd`;
-            
+ 
             exit;
             
              
