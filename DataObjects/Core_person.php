ca490c4df903e2966a27c02aa97fd5705c41c3be
Alan Knowles <alan@roojs.com>
2020-12-17 14:49:44 +0800

    DataObjects/Core_person.php

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -549,7 +549,7 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
     function genAutoLoginURL($url, $expires = false)  
     {
         $expires = $expires  === false ? strtotime("NOW + 1 WEEK") : $expires;
-        echo hash('sha256', serialize(array($url, $time, $this->email, $this->passwd)));
+        echo hash('sha256', serialize(array($url, $expires, $this->email, $this->passwd)));
         
         return $url.'/'.$this->id .'/'.$expires.'/'.
             hash('sha256',
