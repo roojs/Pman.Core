720acdd7e9c141acdd9b22d16853d36286c78d1f
Alan Knowles <alan@roojs.com>
2021-01-25 10:58:39 +0800

    fix check for sesssion settings

--- DataObjects/Core_person.php
+++ DataObjects/Core_person.php
@@ -384,7 +384,7 @@ class Pman_Core_DataObjects_Core_person extends DB_DataObject
         $u = DB_DataObject::factory($this->tableName());
         $u->whereAdd(' LENGTH(passwd) > 0');
         $n = $u->count();
-        if (!is_array($_SESSION[get_class($this)])) { 
+        if (empty($_SESSION[get_class($this)]) || !is_array($_SESSION[get_class($this)])) { 
             $_SESSION[get_class($this)] = array();
         }
         $_SESSION[get_class($this)][$sesPrefix .'-empty']  = $n;
