213e5013906c9151ffbf6a6044eef7cf656cbf34
Alan Knowles <alan@roojs.com>
2021-03-18 14:45:23 +0800

    remove debug

--- NotifySend.php
+++ NotifySend.php
@@ -647,7 +647,7 @@ class Pman_Core_NotifySend extends Pman
     {
         $this->debug_str .= strlen($this->debug_str) ? "\n" : '';
         $this->debug_str .= $message;
-        echo $message ."\n";
+        //echo $message ."\n";
     }
     
     function errorHandler($msg, $success = false)
