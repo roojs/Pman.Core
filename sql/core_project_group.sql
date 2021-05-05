81da632cc8eabc6c29878927868db847dfdfb64c
Alan Knowles <alan@roojs.com>
2021-05-05 14:06:16 +0800

    sql/core_project_group.sql

--- sql/core_project_group.sql
+++ sql/core_project_group.sql
@@ -3,4 +3,7 @@ CREATE TABLE core_project_group (
   id int(11) NOT NULL auto_increment,
   PRIMARY KEY  (id)
   
-) ;
+);
+
+ALTER TABLE  core_project  ADD COLUMN   group_id int(11) default NULL default 0;
+ALTER TABLE  core_project  ADD COLUMN   project_id int(11) default NULL default 0;
