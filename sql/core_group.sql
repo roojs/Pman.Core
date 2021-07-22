f227de702ba85e78681194d0e050ed637646a944
Alan Knowles <alan@roojs.com>
2021-07-22 12:57:48 +0800

    case on update

--- sql/core_group.sql
+++ sql/core_group.sql
@@ -14,6 +14,6 @@ ALTER TABLE core_group ADD COLUMN type  int(11)  NOT NULL DEFAULT 0;
 ALTER TABLE core_group ADD COLUMN leader int(11)  NOT NULL default 0;
 ALTER TABLE core_group ADD COLUMN is_system int(2) NOT NULL default 0;
 
-Update core_group set display_name = name where display_name = '';
+UPDATE core_group SET  display_name = name WHERE display_name = '';
 
 ALTER TABLE core_group ADD INDEX lookup_leader (leader);
