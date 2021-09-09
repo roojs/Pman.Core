bc58d00be54eea163f20e13e5008f85c929d35dc
Alan <alan@roojs.com>
2021-09-09 14:15:46 +0800

    Changed DataObjects/Core_project_group.php

--- DataObjects/Core_project_group.php
+++ DataObjects/Core_project_group.php
@@ -7,5 +7,9 @@ class Pman_Core_DataObjects_Core_project_group extends DB_DataObject
 {
   
     public $__table = 'core_project_group'; 
-  
+ 
+ 
+    var $group_id; // int(11) default NULL default 0;
+    var $project_id;// int(11) default NULL default 0;
+
 }
