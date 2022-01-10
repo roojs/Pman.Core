57c49a050321a826a84c0bd2f6b2eec868ebcd81
Alan <alan@roojs.com>
2022-01-10 14:44:45 +0800

    Images.php

--- Images.php
+++ Images.php
@@ -419,8 +419,7 @@ class Pman_Core_Images extends Pman
             if (!$src|| !strlen(trim($src))) {
                 continue;
             }
-            var_dump($src);
-            
+             
             if (0 === strpos($src, 'data:')) {
                 if (!$obj) {
                     HTML_FlexyFramework::get()->page->jerr("no object to attach data url");
