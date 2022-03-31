0be732fbb3ae165c64fcf96770586143fd1efecc
Alan <alan@roojs.com>
2022-03-31 12:18:19 +0800

    fix #7165 - language on email dialog

--- Pman.Dialog.CoreEmail.js
+++ Pman.Dialog.CoreEmail.js
@@ -33,6 +33,7 @@ Pman.Dialog.CoreEmail = {
   'ea4788705e6873b424c65e91c2846b19' :"Cancel",
   '68b00d723d37122f64da8d9939f836f0' :"BCC Group",
   'c4ca4238a0b923820dcc509a6f75849b' :"1",
+  '4994a8ffeba4ac3140beb89e8d41f174' :"Language",
   'bd88a20b53a47f7b5704a83a15ff5506' :"Saved Version",
   'b20a8b77b05d53b4e695738731400c85' :"Mailout Name",
   '1bd18d39370b7f26c1c5e18067b74c6f' :"Html File",
@@ -55,6 +56,7 @@ Pman.Dialog.CoreEmail = {
   'name_fieldLabel' : 'b20a8b77b05d53b4e695738731400c85' /* Mailout Name */ ,
   'bcc_group_id_name_qtip' : '2c466a2c159463f1d9ef5a7b57b52827' /* Select BCC Group */ ,
   'bcc_group_id_name_emptyText' : '2c466a2c159463f1d9ef5a7b57b52827' /* Select BCC Group */ ,
+  'language_name_fieldLabel' : '4994a8ffeba4ac3140beb89e8d41f174' /* Language */ ,
   'template_qtip' : '31bb2f6e9b8fb11cbb7fb63c6025223f' /* Select Template */ ,
   'template_loadingText' : '1243daf593fa297e07ab03bf06d925af' /* Searching... */ ,
   'from_email_fieldLabel' : 'b357b524e740bc85b9790a0712d84a30' /* Email address */ ,
@@ -649,7 +651,8 @@ Pman.Dialog.CoreEmail = {
               },
               {
                xtype : 'Column',
-               hideLabels : true,
+               labelSeparator : ' ',
+               labelWidth : 0,
                xns : Roo.form,
                '|xns' : 'Roo.form',
                items  : [
@@ -759,13 +762,44 @@ Pman.Dialog.CoreEmail = {
              ]
             },
             {
-             xtype : 'TextField',
-             allowBlank : false,
-             fieldLabel : _this._strings['c7892ebbb139886662c6f2fc8c450710'] /* Subject */,
-             name : 'subject',
-             width : 600,
+             xtype : 'Row',
              xns : Roo.form,
-             '|xns' : 'Roo.form'
+             '|xns' : 'Roo.form',
+             items  : [
+              {
+               xtype : 'TextField',
+               allowBlank : false,
+               fieldLabel : _this._strings['c7892ebbb139886662c6f2fc8c450710'] /* Subject */,
+               name : 'subject',
+               width : 600,
+               xns : Roo.form,
+               '|xns' : 'Roo.form'
+              },
+              {
+               xtype : 'ComboBox',
+               allowBlank : false,
+               displayField : 'title',
+               editable : false,
+               fieldLabel : _this._strings['4994a8ffeba4ac3140beb89e8d41f174'] /* Language */,
+               hiddenName : 'language',
+               listWidth : 200,
+               mode : 'local',
+               name : 'language_name',
+               tpl : '<div class=\"x-grid-cell-text x-btn button\"><b>{title}</b> </div>',
+               triggerAction : 'all',
+               valueField : 'code',
+               width : 200,
+               xns : Roo.form,
+               '|xns' : 'Roo.form',
+               store : {
+                xtype : 'SimpleStore',
+                data : (function() {return typeof(Pman) == 'object'  ? Pman.I18n.simpleStoreData('l') : []})(),
+                fields : ['code', 'title'],
+                xns : Roo.data,
+                '|xns' : 'Roo.data'
+               }
+              }
+             ]
             },
             {
              xtype : 'Row',
