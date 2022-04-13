f3a5eb5b878b656e5c72504f96f70c654aacc3a0
Alan <alan@roojs.com>
2022-04-13 17:26:01 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -615,7 +615,7 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
         }
         if (empty($sz[1])) {
             $ratio =  empty($this->width) ? 1 : $this->height/ ($this->width *1.0);
-            $sy = inval($ratio * $sx);
+            $sy = intval($ratio * $sx);
         } else {
             $sy = $sz[1];
         }
