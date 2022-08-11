a49445c56e8f124f2d8617ca4cd4e3b508e0dc5e
leon <leon@roojs.com>
2022-08-11 10:51:58 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -40,7 +40,7 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
         }
 
         if(!empty($q['_to_base64']) && !empty($q['image_id'])) {
-            $i = DB_Object::factory("Images");
+            $i = DB_DataObject::factory("Images");
             $i->get($q['image_id']);
             $roo->jok($i->toBase64());
         }
