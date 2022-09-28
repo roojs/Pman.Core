f380fe2e72dc1c4d33801df173eac49df259d938
Alan <alan@roojs.com>
2022-09-28 12:14:42 +0800

    note additional column?

--- DataObjects/Core_notify.php
+++ DataObjects/Core_notify.php
@@ -43,7 +43,7 @@ class Pman_Core_DataObjects_Core_notify extends DB_DataObject
     public $evtype;                         // event type (or method to call)fall
     public $act_start;
     public $person_table;
-
+    public $to_email;
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
