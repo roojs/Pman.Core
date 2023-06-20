6c981b1fd116b2d8e0687c16d7d07b8283a50cc8
Alan Knowles <alan@roojs.com>
2023-06-20 12:04:59 +0800

    support for short urls with image domain

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -609,8 +609,10 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
             $id = $matches[1];
         } else if (preg_match('#Images/([0-9]+)/#', $url, $matches)) {
             $id = $matches[1];
+        } else if (preg_match('#images[^/]+/([0-9]+)/#', $url, $matches)) {
+            // supports images.xxxxx.com/{number}/name...
+            $id = $matches[1];
         }
-        
         if ($id === false ||  $id < 1) {
             return false;
         }
