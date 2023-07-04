cd2bbd8ca371b468731191c924ce99a1568c6ede
leon <leon@roojs.com>
2023-07-04 10:53:05 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -334,7 +334,6 @@ class Pman_Core_NotifySend extends Pman
             $w->msgid = '';
             $w->event_id = $ev->id;
             $w->to_email = $p->email; 
-            $w->to_email = $p->email; 
             $w->update($ww);
             $this->errorHandler(date('Y-m-d h:i:s') . " - FAILED -  BAD EMAIL - {$p->email} \n");
             
@@ -349,6 +348,7 @@ class Pman_Core_NotifySend extends Pman
             $w->sent = (!$w->sent || $w->sent == '0000-00-00 00:00:00') ? $w->sqlValue('NOW()') :$w->sent; // do not update if sent.....
             $w->msgid = '';
             $w->event_id = $ev->id;
+            $w->to_email = $p->email; 
             $w->update($ww);
             $this->errorHandler(date('Y-m-d h:i:s') . " - FAILED -  GAVE UP TO OLD - {$p->email} \n");
         }
