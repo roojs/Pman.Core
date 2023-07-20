57a1b2d01de67144dfd41ba7a0b1a4aa7f40e5b1
leon <leon@roojs.com>
2023-07-20 11:19:27 +0800

    DataObjects/Core_template.php

--- DataObjects/Core_template.php
+++ DataObjects/Core_template.php
@@ -514,7 +514,7 @@ WHERE (
         static $done = array();
         $clsname = strtolower($clsname);
 
-        if($clsname == 'pman_pressrelease_distributionreport' && $lang == 'zh_CN') {
+        if($clsname == 'pman_pressrelease_distributionreport') {
             var_dump($clsname);
             var_dump($this->tableName());
             var_dump($lang);
