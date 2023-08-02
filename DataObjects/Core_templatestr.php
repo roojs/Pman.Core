f1ba596b074e51d0244ecf67075f43a674ea20d7
leon <leon@roojs.com>
2023-08-02 13:14:32 +0800

    DataObjects/Core_templatestr.php

--- DataObjects/Core_templatestr.php
+++ DataObjects/Core_templatestr.php
@@ -760,6 +760,7 @@ class Pman_Core_DataObjects_Core_templatestr extends DB_DataObject
 
             $ret[$v['src_id_mdsum']] = $v;
         }
+        var_dumP($ar);
         var_dump(array_values($ret));
         die('test');
 
