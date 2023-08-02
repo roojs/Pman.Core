86dd6e3d24cde8ed3f0cd42566be5b84db9a1dc4
leon <leon@roojs.com>
2023-08-02 12:35:20 +0800

    DataObjects/Core_templatestr.php

--- DataObjects/Core_templatestr.php
+++ DataObjects/Core_templatestr.php
@@ -252,8 +252,6 @@ class Pman_Core_DataObjects_Core_templatestr extends DB_DataObject
     {
         
         $words = $tmpl->words;
-        var_dump($word);
-        exit;
         // mapping for template : 
         //tablename => $n (templatename) 
         //tableid => $k (key value)
