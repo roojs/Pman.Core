6fbce9dc44b28eec76f972580236063f86a8cad2
leon <leon@roojs.com>
2023-09-28 11:19:26 +0800

    TimeZone.php

--- TimeZone.php
+++ TimeZone.php
@@ -16,6 +16,13 @@ class Pman_Core_TimeZone extends Pman
 
     function get($base, $opts=array())
     {
+        $ce = DB_DataObject::factory('core_enum');
+        $ce->query('
+            SELECT
+                *
+            FROM
+                mysql.time_zone_name
+        ');
         die('test');
     }
 
