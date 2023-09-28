0a6804ea0b9d99b155470832dba642179dc3e308
leon <leon@roojs.com>
2023-09-28 11:33:53 +0800

    TimeZone.php

--- TimeZone.php
+++ TimeZone.php
@@ -23,7 +23,7 @@ class Pman_Core_TimeZone extends Pman
             FROM
                 mysql.time_zone_name
         ');
-        var_dump($ce->fetch());
+        var_dump($ce->fetchAll());
         var_dump($ce);
         var_dump($res);
 
