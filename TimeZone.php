31992c5b1d35d804e4e2fb38bb1870c17540a3a2
leon <leon@roojs.com>
2023-10-03 10:48:35 +0800

    TimeZone.php

--- TimeZone.php
+++ TimeZone.php
@@ -16,7 +16,7 @@ class Pman_Core_TimeZone extends Pman
 
     function get($base, $opts=array())
     {
-        self::getTimezones();
+        $timezone = $self::getTimezones();
 
         echo json_encode(array(
             'data' => $data,
