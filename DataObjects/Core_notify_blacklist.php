bebecf57d4e69aa0309608d2b147a99b7a97e951
Alan <alan@roojs.com>
2023-10-06 13:44:18 +0800

    DataObjects/Core_notify_blacklist.php

--- DataObjects/Core_notify_blacklist.php
+++ DataObjects/Core_notify_blacklist.php
@@ -17,9 +17,23 @@ class Pman_Core_DataObjects_Core_notify_blacklist extends DB_DataObject
     public $added_dt;
     
     
-    function messageIsBlacklisted($str)
+    function messageIsBlacklisted($err)
     {
+        $match = array(
+            '5.7.0 DT:SPM'. // 163.com
+            'on our block list ',  // live.com
+            'spameatingmonkey.net', // spameatingmonkey.net (users)
+            'sender is listed on the block', // korian?
+            'proofpoint.com', // another spam detecotr
+            'cloud-security.net', // another spam protector..
         
+        );
+        foreach($match as $str) {
+            if (strpos($err, $str) !== false) {
+                return true;
+            }
+        }
+        return false;
     }
     
 }
