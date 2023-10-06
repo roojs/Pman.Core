7e9670c728b39bb46c095b6f4bc512d300f029c4
Alan <alan@roojs.com>
2023-10-06 11:24:30 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -15,5 +15,14 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
-    
+    function loadOrCreate($dom)
+    {
+        $cd = DB_DataObject::Factory($dom);
+        if ($cd->get('domain', $dom)) {
+            return $cd;
+        }
+        $cd->domain = $dom;
+        $cd->insert();
+        return $cd;
+    }
 }
