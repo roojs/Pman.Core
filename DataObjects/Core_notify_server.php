77a88a8a5842a841f9419e9974a7556521b6278e
Alan <alan@roojs.com>
2023-10-13 14:35:30 +0800

    typo

--- DataObjects/Core_notify_server.php
+++ DataObjects/Core_notify_server.php
@@ -283,7 +283,7 @@ class Pman_Core_DataObjects_Core_notify_server extends DB_DataObject
                 $good = $s;
                 break;
             }
-             $offset = ($start + 1)  % count($servers);
+             $offset = ($offset + 1)  % count($servers);
         }
         if ($good == false && $allow_same) {
             $good = $this;
