d33ad6017c40eb5b209f7c94c77f70a9ded3a860
leon <leon@roojs.com>
2023-10-27 13:20:06 +0800

    TimeZone.php

--- TimeZone.php
+++ TimeZone.php
@@ -121,5 +121,10 @@ class Pman_Core_TimeZone extends Pman
         return $ar[0] . '/' . str_replace('_', ' ', $ar[1]) . ' (GMT ' . self::toTimeOffset($dt,$tz) . ')'; 
     }
 
-    
+    static function toDisplayArea($tz)
+    {
+        $ar = explode('/', $tz);
+        return str_replace('_', ' ', $ar[1]);
+
+    }
 }
