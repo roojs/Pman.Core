e2c38b896f1b81211366cacb8fe178a31bb0b748
leon <leon@roojs.com>
2023-10-31 11:27:39 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -87,7 +87,6 @@ class Pman_Core_NotifySend extends Pman
    
     function get($id,$opts=array())
     {
-        die('e');
 
         //print_r($opts);
         if (!empty($opts['DB_DataObject-debug'])) {
