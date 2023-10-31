943a6bc08f4cfc53b432872952fddb601aca76ad
leon <leon@roojs.com>
2023-10-31 11:35:20 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -114,6 +114,7 @@ class Pman_Core_NotifySend extends Pman
             $this->errorHandler("Server id does not match - use force to try again\n");
         }
         
+        die('f');
         
         if (!empty($opts['debug'])) {
             print_r($w);
