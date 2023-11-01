b713c3f219860e2b8045af866819810cadc35391
Alan <alan@roojs.com>
2023-11-01 08:57:41 +0800

    fix date checks on stored procedures

--- mysql/core_notify_trigger_after_update.sql
+++ mysql/core_notify_trigger_after_update.sql
@@ -10,7 +10,7 @@ CREATE TRIGGER core_notify_trigger_after_update
         FOR EACH ROW
         BEGIN
             -- make sure that act_start does not get modified if sent is set.
-            IF (OLD.sent IS NOT NULL AND  OLD.sent > '1500-00-00 00:00:00')
+            IF (OLD.sent IS NOT NULL AND  OLD.sent > '1500-01-01 00:00:00')
                 AND  ( NEW.act_start != OLD.act_start  OR NEW.act_when != OLD.act_when )  THEN
                   UPDATE `Error: Can not update core_notify action dates  after its sent` SET x = 1;
             END IF;
