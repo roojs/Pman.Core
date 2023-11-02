8a9b929221b9a333023cbc97ded4207314243678
Alan <alan@roojs.com>
2023-11-02 14:31:39 +0800

    DataObjects/Core_notify_sender_blacklist.php

--- DataObjects/Core_notify_sender_blacklist.php
+++ DataObjects/Core_notify_sender_blacklist.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Table Definition for core_notify_blacklist_sender
+ * Table Definition for core_notify_sender_blacklist
  */
 class_exists('DB_DataObject') ? '' : require_once 'DB/DataObject.php';
 
-class Pman_Core_DataObjects_Core_notify_blacklist_sender extends DB_DataObject 
+class Pman_Core_DataObjects_Core_notify_sender_blacklist extends DB_DataObject 
 {
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
 
-    public $__table = 'core_notify_blacklist_sender';    // table name
+    public $__table = 'core_notify_sender_blacklist';    // table name
     public $id;                              // int(11)  not_null primary_key auto_increment
     public $sender_id;
     public $domain_id;
