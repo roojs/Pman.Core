f1be68bd9925d5ffd213f80c9b2fbb44d584a71c
Alan <alan@roojs.com>
2023-12-01 13:43:35 +0800

    fix #7921 - shown name on reports - honor settings

--- mysql/core_translate_lookup.sql
+++ mysql/core_translate_lookup.sql
@@ -14,13 +14,19 @@ RETURNS TEXT NOT DETERMINISTIC READS SQL DATA
 
         DECLARE v_ret TEXT;
         DECLARE v_id INT(11);
+        DECLARE v_src_id INT(11);
+   		DECLARE s_id INT(11);
+
+        IF LENGTH(in_default) < 1 THEN
+            RETURN in_default;
+        END IF;
         
         SET v_id = 0;
         
         SELECT
-            id , txt
+            id , txt, src_id
             INTO
-            v_id, v_ret
+            v_id, v_ret, v_src_id
         FROM
             core_templatestr
         WHERE
@@ -38,6 +44,21 @@ RETURNS TEXT NOT DETERMINISTIC READS SQL DATA
         IF v_id < 1 OR LENGTH(v_ret) < 1 THEN
             RETURN in_default;
         END IF;
+
+       	SELECT 
+       		id
+       		INTO
+       		s_id
+       	FROM
+       		core_templatestr
+   	   	WHERE
+   	   		id = v_src_id
+   			AND
+   			mdsum = MD5(in_default);
+		
+		IF s_id IS NULL THEN
+			RETURN in_default;
+		END IF;
         
         RETURN v_ret;
 
