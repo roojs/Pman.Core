0d2cbabac72952fd84b7d2afcb0beb2d12339a35
leon <leon@roojs.com>
2024-02-01 16:56:56 +0800

    DataObjects/Core_email.php

--- DataObjects/Core_email.php
+++ DataObjects/Core_email.php
@@ -717,9 +717,6 @@ Content-Transfer-Encoding: 7bit
         $content['bcc'] = array();
 	$data = $this->toMailerData($content);
 
-    var_dump($data);
-    die('test');
-
  	return $data;
         
            
