d5e12ce7fbbc930f670963ebbfaa8d5200b0a488
leon <leon@roojs.com>
2024-07-25 14:48:39 +0800

    DataObjects/Core_company.php

--- DataObjects/Core_company.php
+++ DataObjects/Core_company.php
@@ -231,7 +231,8 @@ class Pman_Core_DataObjects_Core_Company extends DB_DataObject
     }
     function onInsert($req, $roo)
     {
-        $i = DB_DataObject::factory('core_company')->get(1);
+        $i = DB_DataObject::factory('core_company');
+        $i->get(1);
         var_dump($i);
         die('test');
         if (!empty($req['password1'])) {
