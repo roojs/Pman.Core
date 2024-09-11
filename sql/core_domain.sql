89631bec2f4f41802cd602326707024be338ad99
Alan <alan@roojs.com>
2024-09-11 13:15:57 +0800

    add mx records to core domain

--- sql/core_domain.sql
+++ sql/core_domain.sql
@@ -6,5 +6,7 @@ CREATE TABLE core_domain (
 -- need to specify engine set, otherwise the unique index get's borked.
 
 ALTER TABLE core_domain ADD COLUMN domain VARCHAR(255) NOT NULL DEFAULT '';
+ALTER TABLE core_domain ADD COLUMN has_mx int(2) NOT NULL  DEFAULT 0;
+ALTER TABLE core_domain ADD COLUMN mx_updated DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00';
 
 CREATE UNIQUE INDEX ui_domain ON core_domain (domain);
