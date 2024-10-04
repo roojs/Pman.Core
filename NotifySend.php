6df17743132309f4e43dc0c0ca56a55743a3231d
leon <leon@roojs.com>
2024-10-04 11:35:38 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -389,9 +389,6 @@ class Pman_Core_NotifySend extends Pman
                     'debug' => 1,
                     'debug_handler' => array($this, 'debugHandler')
             ));
-
-            var_dump($ff->Core_Notify);
-            die('test');
             
             // if the host is the mail host + it's authenticated add auth details
             // this normally will happen if you sent  Pman_Core_NotifySend['host']
