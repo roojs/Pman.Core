8677420acc280a4d750d53db000f49271f1989fd
leon <leon@roojs.com>
2024-10-04 11:20:53 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -215,9 +215,6 @@ class Pman_Core_NotifySend extends Pman
          
         // this may modify $p->email. (it will not update it though)
         $email =  $this->makeEmail($o, $p, $last, $w, $force);
-
-        var_dump($p->email);
-        die('test');
         
         if ($email === true)  {
             $ev = $this->addEvent('NOTIFY', $w, "Notification event cleared (not required any more) - toEmail=true" );;
