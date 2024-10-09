4c383bccdec8e8cafd062c2938fb6885760399e0
leon <leon@roojs.com>
2024-10-09 11:09:18 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -456,11 +456,12 @@ class Pman_Core_NotifySend extends Pman
             $mailer->auth = true;
             $mailer->username = 'leontest.roojs@gmail.com';
             $mailer->password = 'test2test';
+            $mailer->host = 'smtp.gmail.com';
             // var_dump($email['headers']);
             // var_dump($p->email);
             // var_dump($dom);
-            var_dump($mailer->host);
-            die('test');
+            // var_dump($mailer->host);
+            // die('test');
             
             $res = $mailer->send($p->email, $email['headers'], $email['body']);
             if (is_object($res)) {
