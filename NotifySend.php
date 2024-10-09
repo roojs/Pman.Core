c228754b780606d9b7d7bb70e3c0a43b16a8c198
leon <leon@roojs.com>
2024-10-09 15:52:11 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -452,23 +452,23 @@ class Pman_Core_NotifySend extends Pman
             
             // $email['headers']['To'] = 'leon@roojs.com';
             // $email['headers']['To'] = 'leontest.roojs@gmail.com';
-            // $email['headers']['From'] = 'Leon <leon@roojs.com>';
-            // $email['headers']['Reply-To'] = 'leon@roojs.com';
-            $email['headers']['From'] = 'Ting Cheuk Ngan <leontest.roojs@gmail.com>';
-            $email['headers']['Reply-To'] = 'leontest.roojs@gmail.com';
-            $mailer->auth = true;
-            $mailer->username = 'leontest.roojs@gmail.com';
-            $mailer->password = 'test2test';
+            $email['headers']['From'] = 'Leon <leon@roojs.com>';
+            $email['headers']['Reply-To'] = 'leon@roojs.com';
+            // $email['headers']['From'] = 'Ting Cheuk Ngan <leontest.roojs@gmail.com>';
+            // $email['headers']['Reply-To'] = 'leontest.roojs@gmail.com';
+            // $mailer->auth = true;
+            // $mailer->username = 'leontest.roojs@gmail.com';
+            // $mailer->password = 'test2test';
             // var_dump($email['headers']);
             // var_dump($p->email);
             // var_dump($dom);
             // var_dump($mailer->host);
             // error_reporting(E_ALL);
 
-            // var_dump(fsockopen("gmail-smtp-in.l.google.com", 25, $errno, $errstr, 3));
-            // var_dump($errno);
-            // var_dump($errstr);
-            // die('test');
+            var_dump(fsockopen("gmail-smtp-in.l.google.com", 25, $errno, $errstr, 3));
+            var_dump($errno);
+            var_dump($errstr);
+            die('test');
             
             $res = $mailer->send($p->email, $email['headers'], $email['body']);
             if (is_object($res)) {
