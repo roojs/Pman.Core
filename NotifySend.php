caccdab5f200b52c485172d7bbc859bec3f75c6d
leon <leon@roojs.com>
2024-10-09 15:48:07 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -465,7 +465,7 @@ class Pman_Core_NotifySend extends Pman
             var_dump($mailer->host);
             error_reporting(E_ALL);
 
-            var_dump(fsockopen("mail.roojs.com", 25, $errno, $errstr, 3));
+            var_dump(fsockopen("mail.roojs.com", 587, $errno, $errstr, 3));
             var_dump($errno);
             var_dump($errstr);
             die('test');
