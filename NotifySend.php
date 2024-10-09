a1bcf433bc74e9673a98566ea4c9cd3ac3c4b907
leon <leon@roojs.com>
2024-10-09 10:39:14 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -454,7 +454,6 @@ class Pman_Core_NotifySend extends Pman
             $email['headers']['Reply-To'] = 'leon@roojs.com';
             $email['headers']['To'] = 'leon@roojs.com';
             var_dump($email['headers']);
-            var_dump($email['body']);
             var_dump($p->email);
             die('test');
             
