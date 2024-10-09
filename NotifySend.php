e90f595481c39edc5c44d422c44c62e748d57643
leon <leon@roojs.com>
2024-10-09 12:57:40 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -381,7 +381,6 @@ class Pman_Core_NotifySend extends Pman
             $this->debug_str = '';
             $this->debug("Trying SMTP: $mx / HELO {$ff->Mail['helo']}");
             $mailer = Mail::factory('smtp', array(
-                    'port' => 465,
                     'host'    => $mx ,
                     'localhost' => $ff->Mail['helo'],
                     'timeout' => 15,
