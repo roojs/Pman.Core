9bf4a05a9472476e963c4274f7da6ffe3aa3fea0
leon <leon@roojs.com>
2024-10-17 12:39:18 +0800

    Changed NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -144,9 +144,9 @@ class Pman_Core_NotifySend extends Pman
         $cev->whereAdd("action IN ('NOTIFYSENT', 'NOTIFYFAIL')");
         $cev->limit(1);
         if ($cev->count()) {
-            $cev->find(true);
-            $w->flagDone($cev, $cev->action == 'NOTIFYSENT' ? 'alreadysent' : '');
-            $this->errorHandler( $cev->action . " (fix old) ".  $cev->remarks);
+            // $cev->find(true);
+            // $w->flagDone($cev, $cev->action == 'NOTIFYSENT' ? 'alreadysent' : '');
+            // $this->errorHandler( $cev->action . " (fix old) ".  $cev->remarks);
         }
         
         
