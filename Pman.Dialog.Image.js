81406b1460d9ab3d243209f2c39e5d734e81e86b
leon <leon@roojs.com>
2024-11-01 10:20:28 +0800

    Pman.Dialog.Image.bjs Pman.Dialog.Image.js

--- Pman.Dialog.Image.js
+++ Pman.Dialog.Image.js
@@ -121,6 +121,8 @@ Pman.Dialog.Image = {
     listeners : {
      show : function (_self)
       {
+          Roo.log('UPLOAD SHOW');
+          Roo.log(_this.form.findField('imageUpload'));
       
           // this does not really work - escape on the borders works..
           // resize to fit.. if we have styled stuff...
