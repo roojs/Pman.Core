1330c2b987028bf76841819c241ed6903a8ba62f
leon <leon@roojs.com>
2024-11-05 16:08:59 +0800

    Pman.Dialog.Image.bjs Pman.Dialog.Image.js

--- Pman.Dialog.Image.js
+++ Pman.Dialog.Image.js
@@ -214,8 +214,6 @@ Pman.Dialog.Image = {
         listeners : {
          actioncomplete : function(_self,act)
           {
-              Roo.log('ACTION COMPLETE');
-              Roo.log(act.type);
               _this.dialog.uploadComplete = true;
               _this.dialog.haveProgress = 2; 
               Roo.MessageBox.hide(); // force hiding
