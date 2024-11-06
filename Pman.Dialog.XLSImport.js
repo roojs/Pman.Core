a97cc8c521e4f024a45a5af0a9aa0d0e12ab4b0f
leon <leon@roojs.com>
2024-11-06 16:22:20 +0800

    Pman.Dialog.XLSImport.bjs Pman.Dialog.XLSImport.js

--- Pman.Dialog.XLSImport.js
+++ Pman.Dialog.XLSImport.js
@@ -159,6 +159,7 @@ Pman.Dialog.XLSImport = {
                             }, function (data) {
                                 _this.data.fileId = data.id;
                                 _this.data.data = data.data;
+                                _this.dialog.fireEvent('show', this.form,  { type: 'setdata', data: data });
                                 _
                             });
                             
