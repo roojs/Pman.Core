5f3638721ae2ccba1d3c0dbe62aae28c4385330f
leon <leon@roojs.com>
2024-11-19 14:36:21 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -87,7 +87,6 @@ class Pman_Core_NotifySend extends Pman
    
     function get($id,$opts=array())
     {
-        die('test');
         //if ($this->database_is_locked()) {
         //    die("LATER - DATABASE IS LOCKED");
        // }
