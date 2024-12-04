f92e5f6c82dd0c404a101432576b235b5c372abb
leon <leon@roojs.com>
2024-12-04 12:22:28 +0800

    DataObjects/Core_watch.php

--- DataObjects/Core_watch.php
+++ DataObjects/Core_watch.php
@@ -117,6 +117,7 @@ class Pman_Core_DataObjects_Core_watch extends DB_DataObject
                      
                 foreach($arr as $action) {
                     if(($ar = $this->getTableAndMethodFromMedium($action)) === false) {
+                        // invalid medium
                         continue;
                     }
 
