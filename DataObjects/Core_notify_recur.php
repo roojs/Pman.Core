f28d73629bf6fe561732f4d29ade298ac503aaf1
Alan <alan@roojs.com>
2024-12-05 16:56:37 +0800

    fix compat

--- DataObjects/Core_notify_recur.php
+++ DataObjects/Core_notify_recur.php
@@ -210,7 +210,7 @@ class Pman_Core_DataObjects_Core_notify_recur extends DB_DataObject
         return $ret;
     }
 
-    function toRooSingleArray() 
+    function toRooSingleArray($authUser, $request) 
     {
         $ret = $this->toArray();
         $ret['dtstart_day'] = date('Y-m-d', strtotime($this->dtstart));
