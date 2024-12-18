9104a764cf5b13bc7850b7896b7635172b7fc099
leon <leon@roojs.com>
2024-12-18 15:24:03 +0800

    Pman.Dialog.PreviewRowsImport.bjs Pman.Dialog.PreviewRowsImport.js

--- Pman.Dialog.PreviewRowsImport.js
+++ Pman.Dialog.PreviewRowsImport.js
@@ -202,7 +202,6 @@ Pman.Dialog.PreviewRowsImport = {
                                           method : 'GET',
                                           params: {
                                               'fileId': _this.data.fileId,
-                                              'emails': Roo.encode(emails),
                                               'emailCols': Roo.encode(_this.data.emailCols)
                                           }
                                       });
