420a08a8d94e654755f8a965813da7b01d6dbf02
leon <leon@roojs.com>
2024-12-30 12:06:37 +0800

    Pman.Dialog.CoreEmail.bjs Pman.Dialog.CoreEmail.js

--- Pman.Dialog.CoreEmail.js
+++ Pman.Dialog.CoreEmail.js
@@ -672,6 +672,7 @@ Pman.Dialog.CoreEmail = {
               },
               {
                xtype : 'TextField',
+               actionMode : 'fieldEl',
                allowBlank : true,
                fieldLabel : _this._strings['b357b524e740bc85b9790a0712d84a30'] /* Email address */,
                name : 'from_email',
