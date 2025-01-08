2b6fcdd2b3e6847e80fb37efac7fd86808249bea
leon <leon@roojs.com>
2025-01-08 10:29:08 +0800

    Pman.Dialog.PreviewHeaderImport.bjs Pman.Dialog.PreviewHeaderImport.js

--- Pman.Dialog.PreviewHeaderImport.js
+++ Pman.Dialog.PreviewHeaderImport.js
@@ -65,8 +65,6 @@ Pman.Dialog.PreviewHeaderImport = {
     listeners : {
      show : function (_self)
       {
-          Roo.log('SHOW PREVIEW HEADER');
-          Roo.log(_this.data.data.rows.length);
           if(typeof(_this.data.disableMailingList) != 'undefined' && _this.data.disableMailingList === true) {
               _this.mailing_list.hide();
               _this.mailing_list_text.hide();
