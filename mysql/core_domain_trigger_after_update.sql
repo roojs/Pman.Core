f7d5d51044b0cf14a7d82a897cd554dd689b78d9
leon <leon@roojs.com>
2025-02-07 16:47:07 +0800

    mysql/core_domain_trigger_after_update.sql

--- mysql/core_domain_trigger_after_update.sql
+++ mysql/core_domain_trigger_after_update.sql
@@ -9,7 +9,7 @@ CREATE TRIGGER core_domain_trigger_after_update
             AFTER UPDATE ON core_domain
         FOR EACH ROW
         BEGIN
-            IF (NEW.mx_updated != '1000-01-01 00:00:00' AND  NEW.has_mx = 0) THEN
+            IF (OLD.mx_updated != NEW.mx_updated AND  NEW.has_mx = 0) THEN
                   UPDATE core_domain set email_fails =  0 WHERE id = NEW.person_id;
             END IF;
         END;
