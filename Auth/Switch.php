0f3ce83386280209ec3b6f1732c9597802a45b16
Alan <alan@roojs.com>
2025-02-11 13:07:17 +0800

    Auth/Switch.php

--- Auth/Switch.php
+++ Auth/Switch.php
@@ -16,21 +16,20 @@ require_once 'Pman/Core/Auth.php';
 
 
 
-class Pman_Core_Auth_Switch extends Pman_Core_Auth
+class Pman_Core_Auth_Switch extends Pman_Core_Auth_Required
 { 
+     
+    
     function post($v, $opts=array())
     {
         
         $tbl = empty($ff->Pman['authTable']) ? 'core_person' : $ff->Pman['authTable'];
-        $u = DB_DataObject::factory($tbl);
-        if (!$u->isAuth()) {
-            $this->jnotice("AUTH-FAILED", "not logged in");
-        }
+         
         if (empty($_REQUEST['user_id'])) {
             $this->jnotice("NOUID", "Missing User id");
         }
         
-        $au = $u->getAuthUser();
+        $au = $this->authUser;
         
         // first check they have perms to do this..
         if (!$au|| ($au->company()->comptype != 'OWNER') || !$this->hasPerm('Core.Person', 'E')) {
