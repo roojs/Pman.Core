90285b3eac4924333ded9f5716603df2c2a6d6f0
Alan <alan@roojs.com>
2025-02-12 13:04:28 +0800

    Auth/State.php

--- Auth/State.php
+++ Auth/State.php
@@ -44,22 +44,14 @@ class Pman_Core_Auth_State extends Pman_Core_Auth
         //die("got here?");
         $au = $u->getAuthUser();
         
-        $this->window_check($au);
+        DB_DataObject::factory('core_person_window')->check($au, $_REQUEST);
         
-        /*
-         // might occur on shared systems.
-        $ff= HTML_FlexyFramework::get();
-        
-        // IS THIS VALID?? - should be in U-> is auth?
-        if (!empty($ff->Pman['auth_comptype'])  && $au->id > 0 &&
-                ($ff->Pman['auth_comptype'] != $au->company()->comptype)) {
-            $au->logout();
-            $this->jerr("Login not permited to outside companies - please reload");
-        }
-        
-        */
-        
-        //$au = $u->getAuthUser();
+           
+        $this->returnUser($au);
+    }
+    
+    function returnUser($au)
+    {
         
         $aur = $au ?  $au->authUserArray() : array();
         
@@ -96,54 +88,5 @@ class Pman_Core_Auth_State extends Pman_Core_Auth
         
            
     }
-    /**
-     * window checking
-     *  * we use window.sessionStorage on the client to identify windows.
-     *
-     * couple of things
-     *  * restrict user to single window ?? (now or later?)
-     *  * allow admin to log out a user (by flagging core_person_windows to logout)
-     *    * This is a force logout - and affects the 'State calls'
-     *  * if login is presented - (eg session timeout on an existing window)
-     *    * we might have a record of that user being logged in.
-     *    *   ( normally this is ok - unless the force logout exists - in which case we return forced-logout )
-     * 
-     *
-     *
-     */
-    
-    function window_check($user)
-    {
-        if (empty($_REQUEST['window_id']) || empty($_REQUEST['app_id'])) { // we don't do any checks on no window data.
-            return;
-        }
-        $w = DB_DataObject::factory('core_person_window');
-        $w->person_id = $user->id;
-        $w->app_id = $_REQUEST['app_id'];
-        $mw = clone($w);
-        $w->window_id = $_REQUEST['window_id'];
-        if (!$w->find(true)) {
-            if (!$mw->count()) {
-                return;
-            }
-            if (!empty($_REQUEST['logout_other_windows'])) {
-                foreach($mw->fetchAll() as $mw) {
-                    $mmw = clone($mw);
-                    $mw->delete();
-                }
-                return;
-            }
-            $this->jnotice("MULTI-WIN", "window already exists for user");
-            // no record exists - it's ok - it's created later
-            return;
-        }
-        if ($w->force_logout) {
-            $u->logout();
-            session_regenerate_id(true);
-            session_commit();
-            $this->jnotice("FORCE-LOGOUT", "this window must be reloaded");
-        }
-        
-         
-    }
+   
 }
