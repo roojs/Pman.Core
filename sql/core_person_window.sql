c2c89a9db9a7f13f57eb3b788ec297dbd1df8801
Alan <alan@roojs.com>
2025-02-13 16:13:40 +0800

    sql/core_person_window.sql

--- sql/core_person_window.sql
+++ sql/core_person_window.sql
@@ -9,7 +9,7 @@ CREATE  TABLE core_person_window (
 
  
 ALTER TABLE core_person_window ADD UNIQUE INDEX lookup (window_id, person_id, app_id);
-ALTER TABLE core_person_window ADD last_access_dt;
+ALTER TABLE core_person_window ADD last_access_dt  DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
     
 ALTER TABLE core_person_window ADD ip VARCHAR(32) NOT NULL DEFAULT ''
 ALTER TABLE core_person_window ADD user_agent VARCHAR(254) NOT NULL DEFAULT ''
