a46f9a374df54bf451b919bd7528999aa7dd0fe5
Alan <alan@roojs.com>
2025-02-13 16:13:49 +0800

    sql/core_person_window.sql

--- sql/core_person_window.sql
+++ sql/core_person_window.sql
@@ -9,10 +9,13 @@ CREATE  TABLE core_person_window (
 
  
 ALTER TABLE core_person_window ADD UNIQUE INDEX lookup (window_id, person_id, app_id);
-ALTER TABLE core_person_window ADD last_access_dt  DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
+ALTER TABLE core_person_window ADD last_access_dt  DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00';
     
-ALTER TABLE core_person_window ADD ip VARCHAR(32) NOT NULL DEFAULT ''
-ALTER TABLE core_person_window ADD user_agent VARCHAR(254) NOT NULL DEFAULT ''
-ALTER TABLE core_person_window ADD status ENUM('IN','OUT','KILL') NOT NULL DEFAULT 'IN';
+ALTER TABLE core_person_window ADD ip VARCHAR(32) NOT NULL DEFAULT '';
+ALTER TABLE core_person_window ADD user_agent VARCHAR(254) NOT NULL DEFAULT '';
+ALTER TABLE core_person_window ADD status ENUM('IN','OUT','KILL') NOT NULL DEFAULT 'IN';;
+
+
+-- drop
 ALTER TABLE core_person_window DROP force_logout;
     
