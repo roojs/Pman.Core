16bbd7228a30555a53c882a9620629e4ec17ca3f
Alan <alan@roojs.com>
2025-02-13 16:13:24 +0800

    sql/core_person_window.sql

--- sql/core_person_window.sql
+++ sql/core_person_window.sql
@@ -4,10 +4,15 @@ CREATE  TABLE core_person_window (
   window_id VARCHAR(64) NOT NULL DEFAULT '',
   app_id VARCHAR(128) NOT NULL DEFAULT '',
   person_id INT NOT NULL DEFAULT 0,
-  force_logout INT(2) NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
 
  
 ALTER TABLE core_person_window ADD UNIQUE INDEX lookup (window_id, person_id, app_id);
- 
+ALTER TABLE core_person_window ADD last_access_dt;
+    
+ALTER TABLE core_person_window ADD ip VARCHAR(32) NOT NULL DEFAULT ''
+ALTER TABLE core_person_window ADD user_agent VARCHAR(254) NOT NULL DEFAULT ''
+ALTER TABLE core_person_window ADD status ENUM('IN','OUT','KILL') NOT NULL DEFAULT 'IN';
+ALTER TABLE core_person_window DROP force_logout;
+    
