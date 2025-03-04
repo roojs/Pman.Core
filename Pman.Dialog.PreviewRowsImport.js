ce5b246db25053d0cb6d48b76bfe1c0ff7640e0f
leon <leon@roojs.com>
2025-03-04 10:40:58 +0800

    Pman.Dialog.PreviewRowsImport.bjs Pman.Dialog.PreviewRowsImport.js

--- Pman.Dialog.PreviewRowsImport.js
+++ Pman.Dialog.PreviewRowsImport.js
@@ -334,6 +334,8 @@ Pman.Dialog.PreviewRowsImport = {
                       return;
                       */
                   }
+                  
+                  // start validating emails
                   validateEmail();
               }
           });
