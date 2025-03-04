eebd032e164ee03a1993e22cdadf144c231e837c
leon <leon@roojs.com>
2025-03-04 14:53:49 +0800

    Pman.Dialog.PreviewRowsImport.bjs Pman.Dialog.PreviewRowsImport.js

--- Pman.Dialog.PreviewRowsImport.js
+++ Pman.Dialog.PreviewRowsImport.js
@@ -244,7 +244,7 @@ Pman.Dialog.PreviewRowsImport = {
               });
           };
           
-          // Roo.MessageBox.progress("Validating emails", "Starting");
+          Roo.MessageBox.progress("Validating emails", "Starting");
           
           new Pman.Request({
               url: _this.data.url,
