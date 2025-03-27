877575c798957b21911c7f107034f330c29bced4
leon <leon@roojs.com>
2025-03-27 10:34:23 +0800

    Auth/Login.php

--- Auth/Login.php
+++ Auth/Login.php
@@ -223,7 +223,7 @@ class Pman_Core_Auth_Login extends Pman_Core_Auth_State
 
         foreach($rules as $rule) {
             // matching ip -> skip
-            if($rules['configuration']['target'] == 'ip' || $rules['configuration']['value'] == $ip) {
+            if($rules['configuration']['target'] == 'ip' && $rules['configuration']['value'] == $ip) {
                 $matchingRule = $rule;
                 break;
             }
