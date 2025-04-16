9db861efc1b82e840ccf0fce415665b618a39b5c
leon <leon@roojs.com>
2025-04-16 12:04:04 +0800

    Pman.Dialog.ViewWebsite.bjs Pman.Dialog.ViewWebsite.js

--- Pman.Dialog.ViewWebsite.js
+++ Pman.Dialog.ViewWebsite.js
@@ -62,7 +62,7 @@ Pman.Dialog.ViewWebsite = {
           
           _this.websiteViewPanel.setContent(
               '<iframe ' + 
-              'style="border: 0px;width:' + size.width +'px;' +
+              'style="border: 0px;width:' + size.width +'px;" ' +
               'src="' + url + '"/>'
           );
       }
