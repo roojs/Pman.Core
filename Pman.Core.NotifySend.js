f863761c31c0cf8cd4520fa2ff9c75b6599889c7
leon <leon@roojs.com>
2025-05-07 11:16:07 +0800

    Pman.Core.NotifySend.js

--- Pman.Core.NotifySend.js
+++ Pman.Core.NotifySend.js
@@ -1,27 +1,51 @@
 Pman.Core.NotifySend = {
-    emails: [],
 
     sendEmails: function(emails, callback) {
-        this.emails = emails
-    },
-
-    sendEmail: function() {
-        var notificationId = notifications[i]['id'];
-        new Pman.Request({
-            url: baseURL + '/Core/NotifySend/' + notificationId,
-            params: {
-                force: 1
-            },
-            method: 'POST',
-            success: function(res)
-            {
-                postSend();
-            },
-            failure: function (res)
-            {
-                errorMsgs.push(notifications[i]['to_email'] + ': ' + res.errorMsg);
-                postSend();
+        var errorMsgs = [];
+                        
+        var i = 0;  
+        var total = notifications.length;
+        
+        var postSend = function() {
+            i++;
+            Roo.MessageBox.updateProgress(i / total,
+                i + " / " + total + " emails sent");
+            if(i >= total) {
+                Roo.MessageBox.hide();
+                
+                // show errors if any
+                if(errorMsgs.length) {
+                    Roo.MessageBox.alert('Error', errorMsgs.join('<br>'));
+                    return;
+                }
+                Roo.MessageBox.alert('Sent', 'Your message was successfully sent');
+                return;
             }
-        });
-    };
+            sendEmail();
+        }
+        
+        var sendEmail = function() {
+            var notificationId = notifications[i]['id'];
+            new Pman.Request({
+                url: baseURL + '/Core/NotifySend/' + notificationId,
+                params: {
+                    force: 1
+                },
+                method: 'POST',
+                success: function(res)
+                {
+                    postSend();
+                },
+                failure: function (res)
+                {
+                    errorMsgs.push(notifications[i]['to_email'] + ': ' + res.errorMsg);
+                    postSend();
+                }
+            });
+        };
+        
+        Roo.MessageBox.progress("Email Sending", "Starting");
+        
+        sendEmail();
+    }
 };
