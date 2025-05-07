2bc739263f3c2b4f1e31270fd23e27bfcf16430f
leon <leon@roojs.com>
2025-05-07 11:21:25 +0800

    Pman.Core.NotifySend.js

--- Pman.Core.NotifySend.js
+++ Pman.Core.NotifySend.js
@@ -50,5 +50,26 @@ Pman.Core.NotifySend = {
         Roo.MessageBox.progress("Email Sending", "Starting");
         
         sendEmail();
+    },
+
+    sendEmail: function() 
+    {
+        var notificationId = notifications[i]['id'];
+        new Pman.Request({
+            url: baseURL + '/Core/NotifySend/' + notificationId,
+            params: {
+                force: 1
+            },
+            method: 'POST',
+            success: function(res)
+            {
+                postSend();
+            },
+            failure: function (res)
+            {
+                errorMsgs.push(notifications[i]['to_email'] + ': ' + res.errorMsg);
+                postSend();
+            }
+        });
     }
 };
