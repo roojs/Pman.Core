73682b179c8916e3ff1620ea43e913c8cb7787b4
leon <leon@roojs.com>
2025-05-08 16:53:04 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -511,6 +511,9 @@ class Pman_Core_NotifySend extends Pman
                     if (!$match) {
                         continue;
                     }
+
+                    var_dump($settings['auth']);
+                    die('test');
                     
                    
                     // what's the minimum timespan.. - if we have 60/hour.. that's 1 every minute.
