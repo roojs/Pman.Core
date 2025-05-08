b2c03fb1de7f2fb9e44647a39a1bb5df1e520d92
leon <leon@roojs.com>
2025-05-08 16:13:13 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -500,7 +500,7 @@ class Pman_Core_NotifySend extends Pman
                     // mail_imap_user exists for 'From' email
                     if($fromUser !== false && !empty($settings['is_outlook'])) {
                         var_dump($dom);
-                        // $mxRecords = dns_get_record($)
+                        $mxRecords = dns_get_record($dom, DNS_MX);
                         die('test2');
                     }
                     if(!$match && in_array($dom, $settings['domains'])){
