b96c6060cfdfcf62e037746915fb18e72676330f
leon <leon@roojs.com>
2025-05-14 12:09:11 +0800

    mysql/core_notify_trigger_after_update.sql

--- mysql/core_notify_trigger_after_update.sql
+++ mysql/core_notify_trigger_after_update.sql
@@ -14,22 +14,6 @@ CREATE TRIGGER core_notify_trigger_after_update
                 AND  ( NEW.act_start != OLD.act_start  OR NEW.act_when != OLD.act_when )  THEN
                   UPDATE `Error: Can not update core_notify action dates  after its sent` SET x = 1;
             END IF;
-
-            IF
-                person_table = 'crm_person'
-                AND
-                crm_person_id > 0
-            THEN
-                CALL core_notify_crm_update(
-                    OLD.reject_match_id,
-                    NEW.reject_match_id,
-                    NEW.crm_person_id,
-                    NEW.id,
-                    NEW.msgid,
-                    NEW.event_id,
-                    NEW.to_email
-                );
-            END IF;
         END;
 $$
  
