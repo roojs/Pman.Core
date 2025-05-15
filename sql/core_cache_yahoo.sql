8bb50971a960d80e84e300c2bd023c1c2b02aac3
Alan <alan@roojs.com>
2025-05-15 15:42:40 +0800

    fix #8517 - reduce yahoo queries by caching finance lookup (reduce query length)

--- sql/core_cache_yahoo.sql
+++ sql/core_cache_yahoo.sql
@@ -1,6 +1,6 @@
 CREATE TABLE core_cache_yahoo (
   id INT  NOT NULL auto_increment DEFAULT 0,
-  query longtext NOT NULL DEFAULT '',
+  query varchar(254) NOT NULL DEFAULT '',
   result longtext NOT NULL DEFAULT '',
   PRIMARY KEY   (id)
 );
