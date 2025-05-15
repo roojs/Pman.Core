aa5dd34a99169438eca19d1804429e5980048aec
Alan <alan@roojs.com>
2025-05-15 14:36:51 +0800

    Add Pman.Progress

---
+++ Pman.Progress.js
@@ -0,0 +1,110 @@
+/**
+ *
+ *  progress caller:
+ *
+ *  incomming - list of IDs
+ *  -- it will call a method with either '1' id or a batch..?
+ *  
+ */
+
+
+Pman.Progress = function(cfg)
+{
+    Roo.apply(this, cfg)
+    
+    this.items = Array.from(cfg.items); // copy...
+    this.items_len = this.items.length;
+    
+    var _this = this;
+    if (this.items.length< 1) {
+        return;
+    }
+    
+    if (this.confirm == false) {
+        Roo.MessageBox.progress(this.title , this.message_prefix + " 1/" + this.items.length );
+        this.doItem();
+        return;
+    }
+    var _this = this;
+    Roo.MessageBox.confirm("Confirm", String.format(this.confirm,  this.items.length ),
+        function(btn) {
+            if (btn != 'yes') {
+                return;
+            }
+             Roo.MessageBox.progress(_this.title , _this.message_prefix + " 1/" + _this.items.length );
+            _this.doItem();
+            
+        }
+    ); 
+    
+      
+}
+
+Roo.apply(Pman.Progress.prototype, {
+    
+    url : '',
+    batch_size : 1,
+    base_params : {},
+    
+    items : false,
+    items_len : 0,
+    
+    title : '',
+    message_prefix : '',
+    success : false,
+    confirm : false,
+    
+    doCall : function(id)
+    {
+ 
+        var params = Roo.apply({}, this.base_params);
+        params[ this.batch_size > 1 ? 'ids' : 'id' ] = id;
+        
+        new Pman.Request({
+            url: this.url,
+            method: 'POST',
+            params: params,
+            success: function(response) {
+            
+                if (! this.items.length) {
+                    Roo.MessageBox.hide();
+                    this.success();
+                    return;
+                }
+                
+                this.doItem();
+            },
+            failure: function(act) {
+                
+ 
+                var msg = '';
+                try {
+                    msg = act.errorMsg;
+                } catch(e) {
+                    msg = "Error disabling";
+                }
+                
+                Roo.MessageBox.alert("Error", msg);
+            },
+            scope : this
+            
+        });
+    },
+    
+    doItem : function ()
+    {
+        Roo.MessageBox.updateProgress(  
+            (this.items_len - this.items.length) / this.items_len, 
+            this.message_prefix + (this.items_len - this.items.length) + "/" + this.items_len
+        );
+        var ids = this.batch_size > 1 ? this.items.splice(0, this.batch_size).join(',') : this.items.shift();
+        
+        
+        var res = this;
+        this.doCall( ids  );
+    }
+    
+    
+    
+    
+})
