e9f4860b013ae2b23598c71c04f49f22d4682028
leon <leon@roojs.com>
2025-05-19 13:04:14 +0800

    Auth/Login.php

--- Auth/Login.php
+++ Auth/Login.php
@@ -222,7 +222,7 @@ class Pman_Core_Auth_Login extends Pman_Core_Auth_State
 
         $fw = new Services_Cloudflare_Firewall($ff->Pman_Core_Auth['cloudflare']);
 
-        $rules = $fw->update($ip);
+        $rules = $fw->update($ip, "logged in via {$ff->appName}");
 
         // Headers for API requests
         $headers = array(
