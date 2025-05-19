bc72e470e539c266334b872a674dc32b3f81e530
leon <leon@roojs.com>
2025-05-19 12:53:12 +0800

    Auth/Login.php

--- Auth/Login.php
+++ Auth/Login.php
@@ -210,7 +210,7 @@ class Pman_Core_Auth_Login extends Pman_Core_Auth_State
         $account = $ff->Pman_Core_Auth['cloudflare']['acount'];
         $apiToken = $ff->Pman_Core_Auth['cloudflare']['apiToken'];
 
-        var_dump($baseURL);
+        var_dump($account);
         var_dump($apiToken);
 
         $ip = DB_DataObject::factory('core_person_window')->ip_lookup();
