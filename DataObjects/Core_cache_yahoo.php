0aed63c37ced011c5f768d2fadee6a9b0c5d2e1a
leon <leon@roojs.com>
2025-05-20 13:19:38 +0800

    DataObjects/Core_cache_yahoo.php

--- DataObjects/Core_cache_yahoo.php
+++ DataObjects/Core_cache_yahoo.php
@@ -52,6 +52,7 @@ class Pman_Core_DataObjects_Core_Cache_Yahoo extends DB_DataObject
         $ch = curl_init($url);
 
         $header = array(
+            "Host: query1.finance.yahoo.com",
             "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
             "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
         );
