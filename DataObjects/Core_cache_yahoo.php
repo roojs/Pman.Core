8251b0a733be1d1c341a80c0267b654ea954aeb5
leon <leon@roojs.com>
2025-05-20 13:21:31 +0800

    DataObjects/Core_cache_yahoo.php

--- DataObjects/Core_cache_yahoo.php
+++ DataObjects/Core_cache_yahoo.php
@@ -50,6 +50,9 @@ class Pman_Core_DataObjects_Core_Cache_Yahoo extends DB_DataObject
         $url =     'https://query1.finance.yahoo.com/v1/finance/search?' . $request;
  
         $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Host: query1.finance.yahoo.com"
+        ));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($ch);
         var_dump($response);
