0fc63d0996aa106bf92001097f9b97713977981a
Alan <alan@roojs.com>
2025-05-21 14:58:11 +0800

    DataObjects/Core_notify_archive.php

--- DataObjects/Core_notify_archive.php
+++ DataObjects/Core_notify_archive.php
@@ -33,7 +33,169 @@ class Pman_Core_DataObjects_Core_notify_archive extends DB_DataObject
     function archive()
     {
         
+        $start_time = date("Y-m-d H:i:s");
+        echo "$start_time : Start  archive onid: $onid\r\n";
+
+        $pe = DB_DataObject::factory('core_notify');
+        $pe->whereAdd('act_when < NOW() - INTERVAL 6 MONTH');
+        $pe->orderBy('id ASC');
+        $pe->limit(1000);
+        $ids = $pe->fetchAll('id');
+        if (empty($ids)) {
+            return;
+        }
         
+        $pe = DB_DataObject::factory('core_notify');
+        $pe->whereAddIn('id', $ids , 'int');
+        $eids = array_unique($pe->fetchAll('event_id'));
+                      
+        
+        
+        $p = DB_DataObject::Factory('core_notify');
+        //$p->query("DROP TEMPORARY TABLE IF EXISTS $temp_table");
+      
+        $p->query("BEGIN");
+
+        
+
+                           
+        $p->query("
+              REPLACE INTO 
+                  core_notify_archive 
+              SELECT * from 
+                  core_notify 
+              WHERE 
+                  id in (" . implode(',', $ids) . ")
+             ");
+        
+         
+        
+        $p->query("
+              REPLACE INTO
+                  core_events_archive
+              SELECT * from 
+                  Events 
+              WHERE 
+                  id 
+                 IN 
+                    (
+                     ". implode(',', $ids) . "
+                     )
+             ");
+             
+     
+        // looping seems to be far quicker than IN() or any other version..
+        foreach($ids as $id) {     
+            $p->query("                        
+                      DELETE FROM  
+                          Events 
+                      WHERE   
+                          id = {$id}
+            ");
+        }
+                   
+        $p->query("                        
+                  DELETE FROM  
+                      pressrelease_notify                        
+                  WHERE  
+                      ontable = 'pressrelease_entry' 
+                  AND 
+                      onid = $onid
+              ");       
+        $p->query("
+              UPDATE 
+                  pressrelease_entry 
+              SET 
+                  archived_dist_summary_all =   
+                  (            
+                      SELECT 
+                         count(id) 
+                      FROM   
+                          pressrelease_notify_archive 
+                      WHERE
+                          onid = $onid
+                      AND
+                          ontable = 'pressrelease_entry'
+                      AND
+                          evtype = 'MAIL'
+                  ),
+                  archived_dist_summary_complete = 
+                  (            
+                      SELECT 
+                          count(id)                       
+                      FROM   
+                          pressrelease_notify_archive 
+                      WHERE
+                          onid = $onid
+                      AND
+                         ontable = 'pressrelease_entry'
+                      AND
+                         sent < NOW()
+                      AND
+                        event_id > 0
+                      AND
+                        evtype = 'MAIL' 
+                  ),
+                  archived_dist_summary_fail = 
+                  (            
+                      SELECT 
+                          count(id)  
+                      FROM   
+                          pressrelease_notify_archive 
+                      WHERE
+                          onid = $onid
+                      AND
+                          ontable = 'pressrelease_entry'
+                      AND
+                          event_id > 0
+                      AND
+                          evtype = 'MAIL'
+                      AND
+                          msgid = ''     
+                  ),
+                  archived_dist_open_summary = 
+                  (            
+                      SELECT 
+                          count(id) 
+                      FROM   
+                          pressrelease_notify_archive
+                      WHERE
+                          onid = $onid
+                      AND
+                          ontable = 'pressrelease_entry'
+                      AND
+                          evtype = 'MAIL'
+                      AND
+                          is_open = 1 
+                  )    
+              WHERE
+                   id = $onid
+             ");        
+                           
+        $p->query("COMMIT");
+        
+        
+        // 100000 in (10.52 sec) (249 days)
+        // 500000 in (11-35 sec) (150 days)
+        
+        $p = DB_DataObject::factory('Events');
+        $p->query("
+            DELETE FROM
+                 pressrelease_notify_archive 
+            WHERE
+                act_start < NOW() - INTERVAL 2 YEAR
+            ORDER BY
+                id ASC
+            LIMIT
+                50000
+        ");
+                
+        
+        
+        //$p->query("DROP TEMPORARY TABLE IF EXISTS $temp_table");
+        
+        $end_time = date("Y-m-d H:i:s");
+        echo "$end_time : Finish archive onid: $onid\r\n";    
         
         
         
