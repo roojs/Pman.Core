31bb2ab3955aa53179d94d79efffc75da11c1722
leon <leon@roojs.com>
2025-07-03 12:19:44 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -556,6 +556,7 @@ class Pman_Core_NotifySend extends Pman
                             die('test2');
                             continue;
                         }
+                        die('test3');
 
                         $host = $s->smtp_host;
                         $settings['port'] = $s->smtp_port;
