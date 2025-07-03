d148af2d93ed8a4c6e6f25a9b992e32daa507ab7
leon <leon@roojs.com>
2025-07-03 12:55:29 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -596,8 +596,7 @@ class Pman_Core_NotifySend extends Pman
                     $mailer->tls = isset($settings['tls']) ? $settings['tls'] : true;
                     $this->debug("Got Core_Notify route match - " . print_R($mailer,true));
                     
-                    var_dump($ff->cli);
-                    // from post request
+                    // triggered from a post request
                     if($ff->cli) {
 
                     }
