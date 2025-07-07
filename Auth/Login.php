11db7f1f7e069250f290cd3f368df2bc34aa5895
leon <leon@roojs.com>
2025-07-07 12:46:59 +0800

    Auth/Login.php

--- Auth/Login.php
+++ Auth/Login.php
@@ -218,6 +218,10 @@ class Pman_Core_Auth_Login extends Pman_Core_Auth_State
             return;
         }
 
+        $e = DB_DataObject::factory('Events');
+        $e->action = 'CLOUDFLARE-WHITELIST';
+        $e->remarks = $ip;
+
         $this->addEvent("CLOUDFLARE-WHITELIST", false, $ip);
 
         var_dump($ip);
