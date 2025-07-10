5f098fc56b2a2fc5fc21273e8afa0641a51e4556
leon <leon@roojs.com>
2025-07-10 16:40:59 +0800

    DataObjects/Core_event_audit.php

--- DataObjects/Core_event_audit.php
+++ DataObjects/Core_event_audit.php
@@ -90,8 +90,12 @@ class Pman_Core_DataObjects_Core_event_audit extends DB_DataObject
      * @param DB_DataObject $new updated DB_DataObject
      * @param array $remarks remarks (e.g. updated by script XXX)
      */
-    static function logChanges($old, $new, $remarks)
+    static function logChanges($oldObj, $newObj, $remarks)
     {
+        var_dump($oldObj instanceof DB_Dataobject);
+        var_dump($newObj instanceof DB_Dataobject);
+        die('test');
+        
         // only keep keys shared by both arrays
         $old = array_intersect_key($old, $new);
         $new = array_intersect_key($new, $old);
