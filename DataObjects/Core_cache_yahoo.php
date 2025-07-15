c1c8f4b8197b6429cfbaa90046403bc9da81b654
leon <leon@roojs.com>
2025-07-15 16:10:46 +0800

    DataObjects/Core_cache_yahoo.php

--- DataObjects/Core_cache_yahoo.php
+++ DataObjects/Core_cache_yahoo.php
@@ -62,7 +62,7 @@ class Pman_Core_DataObjects_Core_Cache_Yahoo extends DB_DataObject
         $userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36";
         if(!empty($_SERVER['HTTP_USER_AGENT'])) {
             $userAgent = $_SERVER['HTTP_USER_AGENT'];
-            $userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3 Safari/605.1.15";
+            $userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3 Safari/605.1.15";
             $userAgent = preg_replace(
                 '/\(.*?\)/', 
                 '(Windows NT 10.0; Win64; x64)', 
