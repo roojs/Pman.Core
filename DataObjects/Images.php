b93f64acb5cebda8a490cc005284ab215473ddff
leon <leon@roojs.com>
2025-07-21 16:36:28 +0800

    DataObjects/Images.php

--- DataObjects/Images.php
+++ DataObjects/Images.php
@@ -716,11 +716,6 @@ class Pman_Core_DataObjects_Images extends DB_DataObject
      */
     function toHTML($size, $provider = '/Images/Thumb', $extra = '') 
     {
-        // var_dump($size);
-        // var_dump($provider);
-        // var_dump($extra);
-        // die('test');
-        
         
         $sz = explode('x', $size);
         $sx = $sz[0];
