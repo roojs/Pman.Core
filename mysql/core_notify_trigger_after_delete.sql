dba71371d353f2244df124516605f3926bafb82f
Alan <alan@roojs.com>
2025-08-06 12:14:17 +0800

    dont error on bad upload

--- mysql/core_notify_trigger_after_delete.sql
+++ mysql/core_notify_trigger_after_delete.sql
@@ -10,7 +10,11 @@ CREATE TRIGGER core_notify_trigger_after_delete AFTER DELETE on core_notify
     FOR EACH ROW
         BEGIN
             -- I think we changed it so that sent is not null, deafult '0000....'
-            if old.sent is not null AND old.sent > '1500-01-01 00:00:00' THEN 
+            DECLARE v_id INT;
+            SET v_id = 0;
+            SELECT count(*) INTO v_id FROM core_notify_archive WHERE id = old.id ;
+            
+            if v_id < 1 AND old.sent is not null AND old.sent > '1500-01-01 00:00:00' THEN 
                 UPDATE `Error: Can not delete core_notify after it is sent` SET x = 1;
             END IF;
         END;
