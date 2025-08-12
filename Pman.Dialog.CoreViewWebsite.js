6e3cc21fa4d31f7c35b138e596bd1b8dd5006af4
leon <leon@roojs.com>
2025-08-12 12:15:25 +0800

    Pman.Dialog.CoreViewWebsite.bjs Pman.Dialog.CoreViewWebsite.js

--- Pman.Dialog.CoreViewWebsite.js
+++ Pman.Dialog.CoreViewWebsite.js
@@ -68,7 +68,7 @@ Pman.Dialog.CoreViewWebsite = {
           var size = _this.dialog.layout.getRegion('center').el.getSize();
           
           if (url.startsWith('/')) {
-              url = baseURL + url;
+              url = root + url;
           }
           
           url = baseURL + '/Core/ViewWebsite?' + new URLSearchParams({
