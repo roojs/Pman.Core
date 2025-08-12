6864730f5aa32ba5c001c21cedf7e9da9be110a9
leon <leon@roojs.com>
2025-08-12 11:52:29 +0800

    ViewWebsite.php

--- ViewWebsite.php
+++ ViewWebsite.php
@@ -12,7 +12,12 @@ class Pman_Core_ViewWebsite extends Pman
     
     function get($base='', $opts = array())
     {
-
+        $ch = curl_init('https://api.xmware.com/statenewsnetwork/latest/?feed=MOUT&key=mediaKjWOlqkkWo&page=1');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $this->jdata($url);
+        exit;
     }
 
     function post($base = '')
