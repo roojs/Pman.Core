386575b71d0433042b0be05c0c4d51d38619b629
leon <leon@roojs.com>
2025-08-12 11:48:38 +0800

    ViewWebsite.php

---
+++ ViewWebsite.php
@@ -0,0 +1,19 @@
+require_once 'Pman.php';
+
+class Pman_Core_ViewWebsite extends Pman 
+{
+    function getAuth() 
+    {
+        return true;
+    }
+    
+    function get($base='', $opts = array())
+    {
+
+    }
+
+    function post($base = '')
+    {
+        die('invalid post');
+    }
+}
