eb35ec75b154c4f0124f8f4839a260ea148df1bd
leon <leon@roojs.com>
2025-08-19 12:24:58 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -119,6 +119,10 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
         }
 
         $this->selectAddPersonReferenceCount();
+        if(!empty($q['sort']) && $q['sort'] == 'person_reference_count' && !empty($q['dir'])) {
+            $dir = $q['dir'] == 'DESC' ? 'DESC' : 'ASC';
+            $this->orderBy("{$q['sort']} $dir");
+        }
     }
 
     function selectAddPersonReferenceCount()
