a7e5767943d5586c06f35471dfb7844910c8d8ea
Alan <alan@roojs.com>
2025-08-26 14:24:51 +0800

    fix #8677 - RSS workday only  - fix update in core

--- Pman.php
+++ Pman.php
@@ -11,6 +11,8 @@ class Pman_Core_Pman {
     
     function init ($pg)
     {
+        $ch = DB_DataObject::factory('core_holiday');
+        $ch->updateHolidays('hk');
         
         
         $group = DB_DataObject::factory('core_group');
