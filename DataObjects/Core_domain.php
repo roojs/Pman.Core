b3c40ccccfbf125048707ba84c33ed1f84caa923
leon <leon@roojs.com>
2025-09-16 17:03:36 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -187,6 +187,9 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
                 $arr = explode('.', $k);
                 $tbl = $arr[0];
                 $col = $arr[1];
+                if($tbl == 'pressrelease_notify_archive') {
+                    continue;
+                }
                 $sql[] = "SELECT {$tbl}.{$col} AS domain_id FROM {$tbl} WHERE {$tbl}.{$col} = 2";
                 /*
                 $this->_join .= "
