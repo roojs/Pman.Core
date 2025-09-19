7d5a1876d1c740d6acc64298c06c1976de109f07
leon <leon@roojs.com>
2025-09-19 15:33:27 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -583,7 +583,6 @@ class Pman_Core_NotifySend extends Pman
                         $this->errorHandler( " Too many emails sent by {$dom} - requeing");
                     }
 
-                    var_dump("MATCH");
                      
                     
                     
