3d3d5c2ce154e05b1f5bf6d05ed34dbb4d2ec153
leon <leon@roojs.com>
2025-09-29 15:38:58 +0800

    sql/core_notify_server_ipv6.sql sql/core_notify_server_ipv6_range.sql

--- sql/core_notify_server_ipv6.sql
+++ sql/core_notify_server_ipv6.sql
@@ -1,7 +1,7 @@
 CREATE TABLE core_notify_server_ipv6 (
     id INT(11) NOT NULL AUTO_INCREMENT,
     range_id INT(11) NOT NULL DEFAULT 0,
-    ipv6_addr VARCHAR(45) NOT NULL DEFAULT '',
+    ipv6_addr VARCHAR(255) NOT NULL DEFAULT '',
     domain_id INT(11) NOT NULL DEFAULT 0,
     
     PRIMARY KEY (id)
--- sql/core_notify_server_ipv6_range.sql
+++ sql/core_notify_server_ipv6_range.sql
@@ -1,8 +1,8 @@
 CREATE TABLE core_notify_server_ipv6_range (
     id INT(11) NOT NULL AUTO_INCREMENT,
     server_id INT(11) NOT NULL DEFAULT 0,
-    ipv6_range_from VARCHAR(45) NOT NULL DEFAULT '',
-    ipv6_range_to VARCHAR(45) NOT NULL DEFAULT '',
+    ipv6_range_from VARCHAR(255) NOT NULL DEFAULT '',
+    ipv6_range_to VARCHAR(255) NOT NULL DEFAULT '',
     ipv6_ptr VARCHAR(255) NOT NULL DEFAULT '',
     
     PRIMARY KEY (id)
