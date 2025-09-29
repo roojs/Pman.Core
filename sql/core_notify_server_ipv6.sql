379f3ae2f357f0a05851fc34f5ddc07a1b4cf6f0
leon <leon@roojs.com>
2025-09-29 15:37:23 +0800

    sql/core_notify_server_ipv6.sql

---
+++ sql/core_notify_server_ipv6.sql
@@ -0,0 +1,12 @@
+CREATE TABLE core_notify_server_ipv6 (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    range_id INT(11) NOT NULL DEFAULT 0,
+    ipv6_addr VARCHAR(45) NOT NULL DEFAULT '',
+    domain_id INT(11) NOT NULL DEFAULT 0,
+    
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE core_notify_server_ipv6 ADD INDEX lookup_range (range_id);
+ALTER TABLE core_notify_server_ipv6 ADD INDEX lookup_addr (ipv6_addr);
+ALTER TABLE core_notify_server_ipv6 ADD INDEX lookup_domain (domain_id);
