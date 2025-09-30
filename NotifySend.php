9aca4f3a9224f67a51e3a8ad347d918767705704
leon <leon@roojs.com>
2025-09-30 17:01:43 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -464,6 +464,7 @@ class Pman_Core_NotifySend extends Pman
         $email = DB_DataObject::factory('core_notify_sender')->filterEmail($email, $w);
 
         var_dumP($email['headers']);
+        var_dump($ff->Mail['helo']);
         die('test');
                         
         foreach($mxs as $mx) {
