a8a5089183068dfc413d130a57ce381de96d22af
leon <leon@roojs.com>
2025-10-02 11:21:57 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -222,10 +222,15 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
             return;
         }
 
+        $ipv6_addr = $server->findSmallestUnusedIpv6();
+        if(!$ipv6_addr) {
+            return;
+        }
+
         $cnsi = DB_DataObject::factory('core_notify_server_ipv6');
         $cnsi->server_id = $server->id;
         $cnsi->domain_id = $this->id;
-        $cnsi->ipv6_addr = $server->findSmallestUnusedIpv6();
+        $cnsi->ipv6_addr = $ipv6_addr;
         var_dump($cnsi->ipv6_addr);
         die('test');
         if(!$cnsi->find(true)) {
