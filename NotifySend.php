fa1253afecaca6cf65ee0a455fb31d566afc514c
leon <leon@roojs.com>
2025-10-02 12:44:58 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -148,8 +148,8 @@ class Pman_Core_NotifySend extends Pman
         
         $this->server = DB_DataObject::Factory('core_notify_server')->getCurrent($this, $force);
 
-        // $this->server->assignQueues($this);
-        // die('test');
+        $this->server->assignQueues($this);
+        die('test');
         
         // Fetch IPv6 server configuration if available
         $this->server_ipv6 = null;
