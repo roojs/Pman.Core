8884f8f84a2cb58b044a11ce9bf27b387b587d9c
leon <leon@roojs.com>
2025-10-02 10:27:07 +0800

    sync

--- DataObjects/Core_notify_server_ipv6.php
+++ DataObjects/Core_notify_server_ipv6.php
@@ -11,7 +11,7 @@ class Pman_Core_DataObjects_Core_notify_server_ipv6 extends DB_DataObject
 
     public $__table = 'core_notify_server_ipv6';    // table name
     public $id;                              // int  not_null primary_key auto_increment
-    public $range_id;                        // int  not_null
+    public $server_id;                        // int  not_null
     public $ipv6_addr;                       // varchar(255)  not_null
     public $domain_id;                       // int  not_null
     
