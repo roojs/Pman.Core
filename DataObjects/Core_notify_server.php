0fa3d79a21bc208c3013f3c7bfa8bf45c920913c
leon <leon@roojs.com>
2025-10-02 12:10:27 +0800

    DataObjects/Core_notify_server.php

--- DataObjects/Core_notify_server.php
+++ DataObjects/Core_notify_server.php
@@ -194,12 +194,14 @@ class Pman_Core_DataObjects_Core_notify_server extends DB_DataObject
         }
         
         // First, assign servers based on IPv6 domain assignments
-        $assignedIPv6Ids = $this->assignQueuesByIPv6Domain($notify);
+        // $assignedIPv6Ids = $this->assignQueuesByIPv6Domain($notify);
         foreach($ids as $rn) {
             $up[$rn]  = array();
         }
         
         $num_servers = count($ids);
+        var_dump($num_servers);
+        die('test');
         
         if ($num_servers == 1) {
             $p = DB_DataObject::factory($notify->table);
