211da2217743a68a6091f32a735b63c69543f190
leon <leon@roojs.com>
2025-10-02 11:19:55 +0800

    DataObjects/Core_notify_server.php

--- DataObjects/Core_notify_server.php
+++ DataObjects/Core_notify_server.php
@@ -498,8 +498,6 @@ class Pman_Core_DataObjects_Core_notify_server extends DB_DataObject
         $end = $this->ipv6ToDecimal($this->ipv6_range_to);
         $usedIPv6 = ["2a01:111::1", "2a01:111::2"];
         $used = array_map(fn($ipv6) => $this->ipv6ToDecimal($ipv6), $usedIPv6);
-        var_dump($used);
-        die('test');
         $usedSet = array_flip($used);
     
         // Start from the next address after 'from'
