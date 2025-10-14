7e0c6b54136e021f94c53606b2aefce6991448b3
leon <leon@roojs.com>
2025-10-14 16:07:13 +0800

    PruneCheck.php

--- PruneCheck.php
+++ PruneCheck.php
@@ -179,7 +179,6 @@ class Pman_Core_PruneCheck extends Pman
             'total_records' => $total_records,
             'prunable_records' => $prunable_records,
             'runs_needed' => $runs_needed,
-            'prunable_records_per_run' => 10000,
             'prunable_groups_per_run' => 10000,
             'status' => $this->getStatus($runs_needed)
         );
