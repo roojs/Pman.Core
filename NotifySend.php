d2a7e1252168c5a339c804c198e1b2c8374209b3
leon <leon@roojs.com>
2025-10-21 12:56:27 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -120,7 +120,7 @@ class Pman_Core_NotifySend extends Pman
    
     function get($id,$opts=array())
     {   
-        stream_socket_client('172.104.114.91:25', $errno, $errstr, 10);
+        stream_socket_client('172.104.114.91:993', $errno, $errstr, 10);
         die('test');
         
         // DB_DataObject::debugLevel(5);
