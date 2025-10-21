b7391c4b5faaf9f827487aadc764785827a4747e
leon <leon@roojs.com>
2025-10-21 11:09:18 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -131,7 +131,7 @@ class Pman_Core_NotifySend extends Pman
 
         $email = 'nitishchandra@indianews.com';
 
-        $fp = stream_socket_client($email);
+        $fp = stream_socket_client('216.163.188.190:25');
         if($fp) {
             fclose($fp);
         }
