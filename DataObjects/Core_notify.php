3b7661a3dc738e24d1cb272d17371d517c848e59
leon <leon@roojs.com>
2025-10-22 15:54:52 +0800

    DataObjects/Core_notify.php

--- DataObjects/Core_notify.php
+++ DataObjects/Core_notify.php
@@ -255,7 +255,7 @@ class Pman_Core_DataObjects_Core_notify extends DB_DataObject
         $cn->whereAdd("core_notify.msgid IS NOT NULL AND core_notify.msgid != '' AND core_notify.sent > '1000-01-01 00:00:00'"); // successfully sent
         $cn->whereAdd("DATE(core_notify.sent) = DATE('" . $this->act_start . "')"); // on the same day
         $cn->whereAdd("core_notify.id != " . $this->id); // not the same notify
-        if($cn->count > $ce->max_duplicate) {
+        if($cn->count() > $ce->max_duplicate) {
             // exceed the limit
             return true;
         }
