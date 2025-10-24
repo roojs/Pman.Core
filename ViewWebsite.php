0d04152d83a86656cb4852d17d7096cfa53df05b
leon <leon@roojs.com>
2025-10-24 10:59:19 +0800

    ViewWebsite.php

--- ViewWebsite.php
+++ ViewWebsite.php
@@ -73,6 +73,9 @@ class Pman_Core_ViewWebsite extends Pman
      */
     private function prettyPrintXML($xmlContent) 
     {
+        if(empty($xmlContent)) {
+            return $xmlContent;
+        }
         try {
             $dom = new DOMDocument();
             $dom->preserveWhiteSpace = false;
