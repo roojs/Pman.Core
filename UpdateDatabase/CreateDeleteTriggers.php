4f345b7ed5f347251a2bb857f140d2ba23bd4d09
leon <leon@roojs.com>
2025-10-31 13:41:33 +0800

    UpdateDatabase/CreateDeleteTriggers.php

--- UpdateDatabase/CreateDeleteTriggers.php
+++ UpdateDatabase/CreateDeleteTriggers.php
@@ -45,7 +45,7 @@ class Pman_Core_UpdateDatabase_CreateDeleteTriggers extends Pman_Core_Cli
     
     function get($m="", $opts=array())
     {
-        $mysqlLinks = new Pman_Core_UpdateDatabase_MysqlLinks();
+        $mysqlLinks = new Pman_Core_UpdateDatabase_MysqlLinks(true);
         die('test');
         $this->target_table = !empty($opts['table']) ? $opts['table'] : '';
         $this->loadIniFiles();
