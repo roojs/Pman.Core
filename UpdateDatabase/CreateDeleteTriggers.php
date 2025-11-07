a46c66dfd7423914e13a0967344a63b2f4805fa3
leon <leon@roojs.com>
2025-11-07 11:15:22 +0800

    UpdateDatabase/CreateDeleteTriggers.php

--- UpdateDatabase/CreateDeleteTriggers.php
+++ UpdateDatabase/CreateDeleteTriggers.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Create delete triggers for database tables
+ * Create triggers for database tables
  * 
  * This script creates delete triggers that prevent deletion of records
  * that are referenced by other tables, maintaining referential integrity.
  * 
  * Usage:
- *   php press.php Core/UpdateDatabase/CreateDeleteTriggers                    # Create triggers for all tables
- *   php press.php Core/UpdateDatabase/CreateDeleteTriggers -t table_name      # Create trigger for specific table only
+ *   php press.php Core/UpdateDatabase/CreateTriggers                    # Create triggers for all tables
+ *   php press.php Core/UpdateDatabase/CreateTriggers -t table_name      # Create trigger for specific table only
  * 
  * @author System
  */
@@ -16,12 +16,12 @@
 require_once 'Pman/Core/Cli.php';
 require_once 'Pman/Core/UpdateDatabase/MysqlLinks.php';
 
-class Pman_Core_UpdateDatabase_CreateDeleteTriggers extends Pman_Core_Cli
+class Pman_Core_UpdateDatabase_CreateTriggers extends Pman_Core_Cli
 {
-    static $cli_desc = "Create delete triggers for database tables to maintain referential integrity";
+    static $cli_desc = "Create triggers for database tables to maintain referential integrity";
     static $cli_opts = array(
         'table' => array(
-            'desc' => 'Create delete trigger for this table only',
+            'desc' => 'Create trigger for this table only',
             'default' => '',
             'short' => 't',
             'min' => 0,
