0896d695dd78f3f122f055c4da80d28457018e9c
leon <leon@roojs.com>
2025-11-25 15:30:58 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -474,7 +474,9 @@ class Pman_Core_NotifySend extends Pman
         
         $email = DB_DataObject::factory('core_notify_sender')->filterEmail($email, $w);
 
-        $mxs = array('127.0.0.1:9999');
+        var_dump($mxs);
+        die('test');
+        // $mxs = array('127.0.0.1:9999');
                         
         foreach($mxs as $mx) {
             
@@ -652,7 +654,7 @@ class Pman_Core_NotifySend extends Pman
                 $res->backtrace = array(); 
             }
             $this->debug("GOT response to send: ". print_r($res,true));
-            
+
             if ($res === true) {
                 // success....
                 
