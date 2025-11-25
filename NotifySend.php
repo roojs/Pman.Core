ccecca29c927fe06d83b91ed4dac6a0f744ac33c
leon <leon@roojs.com>
2025-11-25 15:40:07 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -120,9 +120,6 @@ class Pman_Core_NotifySend extends Pman
    
     function get($id,$opts=array())
     {
-        $error =  PEAR::getStaticProperty('DB_DataObject','lastError');
-        var_dump("AB");
-        die($error->toString());
         
         // DB_DataObject::debugLevel(5);
         if ($this->database_is_locked()) {
