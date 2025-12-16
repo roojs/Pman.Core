05ef2d55e241044877bf92694561537969146e50
Alan <alan@roojs.com>
2025-12-16 16:01:23 +0800

    Fix #8723 - ipv6 core notify send
    Branch cant merge currnelyt (pman core only I think)
    
    ----
    
    Problem
    
    * Mailing list has entries that trigger us getting blacklisted by spamhaus
      * we are going to try and use ipv6 addresses to send emails to get around these blocks.
    
    * Steps to introduce
    
      * new table `core_notify_server_ipv6_range`
    
             server_id
             ipv6_range_from  (for ranges)
             ipv6_range_to
             ipv6_ptr  (for ranges) - ipv6 helo will be differnt to ipv4
    
      * new table `core_notify_server_ipv6`
    
             range_id  (addrs parent point to range) - which have the ptr
             ipv6_addr  (for individual dmoains)
             domain_id
    
    * User inteface
      * edit notify server
        * add right hand grid for ipv6
            * only shows ipv6 with domain
            * need to be able to search for a domain
            * can show all - or filted by selected server on left.
        * on the edit nofity server add a grid on the dialog to enable adding of ipv6 ranges, ptr
            * on the lower section - if we select a ipv6 then only show failures on that domain
            * currently we are showing mulitiple failures for a single domain?  - work out why
    
     * Rules:
       * Existing
         * assignQueues
         *   if domain_id exists in server_ipv6 then set the server id on notify to same id
         * if no domain_id - it should assign as per usual
    
       * Core Notify Send
         * after fetching server fetch $this->server_ipv6  (can be null if not set up or no need
         * updateNotifyToNextServer :: send[later] / Too many emails sent  / GREYLISTED
           * normally assigns next server (it should not do it if ipv6 is set.
    
         * server->initHelo() -- add ipv6 argument - and use that to determine what our real helo will be
       * if BLACKLISTED
         * currently we go to next server - change to
         * does the end user have a AAAA record for the target MX?
           * yes - set up ipv6 for domain (if we have it configured)
           * no - give up
    
      * other changes
        * sending via smtp will need to bind to a specific IP based on this IPv6 info
           * check Mail::factory() etc.. to see how this can be done
    
       * will need to create reverse pointers for all the new IP's  (on cloudflare and linode?)
        * the range gets a reverse pointer? - so might not need to do this
        * have a different reverse pointer
       * we need to set up the container to send these emails with ipv6 ranges
    
    ** IMPLEMENTAION
     * we will need to send from not media-outreach for these - as that would be blocked by SPF ?? - need to setup new SPF record with new domain and make sure it has the IPV6 addresses ** will need to relay the SMTP?
    * also need dkim probably?

---
+++ DataObjects/Core_notify_server_ipv6.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Table Definition for core_notify_server_ipv6
+ */
+class_exists('DB_DataObject') ? '' : require_once 'DB/DataObject.php';
+
+class Pman_Core_DataObjects_Core_notify_server_ipv6 extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'core_notify_server_ipv6';    // table name
+    public $id;                              // int  not_null primary_key auto_increment
+    public $server_id;                        // int  not_null
+    public $ipv6_addr;                       // varchar(255)  not_null
+    public $domain_id;                       // int  not_null
+    
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+
+    function applyFilters($q, $au, $roo)
+    {
+        if(!empty($q['search']['domain'])){
+            $s = $this->escape($q['search']['domain']);
+            $this->whereAdd("join_domain_id_id.domain like '%$s%'");
+        }
+    }
+}
