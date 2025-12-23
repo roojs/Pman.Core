50e2abebcfff9678e0161a855aa2e8d9e63be2a8
leon <leon@roojs.com>
2025-12-23 10:55:07 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -423,6 +423,8 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
                 // return "This email is invalid - we tested it and it does not exist";
             }
 
+            var_dump($mx);
+
             // Only log errors that aren't known false positives
             // PEAR_Error objects have both ->message property and getMessage() method
             // Using getMessage() method is the standard approach
