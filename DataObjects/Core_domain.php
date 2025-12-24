e968a9008052b65e2c3e5a096eb1b774b04ade44
leon <leon@roojs.com>
2025-12-24 10:43:54 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -272,6 +272,7 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
     function setUpIpv6($allocation_reason = '')
     {
         if(!$this->hasAAAARecord()) {
+            var_dump("No AAAA record found for domain {$this->domain}");
             return false;
         }
 
