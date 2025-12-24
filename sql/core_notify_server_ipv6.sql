d51eeafd76600e1af1fb9552338ff4198b35ec59
leon <leon@roojs.com>
2025-12-24 16:56:49 +0800

    sql/core_notify_server_ipv6.sql

--- sql/core_notify_server_ipv6.sql
+++ sql/core_notify_server_ipv6.sql
@@ -18,3 +18,4 @@ ALTER TABLE core_notify_server_ipv6 ADD COLUMN seq INT NOT NULL DEFAULT 0;
 
 ALTER TABLE core_notify_server_ipv6 ADD UNIQUE INDEX domain_seq (domain_id, seq);
 ALTER TABLE core_notify_server_ipv6 ADD UNIQUE INDEX ipv6_seq (ipv6_addr, seq);
+ALTER TABLE core_notify_server_ipv6 ADD UNIQUE INDEX domain_ipv6 (domain_id, ipv6_addr);
