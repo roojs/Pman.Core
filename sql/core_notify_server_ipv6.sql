f1e4a7310672ba2576c81290197029eb80e137b2
leon <leon@roojs.com>
2025-12-24 15:18:50 +0800

    sql/core_notify_server_ipv6.sql

--- sql/core_notify_server_ipv6.sql
+++ sql/core_notify_server_ipv6.sql
@@ -8,13 +8,14 @@ CREATE TABLE core_notify_server_ipv6 (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE core_notify_server_ipv6 DROP COLUMN server_id;
-ALTER TABLE core_notify_server_ipv6 ADD COLUMN seq INT NOT NULL DEFAULT 0;
 
 ALTER TABLE core_notify_server_ipv6 ADD INDEX lookup_addr (ipv6_addr);
 ALTER TABLE core_notify_server_ipv6 ADD INDEX lookup_domain (domain_id);
 
- 
-
-ALTER TABLE core_notify_server_ipv6 ADD UNIQUE INDEX domain_seq (domain_id, seq);
-
 ALTER TABLE core_notify_server_ipv6 ADD COLUMN allocation_reason TEXT NOT NULL DEFAULT '';
+
+ALTER TABLE core_notify_server_ipv6 ADD UNIQUE INDEX domain_ipv6 (domain_id, ipv6_addr);
+
+-- Migration: remove old seq column and unique key if they exist
+ALTER TABLE core_notify_server_ipv6 DROP INDEX domain_seq;
+ALTER TABLE core_notify_server_ipv6 DROP COLUMN seq;
