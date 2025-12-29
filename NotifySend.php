963b563db0568339b96f07b8694d041a768d3fe6
leon <leon@roojs.com>
2025-12-29 11:11:00 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -998,6 +998,7 @@ class Pman_Core_NotifySend extends Pman
             if ($mx_use_ipv6 && preg_match('/(\.outlook\.com)|(\.office365\.com)|(\.hotmail\.com)|(mail\.protection\.outlook\.com)$/i', $mx)) {
                 $outlook_ipv6 = $this->findOutlookIpv6ForDomain($mx, $core_domain);
                 if ($outlook_ipv6) {
+                    $this->server_ipv6 = $outlook_ipv6;
                     $this->debug("IPv6: Using pre-configured Outlook IPv6 address: " . $outlook_ipv6->ipv6_addr . " for domain: " . $core_domain->domain);
                 } else {
                     $this->debug("IPv6: No pre-configured IPv6 for Outlook server: $mx");
