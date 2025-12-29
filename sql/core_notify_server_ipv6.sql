57b354687ccebf99ada737ae2ef8dd14198203ef
leon <leon@roojs.com>
2025-12-29 10:20:51 +0800

    sql/core_notify_server_ipv6.sql

--- sql/core_notify_server_ipv6.sql
+++ sql/core_notify_server_ipv6.sql
@@ -15,6 +15,7 @@ ALTER TABLE core_notify_server_ipv6 ADD INDEX lookup_domain (domain_id);
 ALTER TABLE core_notify_server_ipv6 ADD COLUMN allocation_reason TEXT NOT NULL DEFAULT '';
 
 ALTER TABLE core_notify_server_ipv6 ADD COLUMN seq INT NOT NULL DEFAULT 0;
+ALTER TABLE core_notify_server_ipv6 ADD COLUMN has_reverse_ptr INT NOT NULL DEFAULT 0;
 
 ALTER TABLE core_notify_server_ipv6 ADD UNIQUE INDEX domain_seq (domain_id, seq);
 ALTER TABLE core_notify_server_ipv6 ADD UNIQUE INDEX ipv6_seq (ipv6_addr, seq);
