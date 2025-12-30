3c59f4ee00b8ca8e6058b8b64f2027f0aa4f166d
leon <leon@roojs.com>
2025-12-30 12:30:46 +0800

    DataObjects/Core_notify_server_ipv6.php

--- DataObjects/Core_notify_server_ipv6.php
+++ DataObjects/Core_notify_server_ipv6.php
@@ -382,7 +382,7 @@ class Pman_Core_DataObjects_Core_notify_server_ipv6 extends DB_DataObject
         
         $new_mapping->insert();
         
-        $this->debug("IPv6: Created new Outlook IPv6 mapping - domain: {$core_domain->domain}, ipv6: $least_used_ipv6");
+        $this->debug("IPv6: Created new IPv6 mapping - domain: {$core_domain->domain}, ipv6: $least_used_ipv6");
         
         return $new_mapping;
     }
