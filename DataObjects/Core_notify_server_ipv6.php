f1c4f6c732f05f3fc29675c875e3b63dd21f0709
leon <leon@roojs.com>
2025-12-30 12:22:46 +0800

    DataObjects/Core_notify_server_ipv6.php

--- DataObjects/Core_notify_server_ipv6.php
+++ DataObjects/Core_notify_server_ipv6.php
@@ -351,9 +351,8 @@ class Pman_Core_DataObjects_Core_notify_server_ipv6 extends DB_DataObject
         $existing = DB_DataObject::factory('core_notify_server_ipv6');
         $existing->domain_id = $core_domain->id;
         if ($existing->find(true)) {
-            $cnsi = DB_DataObject::factory('core_notify_server_ipv6');
             // Check if existing IPv6 is one of the matching IPv6 addresses for this MX
-            if ($cnsi->isIpv6ForMx($existing->ipv6_addr, $mx)) {
+            if ($this->isIpv6ForMx($existing->ipv6_addr, $mx)) {
                 $this->debug("IPv6: Using existing Outlook IPv6 mapping - domain: {$core_domain->domain}, ipv6: {$existing->ipv6_addr}");
                 return $existing;
             }
