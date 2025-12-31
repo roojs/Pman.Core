15a40e96c5d9e70439ebcf49d791b0e20235a183
leon <leon@roojs.com>
2025-12-31 16:16:17 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -277,15 +277,13 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
         }
 
         foreach($mxs as $mx) {
-
-        }
-
-        // Handle Outlook servers - try to use pre-configured IPv6 addresses
-        if (preg_match('/(\.outlook\.com)|(\.office365\.com)|(\.hotmail\.com)|(mail\.protection\.outlook\.com)$/i', $mx)) {
-            if($this->server_ipv6->ensureIpv6ForMx($mx)) {
-                $this->debug("IPv6: Using pre-configured Outlook IPv6 address: " . $this->server_ipv6->getIpv6Addr() . " for mx: $mx");
-            } else {
-                $this->debug("IPv6: No pre-configured IPv6 for Outlook server: $mx");
+            // Handle Outlook servers - try to use pre-configured IPv6 addresses
+            if (preg_match('/(\.outlook\.com)|(\.office365\.com)|(\.hotmail\.com)|(mail\.protection\.outlook\.com)$/i', $mx)) {
+                if($this->server_ipv6->ensureIpv6ForMx($mx)) {
+                    $this->debug("IPv6: Using pre-configured Outlook IPv6 address: " . $this->server_ipv6->getIpv6Addr() . " for mx: $mx");
+                } else {
+                    $this->debug("IPv6: No pre-configured IPv6 for Outlook server: $mx");
+                }
             }
         }
 
