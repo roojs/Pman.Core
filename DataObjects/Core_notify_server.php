3d767898ba48ba9df87a7b172d4779d2d29cbd16
leon <leon@roojs.com>
2025-12-31 12:57:20 +0800

    DataObjects/Core_notify_server.php

--- DataObjects/Core_notify_server.php
+++ DataObjects/Core_notify_server.php
@@ -356,7 +356,8 @@ class Pman_Core_DataObjects_Core_notify_server extends DB_DataObject
         ");
         
         $pending_notifications = $p->fetchAll();
-        
+        var_dump($pending_notifications);
+        die('test3');
         foreach ($pending_notifications as $notification) {
             // Check if this domain_id has an IPv6 server assignment
             $ipv6 = DB_DataObject::factory('core_notify_server_ipv6');
