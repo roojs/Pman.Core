da2a86a965439342ea35e9b271e0c43d7b45cd1e
leon <leon@roojs.com>
2025-12-31 13:41:15 +0800

    DataObjects/Core_notify_server.php

--- DataObjects/Core_notify_server.php
+++ DataObjects/Core_notify_server.php
@@ -378,6 +378,7 @@ class Pman_Core_DataObjects_Core_notify_server extends DB_DataObject
                 }
             }
         }
+        var_dump($assignedIds);
         return $assignedIds;
     }
         // called on current server.
