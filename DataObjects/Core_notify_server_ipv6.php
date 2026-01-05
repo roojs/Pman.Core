334381be6ccea021b2d036fc3327b945d8d24e03
leon <leon@roojs.com>
2026-01-05 17:10:08 +0800

    DataObjects/Core_notify_server_ipv6.php

--- DataObjects/Core_notify_server_ipv6.php
+++ DataObjects/Core_notify_server_ipv6.php
@@ -306,6 +306,7 @@ class Pman_Core_DataObjects_Core_notify_server_ipv6 extends DB_DataObject
         $least_used_ipv6 = $this->getLeastUsedIpv6ForMx($mx);
 
         var_dump("LEAST USED IPV6");
+        var_dump($least_used_ipv6);
         var_dump(inet_ntop($least_used_ipv6));
         
         if (empty($least_used_ipv6)) {
