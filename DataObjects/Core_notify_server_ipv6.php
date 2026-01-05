32d1fe7dd6065845eb4b0b8394e1978807beceed
leon <leon@roojs.com>
2026-01-05 17:17:08 +0800

    DataObjects/Core_notify_server_ipv6.php

--- DataObjects/Core_notify_server_ipv6.php
+++ DataObjects/Core_notify_server_ipv6.php
@@ -217,9 +217,6 @@ class Pman_Core_DataObjects_Core_notify_server_ipv6 extends DB_DataObject
         if (isset($cache[$mx])) {
             return $cache[$mx];
         }
-
-        var_dump("MX");
-        var_dump($mx);
         
         $ipv6_lookup = DB_DataObject::factory('core_notify_server_ipv6');
         $ipv6_lookup->autoJoin();
@@ -251,9 +248,6 @@ class Pman_Core_DataObjects_Core_notify_server_ipv6 extends DB_DataObject
     function getLeastUsedIpv6ForMx($mx)
     {
         $ipv6_list = $this->getIpv6ForMx($mx);
-
-        var_dump("IPV6 LIST");
-        var_dump($ipv6_list);
         
         if (empty($ipv6_list)) {
             return false;
