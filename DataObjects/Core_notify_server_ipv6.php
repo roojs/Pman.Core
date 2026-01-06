be7c710863600ee8080070e13b356ea3f36c6d3b
leon <leon@roojs.com>
2026-01-06 10:37:32 +0800

    DataObjects/Core_notify_server_ipv6.php DataObjects/Core_domain.php

--- DataObjects/Core_notify_server_ipv6.php
+++ DataObjects/Core_notify_server_ipv6.php
@@ -282,9 +282,9 @@ class Pman_Core_DataObjects_Core_notify_server_ipv6 extends DB_DataObject
             $cnsi->seq = $this->getNextSeq();
         }
         
-        
         $cnsi->insert();
 
+        // make sure the ipv6_addr_str is available
         $cnsi2 = DB_DataObject::factory('core_notify_server_ipv6');
         $cnsi2->selectAdd("INET6_NTOA(ipv6_addr) as ipv6_addr_str");
         $cnsi2->get($cnsi->id);
