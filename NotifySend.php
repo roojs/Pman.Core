6407d518cc8491e22765032df7cc630f30ff85b4
leon <leon@roojs.com>
2026-01-12 15:36:12 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -589,9 +589,6 @@ class Pman_Core_NotifySend extends Pman
                     
                     $match = false;
 
-                    var_dump($dom);
-                    die('test');
-
                     if(in_array($dom, $settings['domains'])){
                         $match = true;
                     }
