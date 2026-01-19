b3e4b37b4bf0436d7f824eb8b5f8776a16269964
leon <leon@roojs.com>
2026-01-19 16:17:00 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -1117,6 +1117,7 @@ class Pman_Core_NotifySend extends Pman
         }
 
         var_dump($mx_ip_map);
+        die('test');
 
         // skip any blacklisted ip for this server
         $bl = DB_DataObject::factory('core_notify_blacklist');
