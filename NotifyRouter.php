05147eb0fad8696b76c6175240f3ce9a1fbb263d
leon <leon@roojs.com>
2026-01-20 16:13:11 +0800

    NotifyRouter.php

---
+++ NotifyRouter.php
@@ -0,0 +1,32 @@
+<?php
+
+class Pman_Core_NotifyRouter
+{
+    var $host;
+    var $localhost;
+    var $timeout = 15;
+    var $socket_options = array();
+    var $debug = 1;
+    var $debug_handler;
+    var $dkim = true;
+    
+    function __construct($host, $localhost, $socket_options = array())
+    {
+        $this->host = $host;
+        $this->localhost = $localhost;
+        $this->socket_options = $socket_options;
+    }
+    
+    function toArray()
+    {
+        return array(
+            'host'          => $this->host,
+            'localhost'     => $this->localhost,
+            'timeout'       => $this->timeout,
+            'socket_options'=> $this->socket_options,
+            'debug'         => $this->debug,
+            'debug_handler' => $this->debug_handler,
+            'dkim'          => $this->dkim
+        );
+    }
+}
