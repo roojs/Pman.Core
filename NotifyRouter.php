eb69d4434ad02b37392a8d5c4137f18d2ec00af9
leon <leon@roojs.com>
2026-01-20 17:05:47 +0800

    NotifyRouter.php

--- NotifyRouter.php
+++ NotifyRouter.php
@@ -6,7 +6,7 @@ class Pman_Core_NotifyRouter
     var $localhost;
     var $timeout = 15;
     var $socket_options = array();
-    var $debug = 1;
+    var $debug = 0;
     var $debug_handler;
     var $dkim = true;
 
@@ -82,7 +82,7 @@ class Pman_Core_NotifyRouter
             'localhost'     => $this->localhost,
             'timeout'       => $this->timeout,
             'socket_options'=> $this->socket_options,
-            'debug'         => $this->debug,
+            'debug'         => 1,
             'debug_handler' => $this->debug_handler,
             'dkim'          => $this->dkim
         ));
