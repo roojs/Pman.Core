1b8b261dda3702969c733bc88548223c8f8d3552
leon <leon@roojs.com>
2026-01-20 13:13:19 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -1168,9 +1168,6 @@ class Pman_Core_NotifySend extends Pman
                 }
             }
         }
-
-        var_dump($mx_ip_map);
-        die('test');
         
         return $mx_ip_map;
     }
