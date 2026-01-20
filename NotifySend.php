68c3474d0e72f99460c1d04d1cc04b4f6869964d
leon <leon@roojs.com>
2026-01-20 13:16:41 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -786,7 +786,7 @@ class Pman_Core_NotifySend extends Pman
             
             if ($code < 0) {
                 $this->debug("Connection error with $smtp_host: " . $res->message);
-                // remove the failed ip from the list of valid ipv4 addresses
+                // remove the failed ip from the list of valid ip addresses
                 if(in_array($smtp_host, $validIps)) {
                     $validIps = array_diff($validIps, array($smtp_host));
                 }
