a9d2fe8d3c19370eaaee66ad96dda45c01b4d389
leon <leon@roojs.com>
2026-01-20 11:39:22 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -903,6 +903,9 @@ class Pman_Core_NotifySend extends Pman
                             $this->debug("IPv6: Set spam rejecting for " . $this->server_ipv6->ipv6_addr_str);
                         }
 
+                        // is_spamhaus AND
+                        // IPv6 already exists AND
+                        // this ip mapping is already spam rejecting OR has a reverse pointer
                         if($is_spamhaus && ($this->server_ipv6->is_spam_rejecting || $this->server_ipv6->has_reverse_ptr)) {
                     }
                     $this->debug("IPv6: Skipping setup - " . implode(", ", $reason));
