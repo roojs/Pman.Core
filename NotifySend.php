f506e5873a4cf668d31197ac5df16461a3ca27a8
leon <leon@roojs.com>
2026-01-20 12:23:33 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -1156,9 +1156,6 @@ class Pman_Core_NotifySend extends Pman
             }
             $this->debug("DNS: No IP addresses resolved for any MX, using hostnames");
         }
-
-        var_dump($mx_ip_map);
-        die('test');
         
         return $mx_ip_map;
     }
