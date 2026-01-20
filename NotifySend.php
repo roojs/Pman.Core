e59aa71368d19e8c3b31dbde1fd64966626fe1e1
leon <leon@roojs.com>
2026-01-20 11:26:25 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -822,9 +822,6 @@ class Pman_Core_NotifySend extends Pman
             }
             break;
         }
-        var_dump($next_try_min);
-        var_dump($w->act_start);
-        die('test');
         
         // after trying all mxs - could not connect...
         if  (!$force && !$fail && ($next_try_min > (2*24*60) || strtotime($w->act_start) < strtotime('NOW - 3 DAYS'))) {
