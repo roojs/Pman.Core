6d1823329e95275d56682a1c8ee202c7d6f40297
leon <leon@roojs.com>
2026-01-21 11:46:22 +0800

    NotifyRouter.php

--- NotifyRouter.php
+++ NotifyRouter.php
@@ -128,19 +128,15 @@ class Pman_Core_NotifyRouter
         
         // Return early if not using IPv6
         if (empty($smtp_host) || !$this->use_ipv6) {
+            $this->debug("IPv6: Not binding to IPv6 (server_ipv6=" . (empty($this->server_ipv6) ? 'empty' : 'set') . ", ipv6_addr=" . ($ipv6_addr_str ?: 'empty') . ")");
             return $socket_options;
         }
         
         // Add IPv6 binding if server_ipv6 is configured
-        $ipv6_addr_str = !empty($this->server_ipv6) ? $this->server_ipv6->ipv6_addr_str : false;
-        if ($ipv6_addr_str) {
-            $socket_options['socket'] = array(
-                'bindto' => '[' . $ipv6_addr_str . ']:0'
-            );
-            $this->debug("IPv6: Binding SMTP connection to IPv6 address: " . $ipv6_addr_str);
-        } else {
-            $this->debug("IPv6: Not binding to IPv6 (server_ipv6=" . (empty($this->server_ipv6) ? 'empty' : 'set') . ", ipv6_addr=" . ($ipv6_addr_str ?: 'empty') . ")");
-        }
+        $socket_options['socket'] = array(
+            'bindto' => '[' . $ipv6_addr_str . ']:0'
+        );
+        $this->debug("IPv6: Binding SMTP connection to IPv6 address: " . $ipv6_addr_str);
         
         return $socket_options;
     }
