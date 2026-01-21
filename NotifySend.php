d33036cef3b5707af3e9b0fcec792e444b232eb0
leon <leon@roojs.com>
2026-01-21 13:38:05 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -513,7 +513,7 @@ class Pman_Core_NotifySend extends Pman
             $this->debug_str = '';
 
             require_once 'Pman/Core/NotifyRouter.php';
-            $notifyRouter = new Pman_Core_NotifyRouter($this, $smtp_host, $mx, $dom, $email);
+            $notifyRouter = new Pman_Core_NotifyRouter($this, $smtp_host, $mx, $core_domain, $email);
             $mailer = $notifyRouter->mailer;
 
             $emailHeaders = $email['headers'];
