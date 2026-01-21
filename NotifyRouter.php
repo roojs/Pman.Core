3bd09e701f06c7b6c813f94663c92ce159bfda71
leon <leon@roojs.com>
2026-01-21 10:18:51 +0800

    NotifyRouter.php

--- NotifyRouter.php
+++ NotifyRouter.php
@@ -12,6 +12,9 @@ class Pman_Core_NotifyRouter
     var $socket_options = array();
     var $debug_handler;
     var $dkim = true;
+
+    var $debug = 0;
+    var $server_ipv6 = null;
     
     function __construct($notifySend, $smtp_host, $mx)
     {
