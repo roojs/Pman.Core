c66dcece94010b871df1eb7f95b2ac66b05be0ef
leon <leon@roojs.com>
2026-01-21 13:24:18 +0800

    NotifyRouter.php

--- NotifyRouter.php
+++ NotifyRouter.php
@@ -29,7 +29,7 @@ class Pman_Core_NotifyRouter
         $this->emailDom = $dom;
         $this->server = $notifySend->server;
         $this->serverIpv6 = $notifySend->server_ipv6;
-        $this->useIpv6 = !empty($this->serverIpv6) && !empty($this->serverIpv6->ipv6_addr_str) && filter_var($smtp_host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+        $this->useIpv6 = !empty($this->serverIpv6) && !empty($this->serverIpv6->ipv6_addr_str) && filter_var($this->smtpHost, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
 
         $ff = HTML_FlexyFramework::get();
 
