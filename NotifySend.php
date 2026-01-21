791c6e0997f31344bc749af9db33d5a49ab5fffa
leon <leon@roojs.com>
2026-01-21 15:18:52 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -537,7 +537,7 @@ class Pman_Core_NotifySend extends Pman
             
             $res = $mailer->send($p->email, $emailHeaders, $email['body']);
 
-            var_dump($this->debug_st);
+            var_dump($this->debug_str);
             die('test');
             
             if (is_object($res)) {
