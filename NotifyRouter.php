f28f6021e01bfd1b2361606b8f0fce0d6a94b126
leon <leon@roojs.com>
2026-01-21 13:58:25 +0800

    NotifyRouter.php

--- NotifyRouter.php
+++ NotifyRouter.php
@@ -12,7 +12,7 @@ class Pman_Core_NotifyRouter
     // MX host
     var $mx = '';
     // Core_domain instance
-    var $domain = null; 
+    var $domain = null;
     var $email = null;
     var $notify = null;
 
@@ -28,6 +28,8 @@ class Pman_Core_NotifyRouter
                 $this->$key = $value;
             }
         }
+        var_dump($this->email);
+        die('test');
         $this->useIpv6 = !empty($this->notifySend->server_ipv6) && !empty($this->notifySend->server_ipv6->ipv6_addr_str) && filter_var($this->smtpHost, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
 
         $ff = HTML_FlexyFramework::get();
