ede5215bfa54a1f7f27c89aefebcbc9924af1bdc
leon <leon@roojs.com>
2026-01-29 13:07:02 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -924,7 +924,7 @@ class Pman_Core_NotifySend extends Pman
             // sample ipv4 ips
             $mx_ip_map = array(
                 '52.101.145.0' => 'fortuneindia-com.mail.protection.outlook.com.',
-                '52.101.145.0' => 'fortuneindia-com.mail.protection.outlook.com.'
+                '52.101.145.2' => 'fortuneindia-com.mail.protection.outlook.com.'
             );
         }
         
@@ -932,9 +932,6 @@ class Pman_Core_NotifySend extends Pman
         // An IP that blocks server X might not block server Y
         // This includes both IPv4 and IPv6 addresses
         $this->allMxIps = array_keys($mx_ip_map);
-
-        var_dump($this->allMxIps);
-        die('test');
         
         // If not using IPv6, use IPv4 addresses and skip blacklisted IPs
         if(!$use_ipv6) {
