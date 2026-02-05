2154005683fa1581447ae1e758201056f531fd3a
leon <leon@roojs.com>
2026-02-05 11:31:59 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -983,6 +983,22 @@ class Pman_Core_NotifySend extends Pman
             if(empty($mx_ip_map)) {
                 $mx_ip_map = $mx_ipv4_map;
                 $this->debug("DNS: No IPv6 addresses resolved, using IPv4 addresses");
+
+                // skip any blacklisted ip on which the server is blocked by Spamhaus
+                $bl = DB_DataObject::factory('core_notify_blacklist');
+                $bl->server_id = $this->server->id;
+                $bl->whereAdd('ip IS NOT NULL');
+                $bl->whereAdd('ip != 0x0');
+                $bl->selectAdd();
+                $bl->selectAdd('INET6_NTOA(ip) as ip_str');
+                $blacklistedIps = $bl->fetchAll('ip_str');
+                foreach($mx_ip_map as $ip => $mx) {
+                    if(in_array($ip, $blacklistedIps)) {
+                        $this->debug("DNS: Blacklisted IP: $ip");
+                        $this->isAnyIpv4Blacklisted = true;
+                        unset($mx_ip_map[$ip]);
+                    }
+                }
             }
         }
 
