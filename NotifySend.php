eb47000839c6b09d1771de0f9a7df64973b57b22
leon <leon@roojs.com>
2026-02-05 10:18:38 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -959,6 +959,7 @@ class Pman_Core_NotifySend extends Pman
         else {
             if(empty($this->server_ipv6->has_reverse_ptr)) {
                 foreach($mx_ip_map as $ip => $mx) {
+                    var_dump($ip);
                     if(!str_ends_with($mx, $this->server_ipv6->domain_id_domain)) {
                         $this->debug("DNS: Skipping host $mx because it's suffix does not match the domain of the ipv6 mapping with a reverse pointer: " . $this->server_ipv6->domain_id_domain);
                         unset($mx_ip_map[$ip]);
@@ -966,6 +967,7 @@ class Pman_Core_NotifySend extends Pman
                 }
             }
         }
+        die('test');
         
         // Set validIps AFTER filtering (these are the IPs we'll actually try)
         $this->validIps = array_keys($mx_ip_map);
