c7935b5db652c7f412e1ca59b4754820b176f70b
leon <leon@roojs.com>
2026-02-05 11:21:48 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -913,6 +913,7 @@ class Pman_Core_NotifySend extends Pman
             }
         }
 
+        // use only IPv6 if using IPv6
         $mx_ip_map = $use_ipv6 ? $mx_ipv6_map : $mx_ipv4_map;
 
         // If no IPs resolved, fall back to hostnames
