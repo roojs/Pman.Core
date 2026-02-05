da19ce02554e895a1994a60332a0ddfc672870dc
leon <leon@roojs.com>
2026-02-05 12:51:15 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -1001,6 +1001,7 @@ class Pman_Core_NotifySend extends Pman
                 }
             }
         }
+        var_dump($mx_ip_map);
         die('test');
         
         // Set validIps AFTER filtering (these are the IPs we'll actually try)
