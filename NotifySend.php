c291bcbadeb0e1fa736d1ba0d4056ba5a39b6703
leon <leon@roojs.com>
2026-02-05 12:55:39 +0800

    NotifySend.php

--- NotifySend.php
+++ NotifySend.php
@@ -572,6 +572,7 @@ class Pman_Core_NotifySend extends Pman
         
         // Convert MX hostnames to map of IP addresses => domain
         $this->hasIpv6 = !empty($this->server_ipv6) && !empty($this->server_ipv6->ipv6_addr_str);
+        $this->useIpv6 = $this->hasIpv6;
         $mx_ip_map = $this->convertMxsToIpMap($this->mxRecords);
 
         // ip address that failed the SMTP check
