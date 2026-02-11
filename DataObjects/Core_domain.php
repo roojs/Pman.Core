cfcd5fd47f99263a0ea2f3d207493d3e1fac5e7f
leon <leon@roojs.com>
2026-02-11 13:01:30 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -428,6 +428,7 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
 
             // Check for SMTP error 452 (out of storage space)
             if ($res->code == 452 && preg_match('/out of storage/i', $errorMessage)) {
+                // Don't need to log error for out of storage space
                 return "The email address is over quota - which probably means its a dead email address - " .
                 "we don't add these as we would just get rejections - you should contact this user before adding " .
                 "and see if they have another email address";
