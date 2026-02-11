7e3c98cd2b64506a382cc11b728f6ec076c00646
leon <leon@roojs.com>
2026-02-11 13:09:31 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -459,7 +459,7 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
 
             // We don't need to log these errors and don't need to show these errors to the user
             if(
-                $res->code = 553 && preg_match('/User unknown/i', $errorMessage)
+                $res->code == 553 && preg_match('/User unknown/i', $errorMessage)
                 ||
                 $res->code == 550 && preg_match('/does not exist/i', $errorMessage)
                 ||
