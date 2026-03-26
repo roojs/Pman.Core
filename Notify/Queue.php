ce30363441bc1f181d0185a2cbb27d623c55bf93
leon <leon@roojs.com>
2026-03-26 11:40:32 +0800

    Notify/Queue.php

--- Notify/Queue.php
+++ Notify/Queue.php
@@ -3,15 +3,15 @@
 require_once 'Pman.php';
 
 /**
- * CLI: list unsent core_notify rows for queue tracking (id, to; evtype etc. with --verbose).
+ * CLI: list unsent core_notify rows (id, to, act_when, evtype, server_id, ontable:onid).
  * email_id is set after send, so from/subject are not shown for pending rows.
  * All servers; unsent sent only.
  *
- * php admin.php Core/Notify/Queue [--verbose] [-L N]
+ * php admin.php Core/Notify/Queue [-L N]
  */
 class Pman_Core_Notify_Queue extends Pman
 {
-    static $cli_desc = 'List unsent core_notify rows (all servers): id, to (verbose: act_when, evtype, server, ontable:onid).';
+    static $cli_desc = 'List unsent core_notify rows (all servers): id, to, act_when, evtype, server, ontable:onid.';
     
     static $cli_opts = array(
         'debug' => array(
@@ -19,13 +19,6 @@ class Pman_Core_Notify_Queue extends Pman
             'default' => 0,
             'min' => 0,
             'max' => 1,
-        ),
-        'verbose' => array(
-            'desc' => 'Include act_when, evtype, server_id, ontable, onid',
-            'default' => 0,
-            'short' => 'v',
-            'min' => 0,
-            'max' => 0,
         ),
         'limit' => array(
             'desc' => 'Max rows to print',
@@ -79,33 +72,23 @@ class Pman_Core_Notify_Queue extends Pman
             return;
         }
         
-        $verbose = !empty($opts['verbose']);
-        if ($verbose) {
-            echo str_pad('id', 8) . str_pad('to', 48) . "act_when            evtype          srv  ontable:onid\n";
-        } else {
-            echo str_pad('id', 8) . "to\n";
-        }
-        echo str_repeat('-', $verbose ? 100 : 60) . "\n";
+        echo str_pad('id', 8) . str_pad('to', 48) . "act_when            evtype          srv  ontable:onid\n";
+        echo str_repeat('-', 100) . "\n";
         
         while ($w->fetch()) {
-            $this->printRow($w, $verbose);
+            $this->printRow($w);
         }
     }
     
-    function printRow($w, $verbose)
+    function printRow($w)
     {
         $to = trim($w->to_email);
-        
-        if ($verbose) {
-            echo str_pad($w->id, 8)
-                . str_pad($this->truncate($to, 46), 48)
-                . str_pad($w->act_when, 20)
-                . ' ' . str_pad($this->truncate($w->evtype, 14), 16)
-                . str_pad($w->server_id, 4)
-                . $w->ontable . ':' . $w->onid . "\n";
-            return;
-        }
-        echo str_pad($w->id, 8) . $this->truncate($to, 72) . "\n";
+        echo str_pad($w->id, 8)
+            . str_pad($this->truncate($to, 46), 48)
+            . str_pad($w->act_when, 20)
+            . ' ' . str_pad($this->truncate($w->evtype, 14), 16)
+            . str_pad($w->server_id, 4)
+            . $w->ontable . ':' . $w->onid . "\n";
     }
     
     function truncate($str, $len)
