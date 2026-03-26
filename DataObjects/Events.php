6e914898443a07fcd037ced35219d65d0eda3f81
leon <leon@roojs.com>
2026-03-26 13:02:18 +0800

    DataObjects/Events.php

--- DataObjects/Events.php
+++ DataObjects/Events.php
@@ -695,6 +695,7 @@ class Pman_Core_DataObjects_Events extends DB_DataObject
     {
 
         $logdir = $this->logDir();
+        var_dump($logdir);
         if (!$logdir) {
             return false;
          
