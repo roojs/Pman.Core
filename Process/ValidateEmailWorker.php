922ff2ee3e4ac5c59b2b47d2e50caa37dad071e5
leon <leon@roojs.com>
2026-05-14 15:38:21 +0800

    Process/ValidateEmailWorker.php

---
+++ Process/ValidateEmailWorker.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Flexy route target for Core/Process/ValidateEmailWorker (implementation lives in Pman.Core).
+ */
+require_once dirname(__DIR__) . '/../../../../Pman.Core/Process/ValidateEmailWorker.php';
