ba9b9522d88d8ff7be1b846bf81bfa6c00d381b6
leon <leon@roojs.com>
2026-05-18 16:57:07 +0800

    ValidateEmail.php

--- ValidateEmail.php
+++ ValidateEmail.php
@@ -141,7 +141,7 @@ class Pman_Core_ValidateEmail extends Pman
                     fclose($pipes[2]);
                     proc_close($proc);
                     @unlink($jobFile);
-                    $this->error('Validation timed out for ' . $field, true);
+                    $this->error('Validation timed out for ' . $email, true);
                 }
 
                 $r = array($pipes[1], $pipes[2]);
