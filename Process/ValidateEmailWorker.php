d635dc3a8dc3cabb5031f6e7b943b3eaa31c5fb7
leon <leon@roojs.com>
2026-05-18 10:58:02 +0800

    Process/ValidateEmailWorker.php

--- Process/ValidateEmailWorker.php
+++ Process/ValidateEmailWorker.php
@@ -40,7 +40,7 @@ class Pman_Core_Process_ValidateEmailWorker extends Pman
     function get($request = '', $opts = array(), $isRedirect = false)
     {
         $this->vewOut(array(
-            'type' => 'error',
+            'type' => 'email_fail',
             'message' => 'TEST error message'
         ));
         exit(1);
