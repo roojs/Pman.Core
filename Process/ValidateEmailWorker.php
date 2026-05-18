206e305fba115adbba7d1893a50f75dd51809e84
leon <leon@roojs.com>
2026-05-18 11:38:00 +0800

    Process/ValidateEmailWorker.php

--- Process/ValidateEmailWorker.php
+++ Process/ValidateEmailWorker.php
@@ -216,7 +216,7 @@ class Pman_Core_Process_ValidateEmailWorker extends Pman
             // We don't need to log these errors and don't need to show these errors to the user
             if (
                 $res->code == 553 && preg_match('/User unknown/i', $errorMessage)
-                || $res->code == 550 && preg_match('/does not exist|no mailbox here|User unknown|user not exist/i', $errorMessage)
+                || $res->code == 550 && preg_match('/does not exist|no mailbox here|User unknown|user not exists/i', $errorMessage)
             ) {
                 echo json_encode(array(
                     'type' => 'email_fail',
