80ad5262d214fbd78b2902f76a823077533f4b2e
leon <leon@roojs.com>
2026-05-19 16:28:52 +0800

    ValidateEmail.php

--- ValidateEmail.php
+++ ValidateEmail.php
@@ -172,34 +172,7 @@ class Pman_Core_ValidateEmail extends Pman
                             }
                         }
                     }
-                    while (($p = strpos($bufOut, "\n")) !== false) {
-                        $line = trim(substr($bufOut, 0, $p));
-                        $bufOut = substr($bufOut, $p + 1);
-                        if ($line === '') {
-                            continue;
-                        }
-                        $row = json_decode($line, true);
-                        if (!is_array($row)) {
-                            $jobError = 'Invalid JSON from worker: ' . substr($line, 0, 200);
-                            break;
-                        }
-                        if (!empty($row['type']) && $row['type'] === 'error_log') {
-                            $this->errorlog($row['message']);
-                            if(!empty($row['isHardFailure'])) {
-                                $jobError = 'An error occurred, please contact the website owner.';
-                                break;
-                            }
-                            continue;
-                        }
-                        if (!empty($row['type']) && $row['type'] === 'email_fail') {
-                            $jobError = !empty($row['message']) ? $row['message'] : 'Email validation failed';
-                            break;
-                        }
-                        if (!empty($row['type']) && $row['type'] === 'email_ok') {
-                            $okRow = $row;
-                            continue;
-                        }
-                    }
+                    $this->parseWorkerOutput($bufOut, $jobError, $okRow);
 
                     if($jobError) {
                         break;
