370f8892bde4397c5b988e175dde7e73ea3b3424
leon <leon@roojs.com>
2026-05-19 16:33:36 +0800

    ValidateEmail.php

--- ValidateEmail.php
+++ ValidateEmail.php
@@ -195,7 +195,6 @@ class Pman_Core_ValidateEmail extends Pman
             $bufErr .= stream_get_contents($pipes[2]);
             fclose($pipes[1]);
             fclose($pipes[2]);
-            $exitCode = proc_close($proc);
             @unlink($jobFile);
 
             if(empty($jobError)) {
