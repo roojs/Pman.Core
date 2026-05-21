69fa83d67314a6792abae607a96d69df89b52691
leon <leon@roojs.com>
2026-05-21 16:20:26 +0800

    DataObjects/Core_domain.php

--- DataObjects/Core_domain.php
+++ DataObjects/Core_domain.php
@@ -380,12 +380,10 @@ class Pman_Core_DataObjects_Core_domain extends DB_DataObject
                 $socket_options['socket'] = array(
                     'bindto' => $ipv4_bind_ip . ':0'
                 );
-                var_dump($socket_options['socket']['bindto']);
                 if (is_object($roo) && method_exists($roo, 'out')) {
-                    die('aa');
                     $roo->out('error_log', "ValidateEmail retry: IPv4 bind {$currentServer->interface} ({$ipv4_bind_ip})");
                 }
-                die('bb');
+                $roo->out('error_log', "VValidateEmail retry: IPv4 bind {$currentServer->interface} ({$ipv4_bind_ip})");
             }
         }
         
