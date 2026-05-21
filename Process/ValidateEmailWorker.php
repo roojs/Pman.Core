10747e5e7ead347dd3a87f8794e7e0934ed2cb83
leon <leon@roojs.com>
2026-05-21 15:08:39 +0800

    Process/ValidateEmailWorker.php

--- Process/ValidateEmailWorker.php
+++ Process/ValidateEmailWorker.php
@@ -98,7 +98,7 @@ class Pman_Core_Process_ValidateEmailWorker extends Pman
             }
         }
         
-        
+        // exit loop if mx is ok
         for($pass = 0; $pass < 2 && !$mxOk; $pass++) {
             foreach ($mxs as $mx) {
                 $mailer = $cd->createMailer($this, $mx, $validUser);
