19658efc533ace1c6a38d5c211294f836b122e86
leon <leon@roojs.com>
2026-05-21 10:38:42 +0800

    Pman.Dialog.PreviewRowsImport.js

--- Pman.Dialog.PreviewRowsImport.js
+++ Pman.Dialog.PreviewRowsImport.js
@@ -1,2 +1,2 @@
 
-** (roobuilder:610493): ERROR **: 10:37:48.813: Application.vala:243: invalid project 3ab3f691edabeed20ba58b844cde0cc6, use --list-projects to show project ids
+** (roobuilder:610725): ERROR **: 10:38:33.461: Application.vala:243: invalid project 3ab3f691edabeed20ba58b844cde0cc6, use --list-projects to show project ids
