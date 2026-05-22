2155f3d700d8b6253749b6187538b2eb19472090
leon <leon@roojs.com>
2026-05-22 13:07:07 +0800

    JsonOutputTrait.php

--- JsonOutputTrait.php
+++ JsonOutputTrait.php
@@ -124,7 +124,7 @@ trait Pman_Core_JsonOutputTrait {
              * but it will change the autocommit flag of the connection back to true.
              * (Inconsistent state: MySQL transaction is still open, but the autocommit flag is true.)
              * 
-             * That's why we need to commit the transaction manually and set the AUTOCOMMIT to 1 
+             * That's why we need to manully commit the transaction and set the AUTOCOMMIT to 1 
              * instead of calling "$this->transObj->query('COMMIT')" which won't work as expected.
              */
             if ($this->transObj) {
