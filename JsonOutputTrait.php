bf7ac83c99675da0f525981afb0a39f44e2142b4
leon <leon@roojs.com>
2026-05-22 13:04:03 +0800

    JsonOutputTrait.php

--- JsonOutputTrait.php
+++ JsonOutputTrait.php
@@ -122,7 +122,7 @@ trait Pman_Core_JsonOutputTrait {
              * When no manipulation transaction (INSERT, UPDATE, DELETE, etc...) is done after BEGIN,
              * calling "$this->transObj->query('ROLLBACK')" won't rollback the transaction and set the AUTOCOMMIT to 1 on MySQL,
              * but it will change the autocommit flag of the connection back to true which is a incorrect state.
-             * (MySQL: transaction is still open, but the autocommit flag is true.)
+             * (MySQL transaction is still open, but the autocommit flag is true.)
              * 
              * That's why we need to commit the transaction manually and set the AUTOCOMMIT to 1.
              */
