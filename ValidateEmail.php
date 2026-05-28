e9aacadc06a4bf30b7f2d02139ab0cf6f5ce9d4a
leon <leon@roojs.com>
2026-05-28 13:30:04 +0800

    ValidateEmail.php

--- ValidateEmail.php
+++ ValidateEmail.php
@@ -49,6 +49,34 @@ class Pman_Core_ValidateEmail extends Pman
         ));
     }
 
+    function workerUrl($ff)
+    {
+        if (empty($ff->Pman['local_base_url'])) {
+            $this->errorlog('Pman[local_base_url] is not set');
+            $this->error('An error occurred, please contact the website owner.');
+        }
+        $base = $ff->Pman['local_base_url'];
+        if (strpos($base, 'http') !== 0) {
+            $base = 'http://127.0.0.1' . $base;
+        }
+        $base = preg_replace('#^http://localhost#i', 'http://127.0.0.1', $base);
+        return rtrim($base, '/') . '/Core/Process/ValidateEmailWorker';
+    }
+
+    function workerUrl($ff)
+    {
+        if (empty($ff->Pman['local_base_url'])) {
+            $this->errorlog('Pman[local_base_url] is not set');
+            $this->error('An error occurred, please contact the website owner.');
+        }
+        $base = $ff->Pman['local_base_url'];
+        if (strpos($base, 'http') !== 0) {
+            $base = 'http://127.0.0.1' . $base;
+        }
+        $base = preg_replace('#^http://localhost#i', 'http://127.0.0.1', $base);
+        return rtrim($base, '/') . '/Core/Process/ValidateEmailWorker';
+    }
+
     /**
      * POST one email to loopback worker; SSE progress while waiting (max $childTimeout s).
      *
@@ -180,7 +208,7 @@ class Pman_Core_ValidateEmail extends Pman
             $this->error('An error occurred, please contact the website owner.');
         }
 
-        $workerUrl = 'http://localhost/Core/Process/ValidateEmailWorker';
+        $workerUrl = $this->workerUrl($ff);
         $total = count($jobs);
         $results = array();
         $childTimeout = 90.0;
