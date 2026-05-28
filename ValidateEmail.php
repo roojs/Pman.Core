f1ae1408e66427ecc98bd3b96f62a8ac3966de49
leon <leon@roojs.com>
2026-05-28 15:23:55 +0800

    ValidateEmail.php

--- ValidateEmail.php
+++ ValidateEmail.php
@@ -169,6 +169,8 @@ class Pman_Core_ValidateEmail extends Pman
                 'message' => 'Validating email (' . $email . ') - ' . round($childTimeout) . ' seconds left',
             ));
 
+            $this->error($this->baseURL);
+
             $workerResult = $this->runWorkerHttp(
                 $this->baseURL . '/Core/Process/ValidateEmailWorker',
                 $email,
