f6628697722e2b498f2e8ef7f6a5cdc1c1a19925
Alan <alan@roojs.com>
2026-06-08 14:46:08 +0800

    Fix #8954 - email validation performance
    I've updated quite a bit of the code - can you go through and check if it still delivers all the features as dicussed.
    
    ---
    one big issue is that due to companies protecting their email servers using delays and other tricks our email checking is a bit brittle - it frequently fails silently and doesnt give teh user any feedback.
    
    ** we need to seperate the email validation so it done prior to posting the journalist or crm contact entry
    
    theory should be that we add a hidden property to the form
    email_XXX_validated = md5(email + domain id)
    
    so when the user presses save
    -> we start a streaming request?? - possibly using our code from the streaming stuff
    -> we try and do the email validation only a single request to email validation?? - posted to domain??
     -> that will have to see how we can async check the email and send back stuff to the UI - to stop it hanging
     -> Connecteding .... max 12/30 seconds
    .. counts down - progress bar?
    -> testing email conneciton...
       --> see if we can do async socket - so we can send stuff back to user
    ** we also now have the ability on some server to use 'alternative' hosts - see core_notify_server interface stuff... - we might want to try that if we end up with a spam or odd reject on so we an say 'trying another server'...
    then either say it was rejected we need to tighten up the language as if we know for sure
    
    This is email <B>does not work</B> - we checked it - nothing can be delivered to them.

---
+++ Process/ValidateEmailWorker.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Internal HTTP worker: one email SMTP validation (POST from Core/ValidateEmail).
+ * POST: email, auth_user_id.
+ *
+ * Ops: php-fpm request_terminate_timeout and nginx fastcgi_read_timeout should be >= 90s
+ * for this route (see ValidateEmail parent).
+ */
+
+require_once 'Pman.php';
+
+class Pman_Core_Process_ValidateEmailWorker extends Pman
+{
+    function getAuth()
+    {
+        return true;
+    }
+
+    function get($request = '', $opts = array(), $isRedirect = false)
+    {
+        $this->jerr('Invalid get');
+    }
+
+    function post($base = '')
+    {
+        set_time_limit(90);
+
+        $au = DB_DataObject::factory('core_person');
+        if($au->get($_POST['auth_user_id'])) {
+            $this->authUser = $au;
+        }
+
+        $dar = explode('@', $_POST['email']);
+        $dom = strtolower(array_pop($dar));
+        $dar[] = $dom;
+        $emailNorm = implode('@', $dar);
+
+        $cd = DB_DataObject::factory('core_domain');
+        $cdResult = $cd->getOrCreate($dom);
+        if (!is_object($cdResult)) {
+            $this->jdata(array(
+                'type' => 'email_fail',
+                'message' => is_string($cdResult) ? $cdResult : 'Invalid domain',
+            ));
+        }
+
+        $result = false;
+        for ($pass = 0; $pass < 2; $pass++) {
+            $result = $cd->validateEmail($this, $emailNorm, $pass);
+            if ($result === true) {
+                break;
+            }
+            if ($result !== false) {
+                break;
+            }
+        }
+        if (is_string($result)) {
+            $this->jdata(array(
+                'type' => 'email_fail',
+                'message' => $result,
+            ));
+        }
+
+        $this->jdata(array(
+            'type' => 'email_ok',
+            'domain_id' => $cd->id,
+            'token' => md5($emailNorm . $cd->id),
+        ));
+    }
+}
---
+++ Sse.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once 'Pman.php';
+
+class Pman_Core_Sse extends Pman
+{
+    var $sse = false;
+    var $progressTotal = 100;
+
+    function getAuth()
+    {
+        $ff = HTML_FlexyFramework::get();
+        if (!empty($ff->cli)) {
+            return true;
+        }
+        $this->authRequired();
+
+        set_time_limit(0);
+
+        header('Content-Type: text/event-stream');
+        header('Cache-Control: no-cache');
+        header('Connection: keep-alive');
+        header('X-Accel-Buffering: no');
+
+        while (ob_get_level()) {
+            ob_end_flush();
+        }
+
+        return true;
+    }
+
+    function sendSSE($event, $data)
+    {
+        echo "\n"
+            . "event: {$event}\n"
+            . 'data: ' . json_encode($data) . "\n";
+        if (ob_get_level()) {
+            ob_flush();
+        }
+        flush();
+
+        if ($event === 'error') {
+            exit;
+        }
+    }
+
+    // reuse the name jerr even if the logic is different
+    // easier to read and understand
+    function jerr($str, $errors=array(), $content_type = false)
+    {
+        $this->errorlog($str);
+        $this->sendSSE('error', array(
+            'success' => false,
+            'errorMsg' => $str
+        ));
+    }
+}
