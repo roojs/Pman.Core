02b548a6104163411ffed76cbd6b71cf54a3c32f
leon <leon@roojs.com>
2026-06-16 13:35:11 +0800

    ValidateEmail.php

--- ValidateEmail.php
+++ ValidateEmail.php
@@ -119,10 +119,10 @@ class Pman_Core_ValidateEmail extends Pman_Core_Sse
                 $ipv6->domain_id = $cd->id;
                 if ($ipv6->find(true)) {
                     $server = $ipv6->findServerFromIpv6('core');
-                    // if ($server && $server->id != DB_DataObject::factory('core_notify_server')->getCurrent($this, true, 'core')->id) {
+                    if ($server && $server->id != DB_DataObject::factory('core_notify_server')->getCurrent($this, true, 'core')->id) {
                         $workerUrl = 'http://' . $server->helo . $workerPath;
 
-                    // }
+                    }
                 }
             }
 
