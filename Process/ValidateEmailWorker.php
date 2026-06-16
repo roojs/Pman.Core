6cfb998eddc055a8c864b61bf16a5e00fbb5e3ee
leon <leon@roojs.com>
2026-06-16 13:38:25 +0800

    Process/ValidateEmailWorker.php

--- Process/ValidateEmailWorker.php
+++ Process/ValidateEmailWorker.php
@@ -22,6 +22,7 @@ class Pman_Core_Process_ValidateEmailWorker extends Pman
         }
 
         $remote = $_SERVER['REMOTE_ADDR'];
+        var_dump($remote);
         if ($remote == '127.0.0.1' || $remote == '::1') {
             return true;
         }
